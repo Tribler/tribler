416f247b430847be1a0e10efb6f11a0055185b47
pawel <pawel@001aeff7-3401-0410-a489-f7902fc005dd>
2006-02-22 11:37:07 +0000

    iiii
    git-svn-id: https://svn.tribler.org/abc/branches/arno/d06-01-20-secover-f-jie-abc310-buddycast-dlhelp-r367@683 001aeff7-3401-0410-a489-f7902fc005dd

--- makedist.bat
+++ makedist.bat
@@ -1,28 +1,34 @@
 call clean.bat
 
-set PYTHONPATH="C:\Python24"
+set PYTHONPATH="C:\Python23"
 set NSIS="C:\Program Files\NSIS\makensis.exe"
 set IMGCFG="C:\Program Files\Imagecfg\imagecfg.exe"
 
-%PYTHONPATH%\python.exe setupabc.py py2exe
-REM copy %PYTHONPATH%\msvcr71.dll dist\abc
-copy %PYTHONPATH%\msvcp71.dll dist\abc
-mkdir dist\abc\icons
-copy icons\*.bmp dist\abc\icons
-mkdir dist\abc\lang
-copy lang\*.lang dist\abc\lang
+%PYTHONPATH%\python.exe setuptribler.py py2exe
+REM copy %PYTHONPATH%\msvcr71.dll dist\tribler
+REM copy %PYTHONPATH%\msvcp71.dll dist\tribler
+copy %PYTHONPATH%\msvcp60.dll dist\tribler
+copy SSLEAY32.dll dist\tribler
+copy LIBEAY32.dll dist\tribler
+copy heading.bmp dist\tribler
+copy superpeer.txt dist\tribler
+mkdir dist\tribler\icons
+copy icons\*.bmp dist\tribler\icons
+mkdir dist\tribler\lang
+copy lang\*.lang dist\tribler\lang
 cd dist
-move *.* abc
-cd abc
+move abc.exe tribler.exe
+move *.* tribler
+cd tribler
 
 rem if exist %IMGCFG% goto imageconfig
 rem goto makeinstaller
 rem :imageconfig
-rem %IMGCFG% -u abc.exe
-rem %IMGCFG% -a 0x1 abc.exe
+rem %IMGCFG% -u tribler.exe
+rem %IMGCFG% -a 0x1 tribler.exe
 
 :makeinstaller
-%NSIS% abc.nsi
-move ABC-win32-v*.exe ..
+%NSIS% tribler.nsi
+move Tribler_*.exe ..
 cd ..
 cd ..
---
+++ setuptribler.py
@@ -0,0 +1,28 @@
+# setup.py
+from distutils.core import setup
+import py2exe
+
+
+################################################################
+#
+# Setup script used for py2exe
+#
+# *** Important note: ***
+# Setting Python's optimize flag when building disables
+# "assert" statments, which are used throughout the
+# BitTornado core for error-handling.
+#
+################################################################
+
+target_abc = {
+    "script": "abc.py",
+    "icon_resources": [(1, "tribler.ico")],
+}
+
+setup(
+#    (Disabling bundle_files for now -- apparently causes some issues with Win98)
+#    options = {"py2exe": {"bundle_files": 1}},
+#    zipfile = None,
+    data_files = [("tribler", ["tribler.exe.manifest", "tribler.nsi", "tribler.ico", "torrenticon.ico", "LICENSE.txt", "readme.txt"])], 
+    windows = [target_abc],
+)
