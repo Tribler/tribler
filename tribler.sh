407a0033f0c56c1afc4ff73a6a9a34f1ab8bbb4e
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2006-04-27 08:24:39 +0000

    - Fixed tribler.sh such that it gives an error when there are only   ANSI versions of wxPython available (3.4.0 needs wxPython Unicode)
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@1145 001aeff7-3401-0410-a489-f7902fc005dd

--- tribler.sh
+++ tribler.sh
@@ -4,6 +4,11 @@ PYTHON=python2.3
 
 # don't care about gtk/x11/whatever. Currently (3.4.0) must be unicode
 WXPYTHONVER=`ls -1d /usr/lib/$PYTHON/site-packages/wx-2.6* | grep -v ansi | sed -e 's/.*wx-//g' -e 's/-.*//g' | sort -nr | head -1`
+if [ "$WXPYTHONVER" = "" ];
+then
+	echo "Hmmm... No wxPython unicode package found for $PYTHON, cannot run Tribler, sorry"
+	exit -1
+fi	
 WXPYTHON=`ls -1d /usr/lib/$PYTHON/site-packages/wx-$WXPYTHONVER* | head -1`
 
 PYTHONPATH=/usr/share/tribler/:$WXPYTHON
