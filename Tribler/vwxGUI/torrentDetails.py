b8e3a625311bf90f9f47c3daeee4277c45e74302
mtenbrinke <mtenbrinke@001aeff7-3401-0410-a489-f7902fc005dd>
2007-03-14 14:40:41 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@3151 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/vwxGUI/torrentDetails.py
+++ Tribler/vwxGUI/torrentDetails.py
@@ -2,6 +2,7 @@
 # Don't modify comment 
 
 import wx
+from bgPanel import *
 #[inc]add your include files here
 
 #[inc]end your include
@@ -22,27 +23,13 @@ class torrentDetails(wx.Panel):
 
 
     def VwXinit(self):
-        self.fileImgBuf=[None] * 10
+        self.fileImgBuf=[None] * 3
         self.fileImgBuf[0] = wx.Bitmap("images/triblerpanel_topleft.png",wx.BITMAP_TYPE_PNG)
         self.fileImgBuf[1] = wx.Bitmap("images/triblerpanel_topcenter.png",wx.BITMAP_TYPE_PNG)
         self.fileImgBuf[2] = wx.Bitmap("images/triblerpanel_topright.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[3] = wx.Bitmap("images/triblerpanel_bottomleft.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[4] = wx.Bitmap("images/triblerpanel_bottomcenter.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[5] = wx.Bitmap("images/triblerpanel_bottomright.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[6] = wx.Bitmap("images/6_160x90.jpg",wx.BITMAP_TYPE_JPEG)
-        self.fileImgBuf[7] = wx.Bitmap("images/size.gif",wx.BITMAP_TYPE_GIF)
-        self.fileImgBuf[8] = wx.Bitmap("images/up.gif",wx.BITMAP_TYPE_GIF)
-        self.fileImgBuf[9] = wx.Bitmap("images/down.gif",wx.BITMAP_TYPE_GIF)
         self.pn9cImg0=self.fileImgBuf[0];
         self.pn10cImg0=self.fileImgBuf[1];
         self.pn11cImg0=self.fileImgBuf[2];
-        self.pn9cCImg0=self.fileImgBuf[3];
-        self.pn10cCImg0=self.fileImgBuf[4];
-        self.pn11cCImg0=self.fileImgBuf[5];
-        self.pn15cCImg0=self.fileImgBuf[6];
-        self.bm18cCImg0=self.fileImgBuf[7];
-        self.bm18cImg0=self.fileImgBuf[8];
-        self.bm19cImg0=self.fileImgBuf[9];
         self.Show(True)
         self.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_ACTIVECAPTION))
         self.pn9c = wx.Panel(self,-1,wx.Point(0,0),wx.Size(10,21))
@@ -57,107 +44,48 @@ class torrentDetails(wx.Panel):
         self.pn11c = wx.Panel(self,-1,wx.Point(108,0),wx.Size(10,21))
         self.pn11c.SetBackgroundColour(wx.Colour(0,0,0))
         self.pn11c.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn11c_VwXEvOnEraseBackground)
-        self.pn9cC = wx.Panel(self,-1,wx.Point(0,455),wx.Size(10,28))
-        self.pn9cC.SetBackgroundColour(wx.Colour(0,0,0))
-        self.pn9cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn9cC_VwXEvOnEraseBackground)
-        self.pn10cC = wx.Panel(self,-1,wx.Point(10,455),wx.Size(155,28))
-        self.pn10cC.SetBackgroundColour(wx.Colour(0,0,0))
-        self.pn10cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn10cC_VwXEvOnEraseBackground)
-        self.pn11cC = wx.Panel(self,-1,wx.Point(108,432),wx.Size(190,28))
-        self.pn11cC.SetBackgroundColour(wx.Colour(0,0,0))
-        self.pn11cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn11cC_VwXEvOnEraseBackground)
-        self.pn12c = wx.Panel(self,-1,wx.Point(0,25),wx.Size(298,430))
-        self.pn12c.SetBackgroundColour(wx.Colour(255,255,255))
-        self.pn48c = wx.Panel(self.pn12c,-1,wx.Point(0,0),wx.Size(290,100))
-        self.pn48c.SetBackgroundColour(wx.Colour(219,219,219))
-        self.pn15cC = wx.Panel(self.pn48c,-1,wx.Point(6,6),wx.Size(160,90),wx.SIMPLE_BORDER)
-        self.pn15cC.SetBackgroundColour(wx.Colour(219,219,219))
-        self.pn15cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn15cC_VwXEvOnEraseBackground)
-        self.st5cCCaa = wx.StaticText(self.pn48c,-1,"",wx.Point(175,3),wx.Size(117,13),wx.ST_NO_AUTORESIZE)
-        self.st5cCCaa.SetLabel("> download")
-        self.st5cCCaa.SetForegroundColour(wx.Colour(255,85,0))
-        self.pn44c = wx.Panel(self.pn12c,-1,wx.Point(0,100),wx.Size(20,20))
-        self.pn44c.SetBackgroundColour(wx.Colour(110,110,110))
-        self.pn39cCC = wx.Panel(self.pn44c,-1,wx.Point(3,3),wx.Size(40,15))
-        self.pn39cCC.SetBackgroundColour(wx.Colour(255,255,255))
-        self.st60c = wx.StaticText(self.pn39cCC,-1,"",wx.Point(5,0),wx.Size(49,13),wx.ST_NO_AUTORESIZE)
-        self.st60c.SetLabel("info")
-        self.pn39cCC2 = wx.Panel(self.pn44c,-1,wx.Point(46,3),wx.Size(65,15))
-        self.pn39cCC2.SetBackgroundColour(wx.Colour(205,205,205))
-        self.bm18cC = wx.StaticBitmap(self.pn48c,-1,self.bm18cCImg0,wx.Point(178,-185),wx.Size(11,16))
-        self.st5cCC = wx.StaticText(self.pn48c,-1,"",wx.Point(192,67),wx.Size(18,11),wx.ST_NO_AUTORESIZE)
-        self.st5cCC.SetLabel("avi")
-        self.st5cCC.SetForegroundColour(wx.Colour(128,128,128))
-        self.st5cCfdsf = wx.StaticText(self.pn48c,-1,"",wx.Point(210,67),wx.Size(8,16),wx.ST_NO_AUTORESIZE)
-        self.st5cCfdsf.SetLabel("| ")
-        self.st5cCfdsf.SetForegroundColour(wx.Colour(128,128,128))
-        self.st5cCCCC = wx.StaticText(self.pn48c,-1,"",wx.Point(218,67),wx.Size(35,11))
-        self.st5cCCCC.SetLabel("130 MB")
-        self.st5cCCCC.SetFont(wx.Font(8,74,90,90,0,"Tahoma"))
-        self.st5cCCCC.SetForegroundColour(wx.Colour(128,128,128))
-        self.bm18c = wx.StaticBitmap(self.pn48c,-1,self.bm18cImg0,wx.Point(178,-185),wx.Size(5,8))
-        self.st20c = wx.StaticText(self.pn48c,-1,"",wx.Point(189,82),wx.Size(19,15),wx.ST_NO_AUTORESIZE)
-        self.st20c.SetLabel("40")
-        self.st20c.SetForegroundColour(wx.Colour(128,128,128))
-        self.bm19c = wx.StaticBitmap(self.pn48c,-1,self.bm19cImg0,wx.Point(214,-185),wx.Size(5,8))
-        self.st21c = wx.StaticText(self.pn48c,-1,"",wx.Point(225,74),wx.Size(19,15),wx.ST_NO_AUTORESIZE)
-        self.st21c.SetLabel("10")
-        self.st21c.SetForegroundColour(wx.Colour(128,128,128))
-        self.st60cC = wx.StaticText(self.pn39cCC2,-1,"",wx.Point(5,0),wx.Size(49,13),wx.ST_NO_AUTORESIZE)
-        self.st60cC.SetLabel("comments")
-        self.st60cC.SetForegroundColour(wx.Colour(0,0,0))
+        self.playbackControls = bgPanel(self, -1, wx.Point(0,142), wx.Size(20,20))
+        self.6_160x90 = bgPanel(self, -1, wx.Point(0,21), wx.Size(302,170))
+        self.tabs = wx.Panel(self,-1,wx.Point(0,162),wx.Size(20,20))
+        self.tabs.SetBackgroundColour(wx.Colour(110,110,110))
+        self.pn39cCCC = wx.Panel(self.tabs,-1,wx.Point(3,3),wx.Size(40,15))
+        self.pn39cCCC.SetBackgroundColour(wx.Colour(255,255,255))
+        self.st60cCC = wx.StaticText(self.pn39cCCC,-1,"",wx.Point(3,3),wx.Size(49,13),wx.ST_NO_AUTORESIZE)
+        self.st60cCC.SetLabel("info")
+        self.pn39cCC2C = wx.Panel(self.tabs,-1,wx.Point(46,3),wx.Size(65,15))
+        self.pn39cCC2C.SetBackgroundColour(wx.Colour(205,205,205))
+        self.st60cCC = wx.StaticText(self.pn39cCC2C,-1,"",wx.Point(3,3),wx.Size(49,13),wx.ST_NO_AUTORESIZE)
+        self.st60cCC.SetLabel("comments")
+        self.st60cCC.SetForegroundColour(wx.Colour(0,0,0))
+        self.details = wx.Panel(self,-1,wx.Point(0,182),wx.Size(20,20))
+        self.details.SetBackgroundColour(wx.Colour(255,255,255))
+        self.white_bottom = bgPanel(self, -1, wx.Point(3,437), wx.Size(302,5))
         self.sz3s = wx.BoxSizer(wx.VERTICAL)
-        self.sz8s = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz8sC = wx.BoxSizer(wx.HORIZONTAL)
+        self.header = wx.BoxSizer(wx.HORIZONTAL)
         self.sz65s = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz13s = wx.BoxSizer(wx.VERTICAL)
-        self.sz14sCC = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz16sC = wx.BoxSizer(wx.VERTICAL)
-        self.sz24sC = wx.BoxSizer(wx.VERTICAL)
-        self.tabsCC = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz61s = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz22s = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz17s = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz61sC = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz3s.Add(self.sz8s,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.pn12c,1,wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,1)
-        self.sz3s.Add(self.sz8sC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz8s.Add(self.pn9c,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz8s.Add(self.pn10c,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz8s.SetItemMinSize(self.pn10c,20,10)
-        self.sz8s.Add(self.pn11c,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz8sC.Add(self.pn9cC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz8sC.Add(self.pn10cC,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz8sC.SetItemMinSize(self.pn10cC,20,10)
-        self.sz8sC.Add(self.pn11cC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.tabsCCC = wx.BoxSizer(wx.HORIZONTAL)
+        self.sz61sCC = wx.BoxSizer(wx.HORIZONTAL)
+        self.sz61sCC = wx.BoxSizer(wx.HORIZONTAL)
+        self.sz3s.Add(self.header,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.6_160x90,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.playbackControls,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.tabs,0,wx.BOTTOM|wx.EXPAND|wx.FIXED_MINSIZE,6)
+        self.sz3s.Add(self.details,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.white_bottom,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.header.Add(self.pn9c,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.header.Add(self.pn10c,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.header.SetItemMinSize(self.pn10c,20,10)
+        self.header.Add(self.pn11c,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz65s.Add(self.st64c,0,wx.TOP|wx.EXPAND|wx.FIXED_MINSIZE,4)
-        self.sz13s.Add(self.pn48c,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz13s.Add(self.pn44c,0,wx.BOTTOM|wx.EXPAND|wx.FIXED_MINSIZE,6)
-        self.sz14sCC.Add(self.pn15cC,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,6)
-        self.sz14sCC.Add(self.sz16sC,0,wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz16sC.Add(self.sz24sC,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz16sC.Add(self.sz22s,0,wx.TOP|wx.EXPAND|wx.FIXED_MINSIZE,9)
-        self.sz16sC.Add(self.sz17s,0,wx.BOTTOM|wx.EXPAND|wx.FIXED_MINSIZE,6)
-        self.sz24sC.Add(self.st5cCCaa,0,wx.TOP|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.tabsCC.Add(self.pn39cCC,0,wx.TOP|wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.tabsCC.Add(self.pn39cCC2,0,wx.TOP|wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz61s.Add(self.st60c,0,wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,5)
-        self.sz22s.Add(self.bm18cC,0,wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz22s.Add(self.st5cCC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz22s.Add(self.st5cCfdsf,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz22s.Add(self.st5cCCCC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz17s.Add(self.bm18c,0,wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz17s.Add(self.st20c,0,wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz17s.Add(self.bm19c,0,wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz17s.Add(self.st21c,0,wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz61sC.Add(self.st60cC,0,wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,5)
+        self.tabsCCC.Add(self.pn39cCCC,0,wx.TOP|wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.tabsCCC.Add(self.pn39cCC2C,0,wx.TOP|wx.LEFT|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz61sCC.Add(self.st60cCC,0,wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,5)
+        self.sz61sCC.Add(self.st60cCC,0,wx.LEFT|wx.EXPAND|wx.FIXED_MINSIZE,5)
         self.SetSizer(self.sz3s);self.SetAutoLayout(1);self.Layout();
         self.pn10c.SetSizer(self.sz65s);self.pn10c.SetAutoLayout(1);self.pn10c.Layout();
-        self.pn12c.SetSizer(self.sz13s);self.pn12c.SetAutoLayout(1);self.pn12c.Layout();
-        self.pn48c.SetSizer(self.sz14sCC);self.pn48c.SetAutoLayout(1);self.pn48c.Layout();
-        self.pn44c.SetSizer(self.tabsCC);self.pn44c.SetAutoLayout(1);self.pn44c.Layout();
-        self.pn39cCC.SetSizer(self.sz61s);self.pn39cCC.SetAutoLayout(1);self.pn39cCC.Layout();
-        self.pn39cCC2.SetSizer(self.sz61sC);self.pn39cCC2.SetAutoLayout(1);self.pn39cCC2.Layout();
+        self.tabs.SetSizer(self.tabsCCC);self.tabs.SetAutoLayout(1);self.tabs.Layout();
+        self.pn39cCCC.SetSizer(self.sz61sCC);self.pn39cCCC.SetAutoLayout(1);self.pn39cCCC.Layout();
+        self.pn39cCC2C.SetSizer(self.sz61sCC);self.pn39cCC2C.SetAutoLayout(1);self.pn39cCC2C.Layout();
         self.Refresh()
         return
     def VwXDrawBackImg(self,event,win,bitMap,opz):
@@ -201,39 +129,8 @@ class torrentDetails(wx.Panel):
         event.Skip(False)
 
         return
-    def VwXpn9cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn9cC,self.pn9cCImg0,0)
-        self.pn9cC_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXpn10cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn10cC,self.pn10cCImg0,2)
-        self.pn10cC_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXpn11cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn11cC,self.pn11cCImg0,0)
-        self.pn11cC_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXpn15cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn15cC,self.pn15cCImg0,0)
-        self.pn15cC_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
 
 #[win]add your code here
-
-    def pn9cC_VwXEvOnEraseBackground(self,event): #init function
-        #[146]Code event VwX...Don't modify[146]#
-        #add your code here
-        event.Skip()
-
-        return #end function
 
     def pn9c_VwXEvOnEraseBackground(self,event): #init function
         #[143]Code event VwX...Don't modify[143]#
@@ -242,29 +139,8 @@ class torrentDetails(wx.Panel):
 
         return #end function
 
-    def pn15cC_VwXEvOnEraseBackground(self,event): #init function
-        #[149]Code event VwX...Don't modify[149]#
-        #add your code here
-        event.Skip()
-
-        return #end function
-
-    def pn11cC_VwXEvOnEraseBackground(self,event): #init function
-        #[148]Code event VwX...Don't modify[148]#
-        #add your code here
-        event.Skip()
-
-        return #end function
-
     def pn11c_VwXEvOnEraseBackground(self,event): #init function
         #[145]Code event VwX...Don't modify[145]#
-        #add your code here
-        event.Skip()
-
-        return #end function
-
-    def pn10cC_VwXEvOnEraseBackground(self,event): #init function
-        #[147]Code event VwX...Don't modify[147]#
         #add your code here
         event.Skip()
 
--- Tribler/vwxGUI/torrentOverview.py
+++ Tribler/vwxGUI/torrentOverview.py
@@ -2,6 +2,7 @@
 # Don't modify comment 
 
 import wx
+from bgPanel import *
 from torrentFilter import *
 from torrentTabs import *
 from torrentGrid import *
@@ -25,25 +26,10 @@ class torrentOverview(wx.Panel):
 
 
     def VwXinit(self):
-        self.fileImgBuf=[None] * 7
-        self.fileImgBuf[0] = wx.Bitmap("images/triblerpanel_topleft.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[1] = wx.Bitmap("images/triblerpanel_topcenter.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[2] = wx.Bitmap("images/triblerpanel_topright.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[3] = wx.Bitmap("images/triblerpanel_bottomleft.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[4] = wx.Bitmap("images/triblerpanel_bottomcenter.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[5] = wx.Bitmap("images/add.png",wx.BITMAP_TYPE_PNG)
-        self.fileImgBuf[6] = wx.Bitmap("images/triblerpanel_bottomright.png",wx.BITMAP_TYPE_PNG)
-        self.triblerPanel_topleftImg0=self.fileImgBuf[0];
-        self.pn10cImg0=self.fileImgBuf[1];
-        self.triblerPanel_toprightImg0=self.fileImgBuf[2];
-        self.pn9cCImg0=self.fileImgBuf[3];
-        self.pn10cCImg0=self.fileImgBuf[4];
-        self.bm159cImg0=self.fileImgBuf[5];
-        self.pn11cCImg0=self.fileImgBuf[6];
+        self.fileImgBuf=[None] * 1
+        self.fileImgBuf[0] = wx.Bitmap("images/triblerpanel_topcenter.png",wx.BITMAP_TYPE_PNG)
+        self.pn10cImg0=self.fileImgBuf[0];
         self.Show(True)
-        self.triblerPanel_topleft = wx.Panel(self,-1,wx.Point(0,0),wx.Size(10,21))
-        self.triblerPanel_topleft.SetBackgroundColour(wx.Colour(0,0,0))
-        self.triblerPanel_topleft.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXtriblerPanel_topleft_VwXEvOnEraseBackground)
         self.pn10c = wx.Panel(self,-1,wx.Point(10,0),wx.Size(20,21))
         self.pn10c.SetForegroundColour(wx.Colour(255,255,255))
         self.pn10c.SetBackgroundColour(wx.Colour(255,51,0))
@@ -51,54 +37,36 @@ class torrentOverview(wx.Panel):
         self.st64c = wx.StaticText(self.pn10c,-1,"",wx.Point(0,4),wx.Size(194,17),wx.ST_NO_AUTORESIZE)
         self.st64c.SetLabel("Content")
         self.st64c.SetForegroundColour(wx.Colour(255,255,255))
-        self.triblerPanel_topright = wx.Panel(self,-1,wx.Point(613,0),wx.Size(10,21))
-        self.triblerPanel_topright.SetBackgroundColour(wx.Colour(0,0,0))
-        self.triblerPanel_topright.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXtriblerPanel_topright_VwXEvOnEraseBackground)
+        self.orange_top_right = bgPanel(self, -1, wx.Point(613,0), wx.Size(10,21))
+        self.orange_top_left = bgPanel(self, -1, wx.Point(0,0), wx.Size(10,21))
         self.torrentFilter = torrentFilter(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.torrentFilter.SetDimensions(0,21,448,20)
+        self.torrentFilter.SetDimensions(0,26,448,20)
         self.torrentTabs = torrentTabs(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.torrentTabs.SetDimensions(0,41,20,20)
-        self.pn9cC = wx.Panel(self,-1,wx.Point(0,403),wx.Size(10,28))
-        self.pn9cC.SetBackgroundColour(wx.Colour(0,0,0))
-        self.pn9cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn9cC_VwXEvOnEraseBackground)
-        self.pn10cC = wx.Panel(self,-1,wx.Point(10,399),wx.Size(20,28))
-        self.pn10cC.SetBackgroundColour(wx.Colour(0,0,0))
-        self.pn10cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn10cC_VwXEvOnEraseBackground)
-        self.bm159c = wx.StaticBitmap(self.pn10cC,-1,self.bm159cImg0,wx.Point(3,3),wx.Size(16,16))
-        self.bm159c.SetBackgroundColour(wx.Colour(255,255,255))
-        self.st158c = wx.StaticText(self.pn10cC,-1,"",wx.Point(1,1),wx.Size(279,14),wx.ST_NO_AUTORESIZE)
-        self.st158c.SetLabel("add friend")
-        self.st158c.SetForegroundColour(wx.Colour(0,0,0))
-        self.st158c.SetBackgroundColour(wx.Colour(255,255,255))
-        self.pn11cC = wx.Panel(self,-1,wx.Point(433,400),wx.Size(190,28))
-        self.pn11cC.SetBackgroundColour(wx.Colour(0,0,0))
-        self.pn11cC.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn11cC_VwXEvOnEraseBackground)
+        self.torrentTabs.SetDimensions(0,46,20,20)
         self.torrentGrid = torrentGrid(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.torrentGrid.SetDimensions(3,64,617,235)
+        self.torrentGrid.SetDimensions(0,66,617,235)
+        self.orange_pager = bgPanel(self, -1, wx.Point(440,400), wx.Size(183,28))
+        self.orange_bottom_left = bgPanel(self, -1, wx.Point(0,400), wx.Size(10,28))
+        self.orange_bottom_center = bgPanel(self, -1, wx.Point(10,400), wx.Size(20,20))
         self.sz3s = wx.BoxSizer(wx.VERTICAL)
         self.header = wx.BoxSizer(wx.HORIZONTAL)
         self.footer = wx.BoxSizer(wx.HORIZONTAL)
         self.sz65s = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz157s = wx.BoxSizer(wx.HORIZONTAL)
         self.sz3s.Add(self.header,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.torrentFilter,0,wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.torrentTabs,0,wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.torrentGrid,1,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.torrentFilter,0,wx.EXPAND|wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.torrentTabs,0,wx.EXPAND|wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.torrentGrid,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz3s.Add(self.footer,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.header.Add(self.triblerPanel_topleft,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.header.Add(self.orange_top_left,0,wx.FIXED_MINSIZE,3)
         self.header.Add(self.pn10c,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.header.SetItemMinSize(self.pn10c,20,10)
-        self.header.Add(self.triblerPanel_topright,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.footer.Add(self.pn9cC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.footer.Add(self.pn10cC,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.footer.SetItemMinSize(self.pn10cC,20,10)
-        self.footer.Add(self.pn11cC,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.header.Add(self.orange_top_right,0,wx.FIXED_MINSIZE,3)
+        self.footer.Add(self.orange_bottom_left,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.footer.Add(self.orange_bottom_center,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.footer.Add(self.orange_pager,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz65s.Add(self.st64c,0,wx.TOP|wx.EXPAND|wx.FIXED_MINSIZE,4)
-        self.sz157s.Add(self.bm159c,0,wx.FIXED_MINSIZE,3)
-        self.sz157s.Add(self.st158c,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.FIXED_MINSIZE,1)
         self.SetSizer(self.sz3s);self.SetAutoLayout(1);self.Layout();
         self.pn10c.SetSizer(self.sz65s);self.pn10c.SetAutoLayout(1);self.pn10c.Layout();
-        self.pn10cC.SetSizer(self.sz157s);self.pn10cC.SetAutoLayout(1);self.pn10cC.Layout();
         self.Refresh()
         return
     def VwXDrawBackImg(self,event,win,bitMap,opz):
@@ -124,39 +92,9 @@ class torrentOverview(wx.Panel):
 
     def VwXDelComp(self):
         return
-    def VwXtriblerPanel_topleft_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.triblerPanel_topleft,self.triblerPanel_topleftImg0,0)
-        self.triblerPanel_topleft_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
     def VwXpn10c_VwXEvOnEraseBackground(self,event):
         self.VwXDrawBackImg(event,self.pn10c,self.pn10cImg0,2)
         self.pn10c_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXtriblerPanel_topright_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.triblerPanel_topright,self.triblerPanel_toprightImg0,0)
-        self.triblerPanel_topright_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXpn9cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn9cC,self.pn9cCImg0,0)
-        self.pn9cC_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXpn10cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn10cC,self.pn10cCImg0,2)
-        self.pn10cC_VwXEvOnEraseBackground(event)
-        event.Skip(False)
-
-        return
-    def VwXpn11cC_VwXEvOnEraseBackground(self,event):
-        self.VwXDrawBackImg(event,self.pn11cC,self.pn11cCImg0,0)
-        self.pn11cC_VwXEvOnEraseBackground(event)
         event.Skip(False)
 
         return
@@ -166,42 +104,8 @@ class torrentOverview(wx.Panel):
     def bla(self):
         pass
         
-    def pn9cC_VwXEvOnEraseBackground(self,event): #init function
-        #[325]Code event VwX...Don't modify[325]#
-        #add your code here
-        event.Skip()
-
-        return #end function
-
-    def pn11cC_VwXEvOnEraseBackground(self,event): #init function
-        #[327]Code event VwX...Don't modify[327]#
-        #add your code here
-        event.Skip()
-
-        return #end function
-
-    def pn10cC_VwXEvOnEraseBackground(self,event): #init function
-        #[326]Code event VwX...Don't modify[326]#
-        #add your code here
-        event.Skip()
-
-        return #end function
 
 
-    def triblerPanel_topleft_VwXEvOnEraseBackground(self,event): #init function
-        #[6f2]Code event VwX...Don't modify[6f2]#
-        #add your code here
-        event.Skip()
-
-        return #end function
-
-
-    def triblerPanel_topright_VwXEvOnEraseBackground(self,event): #init function
-        #[ c1]Code event VwX...Don't modify[ c1]#
-        #add your code here
-        event.Skip()
-
-        return #end function
 
     def pn10c_VwXEvOnEraseBackground(self,event): #init function
         #[ c0]Code event VwX...Don't modify[ c0]#
