7b92c3cec0f6a3eb468d013cf81293006c7debb4
mtenbrinke <mtenbrinke@001aeff7-3401-0410-a489-f7902fc005dd>
2007-03-22 16:03:20 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@3229 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/vwxGUI/personsOverview.py
+++ Tribler/vwxGUI/personsOverview.py
@@ -3,15 +3,15 @@
 
 import wx
 from bgPanel import *
-from torrentFilter import *
-from torrentTabs import *
-from torrentGrid import *
+from filesFilter import *
+from filesGrid import *
 from standardPager import *
+from filesTabs import *
 #[inc]add your include files here
 
 #[inc]end your include
 
-class personsOverview(wx.Panel):
+class filesOverview(wx.Panel):
     def __init__(self,parent,id = -1,pos = wx.Point(0,0),size = wx.Size(625,430),style = wx.TAB_TRAVERSAL,name = 'panel'):
         pre=wx.PrePanel()
         self.OnPreCreate()
@@ -31,33 +31,34 @@ class personsOverview(wx.Panel):
         self.fileImgBuf[0] = wx.Bitmap("images/triblerpanel_topcenter.png",wx.BITMAP_TYPE_PNG)
         self.pn10cImg0=self.fileImgBuf[0];
         self.Show(True)
-        self.pn10c = wx.Panel(self,-1,wx.Point(10,0),wx.Size(20,21))
+        self.pn10c = wx.Panel(self,-1,wx.Point(135,0),wx.Size(20,21))
         self.pn10c.SetForegroundColour(wx.Colour(255,255,255))
         self.pn10c.SetBackgroundColour(wx.Colour(255,51,0))
         self.pn10c.Bind(wx.EVT_ERASE_BACKGROUND,self.VwXpn10c_VwXEvOnEraseBackground)
-        self.st64c = wx.StaticText(self.pn10c,-1,"",wx.Point(0,4),wx.Size(194,17),wx.ST_NO_AUTORESIZE)
-        self.st64c.SetLabel("Persons")
+        self.st64c = wx.StaticText(self.pn10c,-1,"",wx.Point(0,4),wx.Size(269,15),wx.ST_NO_AUTORESIZE)
+        self.st64c.SetLabel("Content")
         self.st64c.SetForegroundColour(wx.Colour(255,255,255))
+        self.st64c.SetBackgroundColour(wx.Colour(255,51,0))
         self.orange_top_right = bgPanel(self, -1, wx.Point(613,0), wx.Size(10,21))
         self.orange_top_left = bgPanel(self, -1, wx.Point(0,0), wx.Size(10,21))
-        self.personsFilter = torrentFilter(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.personsFilter.SetDimensions(0,21,448,20)
-        self.personsTabs = torrentTabs(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.personsTabs.SetDimensions(0,41,20,20)
-        self.personsGrid = torrentGrid(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.personsGrid.SetDimensions(0,61,617,235)
+        self.filesFilter = filesFilter(self,-1,wxDefaultPosition,wxDefaultSize)
+        self.filesFilter.SetDimensions(0,21,448,20)
+        self.torrentGrid = filesGrid(self,-1,wxDefaultPosition,wxDefaultSize)
+        self.torrentGrid.SetDimensions(0,61,623,429)
         self.orange_bottom_left = bgPanel(self, -1, wx.Point(0,400), wx.Size(10,28))
-        self.orange_bottom_center = bgPanel(self, -1, wx.Point(10,400), wx.Size(20,20))
+        self.orange_bottom_center = bgPanel(self, -1, wx.Point(10,370), wx.Size(20,20))
         self.standardPager = standardPager(self,-1,wxDefaultPosition,wxDefaultSize)
         self.standardPager.SetDimensions(440,400,183,28)
+        self.torrentTabs = filesTabs(self,-1,wxDefaultPosition,wxDefaultSize)
+        self.torrentTabs.SetDimensions(0,41,20,20)
         self.sz3s = wx.BoxSizer(wx.VERTICAL)
         self.header = wx.BoxSizer(wx.HORIZONTAL)
         self.footer = wx.BoxSizer(wx.HORIZONTAL)
-        self.sz65s = wx.BoxSizer(wx.HORIZONTAL)
+        self.size_title = wx.BoxSizer(wx.HORIZONTAL)
         self.sz3s.Add(self.header,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.personsFilter,0,wx.EXPAND|wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.personsTabs,0,wx.EXPAND|wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.personsGrid,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.filesFilter,0,wx.EXPAND|wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.torrentTabs,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.torrentGrid,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz3s.Add(self.footer,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.header.Add(self.orange_top_left,0,wx.FIXED_MINSIZE,3)
         self.header.Add(self.pn10c,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
@@ -66,9 +67,9 @@ class personsOverview(wx.Panel):
         self.footer.Add(self.orange_bottom_left,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.footer.Add(self.orange_bottom_center,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.footer.Add(self.standardPager,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz65s.Add(self.st64c,0,wx.TOP|wx.EXPAND|wx.FIXED_MINSIZE,4)
+        self.size_title.Add(self.st64c,0,wx.TOP|wx.EXPAND|wx.FIXED_MINSIZE,4)
         self.SetSizer(self.sz3s);self.SetAutoLayout(1);self.Layout();
-        self.pn10c.SetSizer(self.sz65s);self.pn10c.SetAutoLayout(1);self.pn10c.Layout();
+        self.pn10c.SetSizer(self.size_title);self.pn10c.SetAutoLayout(1);self.pn10c.Layout();
         self.Refresh()
         return
     def VwXDrawBackImg(self,event,win,bitMap,opz):
