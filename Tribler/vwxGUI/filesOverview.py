01d22cc12c706ea481347ef18eb655188f36437e
mtenbrinke <mtenbrinke@001aeff7-3401-0410-a489-f7902fc005dd>
2007-03-26 15:53:30 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@3256 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/vwxGUI/filesOverview.py
+++ Tribler/vwxGUI/filesOverview.py
@@ -3,10 +3,10 @@
 
 import wx
 from bgPanel import *
-from filesFilter import *
 from filesTabs import *
 from filesGrid import *
 from standardPager import *
+from tribler_topButton import *
 #[inc]add your include files here
 
 #[inc]end your include
@@ -41,8 +41,14 @@ class filesOverview(wx.Panel):
         self.st64c.SetForegroundColour(wx.Colour(255,255,255))
         self.st64c.SetBackgroundColour(wx.Colour(255,51,0))
         self.orange_top_right = bgPanel(self, -1, wx.Point(613,0), wx.Size(10,21))
-        self.filesFilter = filesFilter(self,-1,wxDefaultPosition,wxDefaultSize)
-        self.filesFilter.SetDimensions(0,21,448,20)
+        self.pn214c = wx.Panel(self,-1,wx.Point(0,21),wx.Size(623,35))
+        self.pn214c.SetBackgroundColour(wx.Colour(153,153,153))
+        self.tx220c = wx.TextCtrl(self.pn214c,-1,"",wx.Point(490,3),wx.Size(100,22))
+        self.tx220c.SetLabel("search a file")
+        self.chc215c = wx.Choice(self.pn214c,-1,wx.Point(109,3),wx.Size(95,21),[r'most popular',r'top rated',r'recommended',r'from network'])
+        self.chc215cCC = wx.Choice(self.pn214c,-1,wx.Point(210,3),wx.Size(100,21),[r'today',r'this week',r'this month',r'all time'])
+        self.chc215cC = wx.Choice(self.pn214c,-1,wx.Point(3,3),wx.Size(100,21),[r'video',r'audio',r'pictures',r'other',r'xxx'])
+        self.chc215cC.SetFont(wx.Font(8,74,90,90,0,"Verdana"))
         self.filesTabs = filesTabs(self,-1,wxDefaultPosition,wxDefaultSize)
         self.filesTabs.SetDimensions(0,41,20,20)
         self.filesGrid = filesGrid(self,-1,wxDefaultPosition,wxDefaultSize)
@@ -52,13 +58,16 @@ class filesOverview(wx.Panel):
         self.pagerBottomRight = bgPanel(self, -1, wx.Point(440,400), wx.Size(183,28))
         self.standardPager = standardPager(self.pagerBottomRight,-1,wxDefaultPosition,wxDefaultSize)
         self.standardPager.SetDimensions(25,5,150,23)
+        self.searchIcon = tribler_topButton(self.pn214c, -1, wx.Point(596,3), wx.Size(22,22))
         self.sz3s = wx.BoxSizer(wx.VERTICAL)
         self.header = wx.BoxSizer(wx.HORIZONTAL)
         self.footer = wx.BoxSizer(wx.HORIZONTAL)
         self.size_title = wx.BoxSizer(wx.HORIZONTAL)
         self.sz211s = wx.BoxSizer(wx.HORIZONTAL)
+        self.sz216s = wx.BoxSizer(wx.HORIZONTAL)
+        self.sz221s = wx.BoxSizer(wx.HORIZONTAL)
         self.sz3s.Add(self.header,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
-        self.sz3s.Add(self.filesFilter,0,wx.EXPAND|wx.ALIGN_LEFT|wx.FIXED_MINSIZE,3)
+        self.sz3s.Add(self.pn214c,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz3s.Add(self.filesTabs,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz3s.Add(self.filesGrid,1,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz3s.Add(self.footer,0,wx.EXPAND|wx.FIXED_MINSIZE,3)
@@ -73,9 +82,16 @@ class filesOverview(wx.Panel):
         self.sz211s.Add([25,28],0,wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.sz211s.Add(self.standardPager,1,wx.TOP|wx.EXPAND,5)
         self.sz211s.Add([8,28],0,wx.EXPAND|wx.FIXED_MINSIZE,0)
+        self.sz216s.Add(self.sz221s,1,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz216s.Add(self.tx220c,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.ALIGN_RIGHT|wx.FIXED_MINSIZE,3)
+        self.sz216s.Add(self.searchIcon,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.FIXED_MINSIZE,3)
+        self.sz221s.Add(self.chc215cC,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz221s.Add(self.chc215c,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
+        self.sz221s.Add(self.chc215cCC,0,wx.TOP|wx.LEFT|wx.BOTTOM|wx.RIGHT|wx.EXPAND|wx.FIXED_MINSIZE,3)
         self.SetSizer(self.sz3s);self.SetAutoLayout(1);self.Layout();
         self.pn10c.SetSizer(self.size_title);self.pn10c.SetAutoLayout(1);self.pn10c.Layout();
         self.pagerBottomRight.SetSizer(self.sz211s);self.pagerBottomRight.SetAutoLayout(1);self.pagerBottomRight.Layout();
+        self.pn214c.SetSizer(self.sz216s);self.pn214c.SetAutoLayout(1);self.pn214c.Layout();
         self.Refresh()
         return
     def VwXDrawBackImg(self,event,win,bitMap,opz):
