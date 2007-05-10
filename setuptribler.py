c6065cd1b2e55c2d1c9e7a2c6ac0d6c0c663076a
jdmol <jdmol@001aeff7-3401-0410-a489-f7902fc005dd>
2007-05-10 06:56:05 +0000

    Updated Windows installer.
    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@3934 001aeff7-3401-0410-a489-f7902fc005dd

--- setuptribler.py
+++ setuptribler.py
@@ -2,6 +2,10 @@
 from distutils.core import setup
 import py2exe
 
+# gui panels to include
+includePanels=[
+     "standardOverview","standardDetails","standardGrid","standardPager","standardFilter",
+     "TextButton","btn_DetailsHeader","tribler_List","profileOverviewPanel"]
 
 ################################################################
 #
@@ -15,7 +19,7 @@ import py2exe
 ################################################################
 
 target_abc = {
-    "script": "abc.py",
+    "script": "abc_vwx.py",
     "icon_resources": [(1, "tribler.ico")],
 }
 
@@ -23,7 +27,7 @@ setup(
 #    (Disabling bundle_files for now -- apparently causes some issues with Win98)
 #    options = {"py2exe": {"bundle_files": 1}},
 #    zipfile = None,
-    options = {"py2exe": {"packages": ["encodings"],
+    options = {"py2exe": {"packages": ["abc_vwx","encodings"] + ["Tribler.vwxGUI.%s" % x for x in includePanels],
                           "optimize": 2}},
     data_files = [("tribler", ["tribler.exe.manifest", "tribler.nsi", "tribler.ico", "torrenticon.ico", "binary-LICENSE.txt", "readme.txt"])], 
     windows = [target_abc],
