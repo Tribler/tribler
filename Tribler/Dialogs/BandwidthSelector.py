bce5e7459556d17049836dc3095a689cc1df4dac
jdmol <jdmol@001aeff7-3401-0410-a489-f7902fc005dd>
2007-05-10 12:27:57 +0000

    Enhanced bandwidth selector
    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@3951 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Dialogs/BandwidthSelector.py
+++ Tribler/Dialogs/BandwidthSelector.py
@@ -10,22 +10,32 @@ class BandwidthSelector(wx.Dialog):
         self.parent = parent
         self.utility = utility
 
-        style = wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
+        style = wx.DEFAULT_DIALOG_STYLE
         title = self.utility.lang.get('selectbandwidthtitle')
-        wx.Dialog.__init__(self,parent,-1,title,style=style)
+        wx.Dialog.__init__(self,parent,-1,title,style=style,size=(500,250))
         
-        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer = wx.GridBagSizer(7,3)
+
         self.bwctrl = BWControl(self)
-        sizer.Add(self.bwctrl, 2, wx.ALIGN_CENTER_VERTICAL|wx.ALL|wx.EXPAND, 5)
+        sizer.Add( self.bwctrl, (3,2) )
 
         buttonbox = wx.BoxSizer(wx.HORIZONTAL)
         okbtn = wx.Button(self, wx.ID_OK, label=self.utility.lang.get('ok'), style = wx.BU_EXACTFIT)
         buttonbox.Add(okbtn, 0, wx.ALIGN_CENTER_VERTICAL|wx.LEFT|wx.RIGHT, 5)
         cancelbtn = wx.Button(self, wx.ID_CANCEL, label=self.utility.lang.get('cancel'), style = wx.BU_EXACTFIT)
         buttonbox.Add(cancelbtn, 0, wx.ALIGN_CENTER_VERTICAL|wx.LEFT|wx.RIGHT, 5)
-        sizer.Add(buttonbox, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
 
-        self.SetSizerAndFit(sizer)
+        explain = wx.StaticText(self, -1, self.utility.lang.get('selectdlulbwexplan'))
+        explain.Wrap( 450 )
+
+        prompt = wx.StaticText(self, -1, self.utility.lang.get('selectdlulbwprompt'))
+        prompt.Wrap( 450 )
+
+        sizer.Add( explain, (1,1), span=(1,2) )
+        sizer.Add( prompt, (3,1) )
+        sizer.Add( buttonbox, (5,1), span=(2,1) )
+
+        self.SetSizer(sizer)
 
 
     def getUploadBandwidth(self):
@@ -40,21 +50,15 @@ class BWControl(wx.Panel):
         
         wx.Panel.__init__(self, parent, -1, *args, **kwargs)
         
-        filebox = wx.BoxSizer(wx.VERTICAL)
         self.uploadbwvals = [ 128/8, 256/8, 512/8, 1024/8, 2048/8, 0]
-        self.bwoptions = ['*/128 kbps', '*/256 kbps', '*/512 kbps', '*/1024 kbps', '*/2048 kbps', '*/100 mbps or more']
-        self.bwsel = wx.RadioBox(self, 
+        self.bwoptions = ['xxxx/128 kbps', 'xxxx/256 kbps', 'xxxx/512 kbps', 'xxxx/1024 kbps', 'xxxx/2048 kbps', 'more (LAN)']
+        self.bwsel = wx.Choice(self, 
                                     -1, 
-                                    self.utility.lang.get('selectdlulbwprompt'), 
+                                    #self.utility.lang.get('selectdlulbwprompt'), 
                                     wx.DefaultPosition, 
                                     wx.DefaultSize, 
                                     self.bwoptions, 
-                                    3, 
-                                    wx.RA_SPECIFY_COLS)
-
-        filebox.Add(self.bwsel, 0, wx.ALIGN_CENTER_VERTICAL|wx.EXPAND, 5)
-        filebox.Add(wx.StaticText(self, -1, self.utility.lang.get('selectdlulbwexplan')), 0, wx.ALIGN_CENTER_VERTICAL, 5)
-        self.SetSizerAndFit(filebox)
+                                    3)
 
     def getUploadBandwidth(self):
         """ in Kbyte/s """
