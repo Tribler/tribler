9ee445a8b4d988e17750cbbc35308270dea076a7
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2007-05-11 15:16:43 +0000

    - Default listenport now 7762 is you are a new Tribler user (i.e. no $HOME/.Tribler/abc.conf)
    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@4008 001aeff7-3401-0410-a489-f7902fc005dd

--- Utility/configreader.py
+++ Utility/configreader.py
@@ -47,6 +47,11 @@ class ConfigReader(ConfigParser):
         dirname = os.path.dirname(self.filename)
         if not os.access(dirname, os.F_OK):
             os.makedirs(dirname)
+
+        # Arno: Apparently port 6881 is poisoned because ISPs have blocked it.
+        # A random port does not work well with Buddycast so, pick a random, fixed one
+        if filename.endswith('abc.conf') and not os.access(filename, os.F_OK):
+            defaults['minport'] = '7762'
         
         try:
             self.read(self.filename)
