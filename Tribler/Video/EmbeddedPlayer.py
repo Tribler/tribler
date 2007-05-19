cba841a08252e14305cd3b8977ffa87523246c65
jdmol <jdmol@001aeff7-3401-0410-a489-f7902fc005dd>
2007-05-19 12:29:49 +0000

    Enabled GOOM audio visualisation.
    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@4115 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Video/EmbeddedPlayer.py
+++ Tribler/Video/EmbeddedPlayer.py
@@ -377,7 +377,7 @@ class VLCMediaCtrl(wx.Window):
 		os.chdir(vlcinstalldir)
 
         # Arno: 2007-05-11: Don't ask me why but without the "--verbose=0" vlc will ignore the key redef.
-        params = ["--verbose=0","--key-fullscreen","Esc"]
+        params = ["--verbose=0","--key-fullscreen","Esc","--audio-visual=goom"]
         if sys.platform == 'darwin':
             params += ["--plugin-path", "%s/lib/vlc" % (
                  # location of plugins: next to tribler.py
