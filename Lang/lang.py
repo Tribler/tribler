a0a827d54616f44a207abbb741ed8e6e39d9d463
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2007-06-15 08:26:22 +0000

    - Uped build rev
    git-svn-id: https://svn.tribler.org/abc/branches/jelle/guiVWX_3.6@4304 001aeff7-3401-0410-a489-f7902fc005dd

--- Lang/lang.py
+++ Lang/lang.py
@@ -80,9 +80,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 4295"
+            return "Build 4303"
         if (label == 'build_date'):
-            return "June 14, 2007"
+            return "June 15, 2007"
 
         # see if it exists in 'user.lang'
         if tryuser:
