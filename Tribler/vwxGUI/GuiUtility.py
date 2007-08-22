8996c682df13704c420541c403f9bda39d378a52
mtenbrinke <mtenbrinke@001aeff7-3401-0410-a489-f7902fc005dd>
2007-08-22 08:49:57 +0000

    - changed green-colours for in-library-items in filesoverview
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@4671 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/vwxGUI/GuiUtility.py
+++ Tribler/vwxGUI/GuiUtility.py
@@ -48,10 +48,10 @@ class GUIUtility:
         
         self.selectedColour = wx.Colour(255,200,187)       
         self.unselectedColour = wx.WHITE
-        self.unselectedColour2 = wx.Colour(230,230,230)
-        self.unselectedColourDownload = wx.Colour(188,221,110)
-        self.unselectedColour2Download = wx.Colour(148,181,90)
-        self.selectedColourDownload = wx.Colour(48,81,0)
+        self.unselectedColour2 = wx.Colour(230,230,230)        
+        self.unselectedColourDownload = wx.Colour(198,226,147)        
+        self.unselectedColour2Download = wx.Colour(190,209,139)
+        self.selectedColourDownload = wx.Colour(145,173,78)
         self.triblerRed = wx.Colour(255, 51, 0)
         
             
