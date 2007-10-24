62b40003693db4b8e625edda8a414e4c25747c49
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2007-10-24 07:39:03 +0000

    - Forgot
    git-svn-id: https://svn.tribler.org/abc/branches/arno/d07-09-21-fastgui-from-mb-r5526@5902 001aeff7-3401-0410-a489-f7902fc005dd

---
+++ main2.py
@@ -0,0 +1,17 @@
+import sys
+import time
+
+from triblerAPI import *
+
+s = Session()
+
+def state_callback(ds):
+    d = ds.get_download()
+    print >>sys.stderr,"main: SingleStats",`d.get_def().get_name()`,dlstatus_strings[ds.get_status()],ds.get_progress(),"%",ds.get_error(),"up",ds.get_current_speed(UPLOAD),"down",ds.get_current_speed(DOWNLOAD),currentThread().getName()
+    return (1.0,False)
+
+if __name__ == "__main__":
+    
+    s.set_download_states_callback(states_callback,getpeerlist=False)
+    
+    time.sleep(2500)
