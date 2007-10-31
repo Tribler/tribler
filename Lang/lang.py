7482509ffc3d5deba672913247261758bcce0c9a
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2007-10-31 10:10:11 +0000

    - Up'ed build
    git-svn-id: https://svn.tribler.org/abc/branches/arno/d07-09-21-fastgui-from-mb-r5526@6003 001aeff7-3401-0410-a489-f7902fc005dd

--- Lang/lang.py
+++ Lang/lang.py
@@ -80,9 +80,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 5990"
+            return "Build 6003"
         if (label == 'build_date'):
-            return "Oct 30, 2007"
+            return "Oct 31, 2007"
 
         # see if it exists in 'user.lang'
         if tryuser:
