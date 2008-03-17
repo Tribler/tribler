c3d106d8843067423d7f4c5e50a7ef38473eaa78
jelle <jelle@001aeff7-3401-0410-a489-f7902fc005dd>
2008-03-17 08:51:28 +0000

    forgot to commit GridState
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@7086 001aeff7-3401-0410-a489-f7902fc005dd

---
+++ Tribler/Main/vwxGUI/GridState.py
@@ -0,0 +1,30 @@
+
+
+class GridState(object):
+    def __init__(self, db, category, sort, reverse = False, library = False):
+        self.db = db        # Constant from simpledefs, f.i. NTFY_TORRENTS
+        self.category = category
+        self.sort = sort
+        self.reverse = reverse
+        self.library = library
+    def __str__(self):
+        return '(db: %s, cat: %s, sort: %s, rev: %s, lib: %s)' % (self.db,self.category,self.sort,self.reverse,self.library)
+        
+    def copy(self):
+        return GridState(self.db, self.category, self.sort, self.reverse, self.library)
+    
+    def setDefault(self, gs):
+        if gs:
+            if self.db is None:
+                self.db = gs.db
+            if self.category is None:
+                self.category = gs.category
+            if self.sort is None:
+                self.sort = gs.sort
+            if self.reverse is None:
+                self.reverse = gs.reverse
+        
+    def isValid(self):
+        return (self.db is not None and
+                self.sort is not None and
+                self.category is not None)
