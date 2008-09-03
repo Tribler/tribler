ddc5feb7737528a7a16ca7e51fe4676f8cd5ee05
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2008-09-03 14:19:05 +0000

    - Clean up
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@8792 001aeff7-3401-0410-a489-f7902fc005dd

--- ns-clean.sh
+++ ns-clean.sh
@@ -1,2 +1,11 @@
 rm Tribler/Category/porncat.txt
 rm Tribler/filter_terms.filter
+rm Tribler/readme.txt
+rm Tribler/LICENSE.txt
+rm Tribler/binary-LICENSE.txt
+rm -rf `find . -name .svn`
+rm Tribler/Test/extend_db_dir/*.gz
+rm Tribler/Test/extend_db_dir/*.log
+rm *.tstream
+rm *.torrent
+rm *.bat
