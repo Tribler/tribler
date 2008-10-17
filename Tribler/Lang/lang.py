d38cdaf84ce34edd57d5d5fc49d37d4ced7a9f35
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2008-10-17 14:19:06 +0000

    sync between crawler.txt and superpeers.txt
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@9201 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,7 +84,7 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 9200"
+            return "Build 9201"
         if (label == 'build_date'):
             return "Oct 17, 2008"
 
