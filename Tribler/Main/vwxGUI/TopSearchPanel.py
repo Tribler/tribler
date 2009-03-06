278b319dd24e070ecd2166593a4389b55754dad6
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2009-03-06 15:55:49 +0000

    - Another fix for Down/Up text on Win32.
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@10750 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Main/vwxGUI/TopSearchPanel.py
+++ Tribler/Main/vwxGUI/TopSearchPanel.py
@@ -210,8 +210,8 @@ class TopSearchPanel(bgPanel):
         self.tribler_logo2 = wx.StaticBitmap(self, -1, self.Bitmap("images/logo4video2.png", wx.BITMAP_TYPE_ANY))
         ##self.left = wx.StaticBitmap(self,-1, self.Bitmap("images/5.0/left.png", wx.BITMAP_TYPE_ANY))
         ##self.right = wx.StaticBitmap(self,-1, self.Bitmap("images/5.0/right.png", wx.BITMAP_TYPE_ANY))
-        self.total_down = wx.StaticText(self, -1, "")
-        self.total_up = wx.StaticText(self, -1, "")
+        self.total_down = wx.StaticText(self, -1, "0B Down")
+        self.total_up = wx.StaticText(self, -1, "0B Up")
         
 
         self.__set_properties()
@@ -315,11 +315,7 @@ class TopSearchPanel(bgPanel):
         ##object_13.Add(self.left, 0, 0, 0)
         object_13.Add((0, 0), 0, 0, 0)
         object_13.Add(self.total_down, 0, 0, 0)
-        # ARNO: our code
-        if sys.platform == "win32":
-            object_13.Add((64, 0), 0, 0, 0)
-        else:
-            object_13.Add((8, 0), 0, 0, 0)
+        object_13.Add((8, 0), 0, 0, 0)
         object_13.Add(self.total_up, 0, 0, 0)
         object_13.Add((0, 0), 0, 0, 0)
         ##object_13.Add(self.right, 0, 0, 0)
