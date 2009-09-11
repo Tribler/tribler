493bd7414e99780d4c5619345a4dae54f8339062
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2009-09-11 12:21:24 +0000

    - Updated date.
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@12849 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,9 +84,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 12075"
+            return "Build 12848"
         if (label == 'build_date'):
-            return "Jul 06, 2009"
+            return "Sep 11, 2009"
         # see if it exists in 'user.lang'
         if tryuser:
             text, found = self.getFromLanguage(label, self.user_lang)
