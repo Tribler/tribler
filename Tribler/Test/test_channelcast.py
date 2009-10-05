0d2429add6a0c61f6bc4d5f4f57d7bc7be66b67b
nitin <nitin@001aeff7-3401-0410-a489-f7902fc005dd>
2009-10-05 16:01:30 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@13196 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Test/test_channelcast.py
+++ Tribler/Test/test_channelcast.py
@@ -13,13 +13,16 @@ import tempfile
 from M2Crypto import Rand,EC
 
 from Tribler.Test.test_as_server import TestAsServer
+from Tribler.Core.Session import *
 from olconn import OLConnection
 import btconn
 from Tribler.Core.BuddyCast.channelcast import ChannelCastCore
+from Tribler.Core.BuddyCast.votecast import VoteCastCore
+from Tribler.Core.SocialNetwork.ChannelQueryMsgHandler import ChannelQueryMsgHandler
 from Tribler.Core.BitTornado.bencode import bencode,bdecode
 from Tribler.Core.BitTornado.BT1.MessageID import *
 from Tribler.Core.simpledefs import *
-from Tribler.Core.CacheDB.CacheDBHandler import ChannelCastDBHandler
+from Tribler.Core.CacheDB.CacheDBHandler import *
 from Tribler.Core.CacheDB.sqlitecachedb import SQLiteCacheDB, bin2str, str2bin, NULL
 from Tribler.Core.Overlay.permid import sign_data, generate_keypair, permid_for_user
 import os
@@ -27,9 +30,27 @@ import sys
 import time
 DEBUG=True
 
+class FakeLaunchmany:
+    
+    def __init__(self):
+        self.peer_db = PeerDBHandler.getInstance()
+        self.superpeer_db = SuperPeerDBHandler.getInstance()
+        self.torrent_db = TorrentDBHandler.getInstance()
+        self.mypref_db = MyPreferenceDBHandler.getInstance()
+        self.pref_db = PreferenceDBHandler.getInstance()
+        self.friend_db =  FriendDBHandler.getInstance()
+        self.listen_port = 6881
+        self.session = Session()
+
+    def get_ext_ip(self):
+        return '127.0.0.1'
+
+class FakeOverlayBridge:
+    def add_task(self, foo, sec=0):
+        foo()
 
 class TestChannelCast(TestAsServer):
-    """   Testing ModerationCast message    """
+    """   Testing ChannelCast message    """
     
     
     def setUp(self):
@@ -42,7 +63,10 @@ class TestChannelCast(TestAsServer):
 
         # New code
         self.channelcastdb = ChannelCastDBHandler.getInstance()
-        self.channelcastdb.registerSession(self.session)        
+        self.channelcastdb.registerSession(self.session) 
+        
+        self.votecastdb = VoteCastDBHandler.getInstance()
+        self.votecastdb.registerSession(self.session)       
 
     def setUpPreSession(self):
         """ override TestAsServer """
@@ -72,115 +96,110 @@ class TestChannelCast(TestAsServer):
             os.remove(self.superpeerfilename)
         except:
             print_exc()
-            
-    def createSubscriptions(self):
-        subs = {}
-        for i in range(10):
-            sleep(1)
-            subs['publisher_id']='nitin' + str(i)
-            subs['subscriber_id']= permid_for_user(self.mypermid)
-            subs['time_stamp'] = int(time())        
-            self.channelcastdb.addSubscription(subs)
-    
-    
-    def test_subscribe(self):
-        
-        subs ={}
-        subs['publisher_id']='nitin'
-        subs['subscriber_id']= permid_for_user(self.mypermid)
-        subs['time_stamp'] = 123123123      
-        
-        self.assertFalse(self.channelcastdb.hasSubscription(subs))
-        
-        self.channelcastdb.addSubscription(subs)
-        
-        print >> sys.stderr, "Test_Subscribe starts"
-        self.assertTrue(self.channelcastdb.hasSubscription(subs))
-        print >> sys.stderr, "Test_Subscribe: sub insertion checked"
-        
-        
-        subs['publisher_id']=None
-        try:
-            self.channelcastdb.addSubscription(subs)
-            self.fail("Fails: Added Subscription but its NULL")
-            print >> sys.stderr, "Test_Subscribe: Fails: Added Subscription but its NULL"
-        except:
-            print >> sys.stderr, "Test_Subscribe: pub"
-            pass
-        
-        subs['subscriber_id']=None
-        try:
-            self.channelcastdb.addSubscription(subs)
-            self.fail("Fails: Added Subscription but its NULL")
-            print >> sys.stderr, "Test_Subscribe: Fails: Added Subscription but its NULL"
-        except:
-            print >> sys.stderr, "Test_Subscribe: sub"
-            pass
-        
-        subs['time_stamp']=None
-        try:
-            self.channelcastdb.addSubscription(subs)
-            self.fail("Fails: Added Subscription but its NULL")
-            print >> sys.stderr, "Test_Subscribe: Fails: Added Subscription but its NULL"
-        except:
-            print >> sys.stderr, "Test_Subscribe: time_stamp"
-            pass
-        
-        subs['publisher_id']= ''
-        try:
-            self.channelcastdb.addSubscription(subs)
-            self.fail("Fails: Added Subscription but pub is empty")
-            print >> sys.stderr, "Test_Subscribe: pub empty"
-        except:
-            pass
-        
-        subs['subscriber_id']=None
-        try:
-            self.channelcastdb.addSubscription(subs)
-            self.fail("Fails: Added Subscription but sub is empty")
-            print >> sys.stderr, "Test_Subscribe: sub empty"
-        except:
-            pass
 
-        subs['time_stamp']= 'hee'
-        try:
-            self.channelcastdb.addSubscription(subs)
-            self.fail("Fails: Added Subscription but its NULL")
-            print >> sys.stderr, "Test_Subscribe: time diff"
-        except:
-            pass        
-                 
-    def test_unsubscribe(self):        
-        subs ={}
-        subs['publisher_id']='nitin'
-        subs['subscriber_id']= permid_for_user(self.hispermid) # testbed holder's permid is in session, ie, session.permid = hispermid
-        subs['time_stamp'] = 123123123      
-        self.channelcastdb.addSubscription(subs)
+    def test_channel_subscription(self):
+        self.votecastdb.unsubscribe(bin2str(self.mypermid))
+        self.assertEqual(self.votecastdb.getVote(bin2str(self.mypermid),bin2str(self.hispermid)),None)
+        print >> sys.stderr, self.votecastdb.getAll()
 
-        self.channelcastdb.removePublisher('nitin')
+        self.votecastdb.spam(bin2str(self.mypermid))
+        self.assertEqual(self.votecastdb.getVote(bin2str(self.mypermid),bin2str(self.hispermid)),-1)
+        print >> sys.stderr, self.votecastdb.getAll()
                 
-        self.assertFalse(self.channelcastdb.hasSubscription(subs))
+        self.votecastdb.subscribe(bin2str(self.mypermid))
+        self.assertEqual(self.votecastdb.getVote(bin2str(self.mypermid),bin2str(self.hispermid)),2)
+        print >> sys.stderr, self.votecastdb.getAll()
         
-        self.channelcastdb.removePublisher('nitin')
-        print >> sys.stderr, "Removed nitin again!"
+        self.votecastdb.unsubscribe(bin2str(self.mypermid))
+        self.assertEqual(self.votecastdb.getVote(bin2str(self.mypermid),bin2str(self.hispermid)),0)
+        print >> sys.stderr, self.votecastdb.getAll()
         
-        self.channelcastdb.removePublisher(None)
+        self.votecastdb.spam(bin2str(self.mypermid))
+        self.assertEqual(self.votecastdb.getVote(bin2str(self.mypermid),bin2str(self.hispermid)),-1)
+        print >> sys.stderr, self.votecastdb.getAll()
+        
+    def check_chquery_reply(self,data):
+        d = bdecode(data)
+        self.assert_(type(d) == DictType)
+        self.assert_(d.has_key('a'))
+        self.assert_(d.has_key('id'))
+        id = d['id']
+        self.assert_(type(id) == StringType)                
 
     def test_channelcast(self):
-        subs ={}
-        subs['publisher_id']='nitin'
-        subs['subscriber_id']= permid_for_user(self.hispermid) # testbed holder's permid is in session, ie, session.permid = hispermid
-        subs['time_stamp'] = 12312323      
-        self.channelcastdb.addSubscription(subs)
+        torrent_data = {'announce':"http://localhost", 'info':{'name':'Hello 123', 'files':[{'length':100, 'path':['license.txt']}]}}
+        infohash = bin2str(sha(bencode(torrent_data['info'])).digest())
+        self.channelcastdb.addOwnTorrent(infohash,torrent_data)
         
         s = OLConnection(self.my_keypair,'localhost',self.hisport)
         chcast = ChannelCastCore(None, s, self.session, None, log = '', dnsindb = None)
+        
+        # Good message
         chdata =  chcast.createChannelCastMessage()
         if chdata is None or len(chdata) ==0:
-            print >>sys.stderr,"test: no subscriptions for us.. hence do not send"       
+            print "test: no subscriptions for us.. hence do not send"       
         else:
             msg = CHANNELCAST + bencode(chdata)        
-            print >>sys.stderr,"test: channelcast msg created", repr(msg)        
+            print "test: channelcast msg created", repr(chdata)        
+            s.send(msg)
+        
+        time.sleep(3)
+        
+        # Bad message
+        if chdata is None or len(chdata)==0:
+            pass
+        else:
+            pub_id, pub_name, infohash, torrenthash, name, timestamp, signature = chdata[0]
+            chdata = [(pub_id, pub_name, infohash, torrenthash, name, 12343, signature)]
+            msg = CHANNELCAST + bencode(chdata)        
+            print "test: channelcast msg created", repr(chdata)        
+            s.send(msg)
+            time.sleep(20)
+            # the other side should have closed the connection, as it is invalid message
+        
+            
+        s.close()        
+        
+    def test_channel_query(self):
+        
+        s = OLConnection(self.my_keypair,'localhost',self.hisport)
+        msg = CHANNEL_QUERY+bencode({'q':'k:dutiet', 'id': 'a' * 20})
+        s.send(msg)
+        resp = s.recv()
+        if len(resp) > 0:
+            print >>sys.stderr,"test: good CH_QUERY: got",getMessageName(resp[0])
+            self.assert_(resp[0] == CHANNEL_QUERY_REPLY)
+            self.check_chquery_reply(resp[1:])
+        time.sleep(10)
+        # the other side should not have closed the connection, as
+        # this is all valid, so this should not throw an exception:
+        #s.send('bla')
+        s.close()        
+    
+    def test_channel_update(self):
+        s = OLConnection(self.my_keypair,'localhost',self.hisport)
+        msg = CHANNEL_QUERY+bencode({'q':'p:345fsdf34fe345ed344g5', 'id': 'a' * 20})
+        s.send(msg)
+        resp = s.recv()
+        if len(resp) > 0:
+            print >>sys.stderr,"test: good CH_QUERY: got",getMessageName(resp[0])
+            self.assert_(resp[0] == CHANNEL_QUERY_REPLY)
+            self.check_chquery_reply(resp[1:])
+        time.sleep(10)
+        # the other side should not have closed the connection, as
+        # this is all valid, so this should not throw an exception:
+        s.send('bla')
+        s.close()
+            
+    def test_votecast(self):
+        self.votecastdb.subscribe('nitin')
+        s = OLConnection(self.my_keypair,'localhost',self.hisport)
+        votecast = VoteCastCore(None, s, self.session, None, log = '', dnsindb = None)
+        data = votecast.createVoteCastMessage()
+        if data is None and len(data)==0:
+            print >>sys.stderr, "test: no votes"
+        else:
+            msg = VOTECAST + bencode(data)
             s.send(msg)
             s.close()
             
@@ -203,3 +222,5 @@ def main():
 if __name__ == "__main__":
     main()
 
+def usercallback(permid,query,d):
+    print >> sys.stderr, "chquery_connected_peers: Processing reply:", permid, query, repr(d)
