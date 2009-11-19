52e5157895f61cce12d448da60aefa7ff681fdc3
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2009-11-19 08:21:10 +0000

    - Updated .inf files for .cab
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@13721 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,7 +84,7 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 13718"
+            return "Build 13719"
         if (label == 'build_date'):
             return "Nov 19, 2009"
         # see if it exists in 'user.lang'
