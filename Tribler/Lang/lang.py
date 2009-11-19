a499ee6ab468ac43b6bd5c265bcbc7fe5b8e511b
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2009-11-19 13:48:18 +0000

    - Up'ed rev# rc13
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@13735 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,7 +84,7 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 13732"
+            return "Build 13735"
         if (label == 'build_date'):
             return "Nov 19, 2009"
         # see if it exists in 'user.lang'
