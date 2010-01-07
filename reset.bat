67cd75cf79a9789734bf6587897bc087eec4d24d
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2010-01-07 08:30:11 +0000

    - Fixes for Vista/Win7
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@14184 001aeff7-3401-0410-a489-f7902fc005dd

--- reset.bat
+++ reset.bat
@@ -1,13 +1,23 @@
 
-set TDIR="%USERPROFILE%\Application Data\.Tribler"
+ver | find "Version 6." > nul
+if %ERRORLEVEL% == 0 goto IFDEFVISTA
+set APPDIR="Application Data"
+goto general
+
+:IFDEFVISTA
+set APPDIR=AppData\Roaming
+goto general
+
+:general
+set TDIR="%USERPROFILE%\%APPDIR%\.Tribler"
 set DDIR="%USERPROFILE%\Desktop\TriblerDownloads"
 rmdir %TDIR% /S /Q
 rmdir %DDIR% /S /Q
 
 REM SwarmPlayer
-set TDIR="%USERPROFILE%\Application Data\.SwarmPlayer"
+set TDIR="%USERPROFILE%\%APPDIR%\.SwarmPlayer"
 rmdir %TDIR% /S /Q
 
 REM SwarmPlugin
-set TDIR="%USERPROFILE%\Application Data\.SwarmPlugin"
+set TDIR="%USERPROFILE%\%APPDIR%\.SwarmPlugin"
 rmdir %TDIR% /S /Q
