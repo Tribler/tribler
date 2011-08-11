59459c5feac7ae7628fa378c977fcb74ffdcb164
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2011-08-11 16:25:39 +0000

    fix
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.4.x@21887 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Core/dispersy/dispersy.py
+++ Tribler/Core/dispersy/dispersy.py
@@ -1840,7 +1840,7 @@ class Dispersy(Singleton):
 
             if not self._is_valid_external_address(address):
                 # this is a programming bug.  apparently an invalid address is being used
-                if __debug__: dprint("aborted sending a ", len(packet), " byte packet (invalid external address) to ", address[0], ":", address[1], level="error")
+                if __debug__: dprint("aborted sending ", sum(len(packet) for packet in packets), " bytes in ", len(packets), " packets (invalid external address) to ", address[0], ":", address[1], level="error")
                 continue
 
             for packet in packets:
