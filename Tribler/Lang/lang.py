cb8c9e3b6e1c3f945a5edc4779f3635ad2074128
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2011-08-30 07:26:40 +0000

    version update
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.4.x@22184 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,9 +84,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 22147"
+            return "Build 22184"
         if (label == 'build_date'):
-            return "August 26, 2011"
+            return "August 30, 2011"
         # see if it exists in 'user.lang'
         if tryuser:
             text, found = self.getFromLanguage(label, self.user_lang)
