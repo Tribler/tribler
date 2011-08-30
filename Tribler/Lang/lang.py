5b94900479da38f6312c17e8df7036f7c6e2eef7
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2011-08-30 07:43:49 +0000

    version update
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.4.x@22187 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,7 +84,7 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 22184"
+            return "Build 22187"
         if (label == 'build_date'):
             return "August 30, 2011"
         # see if it exists in 'user.lang'
--- Tribler/Main/Build/Win32/tribler.nsi
+++ Tribler/Main/Build/Win32/tribler.nsi
@@ -1,5 +1,5 @@
 !define PRODUCT "Tribler"
-!define VERSION "5.4.0"
+!define VERSION "5.4.1"
 
 !include "MUI.nsh"
 
