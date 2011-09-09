27342ac0b667df31dfedd575bdb0d78d9757800d
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2011-09-09 11:55:15 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/release-5.4.x@22297 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Main/vwxGUI/GuiUtility.py
+++ Tribler/Main/vwxGUI/GuiUtility.py
@@ -196,7 +196,7 @@ class GUIUtility:
                     self.ShowPlayer(False)
             
             self.guiPage = page
-            self.self.frame.top_bg._selectPage(page)
+            self.frame.top_bg._selectPage(page)
             
             self.frame.Layout()
             self.frame.Thaw()
