fac3acfe7b9d4b7b49e215a2daaeafa9f4793248
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2011-09-14 15:00:19 +0000

    drop in dummy replacement for M2Crypto.EC
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@22393 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Core/dispersy/crypto.py
+++ Tribler/Core/dispersy/crypto.py
@@ -6,208 +6,282 @@ The crypto module provides a layer between Dispersy and low level crypographic f
 @contact: dispersy@frayja.com
 """
 
-from hashlib import sha1, sha224, sha256, sha512, md5
-from math import ceil
-from random import randint
-from struct import pack
-import M2Crypto
+if False:
+    #
+    # disable crypto
+    #
 
-# We want to provide a few default curves.  We will change these
-# curves as new become available and old ones to small to provide
-# sufficient security.
-_curves = {u"very-low":M2Crypto.EC.NID_sect163k1,
-           u"low":M2Crypto.EC.NID_sect233k1,
-           u"medium":M2Crypto.EC.NID_sect409k1,
-           u"high":M2Crypto.EC.NID_sect571r1}
+    from random import random
 
-def _progress(*args):
-    "Called when no feedback needs to be given."
-    pass
+    _curves = {u"very-low":42,
+               u"low":60,
+               u"medium":104,
+               u"high":144}
 
-def ec_generate_key(security):
-    """
-    Generate a new Elliptic Curve object with a new public / private
-    key pair.
+    def ec_generate_key(security):
+        assert isinstance(security, unicode)
+        assert security in _curves
 
-    Security can be u'low', u'medium', or u'high' depending on how secure you need your Elliptic
-    Curve to be.  Currently these values translate into:
-     - very-low: NID_sect163k1  ~42 byte signatures
-     - low:      NID_sect233k1  ~60 byte signatures
-     - medium:   NID_sect409k1 ~104 byte signatures
-     - high:     NID_sect571r1 ~144 byte signatures
+        length = _curves[security]
+        private_key = "".join(chr(random() * 2**8) for _ in xrange(2*length))
+        public_key = private_key[:length]
 
-    @param security: Level of security {u'very-low', u'low', u'medium', or u'high'}.
-    @type security: unicode
+        return (length, public_key, private_key)
 
-    @note that the NID must always be 160 bits or more, otherwise it will not be able to sign a sha1
-     digest.
-    """
-    assert isinstance(security, unicode)
-    assert security in _curves
-    ec = M2Crypto.EC.gen_params(_curves[security])
-    ec.gen_key()
-    return ec
+    def ec_public_pem_to_public_bin(pem):
+        return ";".join(pem.decode("HEX"))
 
-def ec_public_pem_to_public_bin(pem):
-    "Convert a public key in PEM format into a public key in binary format."
-    return "".join(pem.split("\n")[1:-2]).decode("BASE64")
+    def ec_private_pem_to_private_bin(pem):
+        return pem.decode("HEX")
 
-def ec_private_pem_to_private_bin(pem):
-    """
-    Convert a private key in PEM format into a private key in binary format.
+    def ec_to_private_pem(ec, cipher=None, password=None):
+        return ";".join((str(ec[0]), ec[1].encode("HEX"), ec[2].encode("HEX")))
 
-    @note: Enrcypted pem's are NOT supported and will silently fail.
-    """
-    return "".join(pem.split("\n")[1:-2]).decode("BASE64")
+    def ec_to_public_pem(ec):
+        return ";".join((str(ec[0]), ec[1].encode("HEX")))
 
-def ec_to_private_pem(ec, cipher=None, password=None):
-    "Get the private key in PEM format."
-    def get_password(*args):
-        return password or ""
-    bio = M2Crypto.BIO.MemoryBuffer()
-    ec.save_key_bio(bio, cipher, get_password)
-    return bio.read_all()
+    def ec_from_private_pem(pem, password=None):
+        length, public_key, private_key = pem.split(";")
+        return int(length), public_key.decode("HEX"), private_key.decode("HEX")
 
-def ec_to_public_pem(ec):
-    "Get the public key in PEM format."
-    bio = M2Crypto.BIO.MemoryBuffer()
-    ec.save_pub_key_bio(bio)
-    return bio.read_all()
+    def ec_from_public_pem(pem):
+        length, public_key = pem.split(";")
+        return int(length), public_key.decode("HEX")
 
-def ec_from_private_pem(pem, password=None):
-    "Get the EC from a private PEM."
-    def get_password(*args):
-        return password or ""
-    return M2Crypto.EC.load_key_bio(M2Crypto.BIO.MemoryBuffer(pem), get_password)
+    def ec_to_private_bin(ec):
+        return ec_to_private_pem(ec)
 
-def ec_from_public_pem(pem):
-    "Get the EC from a public PEM."
-    return M2Crypto.EC.load_pub_key_bio(M2Crypto.BIO.MemoryBuffer(pem))
+    def ec_to_public_bin(ec):
+        return ec_to_public_pem(ec)
 
-def ec_to_private_bin(ec):
-    "Get the private key in binary format."
-    return ec_private_pem_to_private_bin(ec_to_private_pem(ec))
+    def ec_check_private_bin(string):
+        return bool(ec_from_private_bin(string))
 
-def ec_to_public_bin(ec):
-    "Get the public key in binary format."
-    return ec_public_pem_to_public_bin(ec_to_public_pem(ec))
+    def ec_check_public_bin(string):
+        return bool(ec_from_public_bin(string))
 
-def ec_check_private_bin(string):
-    "Returns True if the input is a valid private key"
-    try:
-        ec_from_private_bin(string)
-    except:
-        return False
-    return True
+    def ec_from_private_bin(string):
+        return ec_from_private_pem(string)
 
-def ec_check_public_bin(string):
-    "Returns True if the input is a valid public key"
-    try:
-        ec_from_public_bin(string)
-    except:
-        return False
-    return True
+    def ec_from_public_bin(string):
+        return ec_from_public_pem(string)
 
-def ec_from_private_bin(string):
-    "Get the EC from a private key in binary format."
-    return ec_from_private_pem("".join(("-----BEGIN EC PRIVATE KEY-----\n", string.encode("BASE64"), "-----END EC PRIVATE KEY-----\n")))
+    def ec_signature_length(ec):
+        return ec[0]
 
-def ec_from_public_bin(string):
-    "Get the EC from a public key in binary format."
-    return ec_from_public_pem("".join(("-----BEGIN PUBLIC KEY-----\n", string.encode("BASE64"), "-----END PUBLIC KEY-----\n")))
+    def ec_sign(ec, digest):
+        return "-" * ec[0]
 
-def ec_signature_length(ec):
-    """
-    Returns the length, in bytes, of each signature made using EC.
-    """
-    return int(ceil(len(ec) / 8.0)) * 2
+    def ec_verify(ec, digest, signature):
+        return True
 
-def ec_sign(ec, digest):
-    """
-    Returns the signature of DIGEST made using EC.
-    """
-    length = int(ceil(len(ec) / 8.0))
-    r, s = ec.sign_dsa(digest)
-    return "\x00" * (length - len(r) + 4) + r[4:] + "\x00" * (length - len(s) + 4) + s[4:]
+else:
+    #
+    # enable crypto
+    #
 
-def ec_verify(ec, digest, signature):
-    """
-    Returns True when SIGNATURE matches the DIGEST made using EC.
-    """
-    assert len(signature) == ec_signature_length(ec)
-    length = len(signature) / 2
-    prefix = pack("!L", length)
-    try:
-        return bool(ec.verify_dsa(digest, prefix + signature[:length], prefix + signature[length:]))
-    except:
-        return False
+    from hashlib import sha1, sha224, sha256, sha512, md5
+    from math import ceil
+    from random import randint
+    from struct import pack
+    import M2Crypto
 
-def rsa_generate_key(bits=1024, exponent=5, progress=_progress):
-    """
-    Generate a new RSA object with a new public / private key pair.
+    # We want to provide a few default curves.  We will change these
+    # curves as new become available and old ones to small to provide
+    # sufficient security.
+    _curves = {u"very-low":M2Crypto.EC.NID_sect163k1,
+               u"low":M2Crypto.EC.NID_sect233k1,
+               u"medium":M2Crypto.EC.NID_sect409k1,
+               u"high":M2Crypto.EC.NID_sect571r1}
 
-    Note: with RSA it is dangerous to use a small exponent to encrypt
-    the same message to multiple recipients, as this can lead to an
-    algebraic attack.
-    """
-    assert isinstance(bits, (int, long))
-    assert bits % 8 == 0
-    assert isinstance(exponent, int)
-    assert hasattr("__call__", progress)
-    # assert bits >= 512, "Need at least 512 bits to sign sha1 message digests"
-    return M2Crypto.RSA.gen_key(bits, exponent, progress)
+    def _progress(*args):
+        "Called when no feedback needs to be given."
+        pass
 
-def rsa_to_private_pem(rsa, cipher="aes_128_cbc", password=None):
-    """
-    Get the private key in PEM format.
-    """
-    assert isinstance(rsa, M2Crypto.RSA.RSA)
-    assert password is None or isinstance(password, str)
-    def get_password(*args):
-        return password or "-empty-"
-    bio = M2Crypto.BIO.MemoryBuffer()
-    rsa.save_key_bio(bio, cipher, get_password)
-    return bio.read_all()
+    def ec_generate_key(security):
+        """
+        Generate a new Elliptic Curve object with a new public / private
+        key pair.
 
-def rsa_to_private_bin(rsa, cipher="aes_128_cbc", password=None):
-    pem = rsa_to_private_pem(rsa, cipher, password)
-    lines = pem.split("\n")
-    return "".join(lines[4:-2]).decode("BASE64")
+        Security can be u'low', u'medium', or u'high' depending on how secure you need your Elliptic
+        Curve to be.  Currently these values translate into:
+            - very-low: NID_sect163k1  ~42 byte signatures
+            - low:      NID_sect233k1  ~60 byte signatures
+            - medium:   NID_sect409k1 ~104 byte signatures
+            - high:     NID_sect571r1 ~144 byte signatures
 
-def rsa_to_public_pem(rsa):
-    """
-    Get the public key in binary format from RSA.
+        @param security: Level of security {u'very-low', u'low', u'medium', or u'high'}.
+        @type security: unicode
 
-    # note: for some reason the M2Crypto interface does not allow us
-    # to set the cipher or the password.  These two parameters are
-    # therefore ignored.
-    """
-    assert isinstance(rsa, M2Crypto.RSA.RSA)
-    bio = M2Crypto.BIO.MemoryBuffer()
-    rsa.save_pub_key_bio(bio)
-    return bio.read_all()
+        @note that the NID must always be 160 bits or more, otherwise it will not be able to sign a sha1
+            digest.
+        """
+        assert isinstance(security, unicode)
+        assert security in _curves
+        ec = M2Crypto.EC.gen_params(_curves[security])
+        ec.gen_key()
+        return ec
 
-def rsa_to_public_bin(rsa, cipher="aes_128_cbc", password=None):
-    pem = rsa_to_public_pem(rsa, cipher, password)
-    lines = pem.split("\n")
-    return "".join(lines[1:-2]).decode("BASE64")
+    def ec_public_pem_to_public_bin(pem):
+        "Convert a public key in PEM format into a public key in binary format."
+        return "".join(pem.split("\n")[1:-2]).decode("BASE64")
 
-def rsa_from_private_pem(pem, password=None):
-    """
-    Create a RSA public / private key pair from a PEM binary string.
-    """
-    assert isinstance(pem, str)
-    assert password is None or isinstance(password, str)
-    def get_password(*args):
-        return password or "-empty-"
-    return M2Crypto.RSA.load_key_bio(M2Crypto.BIO.MemoryBuffer(pem), get_password)
+    def ec_private_pem_to_private_bin(pem):
+        """
+        Convert a private key in PEM format into a private key in binary format.
 
-def rsa_from_public_pem(pem):
-    """
-    Create a RSA public part from a PEM binary string.
-    """
-    assert isinstance(pem, str)
-    return M2Crypto.RSA.load_pub_key_bio(M2Crypto.BIO.MemoryBuffer(pem))
+        @note: Enrcypted pem's are NOT supported and will silently fail.
+        """
+        return "".join(pem.split("\n")[1:-2]).decode("BASE64")
+
+    def ec_to_private_pem(ec, cipher=None, password=None):
+        "Get the private key in PEM format."
+        def get_password(*args):
+            return password or ""
+        bio = M2Crypto.BIO.MemoryBuffer()
+        ec.save_key_bio(bio, cipher, get_password)
+        return bio.read_all()
+
+    def ec_to_public_pem(ec):
+        "Get the public key in PEM format."
+        bio = M2Crypto.BIO.MemoryBuffer()
+        ec.save_pub_key_bio(bio)
+        return bio.read_all()
+
+    def ec_from_private_pem(pem, password=None):
+        "Get the EC from a private PEM."
+        def get_password(*args):
+            return password or ""
+        return M2Crypto.EC.load_key_bio(M2Crypto.BIO.MemoryBuffer(pem), get_password)
+
+    def ec_from_public_pem(pem):
+        "Get the EC from a public PEM."
+        return M2Crypto.EC.load_pub_key_bio(M2Crypto.BIO.MemoryBuffer(pem))
+
+    def ec_to_private_bin(ec):
+        "Get the private key in binary format."
+        return ec_private_pem_to_private_bin(ec_to_private_pem(ec))
+
+    def ec_to_public_bin(ec):
+        "Get the public key in binary format."
+        return ec_public_pem_to_public_bin(ec_to_public_pem(ec))
+
+    def ec_check_private_bin(string):
+        "Returns True if the input is a valid private key"
+        try:
+            ec_from_private_bin(string)
+        except:
+            return False
+        return True
+
+    def ec_check_public_bin(string):
+        "Returns True if the input is a valid public key"
+        try:
+            ec_from_public_bin(string)
+        except:
+            return False
+        return True
+
+    def ec_from_private_bin(string):
+        "Get the EC from a private key in binary format."
+        return ec_from_private_pem("".join(("-----BEGIN EC PRIVATE KEY-----\n", string.encode("BASE64"), "-----END EC PRIVATE KEY-----\n")))
+
+    def ec_from_public_bin(string):
+        "Get the EC from a public key in binary format."
+        return ec_from_public_pem("".join(("-----BEGIN PUBLIC KEY-----\n", string.encode("BASE64"), "-----END PUBLIC KEY-----\n")))
+
+    def ec_signature_length(ec):
+        """
+        Returns the length, in bytes, of each signature made using EC.
+        """
+        return int(ceil(len(ec) / 8.0)) * 2
+
+    def ec_sign(ec, digest):
+        """
+        Returns the signature of DIGEST made using EC.
+        """
+        length = int(ceil(len(ec) / 8.0))
+        r, s = ec.sign_dsa(digest)
+        return "\x00" * (length - len(r) + 4) + r[4:] + "\x00" * (length - len(s) + 4) + s[4:]
+
+    def ec_verify(ec, digest, signature):
+        """
+        Returns True when SIGNATURE matches the DIGEST made using EC.
+        """
+        assert len(signature) == ec_signature_length(ec)
+        length = len(signature) / 2
+        prefix = pack("!L", length)
+        try:
+            return bool(ec.verify_dsa(digest, prefix + signature[:length], prefix + signature[length:]))
+        except:
+            return False
+
+# def rsa_generate_key(bits=1024, exponent=5, progress=_progress):
+#     """
+#     Generate a new RSA object with a new public / private key pair.
+
+#     Note: with RSA it is dangerous to use a small exponent to encrypt
+#     the same message to multiple recipients, as this can lead to an
+#     algebraic attack.
+#     """
+#     assert isinstance(bits, (int, long))
+#     assert bits % 8 == 0
+#     assert isinstance(exponent, int)
+#     assert hasattr("__call__", progress)
+#     # assert bits >= 512, "Need at least 512 bits to sign sha1 message digests"
+#     return M2Crypto.RSA.gen_key(bits, exponent, progress)
+
+# def rsa_to_private_pem(rsa, cipher="aes_128_cbc", password=None):
+#     """
+#     Get the private key in PEM format.
+#     """
+#     assert isinstance(rsa, M2Crypto.RSA.RSA)
+#     assert password is None or isinstance(password, str)
+#     def get_password(*args):
+#         return password or "-empty-"
+#     bio = M2Crypto.BIO.MemoryBuffer()
+#     rsa.save_key_bio(bio, cipher, get_password)
+#     return bio.read_all()
+
+# def rsa_to_private_bin(rsa, cipher="aes_128_cbc", password=None):
+#     pem = rsa_to_private_pem(rsa, cipher, password)
+#     lines = pem.split("\n")
+#     return "".join(lines[4:-2]).decode("BASE64")
+
+# def rsa_to_public_pem(rsa):
+#     """
+#     Get the public key in binary format from RSA.
+
+#     # note: for some reason the M2Crypto interface does not allow us
+#     # to set the cipher or the password.  These two parameters are
+#     # therefore ignored.
+#     """
+#     assert isinstance(rsa, M2Crypto.RSA.RSA)
+#     bio = M2Crypto.BIO.MemoryBuffer()
+#     rsa.save_pub_key_bio(bio)
+#     return bio.read_all()
+
+# def rsa_to_public_bin(rsa, cipher="aes_128_cbc", password=None):
+#     pem = rsa_to_public_pem(rsa, cipher, password)
+#     lines = pem.split("\n")
+#     return "".join(lines[1:-2]).decode("BASE64")
+
+# def rsa_from_private_pem(pem, password=None):
+#     """
+#     Create a RSA public / private key pair from a PEM binary string.
+#     """
+#     assert isinstance(pem, str)
+#     assert password is None or isinstance(password, str)
+#     def get_password(*args):
+#         return password or "-empty-"
+#     return M2Crypto.RSA.load_key_bio(M2Crypto.BIO.MemoryBuffer(pem), get_password)
+
+# def rsa_from_public_pem(pem):
+#     """
+#     Create a RSA public part from a PEM binary string.
+#     """
+#     assert isinstance(pem, str)
+#     return M2Crypto.RSA.load_pub_key_bio(M2Crypto.BIO.MemoryBuffer(pem))
 
 if __name__ == "__main__":
     def EC_name(curve):
