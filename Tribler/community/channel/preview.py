014f0826efbc76cf321ad0d5b97fdcbe345d1f32
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2011-11-17 10:56:09 +0000

    Reducing nr of requested torrent messages when community is in preview state
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@23652 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/channel/preview.py
+++ Tribler/community/channel/preview.py
@@ -1,4 +1,5 @@
 from Tribler.community.channel.community import ChannelCommunity
+from random import sample
 
 class PreviewChannelCommunity(ChannelCommunity):
     """
@@ -44,3 +45,12 @@ class PreviewChannelCommunity(ChannelCommunity):
     
     def get_channel_mode(self):
         return ChannelCommunity.CHANNEL_CLOSED, False
+    
+    #AllChannel functions
+    def selectTorrentsToCollect(self, infohashes):
+        to_collect = ChannelCommunity.selectTorrentsToCollect(self, infohashes)
+        
+        #Reducing the number of samples to collect for unsubscribed channels        
+        if len(to_collect) > 2:
+            return sample(to_collect, 2)
+        return to_collect
