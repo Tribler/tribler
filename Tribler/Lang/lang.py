74480abbf6ee7e2d955755a2358266858070371b
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2011-11-18 12:19:50 +0000

    new AllChannelCommunity key and updated version numbers
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.5.x@23690 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -84,9 +84,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 23281"
+            return "Build 23690"
         if (label == 'build_date'):
-            return "Oct 27, 2011"
+            return "Nov 18, 2011"
         # see if it exists in 'user.lang'
         if tryuser:
             text, found = self.getFromLanguage(label, self.user_lang)
