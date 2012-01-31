229342e10f69694578aef5fea08d82d593e94144
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-01-31 13:30:09 +0000

    Debugmode visible in home/dispersypanel
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.5.x@24706 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Main/vwxGUI/home.py
+++ Tribler/Main/vwxGUI/home.py
@@ -464,7 +464,8 @@ class DispersyPanel(HomePanel):
                             addColumn("avg_down")
                 else:
                     addColumn(key)
-
+                    
+        addColumn('in_debugmode')
         self.buildColumns = True
 
     def OnMouseEvent(self, event):
@@ -556,6 +557,7 @@ class DispersyPanel(HomePanel):
                         updateColumn('busy_time', self.utility.eta_value(value['busy_time']))
                         updateColumn("avg_down", self.utility.size_format(int(value["total_down"][1] / value["runtime"])) + "/s")
                         updateColumn("avg_up", self.utility.size_format(int(value["total_up"][1] / value["runtime"])) + "/s")
+                        updateColumn("in_debugmode", str(__debug__))
 
                     parentNode = self.tree.AppendItem(fakeRoot, key)
                     addValue(parentNode, value)
