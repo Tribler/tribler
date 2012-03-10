709fcb32ac1993bb0b5ce0b08358df404f498ea9
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-03-10 16:24:14 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/release-5.5.x@25362 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/channel/community.py
+++ Tribler/community/channel/community.py
@@ -66,9 +66,10 @@ def forceAndReturnDispersyThread(func):
                     result[0] = func(*args, **kwargs)
                 finally:
                     event.set()
-                    
+            
+            #Niels: 10-03-2012, setting prio to 1 because we are actively waiting for this
             dispersy_thread.__name__ = func.__name__
-            register_task(dispersy_thread)
+            register_task(dispersy_thread, priority = 1)
             
             if event.wait(100) or event.isSet():
                 return result[0]
