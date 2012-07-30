62a9db9ea2865e4db3c45dad4ee85efacf0fd315
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2012-07-30 07:48:58 +0000

    - Version no to .23
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.9.x@27674 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Core/BitTornado/__init__.py
+++ Tribler/Core/BitTornado/__init__.py
@@ -9,7 +9,7 @@
 from Tribler.__init__ import LIBRARYNAME
 
 if LIBRARYNAME == "Tribler":
-    version_id = '5.9.22'
+    version_id = '5.9.23'
     product_name = 'Tribler'
     version_short = 'Tribler-' + version_id
     report_email = 'tribler@tribler.org'
