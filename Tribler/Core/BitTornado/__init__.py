a1070bd80b84536a368428f9c63d5c23e653170b
arno <arno@001aeff7-3401-0410-a489-f7902fc005dd>
2012-08-01 09:07:17 +0000

    - New version #.
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.9.x@27697 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Core/BitTornado/__init__.py
+++ Tribler/Core/BitTornado/__init__.py
@@ -9,7 +9,7 @@
 from Tribler.__init__ import LIBRARYNAME
 
 if LIBRARYNAME == "Tribler":
-    version_id = '5.9.24'
+    version_id = '5.9.25'
     product_name = 'Tribler'
     version_short = 'Tribler-' + version_id
     report_email = 'tribler@tribler.org'
--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -82,9 +82,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 27684"
+            return "Build 27697"
         if (label == 'build_date'):
-            return "July 30, 2012"
+            return "Aug 1, 2012"
         # see if it exists in 'user.lang'
         if tryuser:
             text, found = self.getFromLanguage(label, self.user_lang)
