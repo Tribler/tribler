43c6844e3eaf09f2febfc3b6a5461b1f67f1445f
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2012-08-02 13:00:44 +0000

    release 5.9.26
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.9.x@27714 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Lang/lang.py
+++ Tribler/Lang/lang.py
@@ -82,9 +82,9 @@ class Lang:
         if (label == 'version'):
             return version_id
         if (label == 'build'):
-            return "Build 27699"
+            return "Build 27714"
         if (label == 'build_date'):
-            return "Aug 1, 2012"
+            return "Aug 2, 2012"
         # see if it exists in 'user.lang'
         if tryuser:
             text, found = self.getFromLanguage(label, self.user_lang)
--- Tribler/Main/Build/Win32/tribler.nsi
+++ Tribler/Main/Build/Win32/tribler.nsi
@@ -1,5 +1,5 @@
 !define PRODUCT "Tribler"
-!define VERSION "5.9.25"
+!define VERSION "5.9.26"
 
 !include "MUI.nsh"
 !include "UAC.nsh"
