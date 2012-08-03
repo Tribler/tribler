e5dd9e0753d57c9e2cc707f9a2e127c85d2c1f8a
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2012-08-03 09:46:54 +0000

    debugging: more output
    git-svn-id: https://svn.tribler.org/abc/branches/release-5.9.x@27724 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/channel/community.py
+++ Tribler/community/channel/community.py
@@ -298,7 +298,9 @@ class ChannelCommunity(Community):
 
     def _disp_on_channel(self, messages):
         for message in messages:
-            if __debug__: dprint(message)
+            if __debug__:
+                assert self._cid == self._master_member.mid
+                dprint(message.candidate, " ", self._cid.encode("HEX"))
 
             authentication_member = message.authentication.member
             if authentication_member == self._my_member:
@@ -831,6 +833,7 @@ class ChannelCommunity(Community):
             cache = MissingChannelCache(timeout)
             self._dispersy.request_cache.set(identifier, cache)
 
+            if __debug__: dprint(candidate, " sending missing-channel ", self._cid.encode("HEX"))
             meta = self._meta_messages[u"missing-channel"]
             request = meta.impl(distribution=(self.global_time,), destination=(candidate,), payload=(includeSnapshot,))
             self._dispersy.store_update_forward([request], False, False, True)
