1592cda7033d8b0ab88e0e6efc56f5bbd6273f75
boudewijn <boudewijn@001aeff7-3401-0410-a489-f7902fc005dd>
2012-09-17 10:05:01 +0000

    tribler effort script
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@28212 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/effort/script.py
+++ Tribler/community/effort/script.py
@@ -7,8 +7,10 @@ from Tribler.community.effort.community import EffortCommunity
 from Tribler.dispersy.community import HardKilledCommunity
 from Tribler.dispersy.dprint import dprint
 from Tribler.dispersy.member import Member
-from Tribler.dispersy.script import ScriptBase
 from Tribler.dispersy.revision import update_revision_information
+from Tribler.dispersy.script import ScriptBase
+from Tribler.dispersy.tool.scenarioscript import ScenarioScript
+from dispersy.tool.lencoder import bz2log
 
 # update version information directly from SVN
 update_revision_information("$HeadURL$", "$Revision$")
@@ -59,3 +61,27 @@ class CrawlerScript(ScriptBase):
         while True:
             yield 60.0
             self._community.create_debug_request()
+
+class TriblerEffortScript(ScenarioScript):
+    """
+    Runs a Tribler instance from console that joins the EffortCommunity to increase the overall
+    workload.
+    """
+    @property
+    def enable_wait_for_wan_address(self):
+        return False
+
+    @property
+    def my_member_security(self):
+        return u"NID_secp224r1"
+
+    @property
+    def master_member_public_key(self):
+        return "3081a7301006072a8648ce3d020106052b810400270381920004039bb20a07b2c09fe2eb0d75a6ab8f23503728fb105c5b34fea181d2b30130fa5b493ee6317b5af3b079d3509a0225d8bafd940438e07aa48b76a37ace874a1612cbcd0878f8b7eb03b95d6bb27992d61a165a657c2b1fe096e2d39998fca7604f3bf3cf317c33be8e449c5015fbef8981f6f9d5d4ddc38f2c728cf823f9faca3224629ab6282b29136117b21737c0f4".decode("HEX")
+
+    @property
+    def community_class(self):
+        return EffortCommunity
+
+    def log(self, _message, **kargs):
+        bz2log("log", _message, **kargs)
