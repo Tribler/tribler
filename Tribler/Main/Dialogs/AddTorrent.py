0811ed62860693b719a5214ab80631a630a5acf0
bouman <bouman@001aeff7-3401-0410-a489-f7902fc005dd>
2012-10-16 10:57:30 +0000

    Fix for missing import in AddTorrent.py
    git-svn-id: https://svn.tribler.org/abc/branches/release-6.0.x@28864 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Main/Dialogs/AddTorrent.py
+++ Tribler/Main/Dialogs/AddTorrent.py
@@ -10,6 +10,7 @@ from Tribler.Main.vwxGUI.widgets import _set_font
 from Tribler.Main.Dialogs.CreateTorrent import CreateTorrent
 from Tribler.Main.vwxGUI.GuiUtility import GUIUtility
 from Tribler.Core.Swift import SwiftDef
+from Tribler.Core.TorrentDef import TorrentDef
 
 class AddTorrent(wx.Dialog):
     def __init__(self, parent, frame, libraryTorrents = None):
