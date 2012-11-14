f9c6806dd710ac1167eb029575f7130c188d33d9
bouman <bouman@001aeff7-3401-0410-a489-f7902fc005dd>
2012-11-14 15:26:58 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/release-6.0.x@29599 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Main/Utility/GuiDBTuples.py
+++ Tribler/Main/Utility/GuiDBTuples.py
@@ -479,7 +479,11 @@ class RemoteChannelTorrent(ChannelTorrent):
     def __init__(self, torrent_id, infohash, swift_hash, swift_torrent_hash, name, length = 0, category_id = None, status_id = None, num_seeders = 0, num_leechers = 0, channel = False, query_candidates = set()):
         ChannelTorrent.__init__(self, torrent_id, infohash, swift_hash, swift_torrent_hash, name, False, length, category_id, status_id, num_seeders, num_leechers, -1, '-1', '', name, '', None, None, channel, None)
         self.query_candidates = query_candidates
-    
+        
+    def updateChannel(self, c):
+        ChannelTorrent.updateChannel(self, c)
+        self._channel.addTorrent(self)
+        
 class Channel(Helper):
     __slots__ = ('id', 'dispersy_cid', 'name', 'description', 'nr_torrents', 'nr_favorites', 'nr_spam', 'my_vote', 'modified', 'my_channel', 'torrents', 'popular_torrents')
     def __init__(self, id, dispersy_cid, name, description, nr_torrents, nr_favorites, nr_spam, my_vote, modified, my_channel):
