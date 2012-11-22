4f57a0be72fcb7afa51b099efce3800e20c3be87
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-11-22 15:34:06 +0000

    Extract-script for demers
    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@29840 001aeff7-3401-0410-a489-f7902fc005dd

---
+++ Tribler/community/demers/extract.py
@@ -0,0 +1,44 @@
+import sys
+
+f = open("sum_received_records.txt")
+f2 = open("received_records_10.txt", 'w')
+print >> f2, "#nrpeers, took, partnr"
+
+piece_created = {}
+
+peer_received = {}
+piece_received = {}
+took_list = []
+
+for line in f:
+    if line[0] == '#':
+        continue
+    
+    parts = line.split()
+    
+    time = int(parts[0])
+    peers = [int(part) for part in parts[1:]]
+    created = max(peers)
+    if created not in piece_created:
+        piece_created[created] = time
+        #piece_created[created] = 120 * created
+        print "new part", peers.index(created), time, created * 120, piece_received.get(created-1, 0)
+
+    updated_parts = set()
+    
+    for peer, part in enumerate(peers):
+        peer += 1
+        
+        if peer_received.get(peer, 0) != part:
+            peer_received[peer] = part
+            piece_received[part] = piece_received.get(part, 0) + 1
+            
+            took = time - piece_created[part]
+    
+            print >> f2, piece_received[part], took, part
+            if piece_received[part] == 1000:
+                took_list.append(took)
+                print "received part", part, took, sum(took_list)/float(len(took_list))
+                        
+f.close()
+f2.close()
