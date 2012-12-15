a3a93ee56b7b1df5cf89f1182bf1df228ece6f4c
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2012-12-15 13:22:25 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@30357 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/channel/preview.py
+++ Tribler/community/channel/preview.py
@@ -23,12 +23,3 @@ class PreviewChannelCommunity(ChannelCommunity):
 
     def get_channel_mode(self):
         return ChannelCommunity.CHANNEL_CLOSED, False
-
-    #AllChannel functions
-    def selectTorrentsToCollect(self, infohashes):
-        to_collect = ChannelCommunity.selectTorrentsToCollect(self, infohashes)
-
-        #Reducing the number of samples to collect for unsubscribed channels
-        if len(to_collect) > 2:
-            return sample(to_collect, 2)
-        return to_collect
