a80d69389f12755228b7a8be77446e8cc8227f8c
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2013-02-01 23:42:09 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@31348 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/privatesearch/conversion.py
+++ Tribler/community/privatesearch/conversion.py
@@ -432,9 +432,9 @@ class PSearchConversion(SearchConversion):
             hashpack = '4sH128s' * (length/134)
             raw_values = unpack_from('!'+hashpack, data, offset)
             for i in range(len(raw_values)/3):
-                ip = inet_ntoa(raw_values[i])
-                port = raw_values[i+1]
-                _sums.append([(ip, port), bytes_to_long(raw_values[i+2])])
+                ip = inet_ntoa(raw_values[i*3])
+                port = raw_values[(i*3)+1]
+                _sums.append([(ip, port), bytes_to_long(raw_values[(i*3)+2])])
                 
             offset += length
         
