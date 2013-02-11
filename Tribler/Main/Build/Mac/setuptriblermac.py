5317972fdc3ebe21fa3dd23a8cf202dcfb8e5ba5
bouman <bouman@001aeff7-3401-0410-a489-f7902fc005dd>
2013-02-11 12:27:37 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/release-6.1.x@31421 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/Main/Build/Mac/setuptriblermac.py
+++ Tribler/Main/Build/Mac/setuptriblermac.py
@@ -112,7 +112,6 @@ setup(
         'optimize': 0 if __debug__ else 2,
         'resources':
             [(LIBRARYNAME+"/Lang", [LIBRARYNAME+"/Lang/english.lang"]),
-             (LIBRARYNAME+"/Core", [LIBRARYNAME+"/Core/superpeer.txt"]),
              (LIBRARYNAME+"/Category", [LIBRARYNAME+"/Category/category.conf"]),
              (LIBRARYNAME+"/Core/Tag", [LIBRARYNAME+"/Core/Tag/stop_snowball.filter"]),
              (LIBRARYNAME+"/Core/DecentralizedTracking/pymdht/core", [LIBRARYNAME+"/Core/DecentralizedTracking/pymdht/core/bootstrap.main"]),
