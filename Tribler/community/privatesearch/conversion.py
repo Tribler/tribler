f3c22d544a83636e8daa1a695fa3dac64cf72db8
niels <niels@001aeff7-3401-0410-a489-f7902fc005dd>
2013-02-27 20:19:26 +0000

    git-svn-id: https://svn.tribler.org/abc/branches/mainbranch@31716 001aeff7-3401-0410-a489-f7902fc005dd

--- Tribler/community/privatesearch/conversion.py
+++ Tribler/community/privatesearch/conversion.py
@@ -494,7 +494,7 @@ class HSearchConversion(SearchConversion):
                 raise DropPacket("Invalid number of bytes available (encr_res)")
             if length:
                 hashpack = '128s' * (length/128)
-                hashes = unpack_from('!'+hashpack, data, offset)
+                hashes = unpack_from('!'+hashpack, str_prefs, offset)
                 hashes = [bytes_to_long(hash) for hash in hashes]
             
             length = len(str_hprefs)
@@ -502,7 +502,7 @@ class HSearchConversion(SearchConversion):
                 raise DropPacket("Invalid number of bytes available (encr_res)")
             if length:
                 hashpack = '20s' * (length/20)
-                his_hashes = unpack_from('!'+hashpack, data, offset)
+                his_hashes = unpack_from('!'+hashpack, str_hprefs, offset)
                 his_hashes = [bytes_to_long(hash) for hash in his_hashes]
                 
             if str_mid:
