cb3a9cc5e276a7a93c6a7b06ff1d58f3a9bf52bd
Elric Milon <whirm@gmx.com>
2013-04-18 16:51:36 +0200

    Dummy version.py file.

---
+++ Tribler/Core/version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#
+###                                                                   ###
+###  This file will be overwritten when building a package/installer! ###
+###                                                                   ###
+#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#WARNING!#
+
+version_id='GIT'
+build_date='0/0/0'
