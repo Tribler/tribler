8dd4b72e055a46ce8c3b919375466c201180d7ff
Elric Milon <whirm@gmx.com>
2013-04-23 13:23:53 +0200

    delete subtitle tests

--- Tribler/Test/test_subtitles.sh
+++
@@ -1,8 +0,0 @@
-
-export PYTHONPATH=../..:"$PYTHONPATH"
-
-python test_subtitles_isolation.py
-python test_channelcast_plus_subtitles.py singtest_plain_nickname
-python test_channelcast_plus_subtitles.py singtest_unicode_nickname
-python test_subtitles_msgs.py singtest_subs_messages
-
