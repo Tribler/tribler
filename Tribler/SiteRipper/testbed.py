b2e7f92262ac759da7ea9e020831acc5228d8a97
Quinten <quinten@ubuntu.(none)>
2013-05-03 11:29:13 +0200

    Added site ripping tools

---
+++ Tribler/SiteRipper/testbed.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2.7
+
+from webpageinjector import WebpageInjector
+
+def imgfilter(tag):
+    return tag.name == "img"
+
+def testDownloadAndInject():
+    wpi = WebpageInjector("http://khmerkromrecipes.com/pages/herbvegg.html")
+    images = wpi.findTags(imgfilter)
+    
+    if images.__len__() > 0:
+        # If there is an image on the webpage, replace it with the Google logo
+        imagetag = images[0]
+        print "Found image: " + imagetag['src']
+        newtag = wpi.createTag("img")
+        newtag['src'] = "http://www.google.com/images/srpr/logo4w.png" 
+        wpi.replaceTag(imagetag, newtag)
+        wpi.commitTagChanges()
+        
+    wpi.saveWebpageFile("out")
+        
+def testDownloadResource():
+    wpi = WebpageInjector("http://khmerkromrecipes.com/pages/herbvegg.html")
+    images = wpi.findTags(imgfilter)
+    
+    if images.__len__() > 0:
+        imagetag = images[0]
+        print "Found image: " + imagetag['src']
+        wpi.saveTagSource(imagetag, "out")
+
+testDownloadAndInject()
+#testDownloadResource()
