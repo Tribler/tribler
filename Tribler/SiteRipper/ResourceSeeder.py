bbb06a8fdd6ab83abcbe9d02a4e7ab792c9375d9
Quinten <quinten@ubuntu.(none)>
2013-05-03 11:48:03 +0200

    Adapted SiteRipper folder to project conventions

---
+++ Tribler/SiteRipper/ResourceSeeder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+
+from Tribler.Core.TorrentDef import TorrentDef
+
+class ResourceSeeder:
+    """ResourceSeeder:
+        Allows you to rip content from a webpage
+        and then start seeding said content
+    """
+    
+    
