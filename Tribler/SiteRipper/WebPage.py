52ee0fb4b874e8a195f49567fe4216e1329b9101
S.D.Norberhuis <steffan@norberhuis.nl>
2013-05-14 10:41:22 +0200

    Added ext to filename

--- Tribler/SiteRipper/WebPage.py
+++ Tribler/SiteRipper/WebPage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2.7
 
 import urllib
+from wx.tools.Editra.src.ebmlib.fileutil import GetFileName
+from xdg.Menu import __getFileName
 
 class WebPage:
     """Webpage:
@@ -10,6 +12,7 @@ class WebPage:
     
     __url = ""          # URL we represent
     __content = ""      # Raw web page content
+    ext = ""          # Extension of the webpage
     
     def __init__(self, url='', content=''):
         self.__url = url
@@ -45,9 +48,42 @@ class WebPage:
         """
         self.__url = url
         
-    def saveToFile(self, filename):
+    def saveToFile(self):
         """Saves the web page in memory to disk
         """
+        filename = self.getFileName()
         file = open(filename,'wb')
         file.write(self.__content)
         file.close()
+    
+    def createFromFile(filename):
+        '''Create a web page from disk'''
+        file = open(filename, 'rb')
+        self.__content = file.read()
+        
+    def getFileName(self):
+        '''Get the appropiate filename by using the given url
+        Args:
+            url (str): The url to be used  to creat the filename.'''
+        #Remove http://www.
+        result = self.__url
+        if result.startswith("http://"):
+            result = result[7:]
+        if result.startswith('www.'):
+            result = result[4:]
+        #Replace all / with -
+        result = ['_' if x=='/' else x for x in result]
+        #Return
+        return ''.join(result) + self.ext
+
+    def getURLName(filename):
+        '''Get the appropiate url by using the given filename
+        Args:
+            filename (str): The filename to be used  to create the url.'''
+        result = filename
+        result = ['_' if x=='/' else x for x in result]
+        #Add http://www.
+        result = ''.join(['http://','www.',result])
+        #return
+        return result
+        
