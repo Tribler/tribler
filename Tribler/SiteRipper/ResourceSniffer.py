8233f34dbfff0f4c729359b9f9c6ffba4c136aa6
Quinten <quinten@ubuntu.(none)>
2013-05-15 14:09:41 +0200

    Modified Resource sniffer behaviour according to webview handler specifications

--- Tribler/SiteRipper/ResourceSniffer.py
+++ Tribler/SiteRipper/ResourceSniffer.py
@@ -6,8 +6,5 @@ class ResourceSniffer:
     def StartLoading(self, url):
     	print "Starting to produce dictionary for " + url
     	
-    def FinishedLoading(self):
-    	print "Got FinishedLoading event"
-    	
     def Seed(self):
     	print "I got a seeding request"
