9e0b9dc96b6f2f10fde0d416ef37e3c574590534
Quinten <quinten@ubuntu.(none)>
2013-05-16 10:19:55 +0200

    Expanded ResourceSeeder to allow easy webpage seeding

--- Tribler/SiteRipper/ResourceSeeder.py
+++ Tribler/SiteRipper/ResourceSeeder.py
@@ -7,6 +7,66 @@ from Tribler.Core.Session import Session
 
 import os
 import urllib2
+import datetime
+
+class WebPageTorrentHeader:
+    """WebPageTorrentHeader
+        Class for creating TorrentDef object specifically for
+        eternal webpages.
+    """
+    
+    __name = ""             # The name of our torrent (ex. http://www.google.com/index.html)
+    __file = ""             # The (tar) file we are sharing
+    __accessdate = ""       # The date we started seeding the webpage
+    
+    def __init__(self, file, webpageurl, accessdate = None):
+        """Initialze our object for a certain (tar) file and a hosted url.
+            Optionally supply an accessdate string (defaults to today). 
+        """
+        self.__name = webpageurl
+        self.__file = file
+        if accessdate:
+            self.__accessdate = accessdate
+        else:
+            now = datetime.datetime.now()
+            self.__accessdate = str(now.day) + "-" + str(now.month) + "-" + str(now.year)
+        
+    def createTorrentDef(self, session):
+        """Create the torrent definition (TorrentDef) for our current
+            Tribler session.
+        """
+        torrentdef = TorrentDef()
+        #Add functional requirements to torrent definition
+        torrentdef.add_content(self.__file)
+        torrentdef.set_tracker(session.get_internal_tracker_url())
+        #Add information to torrent definition
+        torrentdef.set_name(self.__name)
+        torrentdef.set_comment("The eternal copy of " + self.__name + ".\nRetrieved on " + self.__accessdate + ".")
+        torrentdef.finalize()
+        return torrentdef
+    
+    def getSeededFile(self):
+        """Returns the (tar) file we are going to seed
+        """
+        return self.__file
+    
+    def getFileFolder(self):
+        """Returns the folder our (tar) file resides in
+        """
+        return os.path.dirname(self.__file)
+        
+def seedWebpage(tarfile, webpage, accessdate = None):
+    """Seed a compressed webpage.
+        Supply the path to the tarfile and the webpage URL.
+        Optionally supply an accessdate if you do not want to use
+        today as the website retrieval date.
+    """
+    session = Session.get_instance()
+    header = WebPageTorrentHeader(tarfile, webpage, accessdate)
+    torrent = header.createTorrentDef(session)
+    download = DownloadStartupConfig()
+    download.set_dest_dir(header.getFileFolder())
+    session.start_download(torrent, download)
 
 def seedFile(filename):
     """Start seeding an arbitrary file
