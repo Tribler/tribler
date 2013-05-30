8c12af0b515ea4be24f2fd466e10e5033a8a63db
qstokkink <goqs@hotmail.com>
2013-05-30 14:28:04 +0200

    Added MatcherControl innards, closes #58

--- Tribler/TUPT/Matcher/MatcherControl.py
+++ Tribler/TUPT/Matcher/MatcherControl.py
@@ -1,3 +1,4 @@
+from Tribler.TUPT.Movie import Movie
 
 class MatcherControl:
     """MatcherControl:
@@ -5,8 +6,54 @@ class MatcherControl:
         parser.
     """
     
-    def __RegisterPluginResults(self, plugin, movie):
-        pass
+    __termTable = None
+    
+    def __init__(self, pluginManager):
+        self.__termTable = {}
+        self.__pluginManager = pluginManager
+    
+    def __RegisterPluginResults(self, plugin, movie, trust):
+        """Have a plugin match a movie and 'upvote' a certain
+            result in our term list
+        """
+        plugin.MatchMovie(movie)
+        for attribute in plugin.GetMovieAttributes():
+            value = plugin.GetAttribute(attribute)
+            if self.__termTable.has_key(attribute):
+                if self.__termTable[attribute].has_key(value):
+                    self.__termTable[attribute][value] += trust
+                else:
+                    self.__termTable[attribute][value] = trust
+            else:
+                self.__termTable[attribute] = {}
+                self.__termTable[attribute][value] = trust
+    
+    def __GetFinalDict(self, mintrust = 0.5):
+        """Construct the final movie dict from our term table.
+            Whichever result for an attribute has the highest trust, wins.
+        """            
+        finalDict = {}
+        for attribute in terms:
+            highestFrequency = ''
+            for value in terms[attribute]:
+                if terms[attribute][value] > terms[attribute][highestFrequency]:
+                    highestFrequency = value
+            if terms[attribute][highestFrequency] >= mintrust:
+                finalDict[attribute] = value
+        return finalDict
     
     def CorrectMovie(self, movie):
-        pass
+        """Run a sparse movie through our matchers to receive a well filled
+            movie definition
+        """
+        plugins = self.__pluginManager.GetPluginDescriptorsForCategory('Matcher')
+        for plugin_info in plugins:
+            trust = 0.5
+            try:
+                trust = plugin_info.getfloat("Core","Trust")
+            except:
+                trust = 0.5 #Not a valid float
+            self.__RegisterPluginResults(plugin_info.plugin_object, movie, trust)
+        out = Movie()
+        out.dictionary = self.__GetFinalDict()
+        return out
