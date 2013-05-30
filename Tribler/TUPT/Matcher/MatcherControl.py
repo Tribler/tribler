fee77d6158f6d62c837aa16b41e997226a985e04
qstokkink <goqs@hotmail.com>
2013-05-30 12:33:47 +0200

    Added MatcherControl stub

---
+++ Tribler/TUPT/Matcher/MatcherControl.py
@@ -0,0 +1,12 @@
+
+class MatcherControl:
+    """MatcherControl:
+        Try to complete/correct a movie definition delivered by some
+        parser.
+    """
+    
+    def __RegisterPluginResults(self, plugin, movie):
+        pass
+    
+    def CorrectMovie(self, movie):
+        pass
