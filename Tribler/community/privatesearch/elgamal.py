fcf2502e77a216278a724085a16f4214adf75a58
Niels <niels@zeilemaker.nl>
2013-06-16 19:01:50 +0200

    Chanes

--- Tribler/community/privatesearch/elgamal.py
+++ Tribler/community/privatesearch/elgamal.py
@@ -1,10 +1,11 @@
-from Crypto.PublicKey import ElGamal
-from Crypto import Random
-from Crypto.Random.random import StrongRandom
-from Crypto.Util.number import GCD, inverse
+#from Crypto.PublicKey import ElGamal
+#from Crypto import Random
+#from Crypto.Random.random import StrongRandom
+#from Crypto.Util.number import GCD, inverse
+#from gmpy import mpz
 
 from hashlib import md5
-from gmpy import mpz
+
 from time import time
 from collections import namedtuple
 import numpy
@@ -37,46 +38,55 @@ def elgamal_decrypt(key, cipher):
     return plaintext
 
 if __name__ == "__main__":
-    set1 = [randint(0, 2 ** 25) for i in range(100)]
-    a = numpy.poly(set1)
-
-#
-#     set2 = [randint(0, 2 ** 32) for i in range(100, 200)]
-#     y1 = [1] * len(set1)
-#     y2 = [1] * len(set2)
-#
-#     set1.insert(0, randint(0, 2 ** 25))
-#     y1.insert(0, randint(0, 2 ** 32))
-#
-#     a = numpy.polyfit(set1, y1, 32)
-#     b = numpy.polyfit(set2, y2, 32)
-
-    print sum(a)
-    print set1
-    # print y1
-    print a, len(a)
-
-    lower = 0.9999999999
-    upper = 1.0000000001
-
-    nr_false_positive = 0
-    for _ in range(100000):
-        i = randint(0, 2 ** 25)
-        returnval = numpy.polyval(a, i)
-        if returnval == 0 :
-            print i, returnval
-            nr_false_positive += 1
-
-    print nr_false_positive / float(100000)
-
-    nr_positive = 0
-    for i in set1:
-        returnval = numpy.polyval(a, i)
-        print returnval, i, lower < returnval < upper
-        if returnval == 0:
-            nr_positive += 1
-
-    print nr_positive / float(len(set1) - 1)
+    while True:
+    
+        set1 = [randint(0, 2 ** 25) for i in range(15)]
+        set2 = [randint(0, 2 ** 32) for i in range(100, 200)]
+        y1 = [0] * len(set1)
+        y2 = [0] * len(set2)
+    
+        set1.insert(0, 1)
+        y1.insert(0, 1)
+    
+    #    degree = len(set1)
+    #    breakNext = False
+    #    while True:
+    #        try:
+    #            a = numpy.polyfit(set1, y1, degree)
+    #            if breakNext:
+    #                break
+    #            
+    #            degree = sum(bool(i) for i in a) - 1
+    #            breakNext = True
+    #        except:
+    #            degree -= 1
+                
+        a = numpy.polyfit(set1, y1, len(set1))
+    
+    #    print sum(a)
+    #    print set1
+    #    print y1
+    #    print a, len(a), sum(bool(i) for i in a)
+    
+        lower = -0.0000001
+        upper = 0.0000001
+    
+        nr_false_positive = 0
+        for _ in range(100000):
+            i = randint(0, 2 ** 25)
+            if i not in set1:
+                returnval = numpy.polyval(a, i)
+                if lower < returnval < upper:
+                    nr_false_positive += 1
+    
+        nr_positive = 0
+        for i in set1:
+            returnval = numpy.polyval(a, i)
+            #print returnval, i, lower < returnval < upper
+            if lower < returnval < upper:
+                nr_positive += 1
+    
+        print nr_false_positive / float(100000), nr_positive / float(len(set1) - 1)
 
 #     for i in a:
 #         if i:
