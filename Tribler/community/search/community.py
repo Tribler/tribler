ee5659dc34f2db21cd6d004414644e43f4557b81
Boudewijn <boudewijn-tribler@frayja.com>
2013-07-04 16:22:43 +0200

    Dispersy has removed Candidate.is(_any)_active(...).

--- Tribler/community/search/community.py
+++ Tribler/community/search/community.py
@@ -112,7 +112,7 @@ class SearchCommunity(Community):
             active_canidates = [candidate
                                 for candidate
                                 in self._candidates.itervalues()
-                                if candidate.is_active(self, now)]
+                                if candidate.get_category(self, now) in (u"walk", u"stumble")]
             if len(active_canidates) > 20:
                 logger.debug("there are %d active non-bootstrap candidates available, prematurely quitting fast walker", len(active_canidates))
                 break
