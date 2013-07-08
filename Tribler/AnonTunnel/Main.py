cd4e01fe7f72c9cb0f6f15892e841acf32ba1aa5
Risto <risto@live.nl>
2013-07-08 11:28:30 +0200

    Accept start parameter in Main.py

--- Tribler/AnonTunnel/Main.py
+++ Tribler/AnonTunnel/Main.py
@@ -1,5 +1,6 @@
 import os
 import logging.config
+
 logging.config.fileConfig(os.path.dirname(os.path.realpath(__file__)) + "/logger.conf")
 logger = logging.getLogger(__name__)
 
@@ -7,9 +8,36 @@ import time
 from DispersyTunnelProxy import DispersyTunnelProxy
 from Socks5AnonTunnel import Socks5AnonTunnel
 
+import sys, getopt
 
-tunnel = DispersyTunnelProxy()
-Socks5AnonTunnel(tunnel,1080).run()
 
-while 1:
-    time.sleep(1)
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hs", ["start"])
+    except getopt.GetoptError:
+        print 'Main.py [--start]'
+        sys.exit(2)
+
+    should_start = False
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'Main.py [--start]'
+            sys.exit()
+        elif opt in ("-s", "--start"):
+            should_start = True
+
+    tunnel = DispersyTunnelProxy()
+
+    if should_start:
+        tunnel.start()
+
+    Socks5AnonTunnel(tunnel, 1080).run()
+
+    while 1:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
