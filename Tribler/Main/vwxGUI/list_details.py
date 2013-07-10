581fde74b7f7c156782782a2255f923df444ff67
Egbert Bouman <ebouman@gmail.com>
2013-07-10 19:21:16 +0200

    SeedingManager should not stop streaming downloads

--- Tribler/Main/vwxGUI/list_details.py
+++ Tribler/Main/vwxGUI/list_details.py
@@ -1856,7 +1856,7 @@ class ProgressPanel(wx.BoxSizer):
             self.Add(self.status, 0, wx.EXPAND)
 
         # self.AddStretchSpacer()
-        wx.CallAfter(self.Update)
+        wx.CallLater(100, self.Update)
 
     def Update(self, ds=None, torrent=None):
         # return_val, 0 == inactive, 1 == incomplete, 2 == complete/seeding
