554ac55f270de78dac154f20b922d11af242f665
Niels Zeilemaker <niels@zeilemaker.nl>
2013-07-12 13:46:57 +0200

    Removed one Destroy call

--- Tribler/Core/APIImplementation/UserCallbackHandler.py
+++ Tribler/Core/APIImplementation/UserCallbackHandler.py
@@ -25,7 +25,7 @@ class UserCallbackHandler:
         # Notifier for callbacks to API user
         self.threadpool = ThreadNoPool()
 
-        self.notifier = Notifier.getInstance(self.threadpool)
+        self.notifier = Notifier(self.threadpool)
 
     def shutdown(self):
         # stop threadpool
