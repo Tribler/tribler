a6a5a04ccedcf52fedfd40e147a281dbca5afce3
Niels <niels@zeilemaker.nl>
2013-07-15 09:24:56 +0200

    Revert do_assert

--- Tribler/Test/test_as_server.py
+++ Tribler/Test/test_as_server.py
@@ -157,7 +157,7 @@ class TestAsServer(AbstractServer):
 
         print >> sys.stderr, "test_as_server: Session is shutdown"
 
-    def assert_(self, boolean, reason=None):
+    def assert_(self, boolean, reason=None, do_assert = True):
         if not boolean:
             self.quit()
             assert boolean, reason
@@ -185,7 +185,7 @@ class TestAsServer(AbstractServer):
                         self.Call(0.5, DoCheck)
                 else:
                     print >> sys.stderr, "test_as_server: quitting, condition was not satisfied in %d seconds (%s)" % (timeout, assertMsg or "no-assert-msg")
-                    self.assert_(False, assertMsg if assertMsg else "Condition was not satisfied in %d seconds" % timeout)
+                    self.assert_(False, assertMsg if assertMsg else "Condition was not satisfied in %d seconds" % timeout, do_assert=False)
         self.Call(0, DoCheck)
 
     def quit(self):
@@ -215,12 +215,15 @@ class TestGuiAsServer(TestAsServer):
         self.asserts = []
         self.annotate(self._testMethodName, start=True)
 
-    def assert_(self, boolean, reason):
+    def assert_(self, boolean, reason, do_assert = True):
         if not boolean:
             self.screenshot("ASSERT: %s" % reason)
             self.quit()
 
             self.asserts.append((boolean, reason))
+            
+            if do_assert:
+                assert boolean, reason
 
     def startTest(self, callback, min_timeout=5):
         from Tribler.Main.vwxGUI.GuiUtility import GUIUtility
