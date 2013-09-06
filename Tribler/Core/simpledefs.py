5c50bbd107a175ef1d70a91300180e2de11c0133
Egbert Bouman <ebouman@gmail.com>
2013-09-06 08:48:31 +0200

    Added NTFY_VIDEO_STARTED and NTFY_VIDEO_STOPPED

--- Tribler/Core/simpledefs.py
+++ Tribler/Core/simpledefs.py
@@ -93,6 +93,8 @@ NTFY_MAGNET_STARTED = 'magnet_started'
 NTFY_MAGNET_GOT_PEERS = 'magnet_peers'
 NTFY_MAGNET_PROGRESS = 'magnet_progress'
 NTFY_MAGNET_CLOSE = 'magnet_close'
+NTFY_VIDEO_STARTED = 'video_started'
+NTFY_VIDEO_STOPPED = 'video_stopped'
 
 # object IDs for NTFY_ACTIVITIES subject
 NTFY_ACT_NONE = 0
