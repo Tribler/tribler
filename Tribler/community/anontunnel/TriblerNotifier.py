fb2a2cf1d6f14cb2955b40ca3c3a51ee849f7781
Egbert Bouman <ebouman@gmail.com>
2013-09-25 12:25:59 +0200

    fix

--- Tribler/community/anontunnel/TriblerNotifier.py
+++ Tribler/community/anontunnel/TriblerNotifier.py
@@ -1,15 +1,19 @@
 from Observable import Observable
 from Tribler.Core.CacheDB.Notifier import Notifier
-from Tribler.Core.simpledefs import NTFY_ANONTUNNEL, NTFY_EXTENDED
+from Tribler.Core.simpledefs import NTFY_ANONTUNNEL, NTFY_EXTENDED, NTFY_CREATED, NTFY_EXTENDED_FOR
 
 class TriblerNotifier():
     def __init__(self, community):
         self.notifier = Notifier.getInstance()
         community.subscribe("circuit_created", self.on_circuit_created)
         community.subscribe("circuit_extended_for", self.on_circuit_extended_for)
+        community.subscribe("circuit_extended", self.on_circuit_extended)
 
     def on_circuit_created(self, event):
-        self.notifier.notify(NTFY_ANONTUNNEL, NTFY_EXTENDED, event.circuit)
+        self.notifier.notify(NTFY_ANONTUNNEL, NTFY_CREATED, event.circuit)
+
+    def on_circuit_extended(self, event):
+        self.notifier.notify(NTFY_ANONTUNNEL, NTFY_EXTENDED, event.extended_with)
 
     def on_circuit_extended_for(self, event):
-        self.notifier.notify(NTFY_ANONTUNNEL, NTFY_EXTENDED, event.extended_for, event.extended_with)
+        self.notifier.notify(NTFY_ANONTUNNEL, NTFY_EXTENDED_FOR, event.extended_for, event.extended_with)
