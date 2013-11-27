b67d50dbadcb69c0212e4ea4baa19482f23a2bb8
Egbert Bouman <ebouman@gmail.com>
2013-11-27 11:28:17 +0100

    Removed unused functions from DownloadConfig

--- Tribler/Core/defaults.py
+++ Tribler/Core/defaults.py
@@ -1,6 +1,8 @@
 # Written by Arno Bakker and Bram Cohen
 # Updated by George Milescu
+# Updated by Egbert Bouman, added subsection names + using OrderedDict + cleanup 
 # see LICENSE.txt for license information
+
 """ Default values for all configurarable parameters of the Core"""
 #
 # For an explanation of each parameter, see SessionConfig/DownloadConfig.py
@@ -16,10 +18,12 @@
 #    such that your code won't barf because we loaded an older version from
 #    disk that does not have your new fields.
 #
+
 import sys
-import os
-from simpledefs import *
+
 from collections import OrderedDict
+
+from Tribler.Core.simpledefs import DISKALLOC_NORMAL, DISKALLOC_SPARSE
 
 DEFAULTPORT = 7760
 
@@ -29,6 +33,7 @@ DEFAULTPORT = 7760
 # History:
 #  Version 2: as released in Tribler 4.5.0
 #
+
 SESSDEFAULTS_VERSION = 2
 sessdefaults = OrderedDict()
 
@@ -108,60 +113,17 @@ sessdefaults['dispersy']['dispersy_port'] = DEFAULTPORT - 1
 #  Version 4: allow users to specify a download directory every time
 #  Version 6: allow users to overwrite the multifile destination
 #  Version 7: swift params
-DLDEFAULTS_VERSION = 7
+#  Version 8: deleted many of the old params that were not used anymore (due to the switch to libtorrent)
+
+DLDEFAULTS_VERSION = 8
 dldefaults = {}
 dldefaults['version'] = DLDEFAULTS_VERSION
-dldefaults['max_uploads'] = 7
-dldefaults['keepalive_interval'] = 120.0
-dldefaults['download_slice_size'] = 2 ** 14
-dldefaults['upload_unit_size'] = 1460
-dldefaults['request_backlog'] = 10
-dldefaults['max_message_length'] = 2 ** 23
-dldefaults['selector_enabled'] = 1  # whether to enable the file selector and fast resume function. Arno, 2009-02-9: Must be on for checkpoints to work.
-dldefaults['expire_cache_data'] = 10  # the number of days after which you wish to expire old cache data (0 = disabled)
-dldefaults['priority'] = []  # a list of file priorities separated by commas, must be one per file, 0 = highest, 1 = normal, 2 = lowest, -1 = download disabled'
 dldefaults['saveas'] = None  # Set to get_default_destdir()
 dldefaults['showsaveas'] = True  # Allow users to choose directory for every new download
-dldefaults['max_slice_length'] = 2 ** 17
-dldefaults['max_rate_period'] = 20.0
-dldefaults['upload_rate_fudge'] = 5.0
-dldefaults['tcp_ack_fudge'] = 0.03
-dldefaults['rerequest_interval'] = 300
-dldefaults['min_peers'] = 20
-dldefaults['http_timeout'] = 60
-dldefaults['max_initiate'] = 40
-dldefaults['check_hashes'] = 1
 dldefaults['max_upload_rate'] = 0
 dldefaults['max_download_rate'] = 0
-# Arno, 2009-12-11: Sparse as default reduces CPU usage. Previously this was
-# also set, but in DownloadConfig.__init__
-if sys.platform == 'win32':
-    dldefaults['alloc_type'] = DISKALLOC_NORMAL
-else:
-    dldefaults['alloc_type'] = DISKALLOC_SPARSE
-dldefaults['alloc_rate'] = 2.0
-dldefaults['buffer_reads'] = 1
-dldefaults['write_buffer_size'] = 4
-dldefaults['breakup_seed_bitfield'] = 1
-dldefaults['snub_time'] = 30.0
-dldefaults['rarest_first_cutoff'] = 2
-dldefaults['rarest_first_priority_cutoff'] = 5
-dldefaults['min_uploads'] = 4
-dldefaults['max_files_open'] = 20
-dldefaults['round_robin_period'] = 30
+dldefaults['alloc_type'] = DISKALLOC_NORMAL if sys.platform == 'win32' else DISKALLOC_SPARSE
 dldefaults['super_seeder'] = 0
-dldefaults['security'] = 1
-dldefaults['max_connections'] = 0
-dldefaults['auto_kick'] = 1
-dldefaults['double_check'] = 0
-dldefaults['triple_check'] = 0
-dldefaults['lock_files'] = 0
-dldefaults['lock_while_reading'] = 0
-dldefaults['auto_flush'] = 0
-#
-# Tribler per-download opts
-#
-dldefaults['exclude_ips'] = ''
 dldefaults['mode'] = 0
 dldefaults['vod_usercallback'] = None
 dldefaults['vod_userevents'] = []
@@ -169,17 +131,7 @@ dldefaults['video_source'] = None
 dldefaults['video_ratelimit'] = 0
 dldefaults['video_source_authconfig'] = None
 dldefaults['selected_files'] = []
-
-# Niels: in total uTorrent and Azureus clients agree not to accept more than 50 added peers
-# http://wiki.theory.org/BitTorrentPeerExchangeConventions
-dldefaults['ut_pex_max_addrs_from_peer'] = 50
-# Version 3:
-dldefaults['same_nat_try_internal'] = 0
-dldefaults['unchoke_bias_for_internal'] = 0
-# Version 6: add correctedfilename option
 dldefaults['correctedfilename'] = None
-
-# SWIFTPROC v7
 dldefaults['swiftlistenport'] = None
 dldefaults['swiftcmdgwlistenport'] = None
 dldefaults['swifthttpgwlistenport'] = None
