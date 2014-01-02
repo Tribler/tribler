7fc2ec01c582196012c5dcb23c621965fb95a804
Niels <niels@zeilemaker.nl>
2014-01-02 16:41:43 +0100

    Fix for get_stats

--- Tribler/community/privatesocial/database.py
+++ Tribler/community/privatesocial/database.py
@@ -76,10 +76,10 @@ class FriendDatabase(Database):
     def get_database_stats(self):
         stats_dict = {}
         
-        tables = self.execute(u'SELECT name FROM sqlite_master where type = "table"')
-        for tablename, in tables:
-            stats_dict[tablename] = self.execute(u"SELECT COUNT(*) FROM "+tablename).next()
-        return stats_dict 
+        for tablename, in list(self.execute(u'SELECT name FROM sqlite_master WHERE type = "table"')):
+            count, = self.execute(u"SELECT COUNT(*) FROM "+tablename).next()
+            stats_dict[str(tablename)] = count
+        return stats_dict
 
     def add_message(self, sync_id, global_time, keyhash):
         _keyhash = buffer(str(keyhash))
