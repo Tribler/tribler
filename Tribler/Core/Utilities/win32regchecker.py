f51c4a94460d55496632e6f02ae9e96f2413d7b1
Lipu Fei <lipu.fei815@gmail.com>
2014-02-06 11:14:36 +0100

    Remove print_exc().

--- Tribler/Core/Utilities/win32regchecker.py
+++ Tribler/Core/Utilities/win32regchecker.py
@@ -47,7 +47,6 @@ class Win32RegChecker:
             return value_data
         except Exception as ex:
             self._logger.exception("key: %s, key_name: %s, value_name: %s", key, key_name, value_name)
-            print_exc(file=sys.stderr)
             # error, test failed, key don't exist
             # (could also indicate a unicode error)
             return None
@@ -80,7 +79,6 @@ class Win32RegChecker:
             return value_data
         except Exception as ex:
             self._logger.exception("key: %s, key_name: %s, value_name: %s", key, key_name, value_name)
-            print_exc(file=sys.stderr)
             # error, test failed, key don't exist
             # (could also indicate a unicode error)
             return None
