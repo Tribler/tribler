e4b2bbc1b666d5a6f65e98be608a2b45b963ec7d
Chris <risto@live.nl>
2014-02-07 17:04:14 +0100

    Share upon completion, fix 0-hop test and refactored proxy settings usage in ProxyCommunity

--- Tribler/community/anontunnel/community/ProxySettings.py
+++ Tribler/community/anontunnel/community/ProxySettings.py
@@ -2,19 +2,17 @@ from random import randint
 from Tribler.community.anontunnel import extendstrategies, selectionstrategies, lengthstrategies
 from Tribler.community.anontunnel.community import CircuitReturnFactory
 from Tribler.community.anontunnel.crypto import DefaultCrypto
-from Tribler.community.anontunnel.globals import MAX_CIRCUITS_TO_CREATE
 
 __author__ = 'Chris'
 
 
 class ProxySettings:
     def __init__(self):
-        length = randint(1, 4)
-        length = 2
+        length = randint(0, 3)
 
+        self.max_circuits = 1 if length == 0 else 4
         self.extend_strategy = extendstrategies.NeighbourSubset
-        self.select_strategy = selectionstrategies.RoundRobinSelectionStrategy(MAX_CIRCUITS_TO_CREATE)
-        # self.select_strategy = selectionstrategies.RandomSelectionStrategy(1)
+        self.select_strategy = selectionstrategies.RoundRobinSelectionStrategy(self.max_circuits)
         self.length_strategy = lengthstrategies.ConstantCircuitLengthStrategy(length)
         self.return_handler_factory = CircuitReturnFactory()
         self.crypto = DefaultCrypto()
--- Tribler/community/anontunnel/lengthstrategies.py
+++ Tribler/community/anontunnel/lengthstrategies.py
@@ -2,12 +2,14 @@ from random import randint
 
 __author__ = 'chris'
 
+
 class CircuitLengthStrategy(object):
     def __init__(self):
         pass
 
     def circuit_length(self):
         raise NotImplementedError()
+
 
 class RandomCircuitLengthStrategy(CircuitLengthStrategy):
     def __init__(self, min, max):
@@ -16,6 +18,7 @@ class RandomCircuitLengthStrategy(CircuitLengthStrategy):
 
     def circuit_length(self):
         return randint(self.min, self.max)
+
 
 class ConstantCircuitLengthStrategy(CircuitLengthStrategy):
     def __init__(self, desired_length):
