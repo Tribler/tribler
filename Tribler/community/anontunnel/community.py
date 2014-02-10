7f7e994ce33bc22b3eb685f084c5bd1fd070997d
Chris <risto@live.nl>
2014-02-10 13:38:47 +0100

    Fix in Hop class

--- Tribler/community/anontunnel/community.py
+++ Tribler/community/anontunnel/community.py
@@ -139,15 +139,15 @@ class Hop:
     def __init__(self, address, pub_key, dh_first_part):
         self.address = address
         self.pub_key = pub_key
-        self.session_key = None
-        self.dh_first_part = dh_first_part
+        self._session_key = None
+        self._dh_first_part = dh_first_part
 
     @property
     def session_key(self):
-        return self.session_key
+        return self._session_key
     @property
     def dh_first_part(self):
-        return self.dh_first_part
+        return self._dh_first_part
 
     @property
     def host(self):
