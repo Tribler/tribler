96a731d13106ed76b4b1fba9d923ac013888c9cf
Egbert Bouman <ebouman@gmail.com>
2014-03-14 15:30:11 +0100

    Added LibtorrentDownloadImpl decorators

---
+++ Tribler/Core/Libtorrent/__init__.py
@@ -0,0 +1,29 @@
+# Written by Egbert Bouman
+import sys
+import functools
+
+def checkHandleAndSynchronize(default=None):
+    def wrap(f):
+        def invoke_func(*args, **kwargs):
+            download = args[0]
+            with download.dllock:
+                if download.handle and download.handle.is_valid():
+                    return f(*args, **kwargs)
+            return default
+        return invoke_func
+    return wrap
+
+def waitForHandleAndSynchronize(default=None):
+    def wrap(f):
+        def invoke_func(*args, **kwargs):
+            download = args[0]
+            with download.dllock:
+                if download.handle and download.handle.is_valid():
+                    return f(*args, **kwargs)
+                else:
+                    lambda_f = lambda a = args, kwa = kwargs: f(*a, **kwa)
+                    download.rawserver.add_task(lambda_f, 1)
+                    return default
+        return invoke_func
+
+    return wrap
