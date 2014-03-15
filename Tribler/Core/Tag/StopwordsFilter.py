f3f0e7091d83c7616d3624173c516dbef963a942
Lipu Fei <lipu.fei815@gmail.com>
2014-03-15 16:17:58 +0100

    small improvement for StopwordsFilter

--- Tribler/Core/Tag/StopwordsFilter.py
+++ Tribler/Core/Tag/StopwordsFilter.py
@@ -6,32 +6,23 @@ This module contains a class to read stopwords from files in the Snowball format
 
 import os
 
-# For some reason, this sometimes fails on my machine:
-# from Tribler.__init__ import LIBRARYNAME
-# DEFAULT_STOPWORDS_FILE = os.path.join(LIBRARYNAME, 'Core', 'Tag', 'stop_snowball.filter')
-
-# Workaround for now:
-DEFAULT_STOPWORDS_FILE = os.path.join(os.path.dirname(__file__), 'stop_snowball.filter')
-# TODO: Improve this code ^
+from Tribler import LIBRARYNAME
+DEFAULT_STOPWORDS_FILE = os.path.join(LIBRARYNAME, 'Core', 'Tag', 'stop_snowball.filter')
 
 
 class StopwordsFilter:
 
     def __init__(self, stopwordsfilename=DEFAULT_STOPWORDS_FILE):
-        self.stopwords = self._readStopwordsList(stopwordsfilename)
-
-    def _readStopwordsList(self, filename):
-        f = open(filename, 'r')
-        stopwords = set()
-        for line in f:
+        file_stream = open(stopwordsfilename, 'r')
+        self._stopwords = set()
+        for line in file_stream:
             word = line.split('|')[0].rstrip()
             if word and not word[0].isspace():
-                stopwords.add(word)
-        f.close()
-        return stopwords
+                self._stopwords.add(word)
+        file_stream.close()
 
     def isStopWord(self, word):
-        return word in self.stopwords
+        return word in self._stopwords
 
     def getStopWords(self):
-        return set(self.stopwords)  # return a copy
+        return set(self._stopwords)  # return a copy
