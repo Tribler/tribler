be1f398feed2759bcdf44402b40d203d908a6ff6
Lipu Fei <lipu.fei815@gmail.com>
2014-03-17 15:45:22 +0100

    fix tribler profiler

--- Tribler/Main/tribler_profiler.py
+++ Tribler/Main/tribler_profiler.py
@@ -1,20 +1,23 @@
 # Requires yappi to be installed, use easy_install yappi
 
 import yappi
-from tribler import run
+import sys
 from time import time
-import logging
-
-logger = logging.getLogger(__name__)
+from tribler import run
 
 if __name__ == '__main__':
     t1 = time()
     yappi.start()
     run()
     yappi.stop()
-    logger.info("YAPPI: %s tribler has run for %s seconds", yappi.clock_type(), time() - t1)
-    stats = yappi.get_stats(yappi.SORTTYPE_TSUB)
-    for func_stats in stats.func_stats[:50]:
-        logger.info("YAPPI: %10dx  %10.3fs %s", func_stats.ncall, func_stats.tsub, func_stats.name)
-
-    # yappi.print_stats(yappi.SORTTYPE_TTOTAL)
+    print >> sys.stderr, "YAPPI: %s tribler has run for %s seconds" % \
+        (yappi.get_clock_type(), time() - t1)
+    yappi_stats = yappi.get_func_stats()
+    yappi_stats.sort("tsub")
+    count = 0
+    for func_stat in yappi_stats:
+        print >> sys.stderr, "YAPPI: %10dx  %10.3fs %s" % \
+            (func_stat.ncall, func_stat.tsub, func_stat.name)
+        count += 1
+        if count >= 50:
+            break
