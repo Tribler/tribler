731fe4199851aaf052d5ae8b58569464b604cb74
Egbert Bouman <ebouman@gmail.com>
2014-04-07 11:26:35 +0200

    Fix

--- Tribler/Main/vwxGUI/widgets.py
+++ Tribler/Main/vwxGUI/widgets.py
@@ -1791,7 +1791,7 @@ class TorrentStatus(wx.Panel):
             self.fill_colour = self.back_colour
         if status == 'Checking':
             self.fill_colour = self.back_colour
-        if status == 'Downloading':
+        if status in ['Downloading', 'Seeding']:
             self.fill_colour = DOWNLOADING_COLOUR
         if status == 'Stopped':
             self.fill_colour = STOPPED_COLOUR
