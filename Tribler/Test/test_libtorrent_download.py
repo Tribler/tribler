e6d94e60a07852de9d8a62f0a30014e627f8325d
Egbert Bouman <ebouman@gmail.com>
2014-04-07 12:55:39 +0200

    Fix

--- Tribler/Test/test_libtorrent_download.py
+++ Tribler/Test/test_libtorrent_download.py
@@ -141,7 +141,7 @@ class TestLibtorrentDownload(TestGuiAsServer):
             playlist = self.guiUtility.frame.actlist.expandedPanel_videoplayer
 
             do_check = lambda: len(playlist.links) == len(videofiles) and \
-                               playlist.torrent.infohash == VideoPlayer.getInstance().get_vod_download().get_def().get_id() and \
+                               playlist.tdef.get_id() == VideoPlayer.getInstance().get_vod_download().get_def().get_id() and \
                                playlist.fileindex == VideoPlayer.getInstance().get_vod_fileindex()
 
             self.CallConditional(10, do_check, lambda: self.Call(5, lambda: take_screenshot(buffer_complete)), "playlist set incorrectly")
