346b5d396e603095dc7b790c73e2adf2a4f3daf6
Rutger Plak <rsplak@gmail.com>
2014-04-08 13:16:10 +0200

    mpz outside loop

--- Tribler/community/anontunnel/crypto.py
+++ Tribler/community/anontunnel/crypto.py
@@ -194,7 +194,8 @@ class DefaultCrypto(Crypto):
         """
         dh_secret = 0
         while dh_secret >= DIFFIE_HELLMAN_MODULUS or dh_secret < 2:
-              dh_secret = mpz(rand("next", DIFFIE_HELLMAN_MODULUS))
+              dh_secret = rand("next", DIFFIE_HELLMAN_MODULUS)
+        dh_secret = mpz(dh_secret)
 
         dh_first_part = mpz(pow(DIFFIE_HELLMAN_GENERATOR, dh_secret, DIFFIE_HELLMAN_MODULUS))
         return dh_secret, dh_first_part
