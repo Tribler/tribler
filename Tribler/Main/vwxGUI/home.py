b3a3e431b03b67adacc42329a268cbe0fa947497
Egbert Bouman <ebouman@gmail.com>
2014-04-09 21:53:30 +0200

    Delete vertex_max stuff

--- Tribler/Main/vwxGUI/home.py
+++ Tribler/Main/vwxGUI/home.py
@@ -862,7 +862,6 @@ class Anonymity(wx.Panel):
 
         self.selected_edges = []
 
-        self.vertex_max = 100
         self.vertex_active = -1
         self.vertex_hover = -1
         self.vertex_hover_evt = None
@@ -1077,8 +1076,6 @@ class Anonymity(wx.Panel):
 
     def RemoveVertex(self, toremove_id):
         with self.lock:
-
-            # Remove the vertex with the fewest neighbors.
             if toremove_id in self.vertices:
                 self.vertices.pop(toremove_id)
             if toremove_id in self.vertex_to_colour:
@@ -1152,10 +1149,6 @@ class Anonymity(wx.Panel):
             self.taskqueue.add_task(task)
             self.new_data = False
             self.layout_busy = True
-
-        elif len(self.vertices) > self.vertex_max:
-            task = lambda: self.RemoveVertex()
-            self.taskqueue.add_task(task)
 
         if len(self.vertices) > 0:
 
