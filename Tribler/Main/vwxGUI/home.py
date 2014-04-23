df53e21308017076a78068d13b34122941fdc56c
Chris <risto@live.nl>
2014-04-23 13:09:46 +0200

    Small change in home.py

--- Tribler/Main/vwxGUI/home.py
+++ Tribler/Main/vwxGUI/home.py
@@ -594,7 +594,7 @@ class Anonymity(wx.Panel):
 
         self.AddComponents()
 
-        self.my_address = Hop(self.proxy_community.my_member.mid)
+        self.my_address = Hop(self.proxy_community.my_member._ec.pub())
         self.my_address.address = ('127.0.0.1', "SELF")
 
         self.vertices = {}
