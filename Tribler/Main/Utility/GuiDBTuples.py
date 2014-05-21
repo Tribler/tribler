2da87814fe36058132cbf8a4d0bdd26c44b1cac3
wtud <ws+tud@dosoftware.nl>
2014-05-21 21:15:25 +0200

    Make vwxGUI imports conditional so we can reuse these tuples in the XMLRPC code

--- Tribler/Main/Utility/GuiDBTuples.py
+++ Tribler/Main/Utility/GuiDBTuples.py
@@ -1,22 +1,24 @@
 # Niels: getValidArgs based on http://stackoverflow.com/questions/196960/can-you-list-the-keyword-arguments-a-python-function-receives
 import sys
-import os.path
+import os
 import logging
 from datetime import date
 from time import time
 from inspect import getargspec
 from Tribler.Core.Video.utils import videoextdefaults
-from Tribler.Main.vwxGUI import VLC_SUPPORTED_SUBTITLES, PLAYLIST_REQ_COLUMNS, \
-    CHANNEL_REQ_COLUMNS
 from Tribler.Core.simpledefs import DLSTATUS_DOWNLOADING, DLSTATUS_STOPPED, \
     DLSTATUS_SEEDING, DLSTATUS_HASHCHECKING, \
     DLSTATUS_WAITING4HASHCHECK, DLSTATUS_ALLOCATING_DISKSPACE, \
     DLSTATUS_STOPPED_ON_ERROR, DLSTATUS_METADATA
-from Tribler.Main.vwxGUI.GuiImageManager import GuiImageManager, SMALL_ICON_MAX_DIM, data2wxBitmap
 from Tribler.community.channel.community import ChannelCommunity, \
     forceAndReturnDispersyThread
 from Tribler.Core.Search.SearchManager import split_into_keywords
 import binascii
+
+if not 'ANDROID_HOST' in os.environ:
+    from Tribler.Main.vwxGUI import VLC_SUPPORTED_SUBTITLES, PLAYLIST_REQ_COLUMNS, \
+        CHANNEL_REQ_COLUMNS
+    from Tribler.Main.vwxGUI.GuiImageManager import GuiImageManager, SMALL_ICON_MAX_DIM, data2wxBitmap
 
 logger = logging.getLogger(__name__)
 
