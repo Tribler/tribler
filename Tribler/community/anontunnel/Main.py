a6cd4e33af0f0a9689ab3f1f980e4a40131b05be
Chris <risto@live.nl>
2014-05-26 20:46:22 +0200

    Twistified Main.py

--- Tribler/community/anontunnel/Main.py
+++ Tribler/community/anontunnel/Main.py
@@ -9,19 +9,29 @@ import threading
 import time
 from threading import Thread, Event
 from traceback import print_exc
+from twisted.conch import stdio
+from twisted.internet import reactor
+from twisted.internet.stdio import StandardIO
 
 from twisted.internet.task import LoopingCall
+import twisted
+from twisted.protocols.basic import LineReceiver
 
 from Tribler.Core.RawServer.RawServer import RawServer
 from Tribler.community.anontunnel import exitstrategies
 from Tribler.community.anontunnel.Socks5.server import Socks5Server
-from Tribler.community.anontunnel.community import ProxyCommunity, ProxySettings
+from Tribler.community.anontunnel.community import ProxyCommunity, \
+    ProxySettings
 from Tribler.community.anontunnel.endpoint import DispersyBypassEndpoint
-from Tribler.community.anontunnel.extendstrategies import TrustThyNeighbour, NeighbourSubset
-from Tribler.community.anontunnel.lengthstrategies import RandomCircuitLengthStrategy, ConstantCircuitLength
-from Tribler.community.anontunnel.selectionstrategies import RandomSelectionStrategy, LengthSelectionStrategy
+from Tribler.community.anontunnel.extendstrategies import TrustThyNeighbour, \
+    NeighbourSubset
+from Tribler.community.anontunnel.lengthstrategies import \
+    RandomCircuitLengthStrategy, ConstantCircuitLength
+from Tribler.community.anontunnel.selectionstrategies import \
+    RandomSelectionStrategy, LengthSelectionStrategy
 from Tribler.community.anontunnel.stats import StatsCrawler
-from Tribler.community.privatesemantic.crypto.elgamalcrypto import ElgamalCrypto
+from Tribler.community.privatesemantic.crypto.elgamalcrypto import \
+    ElgamalCrypto
 from Tribler.dispersy.dispersy import Dispersy
 from Tribler.dispersy.util import call_on_reactor_thread
 
@@ -37,7 +47,7 @@ except ImportError:
     logger.warning("Yappi not installed, profiling options won't be available")
 
 
-class AnonTunnel(Thread):
+class AnonTunnel():
     """
     The standalone AnonTunnel application. Does not depend on Tribler Session
     or LaunchManyCore but creates all dependencies by itself.
@@ -50,7 +60,6 @@ class AnonTunnel(Thread):
     """
 
     def __init__(self, socks5_port, settings=None, crawl=False):
-        Thread.__init__(self)
         self.crawl = crawl
         self.settings = settings
         self.server_done_flag = Event()
@@ -65,7 +74,8 @@ class AnonTunnel(Thread):
         self.socks5_server = None
 
         endpoint = DispersyBypassEndpoint(self.raw_server, port=10000)
-        self.dispersy = Dispersy(endpoint, u".", u":memory:", crypto=ElgamalCrypto())
+        self.dispersy = Dispersy(endpoint, u".", u":memory:",
+                                 crypto=ElgamalCrypto())
 
         self.community = None
         ''' @type: ProxyCommunity '''
@@ -84,7 +94,8 @@ class AnonTunnel(Thread):
         speed_exit = (stats['bytes_exit'] - bytes_exit0) / diff if then else 0
         bytes_exit = stats['bytes_exit']
 
-        speed_enter = (stats['bytes_enter'] - bytes_enter0) / diff if then else 0
+        speed_enter = (stats[
+                           'bytes_enter'] - bytes_enter0) / diff if then else 0
         bytes_enter = stats['bytes_enter']
 
         relay_2 = sum([r.bytes[1] for r in relay_stats.values()])
@@ -96,14 +107,21 @@ class AnonTunnel(Thread):
                bytes_exit, bytes_enter, bytes_relay
 
     def __speed_stats(self):
-        time = None
-        bytes_exit = 0
-        bytes_enter = 0
-        bytes_relay = 0
+        tmp = dict()
+        tmp['time'] = None
+        tmp['bytes_exit'] = 0
+        tmp['bytes_enter'] = 0
+        tmp['bytes_relay'] = 0
 
         def speed_stats_lc():
-            stats = self.__calc_diff(time, bytes_exit, bytes_enter, bytes_relay)
+            stats = self.__calc_diff(tmp['time'], tmp['bytes_exit'], tmp['bytes_enter'],
+                                     tmp['bytes_relay'] )
             time, speed_exit, speed_enter, speed_relay, bytes_exit, bytes_enter, bytes_relay = stats
+
+            tmp['time'] = time
+            tmp['bytes_exit'] = bytes_exit
+            tmp['bytes_enter'] = bytes_enter
+            tmp['bytes_relay'] = bytes_relay
 
             active_circuits = len(self.community.circuits)
             num_routes = len(self.community.relay_from_to) / 2
@@ -112,10 +130,9 @@ class AnonTunnel(Thread):
                 active_circuits, num_routes, speed_exit / 1024.0,
                 speed_enter / 1024.0, speed_relay / 1024.0),
 
-        self._pending_tasks["speed stats"] = lc = LoopingCall(speed_sats)
+        lc = LoopingCall(speed_stats_lc)
         lc.start(3, now=True)
 
-    @call_on_reactor_thread
     def run(self):
         """
         Start the standalone AnonTunnel
@@ -127,7 +144,9 @@ class AnonTunnel(Thread):
 
         proxy_args = self.raw_server, self.dispersy
         member = self.dispersy.get_new_member(u"NID_secp160k1")
-        self.community = dispersy.define_auto_load(ProxyCommunity, member, (self.settings, False), load=True)[0]
+        self.community = self.dispersy.define_auto_load(ProxyCommunity, member,
+                                                   (False, self.settings),
+                                                   load=True)[0]
         ''' @type: ProxyCommunity '''
 
         if self.socks5_server:
@@ -135,16 +154,19 @@ class AnonTunnel(Thread):
                 self.community, self.raw_server, self.socks5_port)
             self.socks5_server.start()
 
-        exit_strategy = exitstrategies.DefaultExitStrategy(proxy_args, self.community)
+        exit_strategy = exitstrategies.DefaultExitStrategy(proxy_args,
+                                                           self.community)
         self.community.observers.append(exit_strategy)
 
         if self.crawl:
-            self.community.observers.append(StatsCrawler(self.dispersy, self.raw_server))
+            self.community.observers.append(
+                StatsCrawler(self.dispersy, self.raw_server))
 
         ''' :type : Tribler.community.anontunnel.community.ProxyCommunity '''
 
         self.__speed_stats()
-        self.raw_server.listen_forever(None)
+        raw_server_thread = Thread(target=self.raw_server.listen_forever, args=(None,))
+        raw_server_thread.start()
 
     def stop(self):
         """
@@ -158,6 +180,75 @@ class AnonTunnel(Thread):
         if self.raw_server:
             self.raw_server.shutdown()
 
+
+class LineHandler(LineReceiver):
+    from os import linesep as delimiter
+
+    def __init__(self, anon_tunnel, profile):
+        self.anon_tunnel = anon_tunnel
+        self.profile = profile
+
+    def lineReceived(self, line):
+        anon_tunnel = self.anon_tunnel
+        profile = self.profile
+
+        if line == 'threads':
+            for thread in threading.enumerate():
+                print "%s \t %d" % (thread.name, thread.ident)
+        elif line == 'p':
+            if profile:
+
+                for func_stats in yappi.get_func_stats().sort("subtime")[:50]:
+                    print "YAPPI: %10dx  %10.3fs" % (
+                        func_stats.ncall, func_stats.tsub), func_stats.name
+            else:
+                print >> sys.stderr, "Profiling disabled!"
+
+        elif line == 'P':
+            if profile:
+                filename = 'callgrindc_%d.yappi' % \
+                           anon_tunnel.dispersy.lan_address[1]
+                yappi.get_func_stats().save(filename, type='callgrind')
+            else:
+                print >> sys.stderr, "Profiling disabled!"
+
+        elif line == 't':
+            if profile:
+                yappi.get_thread_stats().sort("totaltime").print_all()
+
+            else:
+                print >> sys.stderr, "Profiling disabled!"
+
+        elif line == 'c':
+            stats = anon_tunnel.community.global_stats.circuit_stats
+
+            print "========\nCircuits\n========\n" \
+                  "id\taddress\t\t\t\t\tgoal\thops\tIN (MB)\tOUT (MB)"
+            for circuit_id, circuit in anon_tunnel.community.circuits.items():
+                print "%d\t%s:%d\t%d\t%d\t\t%.2f\t\t%.2f" % (
+                    circuit.circuit_id, circuit.candidate.sock_addr[0],
+                    circuit.candidate.sock_addr[1],
+                    circuit.goal_hops, len(circuit.hops),
+                    stats[circuit_id].bytes_downloaded / 1024.0 / 1024.0,
+                    stats[circuit_id].bytes_uploaded / 1024.0 / 1024.0
+                )
+        elif line == 'q':
+            anon_tunnel.stop()
+            os._exit(0)
+            return
+        elif line == 'r':
+            print "circuit\t\t\tdirection\tcircuit\t\t\tTraffic (MB)"
+
+            from_to = anon_tunnel.community.relay_from_to
+
+            for key in from_to.keys():
+                relay = from_to[key]
+
+                print "%s-->\t%s\t\t%.2f" % (
+                    (key[0], key[1]),
+                    (relay.sock_addr, relay.circuit_id),
+                    relay.bytes[1] / 1024.0 / 1024.0,
+                )
 
 def main(argv):
     """
@@ -250,79 +341,13 @@ def main(argv):
     anon_tunnel = AnonTunnel(socks5_port, proxy_settings, crawl)
     ''' @type: AnonTunnel '''
 
-    anon_tunnel.start()
 
 
+    from twisted.internet import reactor
 
-    while 1:
-        try:
-            line = sys.stdin.readline()
-        except KeyboardInterrupt:
-            anon_tunnel.stop()
-            os._exit(0)
-            break
-
-        if not line:
-            break
-
-        if line == 'threads\n':
-            for thread in threading.enumerate():
-                print "%s \t %d" % (thread.name, thread.ident)
-        elif line == 'p\n':
-            if profile:
-
-                for func_stats in yappi.get_func_stats().sort("subtime")[:50]:
-                    print "YAPPI: %10dx  %10.3fs" % (
-                    func_stats.ncall, func_stats.tsub), func_stats.name
-            else:
-                print >> sys.stderr, "Profiling disabled!"
-
-        elif line == 'P\n':
-            if profile:
-                filename = 'callgrindc_%d.yappi' % \
-                           anon_tunnel.dispersy.lan_address[1]
-                yappi.get_func_stats().save(filename, type='callgrind')
-            else:
-                print >> sys.stderr, "Profiling disabled!"
-
-        elif line == 't\n':
-            if profile:
-                yappi.get_thread_stats().sort("totaltime").print_all()
-
-            else:
-                print >> sys.stderr, "Profiling disabled!"
-
-        elif line == 'c\n':
-            stats = anon_tunnel.community.global_stats.circuit_stats
-
-            print "========\nCircuits\n========\n" \
-                  "id\taddress\t\t\t\t\tgoal\thops\tIN (MB)\tOUT (MB)"
-            for circuit_id, circuit in anon_tunnel.community.circuits.items():
-                print "%d\t%s:%d\t%d\t%d\t\t%.2f\t\t%.2f" % (
-                    circuit.circuit_id, circuit.candidate.sock_addr[0],
-                    circuit.candidate.sock_addr[1],
-                    circuit.goal_hops, len(circuit.hops),
-                    stats[circuit_id].bytes_downloaded / 1024.0 / 1024.0,
-                    stats[circuit_id].bytes_uploaded / 1024.0 / 1024.0
-                )
-        elif line == 'q\n':
-            anon_tunnel.stop()
-            os._exit(0)
-            break
-        elif line == 'r\n':
-            print "circuit\t\t\tdirection\tcircuit\t\t\tTraffic (MB)"
-
-            from_to = anon_tunnel.community.relay_from_to
-
-            for key in from_to.keys():
-                relay = from_to[key]
-
-                print "%s-->\t%s\t\t%.2f" % (
-                    (key[0], key[1]),
-                    (relay.sock_addr, relay.circuit_id),
-                    relay.bytes[1] / 1024.0 / 1024.0,
-                )
-
+    StandardIO(LineHandler(anon_tunnel, profile))
+    reactor.callWhenRunning(anon_tunnel.run)
+    reactor.run()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
