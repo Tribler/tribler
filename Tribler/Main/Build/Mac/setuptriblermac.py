88a787b349c556f46b0316d32134f9f5e8a13b40
Lipu Fei <lipu.fei815@gmail.com>
2014-06-02 16:50:28 +0200

    fix mac building script

--- Tribler/Main/Build/Mac/setuptriblermac.py
+++ Tribler/Main/Build/Mac/setuptriblermac.py
@@ -121,7 +121,7 @@ setup(
              (LIBRARYNAME + "/Core/DecentralizedTracking/pymdht/core", [LIBRARYNAME + "/Core/DecentralizedTracking/pymdht/core/bootstrap_unstable"]),
              LIBRARYNAME + "/readme.txt",
              LIBRARYNAME + "/Main/Build/Mac/TriblerDoc.icns",
-             LIBRARYNAME + "/community/privatesemantic/crypto/curves.ec",
+             (LIBRARYNAME + "/community/privatesemantic/crypto", [LIBRARYNAME + "/community/privatesemantic/crypto/curves.ec"]),
            ]
            # add images
            + includedir(LIBRARYNAME + "/Main/vwxGUI/images")
