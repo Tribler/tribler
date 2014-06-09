0f438e8d334a02fc9211c6d829ea93bfd3535f13
Rolf Jagerman <rjagerman@gmail.com>
2014-06-09 23:39:19 +0200

    Added a global variable for the library's absolute path

--- Tribler/__init__.py
+++ Tribler/__init__.py
@@ -1,4 +1,6 @@
 # Written by Arno Bakker
 # see LICENSE.txt for license information
+import os, intent
 
 LIBRARYNAME = 'Tribler'
+LIBRARYPATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
