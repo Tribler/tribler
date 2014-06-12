a882f5bbb0fbd3772d41a24de6eb3c07160528a7
Lipu Fei <lipu.fei815@gmail.com>
2014-06-12 15:53:22 +0200

    fix Windows build scripts

--- Tribler/Main/Build/Win32/setuptribler.py
+++ Tribler/Main/Build/Win32/setuptribler.py
@@ -47,7 +47,9 @@ includePanels = [
 # packages = ["Tribler.Core","encodings"] + ["Tribler.Main.vwxGUI.%s" % x for x in includePanels]
 packages = ["encodings"] + ["Tribler.Main.vwxGUI.%s" % x for x in includePanels] +\
 ["Tribler.Core.DecentralizedTracking.pymdht.core", \
- "Tribler.Main.tribler_main", "netifaces", "csv", "cherrypy"]
+ "Tribler.Main.tribler_main", "netifaces", "csv", "cherrypy",
+ "twisted", "apsw", "libtorrent", "M2Crypto",
+ "zope.interface", "pyasn1", "gmpy"]
 
 setup(
     #    (Disabling bundle_files for now -- apparently causes some issues with Win98)
