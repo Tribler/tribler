3455d5f2c672068b8526d2db55469ccfb27a1f7a
Lipu Fei <lipu.fei815@gmail.com>
2014-07-07 16:59:37 +0200

    fix Mac OS X run script

--- tribler.sh
+++ tribler.sh
@@ -35,28 +35,9 @@ else
             exit 1
         fi
 
-        PYTHONVER=2.5
-        PYTHON=/usr/local/bin/python$PYTHONVER
+        PYTHONVER=2.7
+        PYTHON="arch -i386 /usr/bin/python$PYTHONVER"
 
-        DIRCHANGE=`dirname $0`
-
-        if [ $DIRCHANGE != "" ]; then
-            cd $DIRCHANGE
-        fi
-
-        if [ ! -e "macbinaries" ]; then
-            echo Please unpack macbinaries-`arch`.tar.gz here, so that the macbinaries directory will be created and filled with the binaries required for operation.
-            exit -1
-        fi
-
-        export PYTHONPATH=`pwd`/macbinaries:`pwd`/lib/Library/Frameworks/Python.framework/Versions/2.5/lib/python2.5/site-packages/
-        export DYLD_LIBRARY_PATH=`pwd`/macbinaries
-
-        # use a hardlink so the script is in the current directory, otherwise
-        # python will start to chdir all over the place
-        rm -f tmp.py
-        ln $1 tmp.py
-        shift
-        $PYTHON tmp.py $@
+        $PYTHON Tribler/Main/tribler.py
     fi
 fi
