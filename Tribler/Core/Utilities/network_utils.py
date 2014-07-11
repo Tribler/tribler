42528f37708d6135223979b4ba3f19b46f78dee9
Lipu Fei <lipu.fei815@gmail.com>
2014-07-11 12:20:47 +0200

    improve network_utils.py functions

--- Tribler/Core/Utilities/network_utils.py
+++ Tribler/Core/Utilities/network_utils.py
@@ -5,15 +5,18 @@
 import socket
 import struct
 import random
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_random_port(socket_family="ipv4", socket_type="all", min_port=5000, max_port=60000):
     """Gets a random port number that works.
-    :param socket_family: The family of the socket, can be "ipv4" or "ipv6".
-    :param socket_type: Type of the socket, can be "all", "tcp", or "udp".
-    :param min_port: The minimal port number to try with.
-    :param max_port: The maximal port number to try with.
-    :return: A working port number.
+    @param socket_family: The family of the socket, can be "ipv4" or "ipv6".
+    @param socket_type: Type of the socket, can be "all", "tcp", or "udp".
+    @param min_port: The minimal port number to try with.
+    @param max_port: The maximal port number to try with.
+    @return: A working port number if exists, otherwise None.
     """
     assert socket_family in ("ipv4", "ipv6"), "Invalid socket family %s" % type(socket_family)
     assert socket_type in ("all", "tcp", "udp"), "Invalid socket type %s" % type(socket_type)
@@ -22,12 +25,14 @@ def get_random_port(socket_family="ipv4", socket_type="all", min_port=5000, max_
     assert 0 < min_port <= max_port <= 65535, "Invalid min_port and mac_port values %s, %s" % (min_port, max_port)
 
     working_port = None
-    while True:
-        port = random.randint(min_port, max_port)
-        if check_random_port(port, socket_family, socket_type):
-            working_port = port
+    try_port = random.randint(min_port, max_port)
+    while try_port <= 65535:
+        if check_random_port(try_port, socket_family, socket_type):
+            working_port = try_port
             break
+        try_port += 1
 
+    logger.debug("Got a working random port %s", working_port)
     return working_port
 
 
@@ -67,21 +72,35 @@ def check_random_port(port, socket_family="ipv4", socket_type="all"):
 
 
 def _test_port(family, sock_type, port):
+    """Tests if a port is available.
+    @param family: The socket family, can be socket.AF_INET or socket.AF_INET6.
+    @param sock_type: The socket type, can be socket.SOCK_DGRAM or socket.SOCK_STREAM.
+    @param port: The port number to test with.
+    @return: True if the port is available or there is no problem with the socket, otherwise False.
+    """
     assert family in (socket.AF_INET, socket.AF_INET6), "Invalid family value %s" % family
     assert sock_type in (socket.SOCK_DGRAM, socket.SOCK_STREAM), "Invalid sock_type value %s" % sock_type
     assert 0 < port <= 65535, "Invalid port value %s" % port
 
-    is_port_working = False
     s = None
     try:
         s = socket.socket(family, sock_type)
         if sock_type == socket.SOCK_STREAM:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
-        s.bind(('', port))
-        is_port_working = True
-    except:
-        is_port_working = False
-    finally:
+    except socket.error as e:
+        logger.error("Could not create socket or set LINGER_TIME (family=%s, type=%s): %s",
+                     family, sock_type, e)
         if s:
             s.close()
+        return False
+
+    try:
+        s.bind(('', port))
+        is_port_working = True
+    except socket.error as e:
+        logger.debug("Port test failed (port=%s, family=%s, type=%s): %s",
+                     port, family, sock_type, e)
+        is_port_working = False
+    finally:
+        s.close()
     return is_port_working
