a111a494682aa8497075b3729722c949c245de47
Lipu Fei <lipu.fei815@gmail.com>
2014-09-15 14:24:09 +0200

    limit metadata thumbs to be at most 512KB

--- Tribler/Core/RemoteTorrentHandler.py
+++ Tribler/Core/RemoteTorrentHandler.py
@@ -900,11 +900,11 @@ class MetadataRequester(Requester):
     def check_progress(self, ds, roothash):
         d = ds.get_download()
         # do not download metadata larger than 5MB
-        if d.get_dynasize() > 5 * 1024 * 1024:
+        if d.get_dynasize() > 512 * 1024:
             remove_lambda = lambda d = d: self._remove_download(d, False)
             self.scheduletask(remove_lambda)
             self.blacklist_set.add(roothash)
-            return (0, False)
+            return 0, False
 
         cdef = d.get_def()
         if ds.get_progress() == 1:
@@ -915,14 +915,14 @@ class MetadataRequester(Requester):
 
             self.remote_th.notify_possible_metadata_roothash(roothash)
             self.requests_success += 1
-            return (0, False)
+            return 0, False
         else:
             diff = time() - getattr(d, 'started_downloading', time()) > 45
             if (diff > self.SWIFT_CANCEL and ds.get_progress() == 0) or diff > 45 or ds.get_status() == DLSTATUS_STOPPED_ON_ERROR:
                 remove_lambda = lambda d = d: self._remove_download(d)
                 self.scheduletask(remove_lambda)
                 self.requests_fail += 1
-                return (0, False)
+                return 0, False
 
         return (self.REQUEST_INTERVAL * (self.prio + 1), True)
 
