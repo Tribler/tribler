c5f844482e336fccb837217613c97a182cdb4586
Lipu Fei <lipu.fei815@gmail.com>
2014-11-10 17:41:31 +0100

    add metadata download test

--- Tribler/Test/test_remote_torrent_handler.py
+++ Tribler/Test/test_remote_torrent_handler.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-from shutil import copyfile, rmtree
+from shutil import copyfile, rmtree, copytree
 from time import sleep
 from threading import Event
 
@@ -13,28 +13,6 @@ from Tribler.dispersy.candidate import Candidate
 class TestRemoteTorrentHandler(TestAsServer):
     """ Tests the download_torrent() method of TestRemoteTorrentHandler.
     """
-
-    def test_remotedownload(self):
-        def do_check_download(torrent_file=None):
-            des_file_path = os.path.join(self.session2.get_torrent_collecting_dir(), self.file_name)
-            self.assertTrue(os.path.exists(des_file_path) and os.path.isfile(des_file_path),
-                            u"Failed to download torrent file.")
-
-            print >> sys.stderr, u"Torrent file downloaded successfully."
-            self.quit()
-            self.download_event.set()
-
-        def do_start_download():
-            self.setup_downloader()
-
-            timeout = 10
-
-            candidate = Candidate(("127.0.0.1", self.session1_port), False)
-            self.session2.lm.rtorrent_handler.download_torrent(candidate, self.infohash, usercallback=do_check_download)
-            self.CallConditional(timeout, self.download_event.is_set, do_check_download,
-                                 u"Failed to download torrent within %s seconds" % timeout)
-
-        self.startTest(do_start_download)
 
     def startTest(self, callback):
         super(TestRemoteTorrentHandler, self).startTest(callback)
@@ -50,7 +28,31 @@ class TestRemoteTorrentHandler(TestAsServer):
         rmtree(self.session2_torrent_collecting_dir)
         super(TestRemoteTorrentHandler, self).tearDown()
 
-    def setup_downloader(self):
+    def test_torrentdownload(self):
+        print >> sys.stderr, u"Start torrent download test..."
+
+        def do_check_download(torrent_file=None):
+            des_file_path = os.path.join(self.session2.get_torrent_collecting_dir(), self.file_name)
+            self.assertTrue(os.path.exists(des_file_path) and os.path.isfile(des_file_path),
+                            u"Failed to download torrent file.")
+
+            print >> sys.stderr, u"Torrent file downloaded successfully."
+            self.quit()
+            self.download_event.set()
+
+        def do_start_download():
+            self.setup_torrentdownloader()
+
+            timeout = 10
+
+            candidate = Candidate(("127.0.0.1", self.session1_port), False)
+            self.session2.lm.rtorrent_handler.download_torrent(candidate, self.infohash, usercallback=do_check_download)
+            self.CallConditional(timeout, self.download_event.is_set, do_check_download,
+                                 u"Failed to download torrent within %s seconds" % timeout)
+
+        self.startTest(do_start_download)
+
+    def setup_torrentdownloader(self):
         self.download_event = Event()
         self.session1_port = self.session.get_dispersy_port()
 
@@ -69,6 +71,7 @@ class TestRemoteTorrentHandler(TestAsServer):
         self.setUpPreSession()
 
         self.config2 = self.config.copy()
+        self.config2.set_megacache(True)
         self.config2.set_torrent_collecting(True)
 
         self.session2_state_dir = self.session.get_state_dir() + "2"
@@ -81,3 +84,63 @@ class TestRemoteTorrentHandler(TestAsServer):
         sleep(1)
 
         print >> sys.stderr, u"Downloader's torrent_collect_dir = %s" % self.session2.get_torrent_collecting_dir()
+
+    def test_metadatadownload(self):
+        print >> sys.stderr, u"Start metadata download test..."
+
+        def do_check_download(torrent_file=None):
+            des_file_path = os.path.join(self.session2.get_torrent_collecting_dir(), self.metadata_dir)
+            self.assertTrue(os.path.exists(des_file_path) and os.path.isdir(des_file_path),
+                            u"Failed to download metadata.")
+
+            print >> sys.stderr, u"metadata downloaded successfully."
+            self.quit()
+            self.download_event.set()
+
+        def do_start_download():
+            self.setup_metadatadownloader()
+
+            timeout = 10
+
+            candidate = Candidate(("127.0.0.1", self.session1_port), False)
+            self.session2.lm.rtorrent_handler.download_metadata(self.metadata_type, candidate, self.infohash,
+                                                                usercallback=do_check_download)
+            self.CallConditional(timeout, self.download_event.is_set, do_check_download,
+                                 u"Failed to download metadata within %s seconds" % timeout)
+
+        self.startTest(do_start_download)
+
+    def setup_metadatadownloader(self):
+        self.download_event = Event()
+        self.session1_port = self.session.get_dispersy_port()
+
+        infohash_str = "41aea20908363a80d44234e8fef07fab506cd3b4"
+        self.infohash = infohash_str.decode('hex')
+        self.metadata_type = "thumbs"
+        self.metadata_dir = u"%s-%s" % (infohash_str, self.metadata_type)
+
+        # copy file to the uploader's torrent_collecting_dir
+        src_dir_path = os.path.join(BASE_DIR, u"data", self.metadata_dir)
+        des_dir_path = os.path.join(self.session.get_torrent_collecting_dir(), self.metadata_dir)
+        print >> sys.stderr, u"Uploader's torrent_collect_dir = %s" % self.session.get_torrent_collecting_dir()
+        copytree(src_dir_path, des_dir_path)
+
+        from Tribler.Core.Session import Session
+
+        self.setUpPreSession()
+
+        self.config2 = self.config.copy()
+        self.config2.set_megacache(True)
+        self.config2.set_torrent_collecting(True)
+
+        self.session2_state_dir = self.session.get_state_dir() + "2"
+        self.session2_torrent_collecting_dir = self.session.get_torrent_collecting_dir() + "2"
+
+        self.config2.set_state_dir(self.session2_state_dir)
+        self.config2.set_torrent_collecting_dir(self.session2_torrent_collecting_dir)
+        self.session2 = Session(self.config2, ignore_singleton=True)
+        self.session2.start()
+        sleep(1)
+
+        print >> sys.stderr, u"Downloader's torrent_collect_dir = %s" % self.session2.get_torrent_collecting_dir()
+        print >> sys.stderr, u"Uploader port: %s, Downloader port: %s" % (self.session1_port, self.session2.get_dispersy_port())
