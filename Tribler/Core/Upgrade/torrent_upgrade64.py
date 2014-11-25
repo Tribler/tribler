f41830bc9749bccf444c2de72298ae99ee2cdeee
Elric Milon <whirm@gmx.com>
2014-11-25 14:40:00 +0100

    Split upgrade module in DB upgrader and collected files upgrader to ease testing/reading.

--- Tribler/Core/Upgrade/torrent_upgrade64.py
+++ Tribler/Core/Upgrade/torrent_upgrade64.py
@@ -28,12 +28,7 @@ class TorrentMigrator64(object):
         self._logger = logging.getLogger(self.__class__.__name__)
         self.session = session
         self.db = db
-        if status_update_func:
-            self.status_update_func = status_update_func
-        else:
-            def default_status_update(text):
-                pass
-            self.status_update_func = default_status_update
+        self.status_update_func = status_update_func if status_update_func else lambda _:None
 
         self.torrent_collecting_dir = self.session.get_torrent_collecting_dir()
 
@@ -67,159 +62,11 @@ class TorrentMigrator64(object):
                 os.unlink(useless_tmp_file)
 
         self._migrate_torrent_collecting_dir()
-        self._migrate_database()
 
         # remove the temporary file if exists
         if os.path.exists(self.tmp_migration_tcd_file):
             os.unlink(self.tmp_migration_tcd_file)
 
-    def _migrate_database(self):
-        """
-        Migrates the database to the new version.
-        """
-        self.status_update_func(u"Upgrading database from v%s to v%s..." % (22, 23))
-
-        self.db.execute(u"""
-DROP TABLE IF EXISTS BarterCast;
-DROP INDEX IF EXISTS bartercast_idx;
-
-DROP INDEX IF EXISTS Torrent_swift_torrent_hash_idx;
-""")
-
-        try:
-            next(self.db.execute(u"SELECT * From sqlite_master WHERE name == '_tmp_Torrent' and type == 'table';"))
-
-        except StopIteration:
-            # no _tmp_Torrent table, check if the current Torrent table is new
-            lines = [(0, u'torrent_id', u'integer', 1, None, 1),
-                     (1, u'infohash', u'text', 1, None, 0),
-                     (2, u'name', u'text', 0, None, 0),
-                     (3, u'torrent_file_name', u'text', 0, None, 0),
-                     (4, u'length', u'integer', 0, None, 0),
-                     (5, u'creation_date', u'integer', 0, None, 0),
-                     (6, u'num_files', u'integer', 0, None, 0),
-                     (7, u'thumbnail', u'integer', 0, None, 0),
-                     (8, u'insert_time', u'numeric', 0, None, 0),
-                     (9, u'secret', u'integer', 0, None, 0),
-                     (10, u'relevance', u'numeric', 0, u'0', 0),
-                     (11, u'source_id', u'integer', 0, None, 0),
-                     (12, u'category_id', u'integer', 0, None, 0),
-                     (13, u'status_id', u'integer', 0, u'0', 0),
-                     (14, u'num_seeders', u'integer', 0, None, 0),
-                     (15, u'num_leechers', u'integer', 0, None, 0),
-                     (16, u'comment', u'text', 0, None, 0),
-                     (17, u'dispersy_id', u'integer', 0, None, 0),
-                     (18, u'last_tracker_check', u'integer', 0, u'0', 0),
-                     (19, u'tracker_check_retries', u'integer', 0, u'0', 0),
-                     (20, u'next_tracker_check', u'integer', 0, u'0', 0)
-                     ]
-            i = 0
-            is_new = True
-            for line in self.db.execute(u"PRAGMA table_info(Torrent);"):
-                if line != lines[i]:
-                    is_new = False
-                    break
-                i += 1
-
-            if not is_new:
-                # create the temporary table
-                self.db.execute(u"""
-CREATE TABLE IF NOT EXISTS _tmp_Torrent (
-  torrent_id       integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-  infohash		   text NOT NULL,
-  name             text,
-  torrent_file_name text,
-  length           integer,
-  creation_date    integer,
-  num_files        integer,
-  thumbnail        integer,
-  insert_time      numeric,
-  secret           integer,
-  relevance        numeric DEFAULT 0,
-  source_id        integer,
-  category_id      integer,
-  status_id        integer DEFAULT 0,
-  num_seeders      integer,
-  num_leechers     integer,
-  comment          text,
-  dispersy_id      integer,
-  last_tracker_check    integer DEFAULT 0,
-  tracker_check_retries integer DEFAULT 0,
-  next_tracker_check    integer DEFAULT 0
-);
-""")
-
-                # migrate Torrent table
-                keys = (u"torrent_id", u"infohash", u"name", u"torrent_file_name", u"length", u"creation_date", u"num_files",
-                        u"thumbnail", u"insert_time", u"secret", u"relevance", u"source_id", u"category_id", u"status_id",
-                        u"num_seeders", u"num_leechers", u"comment", u"dispersy_id", u"last_tracker_check",
-                        u"tracker_check_retries", u"next_tracker_check")
-                keys_str = u", ".join(keys)
-                results = self.db.execute(u"SELECT %s FROM Torrent;" % keys_str)
-
-                keys_str = u", ".join(keys)
-                values_str = u"?," * len(keys)
-                insert_stmt = u"INSERT INTO _tmp_Torrent(%s) VALUES(%s)" % (keys_str, values_str[:-1])
-                current_count = 0
-                for torrent in results:
-                    torrent_id, infohash, name, torrent_file_name = torrent[:4]
-
-                    filepath = os.path.join(self.torrent_collecting_dir, hexlify(str2bin(infohash)) + u".torrent")
-
-                    # check if torrent matches
-                    torrent_file_name = None
-                    if os.path.exists(filepath):
-                        tdef = TorrentDef.load(filepath)
-                        if tdef.get_name_as_unicode() == name:
-                            torrent_file_name = filepath
-
-                    new_torrent = [torrent_id, infohash, name, torrent_file_name]
-                    for d in torrent[4:]:
-                        new_torrent.append(d)
-                    new_torrent = tuple(new_torrent)
-
-                    self.db.execute(insert_stmt, new_torrent)
-
-                    current_count += 1
-                    self.status_update_func(u"Upgrading database, %s records upgraded..." % current_count)
-
-                self.db.execute(u"""
-DROP TABLE IF EXISTS Torrent;
-ALTER TABLE _tmp_Torrent RENAME TO Torrent;
-""")
-
-        # cleanup metadata tables
-        self.db.execute(u"""
-DROP TABLE IF EXISTS MetadataMessage;
-DROP TABLE IF EXISTS MetadataData;
-
-CREATE TABLE IF NOT EXISTS MetadataMessage (
-  message_id             INTEGER PRIMARY KEY AUTOINCREMENT,
-  dispersy_id            INTEGER NOT NULL,
-  this_global_time       INTEGER NOT NULL,
-  this_mid               TEXT NOT NULL,
-  infohash               TEXT NOT NULL,
-  previous_mid           TEXT,
-  previous_global_time   INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS MetadataData (
-  message_id  INTEGER,
-  data_key    TEXT NOT NULL,
-  data_value  INTEGER,
-  FOREIGN KEY (message_id) REFERENCES MetadataMessage(message_id) ON DELETE CASCADE
-);
-""")
-
-        # cleanup all SearchCommunity and MetadataCommunity data in dispersy database
-        self._update_dispersy()
-
-        # update database version
-        self.db.write_version(23)
-
-        # remove the temporary file if exists
-        if os.path.exists(self.tmp_migration_tcd_file):
-            os.unlink(self.tmp_migration_tcd_file)
 
     def _migrate_torrent_collecting_dir(self):
         """
--- Tribler/Core/Upgrade/upgrade.py
+++ Tribler/Core/Upgrade/upgrade.py
@@ -1,9 +1,10 @@
 import logging
 import os
 
-from Tribler.dispersy.util import call_on_reactor_thread
 from Tribler.Core.CacheDB.db_versions import LOWEST_SUPPORTED_DB_VERSION, LATEST_DB_VERSION
-from Tribler.Core.Upgrade.upgrade64 import TorrentMigrator64
+from Tribler.Core.Upgrade.db_upgrader import DBUpgrader
+from Tribler.Core.Upgrade.torrent_upgrade64 import TorrentMigrator64
+from Tribler.dispersy.util import call_on_reactor_thread
 
 
 # Database versions:
@@ -12,14 +13,6 @@ from Tribler.Core.Upgrade.upgrade64 import TorrentMigrator64
 #   18   is used by Tribler 6.1.x - 6.2.0
 #   22   is used by Tribler 6.3.x
 #   23   is used by Tribler 6.4
-
-
-class VersionNoLongerSupportedError(Exception):
-    pass
-
-
-class DatabaseUpgradeError(Exception):
-    pass
 
 
 class TriblerUpgrader(object):
@@ -46,39 +39,15 @@ class TriblerUpgrader(object):
         else:
             # upgrade
             try:
-                # version 17 -> 18
-                if self.db.version == 17:
-                    self._upgrade_17_to_18()
+                torrent_migrator = TorrentMigrator64(self.session, self.db, status_update_func=self.update_status)
+                torrent_migrator.start_migrate()
 
-                # version 18 -> 22
-                if self.db.version == 18:
-                    self._upgrade_18_to_22()
+                db_migrator = DBUpgrader(self.session, self.db, status_update_func=self.update_status)
+                db_migrator.start_migrate()
 
-                # version 22 -> 23
-                if self.db.version == 22:
-                    # TODO(emilon): TorrentMigrator64 shouldn't upgrade the database
-                    # as it breaks the case where the DB version is older than 17.
-                    migrator = TorrentMigrator64(self.session, self.db, status_update_func=self.update_status)
-                    migrator.start_migrate()
-
-                # check if we managed to upgrade to the latest DB version.
-                if self.db.version == LATEST_DB_VERSION:
-                    self.current_status = u"Done."
-                    self.failed = False
-                else:
-                    if self.db.version < LOWEST_SUPPORTED_DB_VERSION:
-                        msg = u"Database is too old %s < %s" % (self.db.version, LOWEST_SUPPORTED_DB_VERSION)
-                        self.update_status(msg)
-                        raise VersionNoLongerSupportedError(msg)
-                    else:
-                        msg = u"Database upgrade failed: %s -> %s" % (self.db.version, LATEST_DB_VERSION)
-                        self.update_status(msg)
-                        raise DatabaseUpgradeError(msg)
-
+                self.failed = False
             except Exception as e:
-                self.failed = True
                 self._logger.error(u"failed to upgrade: %s", e)
-                raise
 
         if self.failed:
             self._stash_database_away()
@@ -92,72 +61,3 @@ class TriblerUpgrader(object):
         os.rename(old_dir, new_dir)
         os.makedirs(old_dir)
         self.db.initialize()
-
-    def _upgrade_17_to_18(self):
-        self.current_status = u"Upgrading database from v%s to v%s..." % (17, 18)
-
-        self.db.execute(u"""
-DROP TABLE IF EXISTS BarterCast;
-DROP INDEX IF EXISTS bartercast_idx;
-INSERT OR IGNORE INTO MetaDataTypes ('name') VALUES ('swift-thumbnails');
-INSERT OR IGNORE INTO MetaDataTypes ('name') VALUES ('video-info');
-""")
-        # update database version
-        self.db.write_version(18)
-
-    def _upgrade_18_to_22(self):
-        self.current_status = u"Upgrading database from v%s to v%s..." % (18, 22)
-
-        self.db.execute(u"""
-DROP INDEX IF EXISTS Torrent_swift_hash_idx;
-
-DROP VIEW IF EXISTS Friend;
-
-ALTER TABLE Peer RENAME TO __Peer_tmp;
-CREATE TABLE IF NOT EXISTS Peer (
-    peer_id    integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-    permid     text NOT NULL,
-    name       text,
-    thumbnail  text
-);
-
-INSERT INTO Peer (peer_id, permid, name, thumbnail) SELECT peer_id, permid, name, thumbnail FROM __Peer_tmp;
-
-DROP TABLE IF EXISTS __Peer_tmp;
-
-ALTER TABLE Torrent ADD COLUMN last_tracker_check integer DEFAULT 0;
-ALTER TABLE Torrent ADD COLUMN tracker_check_retries integer DEFAULT 0;
-ALTER TABLE Torrent ADD COLUMN next_tracker_check integer DEFAULT 0;
-
-CREATE TABLE IF NOT EXISTS TrackerInfo (
-  tracker_id  integer PRIMARY KEY AUTOINCREMENT,
-  tracker     text    UNIQUE NOT NULL,
-  last_check  numeric DEFAULT 0,
-  failures    integer DEFAULT 0,
-  is_alive    integer DEFAULT 1
-);
-
-CREATE TABLE IF NOT EXISTS TorrentTrackerMapping (
-  torrent_id  integer NOT NULL,
-  tracker_id  integer NOT NULL,
-  FOREIGN KEY (torrent_id) REFERENCES Torrent(torrent_id),
-  FOREIGN KEY (tracker_id) REFERENCES TrackerInfo(tracker_id),
-  PRIMARY KEY (torrent_id, tracker_id)
-);
-
-INSERT OR IGNORE INTO TrackerInfo (tracker) VALUES ('no-DHT');
-INSERT OR IGNORE INTO TrackerInfo (tracker) VALUES ('DHT');
-
-DROP INDEX IF EXISTS torrent_biterm_phrase_idx;
-DROP TABLE IF EXISTS TorrentBiTermPhrase;
-DROP INDEX IF EXISTS termfrequency_freq_idx;
-DROP TABLE IF EXISTS TermFrequency;
-DROP INDEX IF EXISTS Torrent_insert_idx;
-DROP INDEX IF EXISTS Torrent_info_roothash_idx;
-
-DROP TABLE IF EXISTS ClicklogSearch;
-DROP INDEX IF EXISTS idx_search_term;
-DROP INDEX IF EXISTS idx_search_torrent;
-""")
-        # update database version
-        self.db.write_version(22)
