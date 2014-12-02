a2554b1f05afc9b75dc8ebaab3b54618e91f0916
Egbert Bouman <ebouman@gmail.com>
2014-12-02 12:58:51 +0100

    Remove forward_from_to

--- Tribler/community/tunnel/routing.py
+++ Tribler/community/tunnel/routing.py
@@ -144,7 +144,7 @@ class RelayRoute(object):
     it is online or not
     """
 
-    def __init__(self, circuit_id, sock_addr):
+    def __init__(self, circuit_id, sock_addr, rendezvous_relay=False):
         """
         @type sock_addr: (str, int)
         @type circuit_id: int
@@ -157,6 +157,7 @@ class RelayRoute(object):
         self.creation_time = time.time()
         self.last_incoming = time.time()
         self.bytes_up = self.bytes_down = 0
+        self.rendezvous_relay = rendezvous_relay
 
 
 class IntroductionPoint(object):
