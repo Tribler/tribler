c0d92d14cfe2ba28822a0f8e557cc47a4030bd84
Lipu Fei <lipu.fei815@gmail.com>
2014-12-08 20:14:09 +0100

    change TFTP file not found message to a warning

--- Tribler/Core/TFTP/handler.py
+++ Tribler/Core/TFTP/handler.py
@@ -269,7 +269,7 @@ class TftpHandler(TaskManager):
                 file_data, file_size = self._load_file(file_name)
             checksum = b64encode(sha1(file_data).digest())
         except FileNotFound as e:
-            self._logger.error(u"[READ %s:%s] file/dir not found: %s", ip, port, e)
+            self._logger.warn(u"[READ %s:%s] file/dir not found: %s", ip, port, e)
             dummy_session = Session(False, packet['session_id'], (ip, port), packet['opcode'],
                                     file_name, None, None, None, block_size=block_size, timeout=timeout)
             self._handle_error(dummy_session, 1)
