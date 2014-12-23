abf3d07541814f8ed63beef613b18ad7b0277fac
Niels Zeilemaker <nielszeilemaker@godatadriven.com>
2014-12-23 08:47:54 +0100

    Not using custom DH implementation anymore, switched to DH included in M2Crypto/OpenSSL. Old version which includes custom DH renamed to OldTunnelCrypto.

--- Tribler/community/tunnel/crypto/cryptowrapper.py
+++ Tribler/community/tunnel/crypto/cryptowrapper.py
@@ -17,7 +17,8 @@ except ImportError:
     raise RuntimeError("Cannot continue without pycrypto")
 
 try:
-    from M2Crypto import EVP
+    from M2Crypto import EVP, DH
+    from M2Crypto.m2 import dec_to_bn, bn_to_mpi, mpi_to_bn, bn_to_hex, bin_to_bn
 
     def aes_encrypt_str(aes_key, plain_str):
         if isinstance(aes_key, long):
@@ -32,6 +33,23 @@ try:
         cipher = EVP.Cipher(alg='aes_128_ecb', key=aes_key, iv='\x00' * 16, op=0)
         ret = cipher.update(encr_str)
         return ret + cipher.final()
+
+    def mpi_to_dec(mpi):
+        bn = mpi_to_bn(mpi)
+        hexval = bn_to_hex(bn)
+        dec = int(hexval, 16)
+        return dec
+
+    def dec_to_mpi(dec):
+        bn = dec_to_bn('%s' % dec)
+        mpi = bn_to_mpi(bn)
+        return mpi
+
+    def bin_to_dec(binval):
+        bn = bin_to_bn(binval)
+        hexval = bn_to_hex(bn)
+        dec = int(hexval, 16)
+        return dec
 
 except ImportError as e:
     raise RuntimeError("Cannot continue without M2Crypto" + str(e))
