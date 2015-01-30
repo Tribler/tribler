afff729387f5f2c9883a87499dd368b5e0d0f10c
Lipu Fei <lipu.fei815@gmail.com>
2015-01-30 15:33:48 +0100

    show an error dialog when add magnet download fails

--- Tribler/Main/vwxGUI/MainFrame.py
+++ Tribler/Main/vwxGUI/MainFrame.py
@@ -418,8 +418,15 @@ class MainFrame(wx.Frame):
         name, infohash, _ = parse_magnetlink(url)
         if name is None:
             name = ""
-        tdef = TorrentDefNoMetainfo(infohash, name, url=url)
-        wx.CallAfter(self.startDownload, tdef=tdef, cmdline=cmdline, destdir=destdir, selectedFiles=selectedFiles, vodmode=vodmode, hops=0)
+        try:
+            tdef = TorrentDefNoMetainfo(infohash, name, url=url)
+            wx.CallAfter(self.startDownload, tdef=tdef, cmdline=cmdline, destdir=destdir, selectedFiles=selectedFiles, vodmode=vodmode, hops=0)
+        except Exception, e:
+            # show an error dialog
+            dlg = wx.MessageBox(self, "Failed to add magnet download: %s" % str(e),
+                                "Failed to add magnet download", wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
         return True
 
     def startDownloadFromUrl(self, url, destdir=None, cmdline=False, selectedFiles=None, vodmode=False, hops=0):
