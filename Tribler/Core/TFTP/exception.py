c162173363e290613b29a5eb1f34592581ad929f
Elric Milon <whirm@gmx.com>
2015-02-10 18:23:56 +0100

    autopep8 Core/TFTP

--- Tribler/Core/TFTP/exception.py
+++ Tribler/Core/TFTP/exception.py
@@ -1,22 +1,26 @@
 class InvalidPacketException(Exception):
+
     """ Indicates an invalid packet.
     """
     pass
 
 
 class InvalidStringException(Exception):
+
     """ Indicates an invalid zero-terminated string.
     """
     pass
 
 
 class InvalidOptionException(Exception):
+
     """ Indicates an invalid option.
     """
     pass
 
 
 class FileNotFound(OSError):
+
     """ Indicates that a file is not found.
     """
     pass
