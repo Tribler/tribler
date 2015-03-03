5178784fcad3501f721fe677a80a050d439a9bf6
Lipu Fei <lipu.fei815@gmail.com>
2015-03-03 21:38:20 +0100

    remove parse_playtime_to_secs from miscutils

--- Tribler/Core/APIImplementation/miscutils.py
+++ Tribler/Core/APIImplementation/miscutils.py
@@ -1,26 +1,6 @@
 # Written by Arno Bakker
 # see LICENSE.txt for license information
 
-import re
-
-
-def parse_playtime_to_secs(hhmmss):
-    r = re.compile("([0-9\.]+):*")
-    occ = r.findall(hhmmss)
-    t = None
-    if len(occ) > 0:
-        if len(occ) == 3:
-            # hours as well
-            t = int(occ[0]) * 3600 + int(occ[1]) * 60 + float(occ[2])
-        elif len(occ) == 2:
-            # minutes and seconds
-            t = int(occ[0]) * 60 + float(occ[1])
-        elif len(occ) == 1:
-            # seconds
-            t = float(occ[0])
-    # Arno, 2010-07-05: Bencode doesn't support floats
-    return int(t)
-
 
 def offset2piece(offset, piecesize, endpoint=True):
     p = offset / piecesize
