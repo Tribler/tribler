48dd70cb0e4a079c83298dee7471dcda5d67ba94
Lipu Fei <lipu.fei815@gmail.com>
2015-03-23 11:13:22 +0100

    fix category capital letter problem

--- Tribler/Main/vwxGUI/SearchGridManager.py
+++ Tribler/Main/vwxGUI/SearchGridManager.py
@@ -1352,14 +1352,14 @@ class ChannelManager(object):
         return self.channelcast_db.getSubscribersCount(channel.id)
 
     def _applyFF(self, hits):
-        enabled_category_keys = [key.lower() for key, _ in self.category.getCategoryNames()] + ['other']
+        enabled_category_keys = [key for key, _ in self.category.getCategoryNames()]
 
         def torrentFilter(torrent):
             okCategory = False
 
             category = torrent.get("category", u"unknown")
 
-            if category.lower() in enabled_category_keys:
+            if category in enabled_category_keys:
                 okCategory = True
 
             if not okCategory:
