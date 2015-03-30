0337bff33b684b23e78ca5d5368e0071c543bf8a
Egbert Bouman <ebouman@gmail.com>
2015-03-30 19:13:12 +0200

    Don't count bytes of create messages twice

--- Tribler/community/tunnel/tunnel_community.py
+++ Tribler/community/tunnel/tunnel_community.py
@@ -445,7 +445,6 @@ class TunnelCommunity(Community):
                                                                                   circuit.unverified_hop.node_public_key,
                                                                                   circuit.unverified_hop.dh_first_part)))
 
-        _barter_statistics.dict_inc_bartercast(BartercastStatisticTypes.TUNNELS_BYTES_SENT, circuit.mid)
         return True
 
     def readd_bittorrent_peers(self):
