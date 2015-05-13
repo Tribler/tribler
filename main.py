a39bd78470462c739f8d84e153dd2b2acff9e9be
Harapuia <mark.vanbeusekom@gmail.com>
2015-05-13 15:38:22 +0200

    Start of file access and nfc

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.lang import Builder
 import android
+import os
 
 
 from jnius import autoclass, cast
@@ -15,6 +16,7 @@ mContext = autoclass('android.content.Context')
 PythonActivity = autoclass('org.renpy.android.PythonActivity')
 activity = PythonActivity.mActivity
 Intent = autoclass('android.content.Intent')
+mEnvironment = autoclass('android.os.Environment')
   
 Builder.load_file('main.kv')
 
@@ -25,7 +27,12 @@ class HomeScreen(Screen):
 		self.ids.button1.text = self.ids.button1.text+"!"
 	def AndroidTest(self):
 		vibrator = activity.getSystemService(mContext.VIBRATOR_SERVICE)
-		vibrator.vibrate(10000)
+		#vibrator.vibrate(10000)
+		if 'ANDROID_ROOT' in os.environ:
+			vibrator.vibrate(10000)	
+		else:
+			print 'not android?'
+			print os.environ
 
 class CameraScreen(Screen):
 	mMediaStore = autoclass('android.provider.MediaStore')
@@ -34,12 +41,17 @@ class CameraScreen(Screen):
 		intention = Intent(self.mMediaStore.ACTION_VIDEO_CAPTURE)
 		self.con = cast(mContext, PythonActivity.mActivity)			
 		intention.resolveActivity(self.con.getPackageManager())	
-		if intention.resolveActivity(self.con.getPackageManager()) != None:
+		if intention.resolveActivity( self.con.getPackageManager()) != None:
 			activity.startActivityForResult(intention,1)
 	#def on_resume(self):
 	#	root.manager.current='home'
 class NfcScreen(Screen):
 	mNfcAdapter = autoclass('android.nfc.NfcAdapter')
+	#mIO = autoclass('java.io')
+	mFile = autoclass('java.io.File')
+	def printDir(self):	
+		DCIMdir = mEnvironment.getExternalStoragePublicDirectory(mEnvironment.DIRECTORY_DCIM)
+		print DCIMdir.list()
 
 
 sm = ScreenManager()
