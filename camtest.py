212bc355ed7347ce362510287321b8a3446cccb3
NCHerckenrath <n.c.herckenrath@student.tudelft.nl>
2015-06-08 14:31:16 +0200

    Added comments to camtest.py.

--- camtest.py
+++ camtest.py
@@ -26,6 +26,7 @@ LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
 ImageFormat = autoclass('android.graphics.ImageFormat')
 LinearLayout = autoclass('android.widget.LinearLayout')
 Integer = autoclass('java.lang.Integer')
+Double = autoclass('java.lang.Double')
 
 from collections import namedtuple
 from kivy.app import App
@@ -37,38 +38,48 @@ from kivy.core.window import Window
 
 import sys
 
+#Implementation of the PreviewCallback Interface
+#Saves a Callback function to which it passes along it's input once the class is called
 class PreviewCallback(PythonJavaClass):
 	__javainterfaces__ = ['android.hardware.Camera$PreviewCallback']
 	__javacontext__ = 'app'
 
+	#Intialize
 	def __init__(self, callback):
 		super(PreviewCallback, self).__init__()
 		self.callback = callback
-	
-	@java_method('(BLandroid/hardware/Camera;):V')
+
+	#Function that passes along the data and camera to the actual callback function
+	@java_method('(BLandroid/hardware/Camera;)V')
 	def onPreviewFrame(self, data, camera):
 		self.callback(camera, data)
 
+#Implementation of the SurfaceHolderCallback Interface
 class SurfaceHolderCallback(PythonJavaClass):
 	__javainterfaces__ = ['android.view.SurfaceHolder$Callback']
 	__javacontext__ = 'app'
 
+	#Initialize
 	def __init__(self, callback):
 		super(SurfaceHolderCallback, self).__init__()
 		self.callback = callback
 
+	#Function that passes the variables to the actual callback function
 	@java_method('(Landroid/view/SurfaceHolder;III)V')
 	def surfaceChanged(self, surface, frmat, width, height):
 		self.callback(frmat, width, height)
 
+	#Handled internally
 	@java_method('(Landroid/view/SurfaceHolder;)V')
 	def surfaceCreated(self, surface):
 		pass
 
+	#Handled internally
 	@java_method('(Landroid/view/SurfaceHolder;)V')
 	def surfaceDestroyed(self, surface):
 		pass
 
+#Widget that holds the SurfaceView created for the Camera Preview
 class AndroidWidgetHolder(Widget):
 	view = ObjectProperty(allownone = True)
 
@@ -78,15 +89,19 @@ class AndroidWidgetHolder(Widget):
 		kwargs['size_hint'] = (None, None)
 		super(AndroidWidgetHolder, self).__init__(**kwargs)
 
+	#Function that is called once the view is being shown by the App
 	def on_view(self, instance, view):
+		#Remove the previous View
 		if self.oldView is not None:
 			layout = cast(LinearLayout, self.oldView.getParent())
 			layout.removeView(self.oldView)
 			self.oldView = None
 
+		#Exit is there is no view
 		if view is None:
 			return
 
+		#Adjust and display the new View, then set is as the old View
 		activity = PythonActivity.mActivity
 		activity.addContentView(view, LayoutParams(*self.size))
 		view.setZOrderOnTop(True)
@@ -94,6 +109,7 @@ class AndroidWidgetHolder(Widget):
 		view.setY(self.window.height - self.y - self.height)
 		self.oldView = view
 
+	#Function that sets the sizes of the View
 	def on_size(self, instance, size):
 		if self.view:
 			params = self.view.getLayoutParams()
@@ -110,6 +126,7 @@ class AndroidWidgetHolder(Widget):
 		if self.view:
 			self.view.setY(y)
 
+#Class that creates and manages the Camera
 class CamTestCamera(Widget):
 	__events__ = ['on_preview_frame']
 
@@ -122,11 +139,14 @@ class CamTestCamera(Widget):
 		self.holder = AndroidWidgetHolder(size = self.size, pos = self.pos)
 		self.add_widget(self.holder)
 
+	#Stops the recording and then closes the MediaRecorder, Camera and View
 	@run_on_ui_thread
 	def stop(self):
+		#Check if the Camera and MediaRecorder exists and if the App is recording
 		if self.vCamera is None or self.vRecorder is None or not self.recording:
 			return
 
+		#Stop the MediaRecorder and close it. Lock camera so that it can be modified.
 		self.vRecorder.stop()
 		self.vRecorder.reset()
 		self.vRecorder.release()
@@ -134,50 +154,59 @@ class CamTestCamera(Widget):
 		self.recording = False
 		self.vCamera.lock()
 
+		#Remove previewsurface and close the Camera and View
 		self.vCamera.setPreviewCallback(None)
 		self.vCamera.release()
 		self.vCamera = None
 		self.holder.view = None
 
+	#Start the Camera and the SurfaceView
 	@run_on_ui_thread
 	def start(self):
+		#Checks if there is no Camera running
 		if self.vCamera is not None:
 			return
-
+		#Opens the rear camera (Int value 0) and sets the proper rotation
 		self.vCamera = Camera.open(0)
 		self.rotation = PythonActivity.mActivity.getWindowManager().getDefaultDisplay().getRotation()
 		self.vCamera.setDisplayOrientation(self.rotationDictionary(self.rotation))
 
+		#Creates a 'fake' SurfaceView so that the Callback functions can be set
 		self.surfView = SurfaceView(PythonActivity.mActivity)
 		surfHolder = self.surfView.getHolder()
 
+		#Sets the SurfaceHolderCallback to _on_surface_changed
 		self.surfHolderCallback = SurfaceHolderCallback(self._on_surface_changed)
 		surfHolder.addCallback(self.surfHolderCallback)
 
+		#Attaches the View to the AndroidWidgetHolder
 		self.holder.view = self.surfView
 
+	#Override function for the SurfaceHolderCallback
 	def _on_surface_changed(self, frmt, width, height):
+		#Sets the proper width and height for the preview
 		params = self.vCamera.getParameters()
-		wantedSize = params.getSupportedPreviewSizes().toArray()
-		params.setPreviewSize(wantedSize[0].width, wantedSize[0].height)
+		wantedSize = self.getOptimalPreviewSize(params.getSupportedPreviewSizes(), width, height)
+		params.setPreviewSize(wantedSize.width, wantedSize.height)
 		self.vCamera.setParameters(params)
 
-		# now that we know the camera size, we'll create 2 buffers for faster
-		# result (using Callback buffer approach, as described in Camera android
-		# documentation)
-		# it also reduce the GC collection
+		#Now that we know the camera size, we'll create 2 buffers for faster
+		#result (using Callback buffer approach, as described in Camera android
+		#documentation)
+		#It also reduce the GC collection
 		bpp = ImageFormat.getBitsPerPixel(params.getPreviewFormat()) / 8.
 		buf = '\x00' * int(width * height * bpp)
 		self.vCamera.addCallbackBuffer(buf)
 		self.vCamera.addCallbackBuffer(buf)
 
-		# create a PreviewCallback to get back the onPreviewFrame into python
+		#Create a PreviewCallback to get back the onPreviewFrame into python
 		self.previewCallback = PreviewCallback(self._on_preview_frame)
 
-		# connect everything and start the preview
+		#Connect the Buffer and the PreviewDisplay to the Camera
 		self.vCamera.setPreviewCallbackWithBuffer(self.previewCallback)
 		self.vCamera.setPreviewDisplay(self.surfView.getHolder())
 		
+		#Start the MediaRecorder
 		if self.prepareRecorder():
 			self.vRecorder.start()
 			self.recording = True
@@ -197,36 +226,38 @@ class CamTestCamera(Widget):
 		if self.holder:
 			self.holder.pos = pos
 
+	#Function to generate an output path for a new Video
 	def getOutputMediaFile(self):
+		#Checks if the Storage is mounted
 		if not (Environment.getExternalStorageState()).lower() == (Environment.MEDIA_MOUNTED).lower():
 			return None
 
-#		mediaStorageDir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), 'SkeletonCam')
+		#Gets file folder and creates it, if necessary
 		mediaStorageDir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), 'Camera')
 		if not mediaStorageDir.exists():
 			mediaStorageDir.mkdirs()
 
+		#Create file name using a timestamp and standard file indentifiers
 		timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-
 		mediaFile = File(mediaStorageDir.getPath() + File.separator + 'VID_' + timeStamp + '.mp4')
-
-		for x in range(0,5):
-			print 'File location = ' + mediaFile.toString()
 
 		return mediaFile
 
+	#Function to set up the MediaRecorder
 	def prepareRecorder(self):
 		self.vRecorder = MediaRecorder()
 
 		self.vCamera.unlock()
 		self.vRecorder.setCamera(self.vCamera)
 
+		#Sets the proper arguments for the MediaRecorder
 		self.vRecorder.setAudioSource(AudioSource.CAMCORDER)
 		self.vRecorder.setVideoSource(VideoSource.CAMERA)
 		self.vRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH))
 		self.vRecorder.setOutputFile(self.getOutputMediaFile().toString())
 		self.vRecorder.setPreviewDisplay(self.surfView.getHolder().getSurface())
 
+		#Tries to connect the MediaRecorder, throws exception if it fails
 		try:
 			self.vRecorder.prepare()
 		except Exception as ex:
@@ -237,6 +268,7 @@ class CamTestCamera(Widget):
 
 		return True
 
+	#Function that obtains the Screen rotation
 	def rotationDictionary(self, rotation):
 		degrees = {Surface.ROTATION_0 : 0, Surface.ROTATION_90 : 90, Surface.ROTATION_180 : 180, Surface.ROTATION_270 : 270}[rotation]
 		info = CameraInfo()
@@ -246,10 +278,12 @@ class CamTestCamera(Widget):
 
 		return result
 
+	#Function that returns the optimal preview screen resolution, based on the preview screen size
 	def getOptimalPreviewSize(self, sizes, width, height):
 		ASPECT_TOLERANCE = 0.1
 		targetRatio =  1.0 * width / height
 
+		#Stop if the Camera doesn't support preview sizes
 		if sizes is None:
 			return None
 
@@ -258,6 +292,8 @@ class CamTestCamera(Widget):
 		minDiff = sys.float_info.max
 		targetHeight = height
 
+		#Check if one of the supported preview sizes has the same resolution as the preview screen
+		#If one or more do, it picks the preview size whose height fits best within the preview screen
 		for size in sizes.toArray():
 			ratio = 1.0 * size.width / size.height
 
@@ -267,6 +303,7 @@ class CamTestCamera(Widget):
 				optimalSize = size
 				minDiff = abs(size.height - targetHeight)
 
+		#If none of the preview sizes has a matching resolution, it returns the preview size whose height fits best
 		if optimalSize is None:
 			minDiff = sys.float_info.max
 
