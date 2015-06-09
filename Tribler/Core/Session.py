66ca425640a24abcb8c6ed5d325f6d65572d303f
Lipu Fei <lipu.fei815@gmail.com>
2015-06-09 12:55:58 +0200

    remove get_externally_reachable() from Session

--- Tribler/Core/Session.py
+++ Tribler/Core/Session.py
@@ -308,20 +308,6 @@ class Session(SessionConfigInterface):
         @return A string. """
         return self.lm.get_external_ip()
 
-    def get_externally_reachable(self):
-        """ Returns whether the Session is externally reachable, i.e., its
-          listen port is not firewalled. Use add_observer() with NTFY_REACHABLE
-          to register to the event of detecting reachablility. Note that due to
-          the use of UPnP a Session may become reachable some time after
-          startup and due to the Dialback mechanism, this method may return
-          False while the Session is actually already reachable. Note that True
-          doesn't mean the Session is reachable from the open Internet, could just
-          be from the local (otherwise firewalled) LAN.
-          @return A boolean. """
-
-        # Arno, LICHT: make it throw exception when used in LITE versie.
-        raise NotYetImplementedException()
-
     def get_current_startup_config_copy(self):
         """ Returns a SessionStartupConfig that is a copy of the current runtime
         SessionConfig.
