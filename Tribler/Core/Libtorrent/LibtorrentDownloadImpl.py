18b02d41f3f1551c516f5d26c628f7ba2bee2485
Elric Milon <whirm@gmx.com>
2015-07-03 16:10:42 +0200

    Don't try to add a task if the threadpool is gone.
    Closes #1525

--- Tribler/Core/Libtorrent/LibtorrentDownloadImpl.py
+++ Tribler/Core/Libtorrent/LibtorrentDownloadImpl.py
@@ -834,7 +834,7 @@ class LibtorrentDownloadImpl(DownloadConfigInterface):
                     # runs on the reactor
                     def session_getstate_usercallback_target():
                         when, getpeerlist = usercallback(ds)
-                        if when > 0.0:
+                        if when > 0.0 and self.session.lm.threadpool:
                             # Schedule next invocation, either on general or DL specific
                             self.session.lm.threadpool.add_task(lambda: self.network_get_state(usercallback, getpeerlist), when)
 
