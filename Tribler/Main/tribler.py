d04080daeddf711ecb24b67fd8ce0f7b73555173
Lipu Fei <lipu.fei815@gmail.com>
2015-08-07 18:30:20 +0200

    some minor changes

--- Tribler/Main/tribler.py
+++ Tribler/Main/tribler.py
@@ -18,14 +18,13 @@ logger = logging.getLogger(__name__)
 from Tribler.Core.Utilities.twisted_thread import reactor, stop_reactor
 
 # set wxpython version
+import wxversion
 try:
-    import wxversion
     # in the windows and mac distribution, there may be no version available.
     # so select a version only when there is any available.
     if wxversion.getInstalled():
         wxversion.select("2.8-unicode")
-except:
-    import wxversion
+except wxversion.VersionError:
     logger.exception("Unable to use wxversion installed wxversions: %s", repr(wxversion.getInstalled()))
 
 
