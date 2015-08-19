6c1b873c17d2121a93382c1cc42e38d605ed98fb
Lipu Fei <lipu.fei815@gmail.com>
2015-08-19 15:05:11 +0200

    merge network_calc_prebuf_frac and _consec

--- Tribler/Core/Libtorrent/LibtorrentDownloadImpl.py
+++ Tribler/Core/Libtorrent/LibtorrentDownloadImpl.py
@@ -658,7 +658,7 @@ class LibtorrentDownloadImpl(DownloadConfigInterface):
         stats['stats'] = self.network_create_statistics_reponse()
         stats['time'] = self.network_calc_eta()
         stats['vod_prebuf_frac'] = self.network_calc_prebuf_frac()
-        stats['vod_prebuf_frac_consec'] = self.network_calc_prebuf_frac_consec()
+        stats['vod_prebuf_frac_consec'] = self.network_calc_prebuf_frac(consecutive=True)
         stats['vod'] = self.get_mode()
         stats['vod_playable'] = self.progress == 1.0 or (
             stats['vod_prebuf_frac'] == 1.0 and self.curspeeds[DOWNLOAD] > 0.0)
@@ -695,25 +695,15 @@ class LibtorrentDownloadImpl(DownloadConfigInterface):
         dlspeed = max(0.000001, self.curspeeds[DOWNLOAD])
         return bytestogof / dlspeed
 
-    def network_calc_prebuf_frac(self):
+    def network_calc_prebuf_frac(self, consecutive=False):
         if self.get_mode() == DLMODE_VOD and self.get_vod_fileindex() >= 0 and self.vod_seekpos is not None:
             if self.endbuffsize:
                 return self.get_byte_progress(
                     [(self.get_vod_fileindex(), self.vod_seekpos, self.vod_seekpos + self.prebuffsize),
-                     (self.get_vod_fileindex(), -self.endbuffsize - 1, -1)])
+                     (self.get_vod_fileindex(), -self.endbuffsize - 1, -1)], consecutive=consecutive)
             else:
-                return self.get_byte_progress([(self.get_vod_fileindex(), self.vod_seekpos, self.vod_seekpos + self.prebuffsize)])
-        else:
-            return 0.0
-
-    def network_calc_prebuf_frac_consec(self):
-        if self.get_mode() == DLMODE_VOD and self.get_vod_fileindex() >= 0 and self.vod_seekpos is not None:
-            if self.endbuffsize:
-                return self.get_byte_progress(
-                    [(self.get_vod_fileindex(), self.vod_seekpos, self.vod_seekpos + self.prebuffsize),
-                     (self.get_vod_fileindex(), -self.endbuffsize - 1, -1)], consecutive=True)
-            else:
-                return self.get_byte_progress([(self.get_vod_fileindex(), self.vod_seekpos, self.vod_seekpos + self.prebuffsize)], consecutive=True)
+                return self.get_byte_progress([(self.get_vod_fileindex(), self.vod_seekpos, self.vod_seekpos + self.prebuffsize)],
+                                              consecutive=consecutive)
         else:
             return 0.0
 
