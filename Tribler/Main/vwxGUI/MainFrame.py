6dcbee20b0639e7075c75c21d4902f9714951da7
Lipu Fei <lipu.fei815@gmail.com>
2015-09-07 15:09:54 +0200

    fix restart tribler on Windows

--- Tribler/Main/vwxGUI/MainFrame.py
+++ Tribler/Main/vwxGUI/MainFrame.py
@@ -652,7 +652,8 @@ class MainFrame(wx.Frame):
         elif sys.platform == "darwin":
             executable = "?"
 
-        executable = os.path.join(path, executable)
+        if sys.platform != "win32":
+            executable = os.path.join(path, executable)
         self._logger.info(repr(executable))
 
         def start_tribler():
