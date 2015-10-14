3496d2821e7c5ef95feae7bca52615ebb128cfd8
Elric Milon <whirm@gmx.com>
2015-10-14 18:50:44 +0200

    Obtain unicode version of sys.argv
    This is needed until Tribler gets migrated to Python3.

--- Tribler/Main/hacks.py
+++ Tribler/Main/hacks.py
@@ -69,5 +69,37 @@ def patch_crypto_be_discovery():
             be for be in (be_cc, be_ossl) if be is not None
         ]
 
+# Python2 sys.argv uses the non-unicode windows API, so non-ascii chars will be mangled.
+# This should be removed once we move to Python3
+# Based on code from:
+# https://stackoverflow.com/questions/846850/read-unicode-characters-from-command-line-arguments-in-python-2-x-on-windows
+def get_unicode_sys_argv():
+    """Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode
+    strings.
+
+    Versions 2.x of Python don't support Unicode in sys.argv on
+    Windows, with the underlying Windows API instead replacing multi-byte
+    characters with '?'.
+    """
+
+    from ctypes import POINTER, byref, cdll, c_int, windll
+    from ctypes.wintypes import LPCWSTR, LPWSTR
+
+    GetCommandLineW = cdll.kernel32.GetCommandLineW
+    GetCommandLineW.argtypes = []
+    GetCommandLineW.restype = LPCWSTR
+
+    CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+    CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+    CommandLineToArgvW.restype = POINTER(LPWSTR)
+
+    cmd = GetCommandLineW()
+    argc = c_int(0)
+    argv = CommandLineToArgvW(cmd, byref(argc))
+    if argc.value > 0:
+        # Remove Python executable and commands if present
+        start = argc.value - len(sys.argv)
+        return [argv[i] for i in xrange(start, argc.value)]
+
 #
 # hacks.py ends here
--- Tribler/Main/tribler_main.py
+++ Tribler/Main/tribler_main.py
@@ -1002,7 +1002,8 @@ def run(params=[""], autoload_discovery=True, use_torrent_search=True, use_chann
     patch_crypto_be_discovery()
 
     if len(sys.argv) > 1:
-        params = sys.argv[1:]
+        from .hacks import get_unicode_sys_argv
+        params = get_unicode_sys_argv()[1:]
     try:
         # Create single instance semaphore
         single_instance_checker = SingleInstanceChecker("tribler")
@@ -1036,6 +1037,7 @@ def run(params=[""], autoload_discovery=True, use_torrent_search=True, use_chann
             app = wx.GetApp()
             if not app:
                 app = TriblerApp(redirect=False)
+
             abc = ABCApp(params, installdir, autoload_discovery=autoload_discovery,
                          use_torrent_search=use_torrent_search, use_channel_search=use_channel_search)
             app.set_abcapp(abc)
