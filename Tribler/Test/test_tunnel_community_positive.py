3d6f48dd62248e97bbbd4d7abc993a16396047e3
Niels Zeilemaker <nielszeilemaker@godatadriven.com>
2015-11-19 20:49:34 +0100

    Cleanup of tunnel community code

--- Tribler/Test/test_tunnel_community_positive.py
+++ Tribler/Test/test_tunnel_community_positive.py
@@ -24,7 +24,8 @@ class TestTunnelCommunityPositive(TestTunnelBase):
             self.assert_(data == "42", "Data is not 42, it is '%s'" % data.encode("HEX"))
             self.assert_(destination == ("127.0.0.1", 12345), "Destination is not 127.0.0.1:12345, it is '%s:%d'" %
                          (destination[0], destination[1]))
-            community.tunnel_data_to_origin(circuit_id, sock_addr, ("127.0.0.1", 12345), "4242")
+
+            community.exit_sockets[circuit_id].tunnel_data(("127.0.0.1", 12345), "4242")
 
         def start_test(tunnel_communities):
             # assuming that the last tunnel community is that loaded by the tribler gui
