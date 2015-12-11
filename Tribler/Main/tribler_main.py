4c98cd192035591d7986bb1f870f61c5d599defb
lfdversluis <laurens-versluis@hotmail.com>
2015-12-11 14:43:08 +0100

    forgot one upgrade

--- Tribler/Main/tribler_main.py
+++ Tribler/Main/tribler_main.py
@@ -393,7 +393,7 @@ class ABCApp(object):
     def show_upgrade_dialog(self, session):
         assert wx.Thread_IsMain()
         
-        upgrader = TriblerUpgrader(session, session.sqlite_db)
+        upgrader = self.utility.session.upgrader
         upgrade_dialog = TriblerUpgradeDialog(self.gui_image_manager, upgrader)
         failed = upgrade_dialog.ShowModal()
         upgrade_dialog.Destroy()
