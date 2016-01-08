4897445fcef73fd1e10581813f43c179cc441b61
Martijn de Vos <martijn@code-up.nl>
2016-01-08 16:24:27 +0100

    Added 64 bit setup script and new makedistmac.sh file

---
+++ Tribler/Main/Build/Mac/setuptriblermac_64bit.py
@@ -0,0 +1,278 @@
+# ---------------
+# This script builds build/Tribler.app
+#
+# Meant to be called from mac/Makefile
+# ---------------
+
+import sys
+import os
+import logging
+from setuptools import setup
+from Tribler import LIBRARYNAME
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../dispersy/libnacl'))
+
+logger = logging.getLogger(__name__)
+
+# modules to include into bundle
+includeModules = ["encodings.hex_codec", "encodings.utf_8", "encodings.latin_1", "xml.sax", "email.iterators",
+                  "netifaces", "apsw", "libtorrent", "twisted", "M2Crypto", "pyasn1", "PIL", "feedparser",
+                  "urllib3", "requests", "leveldb", "cryptography", "libnacl", "pycparser", "six", "hashlib", "enum34",
+                  "csv", "cherrypy", "decorator",
+
+                  "cryptography",
+                  "cryptography._Cryptography_cffi_2a871178xb3816a41",
+                  "cryptography._Cryptography_cffi_684bb40axf342507b",
+                  "cryptography._Cryptography_cffi_8f86901cxc1767c5a",
+                  "cryptography._Cryptography_cffi_f3e4673fx399b1113",
+                  "cryptography.__about__",
+                  "cryptography.exceptions",
+                  "cryptography.fernet",
+                  "cryptography.utils",
+                  "cryptography.x509",
+
+                  "cryptography.hazmat",
+
+                  "cryptography.hazmat.primitives",
+                  "cryptography.hazmat.primitives.cmac",
+                  "cryptography.hazmat.primitives.constant_time",
+                  "cryptography.hazmat.primitives.hashes",
+                  "cryptography.hazmat.primitives.hmac",
+                  "cryptography.hazmat.primitives.padding",
+                  "cryptography.hazmat.primitives.serialization",
+
+                  "cryptography.hazmat.primitives.asymmetric",
+                  "cryptography.hazmat.primitives.asymmetric.dh",
+                  "cryptography.hazmat.primitives.asymmetric.dsa",
+                  "cryptography.hazmat.primitives.asymmetric.ec",
+                  "cryptography.hazmat.primitives.asymmetric.padding",
+                  "cryptography.hazmat.primitives.asymmetric.rsa",
+                  "cryptography.hazmat.primitives.asymmetric.utils",
+
+                  "cryptography.hazmat.primitives.ciphers",
+                  "cryptography.hazmat.primitives.ciphers.algorithms",
+                  "cryptography.hazmat.primitives.ciphers.base",
+                  "cryptography.hazmat.primitives.ciphers.modes",
+
+                  "cryptography.hazmat.primitives.interfaces",
+
+                  "cryptography.hazmat.primitives.kdf",
+                  "cryptography.hazmat.primitives.kdf.hkdf",
+                  "cryptography.hazmat.primitives.kdf.pbkdf2",
+
+                  "cryptography.hazmat.primitives.twofactor.htop",
+                  "cryptography.hazmat.primitives.twofactor.totp",
+
+                  "cryptography.hazmat.backends.commoncrypto",
+                  "cryptography.hazmat.backends.openssl",
+                  "cryptography.hazmat.bindings.commoncrypto",
+                  "cryptography.hazmat.bindings.commoncrypto.binding",
+                  "cryptography.hazmat.bindings.commoncrypto.cf",
+                  "cryptography.hazmat.bindings.commoncrypto.common_cryptor",
+                  "cryptography.hazmat.bindings.commoncrypto.common_digest",
+                  "cryptography.hazmat.bindings.commoncrypto.common_hmac",
+                  "cryptography.hazmat.bindings.commoncrypto.common_key_derivation",
+                  "cryptography.hazmat.bindings.commoncrypto.secimport",
+                  "cryptography.hazmat.bindings.commoncrypto.secitem",
+                  "cryptography.hazmat.bindings.commoncrypto.seckeychain",
+                  "cryptography.hazmat.bindings.commoncrypto.seckey",
+                  "cryptography.hazmat.bindings.commoncrypto.sectransform",
+                  "cryptography.hazmat.bindings.openssl",
+                  "cryptography.hazmat.bindings.openssl.binding",
+                  "cryptography.hazmat.bindings.openssl.aes",
+                  "cryptography.hazmat.bindings.openssl.asn1",
+                  "cryptography.hazmat.bindings.openssl.bignum",
+                  "cryptography.hazmat.bindings.openssl.bio",
+                  "cryptography.hazmat.bindings.openssl.cmac",
+                  "cryptography.hazmat.bindings.openssl.cms",
+                  "cryptography.hazmat.bindings.openssl.conf",
+                  "cryptography.hazmat.bindings.openssl.crypto",
+                  "cryptography.hazmat.bindings.openssl.dh",
+                  "cryptography.hazmat.bindings.openssl.dsa",
+                  "cryptography.hazmat.bindings.openssl.ecdh",
+                  "cryptography.hazmat.bindings.openssl.ecdsa",
+                  "cryptography.hazmat.bindings.openssl.ec",
+                  "cryptography.hazmat.bindings.openssl.engine",
+                  "cryptography.hazmat.bindings.openssl.err",
+                  "cryptography.hazmat.bindings.openssl.evp",
+                  "cryptography.hazmat.bindings.openssl.hmac",
+                  "cryptography.hazmat.bindings.openssl.nid",
+                  "cryptography.hazmat.bindings.openssl.objects",
+                  "cryptography.hazmat.bindings.openssl.opensslv",
+                  "cryptography.hazmat.bindings.openssl.osrandom_engine",
+                  "cryptography.hazmat.bindings.openssl.pem",
+                  "cryptography.hazmat.bindings.openssl.pkcs12",
+                  "cryptography.hazmat.bindings.openssl.pkcs7",
+                  "cryptography.hazmat.bindings.openssl.rand",
+                  "cryptography.hazmat.bindings.openssl.rsa",
+                  "cryptography.hazmat.bindings.openssl.ssl",
+                  "cryptography.hazmat.bindings.openssl.x509name",
+                  "cryptography.hazmat.bindings.openssl.x509",
+                  "cryptography.hazmat.bindings.openssl.x509v3",
+                  "cryptography.hazmat.bindings.openssl.x509_vfy",
+
+                  "pycparser",
+                  "pycparser._ast_gen",
+                  "pycparser._build_tables",
+                  "pycparser.ast_transforms",
+                  "pycparser.c_ast",
+                  "pycparser.c_generator",
+                  "pycparser.c_lexer",
+                  "pycparser.c_parser",
+                  "pycparser.lextab",
+                  "pycparser.plyparser",
+                  "pycparser.yacctab",
+
+                  "pycparser.ply",
+                  "pycparser.ply.cpp",
+                  "pycparser.ply.ctokens",
+                  "pycparser.ply.lex",
+                  "pycparser.ply.yacc",
+
+                  "cffi",
+                  "cffi.api",
+                  "cffi.backend_ctypes",
+                  "cffi.commontypes",
+                  "cffi.cparser",
+                  "cffi.ffiplatform",
+                  "cffi.gc_weakref",
+                  "cffi.lock",
+                  "cffi.model",
+                  "cffi.vengine_cpy",
+                  "cffi.vengine_gen",
+                  "cffi.verifier",
+
+                  "Tribler.Core.DecentralizedTracking.pymdht.core.routing_table",
+                  ]
+
+# gui panels to include
+includePanels = [
+    "list", "list_header", "list_body", "list_footer", "list_details",
+    "home", "settingsDialog", "TopSearchPanel", "SearchGridManager", "SRstatusbar"]
+    # ,"btn_DetailsHeader","tribler_List","TopSearchPanel","settingsOverviewPanel"] # TextButton
+
+
+includeModules += ["Tribler.Main.vwxGUI.%s" % x for x in includePanels]
+
+# ----- some basic checks
+
+if __debug__:
+    logger.warn(
+        "WARNING: Non optimised python bytecode (.pyc) will be produced. Run with -OO instead to produce and bundle .pyo files.")
+
+if sys.platform != "darwin":
+    logger.warn("WARNING: You do not seem to be running Mac OS/X.")
+
+# ----- import and verify wxPython
+
+"""
+import wxversion
+
+wxversion.select('2.8-unicode')
+"""
+import wx
+
+v = wx.__version__
+
+if v < "2.6":
+    logger.warn("WARNING: You need wxPython 2.6 or higher but are using %s.", v)
+
+if v < "2.8.4.2":
+    logger.warn("WARNING: wxPython before 2.8.4.2 could crash when loading non-present fonts. You are using %s.", v)
+
+# ----- import and verify M2Crypto
+
+import M2Crypto
+import M2Crypto.m2
+if "ec_init" not in M2Crypto.m2.__dict__:
+    logger.warn("WARNING: Could not import specialistic M2Crypto (imported %s)", M2Crypto.__file__)
+
+# ----- import Growl
+try:
+    import Growl
+
+    includeModules += ["Growl"]
+except:
+    logger.warn("WARNING: Not including Growl support.")
+
+
+# =================
+# build Tribler.app
+# =================
+
+from plistlib import Plist
+
+
+def includedir(srcpath, dstpath=None):
+    """ Recursive directory listing, filtering out svn files. """
+
+    total = []
+
+    cwd = os.getcwd()
+    os.chdir(srcpath)
+
+    if dstpath is None:
+        dstpath = srcpath
+
+    for root, dirs, files in os.walk("."):
+        if '.svn' in dirs:
+            dirs.remove('.svn')
+
+        for f in files:
+            total.append((root, f))
+
+    os.chdir(cwd)
+
+    # format: (targetdir,[file])
+    # so for us, (dstpath/filedir,[srcpath/filedir/filename])
+    return [("%s/%s" % (dstpath, root), ["%s/%s/%s" % (srcpath, root, f)]) for root, f in total]
+
+
+def filterincludes(l, f):
+    """ Return includes which pass filter f. """
+
+    return [(x, y) for (x, y) in l if f(y[0])]
+
+PYTHON_CRYPTOGRAPHY_PATH = "/Users/tribler/Workspace/install/python-libs/lib/python2.7/site-packages/cryptography-0.7.2-py2.7-macosx-10.6-intel.egg"
+
+# ----- build the app bundle
+mainfile = os.path.join(LIBRARYNAME, 'Main', 'tribler.py')
+setup(
+    setup_requires=['py2app'],
+    name='Tribler',
+    app=[mainfile],
+    options={'py2app': {
+        'argv_emulation': True,
+        'includes': includeModules,
+        'excludes': ["Tkinter", "Tkconstants", "tcl"],
+        'iconfile': LIBRARYNAME + '/Main/Build/Mac/tribler.icns',
+        'plist': Plist.fromFile(LIBRARYNAME + '/Main/Build/Mac/Info.plist'),
+        'optimize': 0 if __debug__ else 2,
+        'resources':
+            [(LIBRARYNAME + "/Category", [LIBRARYNAME + "/Category/category.conf"]),
+             (LIBRARYNAME + "/Core/DecentralizedTracking/pymdht/core",
+              [LIBRARYNAME + "/Core/DecentralizedTracking/pymdht/core/bootstrap_stable"]),
+             (LIBRARYNAME + "/Core/DecentralizedTracking/pymdht/core",
+              [LIBRARYNAME + "/Core/DecentralizedTracking/pymdht/core/bootstrap_unstable"]),
+             LIBRARYNAME + "/readme.txt",
+             LIBRARYNAME + "/Main/Build/Mac/TriblerDoc.icns",
+             ]
+            + ["/Users/tribler/Workspace_new/install/lib/libsodium.dylib"]
+
+            # add images
+            + includedir(LIBRARYNAME + "/Main/vwxGUI/images")
+            + includedir(LIBRARYNAME + "/Main/webUI/static")
+
+            # add GUI elements
+            + filterincludes(includedir(LIBRARYNAME + "/Main/vwxGUI"), lambda x: x.endswith(".xrc"))
+
+            # add crawler info and SQL statements
+            + filterincludes(includedir(LIBRARYNAME + "/"), lambda x: x.endswith(".sql"))
+
+            # add VLC plugins
+            + includedir("vlc")
+
+            # add ffmpeg binary
+            + [("vlc", ["vlc/ffmpeg"])],
+    }}
+)
---
+++ mac/makedistmac_64bit.sh
@@ -0,0 +1,131 @@
+#!/bin/sh -xe
+#
+# Written by Riccardo Petrocco, Arno Bakker
+# see LICENSE.txt for license information
+#
+# Script to build Tribler on Mac
+#
+# Based on original Makefile by JD Mol.
+
+APPNAME=Tribler
+if [ -e .TriblerVersion ]; then
+    DMGNAME="Tribler-$(cat .TriblerVersion)"
+fi
+
+export LIBRARYNAME=Tribler
+
+PYTHON_VERSION=2.7
+PYTHON="arch -i386 /Library/Frameworks/Python.framework/Versions/$PYTHON_VERSION/bin/python$PYTHON_VERSION"
+
+# ----- Set python paths TODO dynamic checkout
+
+# Niels 2012-01-03 removed py2app, installed 0.6.5 in python 2.7 site-packages. py2app 0.6.5 fixes actual -O running of Tribler
+# Arno 2013-02-12: Added libtorrent
+PYTHONPATH="$PWD:$PYTHONPATH"
+PYTHONPATH="$HOME/Workspace_new/install/lib/python2.7/site-packages:$PYTHONPATH"
+export PYTHONPATH
+
+# Force 32-bit:
+export VERSIONER_PYTHON_PREFER_32_BIT=yes
+
+# ----- Clean up
+/bin/rm -rf dist build
+
+# ----- Build
+${PYTHON} -OO - < ${LIBRARYNAME}/Main/Build/Mac/setuptriblermac.py py2app
+
+mkdir -p dist/installdir
+mv dist/$APPNAME.app dist/installdir
+
+# From original Makefile
+# Background
+mkdir -p dist/installdir/.background
+cp $LIBRARYNAME/Main/Build/Mac/background.png dist/installdir/.background
+
+# Volume Icon
+cp $LIBRARYNAME/Main/Build/Mac/VolumeIcon.icns dist/installdir/.VolumeIcon.icns
+
+# Shortcut to /Applications
+ln -s /Applications dist/installdir/Applications
+
+touch dist/installdir
+
+#Copy logger.conf
+cp logger.conf dist/installdir/Tribler.app/Contents/Resources/
+
+# Copy family filter
+cp Tribler/Category/category.conf dist/installdir/Tribler.app/Contents/Resources/Tribler/Category/
+cp Tribler/Category/filter_terms.filter dist/installdir/Tribler.app/Contents/Resources/Tribler/Category/
+
+# Forced 32-bit mode
+mv dist/installdir/Tribler.app dist/installdir/Tribler-origin.app
+ditto --rsrc --arch i386 dist/installdir/Tribler-origin.app dist/installdir/Tribler.app
+rm -rf dist/installdir/Tribler-origin.app
+
+mkdir -p dist/temp
+
+# create image
+hdiutil create -srcfolder dist/installdir -format UDRW -scrub -volname ${APPNAME} dist/$APPNAME.dmg
+
+# open it
+hdiutil attach -readwrite -noverify -noautoopen dist/$APPNAME.dmg -mountpoint dist/temp/mnt
+
+# make sure root folder is opened when image is
+bless --folder dist/temp/mnt --openfolder dist/temp/mnt
+# hack: wait for completion
+sleep 1
+
+# Arno, 2011-05-15: Snow Leopard gives diff behaviour, so set initial 1000 bounds to normal size
+# and added close/open after set position, following
+# http://stackoverflow.com/questions/96882/how-do-i-create-a-nice-looking-dmg-for-mac-os-x-using-command-line-tools
+
+# position items
+# oddly enough, 'set f .. as alias' can fail, but a reboot fixes that
+osascript -e "tell application \"Finder\"" \
+-e "   set f to POSIX file (\"${PWD}/dist/temp/mnt\" as string) as alias" \
+-e "   tell folder f" \
+-e "       open" \
+-e "       tell container window" \
+-e "          set toolbar visible to false" \
+-e "          set statusbar visible to false" \
+-e "          set current view to icon view" \
+-e "          delay 1 -- Sync" \
+-e "          set the bounds to {50, 100, 600, 400} -- Big size so the finder won't do silly things" \
+-e "       end tell" \
+-e "       delay 1 -- Sync" \
+-e "       set icon size of the icon view options of container window to 128" \
+-e "       set arrangement of the icon view options of container window to not arranged" \
+-e "       set background picture of the icon view options of container window to file \".background:background.png\"" \
+-e "       set position of item \"${APPNAME}.app\" to {150, 140}" \
+-e "       set position of item \"Applications\" to {410, 140}" \
+-e "       set the bounds of the container window to {50, 100, 600, 400}" \
+-e "       close" \
+-e "       open" \
+-e "       update without registering applications" \
+-e "       delay 5 -- Sync" \
+-e "       close" \
+-e "   end tell" \
+-e "   -- Sync" \
+-e "   delay 5" \
+-e "end tell" || true
+
+# turn on custom volume icon
+/Developer/Tools/SetFile -a C dist/temp/mnt || true
+
+# close
+hdiutil detach dist/temp/mnt || true
+
+# make read-only
+mv dist/$APPNAME.dmg dist/temp/rw.dmg
+hdiutil convert dist/temp/rw.dmg -format UDZO -imagekey zlib-level=9 -o dist/$APPNAME.dmg
+rm -f dist/temp/rw.dmg
+
+# add EULA
+hdiutil unflatten dist/$APPNAME.dmg
+DeRez -useDF $LIBRARYNAME/Main/Build/Mac/SLAResources.rsrc > dist/temp/sla.r
+Rez -a dist/temp/sla.r -o dist/$APPNAME.dmg
+hdiutil flatten dist/$APPNAME.dmg
+
+if [ ! -z "$DMGNAME" ]; then
+    mv dist/$APPNAME.dmg dist/$DMGNAME.dmg
+fi
