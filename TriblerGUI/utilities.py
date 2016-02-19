0313b128c3365f1988edd5f35d8d73d11c5cde5e
Martijn de Vos <martijn@code-up.nl>
2016-02-19 22:19:59 +0100

    Implemented the HTTP event loop and free space signal

---
+++ TriblerGUI/utilities.py
@@ -0,0 +1,6 @@
+def format_size(num, suffix='B'):
+    for unit in ['','K','M','Gi','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
