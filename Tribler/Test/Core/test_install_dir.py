082c70d24ecd8d1d4fb78be77a8ec1e55e034054
Martijn de Vos <martijn@code-up.nl>
2016-02-20 20:11:23 +0100

    Added test to determine installation directory

---
+++ Tribler/Test/Core/test_install_dir.py
@@ -0,0 +1,13 @@
+import os
+from Tribler.Core.Utilities.install_dir import determine_install_dir
+from Tribler.Test.Core.base_test import TriblerCoreTest
+
+
+class TriblerCoreTestInstallDir(TriblerCoreTest):
+
+    def test_install_dir(self):
+        install_dir = determine_install_dir()
+        print install_dir
+        self.assertIsInstance(install_dir, str)
+        self.assertTrue(os.path.isdir(install_dir))
+        self.assertTrue(os.path.exists(os.path.join(install_dir, 'Tribler')))
