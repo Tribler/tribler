4f1f0f0a574a86af90af2017d7ba9a08dbdb764e
Martijn de Vos <martijn@code-up.nl>
2016-02-21 14:40:59 +0100

    Added tests for torrent utilities
    Improved test_torrent_utils

--- Tribler/Test/Core/test_torrent_utils.py
+++ Tribler/Test/Core/test_torrent_utils.py
@@ -7,14 +7,21 @@ class TriblerCoreTestTorrentUtils(TriblerCoreTest):
 
     FILE_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
     TORRENT_DATA_DIR = os.path.abspath(os.path.join(FILE_DIR, u"data/torrent_creation_files/"))
+    FILE1_NAME = "file1.txt"
+    FILE2_NAME = "file2.txt"
 
     def get_params(self):
-        return { "piece length" : 42, "comment" : "Proudly created by Tribler", "created by" : "someone",
+        return { "piece length" : 65536, "comment" : "Proudly created by Tribler", "created by" : "someone",
                  "announce" : "http://tracker.com/announce", "announce-list" : ["http://tracker.com/announce"],
-                 "httpseeds" : "http://seed.com", "urllist" : "http://urlseed.com/seed.php" }
+                 "httpseeds" : "http://seed.com", "urllist" : "http://urlseed.com/seed.php",
+                 "nodes" : [] }
 
     def test_create_torrent_one_file(self):
-        create_torrent_file([self.TORRENT_DATA_DIR + "/file1.txt"], self.get_params(), self.created_torrent_one_file)
+        create_torrent_file([os.path.join(self.TORRENT_DATA_DIR, self.FILE1_NAME)],
+                            self.get_params(), self.created_torrent_one_file)
+
+    def test_create_torrent_one_file_2(self):
+        create_torrent_file([os.path.join(self.TORRENT_DATA_DIR, self.FILE2_NAME)], {})
 
     def created_torrent_one_file(self, result):
         self.assertIsInstance(result, dict)
@@ -25,7 +32,8 @@ class TriblerCoreTestTorrentUtils(TriblerCoreTest):
         os.remove(result["torrent_file_path"])
 
     def test_create_torrent_two_files(self):
-        create_torrent_file([self.TORRENT_DATA_DIR + "/file1.txt", self.TORRENT_DATA_DIR + "/file2.txt"],
+        create_torrent_file([os.path.join(self.TORRENT_DATA_DIR, self.FILE1_NAME),
+                             os.path.join(self.TORRENT_DATA_DIR, self.FILE2_NAME)],
                             self.get_params())
         self.assertTrue(os.path.isfile(os.path.abspath(os.path.join(self.TORRENT_DATA_DIR, u"file2.txt.torrent"))))
         os.remove(os.path.abspath(os.path.join(self.TORRENT_DATA_DIR, u"file2.txt.torrent")))
