ce4c130b88ad58608d11c12a05bcd3cea654d95a
Martijn de Vos <martijn@code-up.nl>
2016-02-23 13:50:40 +0100

    Hide the Artwork Panel on the home page

--- Tribler/Main/vwxGUI/home.py
+++ Tribler/Main/vwxGUI/home.py
@@ -102,7 +102,7 @@ class Home(wx.Panel):
 
         self.aw_panel = ArtworkPanel(self)
         self.aw_panel.SetMinSize((-1, 275))
-        self.aw_panel.Show(self.guiutility.ReadGuiSetting('show_artwork', True))
+        self.aw_panel.Hide()
         vSizer.Add(self.aw_panel, 0, wx.EXPAND)
 
         self.SetSizer(vSizer)
