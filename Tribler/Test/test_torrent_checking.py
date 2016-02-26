61bd9e2320d6c05f4d58a5abc2a4c24ed3c5f3b3
Martijn de Vos <martijn@code-up.nl>
2016-02-26 16:15:08 +0100

    Replaced torrent in test_torrent_checking.py

--- Tribler/Test/test_torrent_checking.py
+++ Tribler/Test/test_torrent_checking.py
@@ -3,7 +3,7 @@ from time import sleep
 from Tribler.Core.TorrentDef import TorrentDef
 from Tribler.Core.simpledefs import NTFY_MYPREFERENCES, NTFY_TORRENTS
 from Tribler.Test.test_as_server import TestAsServer
-from Tribler.Test.test_libtorrent_download import TORRENT_VIDEO_FILE
+from Tribler.Test.test_libtorrent_download import TORRENT_FILE
 
 
 class TestTorrentChecking(TestAsServer):
@@ -22,7 +22,7 @@ class TestTorrentChecking(TestAsServer):
         self.config.set_libtorrent(True)
 
     def test_torrent_checking(self):
-        tdef = TorrentDef.load(TORRENT_VIDEO_FILE)
+        tdef = TorrentDef.load(TORRENT_FILE)
         # TODO(emilon): This tracker is no more, we need to set up a new one
         # tdef.set_tracker("http://95.211.198.141:2710/announce")
         tdef.metainfo_valid = True
