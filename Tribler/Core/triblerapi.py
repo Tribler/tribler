7d1bee28b8667afce4a7a881fb72231755f4eb38
Martijn de Vos <martijn@code-up.nl>
2016-02-29 11:05:22 +0100

    Added endpoints for channel search (only locally for now)

--- Tribler/Core/triblerapi.py
+++ Tribler/Core/triblerapi.py
@@ -1,7 +1,7 @@
 import json
 from twisted.web import server
 from twisted.web import resource
-from Tribler.Core.simpledefs import NTFY_FREE_SPACE, NTFY_INSERT
+from Tribler.Core.simpledefs import NTFY_FREE_SPACE, NTFY_INSERT, NTFY_CHANNELCAST
 
 
 class TriblerAPI(resource.Resource):
@@ -16,11 +16,49 @@ class TriblerAPI(resource.Resource):
         # Initialize the TriblerAPI, create the child resources and attach important observers.
         resource.Resource.__init__(self)
         self.session = session
+
         self.event_request_handler = EventRequestHandler()
         self.putChild("events", self.event_request_handler)
 
+        self.channel_request_handler = ChannelRequestHandler(self.session)
+        self.putChild("channel", self.channel_request_handler)
+
         # Add all observers for the api
         self.session.add_observer(self.event_request_handler.on_free_space, NTFY_FREE_SPACE, [NTFY_INSERT])
+
+
+class ChannelRequestHandler(resource.Resource):
+
+    def __init__(self, session):
+        resource.Resource.__init__(self)
+
+        self.channel_search_request_handler = ChannelSearchRequestHandler(session)
+        self.putChild("search", self.channel_search_request_handler)
+
+
+class ChannelSearchRequestHandler(resource.Resource):
+
+    isLeaf = True
+
+    def __init__(self, session):
+        resource.Resource.__init__(self)
+        self.session = session
+        self.channel_db_handler = self.session.open_dbhandler(NTFY_CHANNELCAST)
+
+    def render_GET(self, request):
+        # TODO martijn: better error checking (parameters available? If not -> return 500?)
+        # TODO martijn: this only performs a local search
+        # TODO martijn: maybe use an object model here?
+        results_local_channels = self.channel_db_handler.searchChannels(request.args['q'])
+
+        results_json = []
+        for channel_result in results_local_channels:
+            print channel_result
+            results_json.append({"name": channel_result[2]})
+
+        response = {"channels": results_json}
+        return json.dumps(response)
+
 
 
 class EventRequestHandler(resource.Resource):
