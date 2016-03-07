f65fe7f4cc511aa47ee82f9e3c5d8a55c540a8fc
Martijn de Vos <mdmartijndevos@gmail.com>
2016-03-07 19:34:39 +0100

    Changed base class of category tests

--- Tribler/Test/Category/test_init_category.py
+++ Tribler/Test/Category/test_init_category.py
@@ -1,9 +1,9 @@
 import os
 from Tribler.Category.init_category import INIT_FUNC_DICT, getCategoryInfo
-from Tribler.Test.test_as_server import BaseTestCase
+from Tribler.Test.test_as_server import AbstractServer
 
 
-class TriblerCategoryTestInit(BaseTestCase):
+class TriblerCategoryTestInit(AbstractServer):
 
     FILE_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
     CATEGORY_TEST_DATA_DIR = os.path.abspath(os.path.join(FILE_DIR, u"data/Tribler/Category/"))
