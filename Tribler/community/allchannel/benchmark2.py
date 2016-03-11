111be5a54cc3e8a61468ed8cf3dfc0caaf0d8edf
lfdversluis <laurens-versluis@hotmail.com>
2016-03-11 14:19:08 +0100

    cleaned up the code and made use of deferLater

--- Tribler/community/allchannel/benchmark2.py
+++ Tribler/community/allchannel/benchmark2.py
@@ -1,11 +1,9 @@
 import os
-
 import time
+from twisted.internet.task import deferLater
+
 from twisted.internet.threads import deferToThread
-
-import yappi
 from twisted.internet.defer import inlineCallbacks, Deferred, DeferredList
-
 from twisted.internet import reactor
 
 import apsw
@@ -28,7 +26,6 @@ class Benchmark:
     @inlineCallbacks
     def run(self):
         delay = [0.01, 0.02, 0.04, 0.08, 0.1]
-        # delay = [0.2]
         for d in delay:
             yield self.query_time(d, True) # One run with blocking code
             yield self.query_time(d, False) # and one without.
@@ -47,30 +44,20 @@ class Benchmark:
     @inlineCallbacks
     def query_time(self, call_delay, blocking):
         print "Starting %s with delay %s" % ("blocking" if blocking else "async", str(call_delay))
-        # blocking = True  # Asynchronous, but blocking?
-        # max_delay = 1  # max delay in seconds
         calls = 100  # amount of calls
-        # step_delay = float(float(max_delay) / float(calls))
-        # call_delay = 0.1  # delay in seconds
-        # write_statistics = False
 
         # make sure the threadpool is initialized by doing a bogus call
-        yield self.nice_query(0, blocking, Deferred())
+        yield self.nice_query(0, blocking)
 
         calls_made = open("profiling/latencybenchmark_made_%s_%s_%s" % (
         "blocking" if blocking else "async", str(calls), str(call_delay)), 'w')
         calls_done = open("profiling/latencybenchmark_done_%s_%s_%s" % (
         "blocking" if blocking else "async", str(calls), str(call_delay)), "w")
 
-        def print_done(i, deferred):
+        def print_done(i):
             calls_done.write("%s %s %s\n" % (i, call_delay * i * 1000, int(round(time.time() * 1000))))
-            calls_done.flush()
-            deferred.callback(None)
 
-        # yappi.set_clock_type('cpu')
-        # yappi.start(builtins=True)
-
-        def on_write_done(x):
+        def on_write_done(ignored):
             calls_made.flush()
             calls_done.flush()
 
@@ -78,12 +65,9 @@ class Benchmark:
         deferred_list = []
 
         for i in xrange(1, calls + 1):
-            d1 = Deferred()
-            d2 = Deferred()
             calls_made.write("%s %s %s\n" % (i, call_delay * i * 1000, int(round(time.time() * 1000))))
-            calls_made.flush()
-            reactor.callLater(i * call_delay, self.nice_query, i, blocking, d2)
-            reactor.callLater(i * call_delay, print_done, i, d1)
+            d2 = deferLater(reactor, i * call_delay, self.nice_query, i, blocking)
+            d1 = deferLater(reactor, i * call_delay, print_done, i)
             deferred_list_write.append(d1)
             deferred_list.append(d1)
             deferred_list.append(d2)
@@ -91,27 +75,14 @@ class Benchmark:
         DeferredList(deferred_list_write).addCallback(on_write_done)
         yield DeferredList(deferred_list)
 
-        # yappi.stop()
-        #
-        # stats = yappi.get_func_stats()
-        # stats.sort("tsub").print_all(columns={0:("name",100), 1:("ncall", 12), 2:("tsub", 10), 3:("ttot", 10), 4:("tavg",10)})
-        #
-        # if write_statistics:
-        #     name = "profiling/simple_benchmark_%s_%s.pstat" % (calls, time.strftime("%d-%m-%Y-%H-%M-%S"))
-        #     stats.sort("tsub").save(name, type="pstat")
-        #
-        # calls_made.flush()
-        # calls_done.flush()
-
     @inlineCallbacks
-    def nice_query(self, i, blocking, deferred):
+    def nice_query(self, i, blocking):
         cursor = self.connection.cursor()
         sql = u"SELECT COUNT(*) FROM test WHERE x > ? AND z like ?"
         if blocking:
             result = yield cursor.execute(sql, (2, "%3%"))
         else:
             result = yield deferToThread(cursor.execute, sql, (2, "%3%"))
-        deferred.callback(None)
 
 
 if __name__ == "__main__":
