16abb72e27eea85411eb4abb902ee3aab672c8eb
Martijn de Vos <martijn@code-up.nl>
2016-03-21 15:13:34 +0100

    Torrent file type is now displayed correctly

--- TriblerGUI/channel_torrent_list_item.py
+++ TriblerGUI/channel_torrent_list_item.py
@@ -2,6 +2,7 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtWidgets import QWidget
+from TriblerGUI.utilities import format_size
 
 
 class ChannelTorrentListItem(QWidget):
@@ -11,6 +12,12 @@ class ChannelTorrentListItem(QWidget):
         uic.loadUi('qt_resources/channel_torrent_list_item.ui', self)
 
         self.channel_torrent_name.setText(torrent["name"])
+        if torrent["length"] is None:
+            self.torrent_size.setText("Size: -")
+        else:
+            self.torrent_size.setText("Size: %s" % format_size(float(torrent["length"])))
+
+        self.torrent_type.setText("File type: %s" % torrent["category"])
 
         placeholder_pix = QPixmap("images/download.png")
         placeholder_pix = placeholder_pix.scaled(self.torrent_download_button.width(), self.torrent_download_button.height(), Qt.KeepAspectRatio)
