26c30859c0a040467fdd5511df13b80c588d744b
Martijn de Vos <martijn@code-up.nl>
2016-03-30 15:03:01 +0200

    Written tests for pong message and some request caches

--- Tribler/Test/Community/Tunnel/test_tunnelcommunity.py
+++ Tribler/Test/Community/Tunnel/test_tunnelcommunity.py
@@ -1,30 +1,60 @@
-from Tribler.Test.test_as_server import AbstractServer
-from Tribler.community.tunnel.conversion import TunnelConversion
-from Tribler.community.tunnel.hidden_community import HiddenTunnelCommunity
+import time
+from Tribler.Test.Community.Tunnel.test_tunnel_base import AbstractTestTunnelCommunity
 from Tribler.community.tunnel.routing import Circuit, RelayRoute
-from Tribler.community.tunnel.tunnel_community import TunnelExitSocket
+from Tribler.community.tunnel.tunnel_community import TunnelExitSocket, CircuitRequestCache, PingRequestCache
 from Tribler.dispersy.candidate import Candidate
-from Tribler.dispersy.dispersy import Dispersy
-from Tribler.dispersy.endpoint import ManualEnpoint
-from Tribler.dispersy.member import DummyMember
 from Tribler.dispersy.message import DropMessage
 from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 
-class TestTunnelCommunity(AbstractServer):
+class TestTunnelCommunity(AbstractTestTunnelCommunity):
 
-    # We have to initialize Dispersy and the tunnel community on the reactor thread
     @blocking_call_on_reactor_thread
-    def setUp(self):
-        super(TestTunnelCommunity, self).setUp()
+    def test_circuit_request_cache(self):
+        circuit = Circuit(42L)
+        circuit_request_cache = CircuitRequestCache(self.tunnel_community, circuit)
+        self.tunnel_community.circuits[42] = circuit
+        circuit_request_cache.on_timeout()
+        self.assertIn(42, self.tunnel_community.circuits)
 
-        self.dispersy = Dispersy(ManualEnpoint(0), self.getStateDir())
-        self.dispersy._database.open()
-        self.master_member = DummyMember(self.dispersy, 1, "a" * 20)
-        self.member = self.dispersy.get_new_member(u"curve25519")
-        self.tunnel_community = HiddenTunnelCommunity(self.dispersy, self.master_member, self.member)
-        self.tunnel_community._initialize_meta_messages()
-        self.tunnel_community.add_conversion(TunnelConversion(self.tunnel_community))
+        circuit._broken = True
+        circuit_request_cache.on_timeout()
+        self.assertNotIn(42, self.tunnel_community.circuits)
+
+    @blocking_call_on_reactor_thread
+    def test_ping_request_cache(self):
+        circuit = Circuit(42L)
+        ping_request_cache = PingRequestCache(self.tunnel_community, circuit)
+        self.assertGreater(ping_request_cache.timeout_delay, 0)
+        self.tunnel_community.circuits[42] = circuit
+
+        circuit.last_incoming = time.time() + 1000
+        ping_request_cache.on_timeout()
+        self.assertIn(42, self.tunnel_community.circuits)
+
+        circuit.last_incoming = time.time() - ping_request_cache.timeout_delay - 200
+        ping_request_cache.on_timeout()
+        self.assertNotIn(42, self.tunnel_community.circuits)
+
+    @blocking_call_on_reactor_thread
+    def test_check_pong(self):
+        circuit = Circuit(42L)
+        ping_request_cache = PingRequestCache(self.tunnel_community, circuit)
+        ping_num = self.tunnel_community.request_cache.add(ping_request_cache).number
+        meta = self.tunnel_community.get_meta_message(u"ping")
+        msg1 = meta.impl(distribution=(self.tunnel_community.global_time,),
+                         candidate=Candidate(("127.0.0.1", 1234), False), payload=(42, ping_num - 1))
+        msg2 = meta.impl(distribution=(self.tunnel_community.global_time,),
+                         candidate=Candidate(("127.0.0.1", 1234), False), payload=(42, ping_num))
+
+        # Invalid ping identifier
+        for i in self.tunnel_community.check_pong([msg1]):
+            self.assertIsInstance(i, DropMessage)
+
+        for i in self.tunnel_community.check_pong([msg2]):
+            self.assertIsInstance(i, type(msg2))
+
+        self.tunnel_community.request_cache.pop(u"ping", ping_num)
 
     def test_check_destroy(self):
         # Only the first and last node in the circuit may check a destroy message
