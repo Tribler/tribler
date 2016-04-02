7070b47698547f6cb596b4d2189385104888f4fb
Martijn de Vos <martijn@code-up.nl>
2016-04-02 14:15:27 +0200

    Written some tests for the watch folder module

---
+++ Tribler/Test/Core/Modules/__init__.py
@@ -0,0 +1,3 @@
+"""
+This testing package contains tests for the various modules that can be found in Tribler.
+"""
---
+++ Tribler/Test/Core/Modules/test_watch_folder.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+from Tribler.Test.test_as_server import TestAsServer, TESTS_DATA_DIR
+from Tribler.Test.test_libtorrent_download import TORRENT_FILE
+
+
+class TestWatchFolder(TestAsServer):
+
+    def setUpPreSession(self):
+        super(TestWatchFolder, self).setUpPreSession()
+        self.config.set_libtorrent(True)
+        self.config.set_watch_folder_enabled(True)
+
+        self.watch_dir = os.path.join(self.session_base_dir, 'watch')
+        os.mkdir(self.watch_dir)
+
+        self.config.set_watch_folder_path(self.watch_dir)
+
+    def test_watchfolder_no_files(self):
+        self.session.lm.watch_folder.check_watch_folder()
+        self.assertEqual(len(self.session.get_downloads()), 0)
+
+    def test_watchfolder_no_torrent_file(self):
+        shutil.copyfile(TORRENT_FILE, os.path.join(self.watch_dir, "test.txt"))
+        self.session.lm.watch_folder.check_watch_folder()
+        self.assertEqual(len(self.session.get_downloads()), 0)
+
+    def test_watchfolder_invalid_dir(self):
+        shutil.copyfile(TORRENT_FILE, os.path.join(self.watch_dir, "test.txt"))
+        self.session.set_watch_folder_path(os.path.join(self.watch_dir, "test.txt"))
+        self.session.lm.watch_folder.check_watch_folder()
+        self.assertEqual(len(self.session.get_downloads()), 0)
+
+    def test_watchfolder_torrent_file_one_corrupt(self):
+        shutil.copyfile(TORRENT_FILE, os.path.join(self.watch_dir, "test.torrent"))
+        shutil.copyfile(os.path.join(TESTS_DATA_DIR, 'test_rss.xml'), os.path.join(self.watch_dir, "test2.torrent"))
+        self.session.lm.watch_folder.check_watch_folder()
+        self.assertEqual(len(self.session.get_downloads()), 1)
