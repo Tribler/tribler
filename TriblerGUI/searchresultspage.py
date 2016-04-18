76e5ca8d2a5e4c2bacfcc9721228a0cd28fb13f5
Martijn de Vos <mdmartijndevos@gmail.com>
2016-04-18 16:42:13 +0200

    Finished search results screen

--- TriblerGUI/searchresultspage.py
+++ TriblerGUI/searchresultspage.py
@@ -1,42 +1,52 @@
 import json
+from random import shuffle
+
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QWidget, QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import QWidget, QListWidget, QListWidgetItem, QLabel
 from TriblerGUI.channel_list_item import ChannelListItem
+from TriblerGUI.channel_torrent_list_item import ChannelTorrentListItem
 
 
 class SearchResultsPage(QWidget):
 
     def initialize_search_results_page(self):
         self.search_results_list = self.findChild(QListWidget, "search_results_list")
-        self.search_results_list.verticalScrollBar().valueChanged.connect(self.on_search_results_list_scroll)
-        self.search_results_items_loaded = 0
-        self.search_results = []
+        self.search_results_header_label = self.findChild(QLabel, "search_results_header_label")
+        self.num_search_results_label = self.findChild(QLabel, "num_search_results_label")
+        self.search_results = {}
 
-        # Tab bar buttons
         self.search_results_tab = self.findChild(QWidget, "search_results_tab")
         self.search_results_tab.initialize()
         self.search_results_tab.clicked_tab_button.connect(self.clicked_tab_button)
 
+    def perform_search(self, query):
+        self.num_search_results_label.setText("")
+        self.search_results_header_label.setText("Search results for '%s'" % query)
+
     def clicked_tab_button(self, tab_button_name):
         if tab_button_name == "search_results_all_button":
-            print "display all search results"
+            self.load_search_results_in_list()
         elif tab_button_name == "search_results_channels_button":
-            print "display channels"
+            self.load_search_results_in_list(show_torrents=False)
         elif tab_button_name == "search_results_torrents_button":
-            print "display torrents"
+            self.load_search_results_in_list(show_channels=False)
 
-    def on_search_results_list_scroll(self, event):
-        if self.search_results_list.verticalScrollBar().value() == self.search_results_list.verticalScrollBar().maximum():
-            self.search_results_list()
+    def load_search_results_in_list(self, show_channels=True, show_torrents=True):
+        self.num_search_results_label.setText("%d results" % (len(self.search_results['channels']) + len(self.search_results['torrents'])))
+        all_items = []
+        if show_channels:
+            for channel_item in self.search_results['channels']:
+                all_items.append((ChannelListItem, channel_item))
 
-    def received_search_results(self, json_results):
-        self.search_results_list.clear()
-        results = json.loads(json_results)
+        if show_torrents:
+            for torrent_item in self.search_results['torrents']:
+                all_items.append((ChannelTorrentListItem, torrent_item))
 
-        for result in results['channels']:
-            item = QListWidgetItem(self.channels_list)
-            item.setSizeHint(QSize(-1, 60))
-            item.setData(Qt.UserRole, result)
-            widget_item = ChannelListItem(self.search_results_list, 0, result, should_fade=False)
-            self.search_results_list.addItem(item)
-            self.search_results_list.setItemWidget(item, widget_item)
+        # Just sort them randomly, channels and torrents mixed
+        shuffle(all_items)
+
+        self.search_results_list.set_data_items(all_items)
+
+    def received_search_results(self, results):
+        self.search_results = results
+        self.load_search_results_in_list()
