ab978d8f8a020286de70807f40c7e79f13a2e66a
Martijn de Vos <martijn@code-up.nl>
2016-04-25 14:16:35 +0200

    Refactored confirmation dialog

--- TriblerGUI/dialogs/confirmationdialog.py
+++ TriblerGUI/dialogs/confirmationdialog.py
@@ -1,21 +1,17 @@
 from PyQt5 import uic
-from PyQt5.QtCore import QPoint, pyqtSignal
-from PyQt5.QtGui import QPainter
-from PyQt5.QtWidgets import QWidget, QSizePolicy, QStyleOption, QStyle
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QSizePolicy
+from TriblerGUI.dialogs.dialogcontainer import DialogContainer
 
 
-class ConfirmationDialog(QWidget):
+class ConfirmationDialog(DialogContainer):
 
     button_clicked = pyqtSignal(int)
 
     def __init__(self, parent, title, main_text):
-        super(QWidget, self).__init__(parent)
-
-        self.dialog_widget = QWidget(self)
+        super(ConfirmationDialog, self).__init__(parent)
 
         uic.loadUi('qt_resources/yes_no_dialog.ui', self.dialog_widget)
-
-        self.setStyleSheet("background-color: rgba(30, 30, 30, 0.75);")
 
         self.dialog_widget.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
 
@@ -27,17 +23,4 @@ class ConfirmationDialog(QWidget):
         self.dialog_widget.dialog_cancel_button.clicked.connect(lambda: self.button_clicked.emit(0))
         self.dialog_widget.dialog_confirm_button.clicked.connect(lambda: self.button_clicked.emit(1))
 
-        self.window().resize_event.connect(self.on_main_window_resize)
         self.on_main_window_resize()
-
-    def paintEvent(self, event):
-        opt = QStyleOption()
-        opt.initFrom(self)
-        painter = QPainter(self)
-        self.style().drawPrimitive(QStyle.PE_Widget, opt, painter, self)
-
-    def on_main_window_resize(self):
-        self.setFixedSize(self.parentWidget().size())
-        self.dialog_widget.setFixedWidth(self.width() - 100)
-        self.dialog_widget.move(QPoint(self.geometry().center().x() - self.dialog_widget.geometry().width() / 2,
-                         self.geometry().center().y() - self.dialog_widget.geometry().height() / 2))
