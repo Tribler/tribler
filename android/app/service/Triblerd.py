740ec503d808f7ef8e179f50e98a82bf41e79c01
Paul Brussee <brussee@live.nl>
2016-05-02 21:40:00 +0200

    enable rest api

--- android/app/service/Triblerd.py
+++ android/app/service/Triblerd.py
@@ -1,4 +1,9 @@
+import os
+import ast
+
 from time import sleep
+
+from kivy.logger import Logger
 
 from Tribler.Core.Session import Session
 from Tribler.Core.SessionConfig import SessionStartupConfig
@@ -16,20 +21,28 @@ class Triblerd(object):
 
     def run(self):
 
-        def start_tribler(arg):
-            self.session = Session(SessionStartupConfig())
+        def start_tribler(options):
+            config = SessionStartupConfig()
+            config.set_http_api_enabled(True)
+            config.set_http_api_port(options['restapi'])
+
+            self.session = Session(config)
+
+            Logger.info('Run upgrader...')
             upgrader = self.session.prestart()
-            while not upgrader.is_done:
-                sleep(0.1)
-            self.session.start()
 
-            while not self.session.lm.initComplete:
-                sleep(0.2)
+            if upgrader.failed:
+                Logger.error('The upgrader failed')
+            else:
+                Logger.info('Starting Tribler...')
+                self.session.start()
+                Logger.info('Tribler started!')
 
-            while True:
-                sleep(1)
-
-        start_tribler(os.getenv('PYTHON_SERVICE_ARGUMENT'))
+        """
+        Pass through options
+        """
+        options = ast.literal_eval(os.getenv('PYTHON_SERVICE_ARGUMENT'))
+        start_tribler(options)
 
 
 if __name__ == '__main__':
