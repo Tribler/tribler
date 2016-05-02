d379d659a297f632db4c9a8e9b4b33e3932352f6
Paul Brussee <brussee@live.nl>
2016-05-02 17:16:15 +0200

    wip HomeScreen

---
+++ android/app/HomeScreen.py
@@ -0,0 +1,28 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, StringProperty
+
+
+class HomeScreen(BoxLayout):
+
+    title = StringProperty('Tribler')
+
+    top_pane = ObjectProperty()
+    content_pane = ObjectProperty()
+
+    def go_favs(self):
+        print self
+
+    def go_recent(self):
+        print self
+
+    def go_mine(self):
+        print self
+
+    def go_search(self):
+        print self
+
+    def go_back(self):
+        print self
+
+    def add_video(self):
+        print self
