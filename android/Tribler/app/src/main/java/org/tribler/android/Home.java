2dc27dd1302f8f5585c5cbfdae856ff72282040b
Paul Brussee <brussee@live.nl>
2016-05-03 17:17:20 +0200

    first demo app with recycleview

---
+++ android/Tribler/app/src/main/java/org/tribler/android/Home.java
@@ -0,0 +1,42 @@
+package org.tribler.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+    private List<Video> videoList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private VideosViewAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.content_list);
+
+        mAdapter = new VideosViewAdapter(videoList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareMovieData();
+    }
+
+    private void prepareMovieData() {
+
+
+        mAdapter.notifyDataSetChanged();
+    }
+}
---
+++ android/Tribler/app/src/main/java/org/tribler/android/Video.java
@@ -0,0 +1,38 @@
+package org.tribler.android;
+
+public class Video {
+    private String title, description, duration;
+
+    public Video() {
+    }
+
+    public Video(String title, String description, String duration) {
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
