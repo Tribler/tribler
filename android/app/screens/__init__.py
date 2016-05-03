d6428c4c02a506ac5964e6892cf16a369f12eee1
Paul Brussee <brussee@live.nl>
2016-05-03 13:59:14 +0200

    package doc

---
+++ android/app/screens/__init__.py
@@ -0,0 +1,5 @@
+# Written by P.W.G. Brussee
+# see LICENSE.txt for license information
+'''
+The screens package contains code to show the GUI and navigation logic.
+'''
---
+++ android/app/service/__init__.py
@@ -0,0 +1,5 @@
+# Written by P.W.G. Brussee
+# see LICENSE.txt for license information
+'''
+The service package contains code to start Tribler as a background service.
+'''
