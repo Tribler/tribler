b988868c468e8e5217422f1baa8d790e634f69cf
Paul Brussee <brussee@live.nl>
2016-05-03 17:19:09 +0200

    example data

--- android/Tribler/app/src/main/java/org/tribler/android/Home.java
+++ android/Tribler/app/src/main/java/org/tribler/android/Home.java
@@ -35,7 +35,53 @@ public class Home extends AppCompatActivity {
     }
 
     private void prepareMovieData() {
+        Video video = new Video("Mad Max: Fury Road", "Action & Adventure", "2015");
+        videoList.add(video);
 
+        video = new Video("Inside Out", "Animation, Kids & Family", "2015");
+        videoList.add(video);
+
+        video = new Video("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+        videoList.add(video);
+
+        video = new Video("Shaun the Sheep", "Animation", "2015");
+        videoList.add(video);
+
+        video = new Video("The Martian", "Science Fiction & Fantasy", "2015");
+        videoList.add(video);
+
+        video = new Video("Mission: Impossible Rogue Nation", "Action", "2015");
+        videoList.add(video);
+
+        video = new Video("Up", "Animation", "2009");
+        videoList.add(video);
+
+        video = new Video("Star Trek", "Science Fiction", "2009");
+        videoList.add(video);
+
+        video = new Video("The LEGO Movie", "Animation", "2014");
+        videoList.add(video);
+
+        video = new Video("Iron Man", "Action & Adventure", "2008");
+        videoList.add(video);
+
+        video = new Video("Aliens", "Science Fiction", "1986");
+        videoList.add(video);
+
+        video = new Video("Chicken Run", "Animation", "2000");
+        videoList.add(video);
+
+        video = new Video("Back to the Future", "Science Fiction", "1985");
+        videoList.add(video);
+
+        video = new Video("Raiders of the Lost Ark", "Action & Adventure", "1981");
+        videoList.add(video);
+
+        video = new Video("Goldfinger", "Action & Adventure", "1965");
+        videoList.add(video);
+
+        video = new Video("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+        videoList.add(video);
 
         mAdapter.notifyDataSetChanged();
     }
