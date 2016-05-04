42dc7fde31fec5acec8e095433ff3ba6879587e9
Paul Brussee <brussee@live.nl>
2016-05-04 15:46:14 +0200

    wip ViewAdapter

--- android/Tribler/app/src/main/java/org/tribler/android/Home.java
+++ android/Tribler/app/src/main/java/org/tribler/android/Home.java
@@ -5,15 +5,51 @@ import android.os.Bundle;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
+import java.nio.channels.Channel;
 import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
-    private ArrayList<AbstractContent> mTorrentList = new ArrayList<AbstractContent>();
+    private ArrayList<Object> mList = new ArrayList<Object>();
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private ItemTouchHelper.SimpleCallback mItemTouchCallback =
+            new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                @Override
+                public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                    // Swipe channel
+                    if (viewHolder instanceof MyViewAdapter.ChannelViewHolder) {
+                        if (swipeDir == ItemTouchHelper.LEFT) {
+                            // un-subscribe / not interested
+                        } else if (swipeDir == ItemTouchHelper.RIGHT) {
+                            // subscribe / favorite
+                        }
+                    }
+                    // Swipe torrent
+                    else if (viewHolder instanceof MyViewAdapter.TorrentViewHolder) {
+                        if (swipeDir == ItemTouchHelper.LEFT) {
+                            // not interested
+                        } else if (swipeDir == ItemTouchHelper.RIGHT) {
+                            // watch later
+                        }
+                    }
+                }
+
+                @Override
+                /**
+                 * Not draggable
+                 */
+                public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                      RecyclerView.ViewHolder target) {
+                    return false;
+                }
+            };
+
+    private ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(mItemTouchCallback);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,66 +61,13 @@ public class Home extends AppCompatActivity {
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-        mAdapter = new MyViewAdapter(mTorrentList);
+        mAdapter = new MyViewAdapter(mList);
         mRecyclerView.setAdapter(mAdapter);
 
-        fetchVideosData();
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
 
-    private ArrayList<TriblerTorrent> fetchVideosData() {
-        TriblerTorrent torrentItem = new TriblerTorrent("Mad Max: Fury Road", "Action & Adventure", "2015");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Inside Out", "Animation, Kids & Family", "2015");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Shaun the Sheep", "Animation", "2015");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("The Martian", "Science Fiction & Fantasy", "2015");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Mission: Impossible Rogue Nation", "Action", "2015");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Up", "Animation", "2009");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Star Trek", "Science Fiction", "2009");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("The LEGO Movie", "Animation", "2014");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Iron Man", "Action & Adventure", "2008");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Aliens", "Science Fiction", "1986");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Chicken Run", "Animation", "2000");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Back to the Future", "Science Fiction", "1985");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Raiders of the Lost Ark", "Action & Adventure", "1981");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Goldfinger", "Action & Adventure", "1965");
-        mTorrentList.add(torrentItem);
-
-        torrentItem = new TriblerTorrent("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
-        mTorrentList.add(torrentItem);
-
-        mAdapter.notifyDataSetChanged();
-    }
 }
--- android/Tribler/app/src/main/java/org/tribler/android/MyViewAdapter.java
+++ android/Tribler/app/src/main/java/org/tribler/android/MyViewAdapter.java
@@ -2,7 +2,6 @@ package org.tribler.android;
 
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,14 +14,14 @@ import java.util.List;
  * Creates visual representation for channels and torrents in a list
  */
 public class MyViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
+    private static final int VIEW_TYPE_UNKNOWN = 0;
     private static final int VIEW_TYPE_CHANNEL = 1;
     private static final int VIEW_TYPE_TORRENT = 2;
 
-    private List<Object> mData;
+    private List<Object> mList;
 
-    public MyViewAdapter(List<Object> mData) {
-        this.mData = mData;
+    public MyViewAdapter(List<Object> mList) {
+        this.mList = mList;
     }
 
     @Override
@@ -30,18 +29,21 @@ public class MyViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
      * @return The amount of items in the data set (invoked by the layout manager)
      */
     public int getItemCount() {
-        return mData.size();
+        return mList.size();
     }
 
     @Override
     /**
-     * @return VIEW_TYPE_CHANNEL | VIEW_TYPE_TORRENT based on class type
+     * @return VIEW_TYPE_CHANNEL | VIEW_TYPE_TORRENT | VIEW_TYPE_UNKNOWN based on class type
      */
     public int getItemViewType(int position) {
-        if (mData.get(position) instanceof TriblerChannel) {
+        Object item = mList.get(position);
+        if (item instanceof TriblerChannel) {
             return VIEW_TYPE_CHANNEL;
+        } else if (item instanceof TriblerTorrent) {
+            return VIEW_TYPE_TORRENT;
         }
-        return VIEW_TYPE_TORRENT;
+        return VIEW_TYPE_UNKNOWN;
     }
 
     public class ChannelViewHolder extends RecyclerView.ViewHolder {
@@ -73,19 +75,26 @@ public class MyViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     /**
      * @param parent    The group to which the view should be added
-     * @param viewType  The
+     * @param viewType  The type of view to create
      */
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Create new channel view
         if (viewType == VIEW_TYPE_CHANNEL) {
-            // Create new channel view
             View channelView = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.list_item_channel, parent, false);
             return new ChannelViewHolder(channelView);
         }
         // Create new torrent view
-        View torrentView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item_torrent, parent, false);
-        return new TorrentViewHolder(torrentView);
+        else if (viewType == VIEW_TYPE_TORRENT) {
+
+            View torrentView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_torrent, parent, false);
+            return new TorrentViewHolder(torrentView);
+        }
+        // Unknown view type
+        else {
+            return null;
+        }
     }
 
     @Override
@@ -95,16 +104,19 @@ public class MyViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
      * @param position  The position of the item in the data set
      */
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        // Channel
         if (holder instanceof ChannelViewHolder) {
             ChannelViewHolder view = (ChannelViewHolder) holder;
-            TriblerChannel channel = (TriblerChannel) mData.get(position);
+            TriblerChannel channel = (TriblerChannel) mList.get(position);
             view.name.setText(channel.getName());
             view.videosCount.setText(channel.getVideosCount());
             view.commentsCount.setText(channel.getCommentsCount());
             view.icon.setImageURI(Uri.parse(channel.getIconUrl()));
-        } else {
+        }
+        // Torrent
+        else if (holder instanceof TorrentViewHolder) {
             TorrentViewHolder view = (TorrentViewHolder) holder;
-            TriblerTorrent torrent = (TriblerTorrent) mData.get(position);
+            TriblerTorrent torrent = (TriblerTorrent) mList.get(position);
             view.title.setText(torrent.getTitle());
             view.duration.setText(torrent.getDuration());
             view.bitrate.setText(torrent.getBitrate());
@@ -112,26 +124,4 @@ public class MyViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         }
     }
 
-    ItemTouchHelper.SimpleCallback mItemTouchCallback =
-            new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-
-                @Override
-                public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
-                    if (swipeDir == ItemTouchHelper.LEFT) {
-                        //Remove swiped item from list and notify the RecyclerView
-                    } else {
-                        //Do something else
-                    }
-                }
-
-                @Override
-                /**
-                 * Not draggable
-                 */
-                public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                    return false;
-                }
-            };
-
-    ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(mItemTouchCallback);
 }
