1b4460f81cef10be1837531ad857a51b390e96d0
Paul Brussee <brussee@live.nl>
2016-05-06 20:05:25 +0200

    paths bugfix

--- android/TriblerService/build_dist.sh
+++ android/TriblerService/build_dist.sh
@@ -5,7 +5,7 @@ cd dist/TriblerService
 python build.py \
 --package=org.tribler.android \
 --service=Triblerd:Triblerd.py \
---private=./service \
---whitelist=.p4a-whitelist
+--private=../../service \
+--whitelist=../../.p4a-whitelist
 
 cd ../..
