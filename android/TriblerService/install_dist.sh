24b0a57e83465fcf284d0fe584d9509a54e25bc0
Paul Brussee <brussee@live.nl>
2016-05-07 21:14:01 +0200

    improve install dist script

--- android/TriblerService/install_dist.sh
+++ android/TriblerService/install_dist.sh
@@ -4,18 +4,24 @@ echo Install dist
 
 cd dist/TriblerService
 
+mv libs jniLibs
+mv python-install/include jni/include
+mv python-install/lib jni/lib
+
+rm -rf python-install
 rm -rf collated_objects
 rm -rf private
 rm -rf python-install
 rm -rf templates
 rm -rf build
+rm -r jni/*.mk
+rm -r jni/src/*.mk
 rm -f blacklist.txt
 rm -f whitelist.txt
 rm -f build.py
 rm -f dist_info.json
 rm -f project.properties
-rm -f local.properties
 
 cd ../..
 
-mv dist/TriblerService dist/TriblerApp
+mv dist/TriblerService dist/TriblerApp-import
