3080fa58830153f26a70c6b46cda9fe9bd7b3bd1
Paul Brussee <brussee@live.nl>
2016-05-07 20:40:52 +0200

    add PyCrypto dependency

---
+++ android/TriblerService/recipes/libtribler_local/__init__.py
@@ -0,0 +1,52 @@
+from pythonforandroid.toolchain import PythonRecipe, shutil, current_directory
+from os.path import join
+from sh import mkdir, cp
+
+"""
+Privacy with BitTorrent and resilient to shut down
+
+http://www.tribler.org
+"""
+class LocalTriblerRecipe(PythonRecipe):
+
+    version = 'local'
+
+    depends = ['apsw', 'cherrypy', 'cryptography', 'decorator', 'feedparser',
+               'ffmpeg', 'libnacl', 'libsodium', 'libtorrent', 'm2crypto',
+               'netifaces', 'openssl', 'pyasn1', 'pil', 'pycrypto', 'pyleveldb',
+               'python2', 'requests', 'twisted']
+
+    site_packages_name = 'Tribler'
+
+    def should_build(self, arch):
+        # Overwrite old build
+        return True
+
+
+    def prebuild_arch(self, arch):
+        # Remove from site-packages
+        super(LocalTriblerRecipe, self).clean_build(arch.arch)
+
+        # Create empty build dir
+        container_dir = self.get_build_container_dir(arch.arch)
+        mkdir('-p', container_dir)
+
+        with current_directory(container_dir):
+            # Copy source from working copy
+            cp('-rf', '/home/paul/repos/tribler', self.name)
+
+            # Copy twisted plugin
+            shutil.copyfile(join(self.name, 'twisted/twisted/plugins/tribler_plugin.py'),
+                            '/home/paul/repos/tribler/android/TriblerService/service/tribler_plugin.py')
+
+        super(LocalTriblerRecipe, self).prebuild_arch(arch)
+
+
+    def postbuild_arch(self, arch):
+        super(LocalTriblerRecipe, self).postbuild_arch(arch)
+        # Install ffmpeg binary
+        shutil.copyfile(self.get_recipe('ffmpeg', self.ctx).get_build_bin(arch),
+                        '/home/paul/repos/tribler/android/TriblerService/service/ffmpeg')
+
+
+recipe = LocalTriblerRecipe()
