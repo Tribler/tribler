aeef949900a97239bd2f2950776f6e48301bec5f
Paul Brussee <brussee@live.nl>
2016-05-09 14:51:32 +0200

    search view & camera rec button

--- android/TriblerApp/app/src/main/java/org/tribler/android/Home.java
+++ android/TriblerApp/app/src/main/java/org/tribler/android/Home.java
@@ -1,12 +1,19 @@
 package org.tribler.android;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.widget.SearchView;
 
 import com.google.gson.Gson;
 
@@ -76,6 +83,39 @@ public class Home extends AppCompatActivity {
 
         initGui();
         initBeam();
+        initSearch();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
+    }
+
+    private void initSearch() {
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    private void doMySearch(String query) {
+        mList.clear();
+        mAdapter.notifyDataSetChanged();
+        //TODO
         exampleData();
     }
 
