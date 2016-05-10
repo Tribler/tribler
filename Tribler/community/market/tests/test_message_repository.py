e6d8fdfaa856bf32cf1bf033930147782fa9cb01
joswinter <info@joswinter.nl>
2016-05-10 18:58:19 +0200

    Added test for message repository

---
+++ Tribler/community/market/tests/test_message_repository.py
@@ -0,0 +1,25 @@
+import unittest
+
+from Tribler.community.market.core.message_repository import MessageRepository, MemoryMessageRepository
+from Tribler.community.market.core.tick import MessageId, TraderId, MessageNumber
+
+class MessageRepositoryTestSuite(unittest.TestCase):
+    """Message repository test cases."""
+
+    def test_message_repository(self):
+        # Object creation
+        message_repository = MessageRepository()
+
+        # Test for next identity
+        self.assertEquals(NotImplemented, message_repository.next_identity())
+
+    def test_memory_message_repository(self):
+        # Object creation
+        memory_message_repository = MemoryMessageRepository('trader_id')
+        message_id = MessageId(TraderId('trader_id'), MessageNumber('1'))
+
+        # Test for next identity
+        self.assertEquals(message_id, memory_message_repository.next_identity())
+
+if __name__ == '__main__':
+    unittest.main()
