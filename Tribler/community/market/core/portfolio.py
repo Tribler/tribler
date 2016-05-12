135ad7cca6512113bb6435cf293343e4e9b26c98
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2016-05-12 22:38:09 +0200

    Cleanup portfolio code
    Added comments to the portfolio
    Renamed delete order to cancel order
    Added logging

--- Tribler/community/market/core/portfolio.py
+++ Tribler/community/market/core/portfolio.py
@@ -1,7 +1,7 @@
 import logging
 
 from Tribler.community.market.core.order_repository import OrderRepository
-from Tribler.community.market.core.tick import Order, Timestamp, OrderId
+from Tribler.community.market.core.tick import Price, Quantity, Timeout, Timestamp, OrderId, Order
 
 
 class Portfolio(object):
@@ -16,20 +16,71 @@ class Portfolio(object):
         """
         super(Portfolio, self).__init__()
         self._logger = logging.getLogger(self.__class__.__name__)
+        self._logger.info("Market Portfolio initialized")
 
         assert isinstance(order_repository, OrderRepository), type(order_repository)
+
         self.order_repository = order_repository
 
     def create_ask_order(self, price, quantity, timeout):
+        """
+        Create an ask order (sell order)
+
+        :param price: The price for the order
+        :param quantity: The quantity of the order
+        :param timeout: The timeout of the order, when does the order need to be timed out
+        :type price: Price
+        :type quantity: Quantity
+        :type timeout: Timeout
+        :return: The order that is created
+        :rtype: Order
+        """
+        assert isinstance(price, Price), type(price)
+        assert isinstance(quantity, Quantity), type(quantity)
+        assert isinstance(timeout, Timeout), type(timeout)
+
         order = Order(self.order_repository.next_identity(), price, quantity, timeout, Timestamp.now(), True)
         self.order_repository.add(order)
+
+        self._logger.info("Ask order created with id: " + str(order.order_id))
+
         return order
 
     def create_bid_order(self, price, quantity, timeout):
+        """
+        Create a bid order (buy order)
+
+        :param price: The price for the order
+        :param quantity: The quantity of the order
+        :param timeout: The timeout of the order, when does the order need to be timed out
+        :type price: Price
+        :type quantity: Quantity
+        :type timeout: Timeout
+        :return: The order that is created
+        :rtype: Order
+        """
+        assert isinstance(price, Price), type(price)
+        assert isinstance(quantity, Quantity), type(quantity)
+        assert isinstance(timeout, Timeout), type(timeout)
+
         order = Order(self.order_repository.next_identity(), price, quantity, timeout, Timestamp.now(), False)
         self.order_repository.add(order)
+
+        self._logger.info("Bid order created with id: " + str(order.order_id))
+
         return order
 
-    def delete_order(self, order_id):
+    def cancel_order(self, order_id):
+        """
+        Cancel an order that was created by the user
+
+        It only marks it as timed out, because some quantity might already be sold or bought
+
+        :return: The order that is created
+        :rtype: Order
+        """
         assert isinstance(order_id, OrderId), type(order_id)
+
         self.order_repository.delete_by_id(order_id)
+
+        self._logger.info("Order canceled with id: " + str(order_id))
