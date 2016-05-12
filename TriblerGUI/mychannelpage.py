b1fb804df6de6569b348f63063042481865e2d36
Martijn de Vos <martijn@code-up.nl>
2016-05-12 13:16:31 +0200

    Improved GUI: added icons to left bar, moved signals/slots to .ui file and optimized code

--- TriblerGUI/mychannelpage.py
+++ TriblerGUI/mychannelpage.py
@@ -14,53 +14,27 @@ class MyChannelPage(QWidget):
     """
 
     def initialize_my_channel_page(self):
-        self.my_channel_stacked_widget = self.findChild(QStackedWidget, "my_channel_stacked_widget")
-        self.my_channel_details_stacked_widget = self.findChild(QStackedWidget, "my_channel_details_stacked_widget")
-        self.create_channel_form = self.findChild(QWidget, "create_channel_form")
-        self.create_new_channel_intro_label = self.findChild(QLabel, "create_new_channel_intro_label")
-        self.create_channel_intro_button = self.findChild(QPushButton, "create_channel_intro_button")
-        self.create_channel_intro_button.clicked.connect(self.on_create_channel_intro_button_clicked)
+        self.window().create_channel_intro_button.clicked.connect(self.on_create_channel_intro_button_clicked)
 
-        self.my_channel_name_label = self.findChild(QLabel, "my_channel_name_label")
-        self.my_channel_description_label = self.findChild(QLabel, "my_channel_description_label")
-        self.my_channel_identifier_label = self.findChild(QLabel, "my_channel_identifier_label")
+        self.window().create_channel_form.hide()
 
-        self.my_channel_name_input = self.findChild(QLineEdit, "my_channel_name_input")
-        self.my_channel_description_input = self.findChild(QTextEdit, "my_channel_description_input")
+        self.window().my_channel_stacked_widget.setCurrentIndex(1)
+        self.window().my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_OVERVIEW)
 
-        self.my_channel_torrents_list = self.findChild(QTreeWidget, "my_channel_torrents_list")
-        self.my_channel_rss_feeds_list = self.findChild(QTreeWidget, "my_channel_rss_feeds_list")
-
-        self.create_channel_intro_button_container = self.findChild(QWidget, "create_channel_intro_button_container")
-        self.create_channel_form.hide()
-
-        self.my_channel_stacked_widget.setCurrentIndex(1)
-        self.my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_OVERVIEW)
-
-        self.channel_settings_page = self.findChild(QWidget, "channel_settings_page")
-
-        self.my_channel_torrents_remove_selected_button = self.findChild(QToolButton,
-                                                                         "my_channel_torrents_remove_selected_button")
-        self.my_channel_torrents_remove_selected_button.clicked.connect(self.on_torrents_remove_selected_clicked)
-
-        self.my_channel_torrents_remove_all_button = self.findChild(QToolButton,
-                                                                    "my_channel_torrents_remove_all_button")
-        self.my_channel_torrents_remove_all_button.clicked.connect(self.on_torrents_remove_all_clicked)
-
-        self.my_channel_torrents_export_button = self.findChild(QToolButton, "my_channel_torrents_export_button")
-        self.my_channel_torrents_export_button.clicked.connect(self.on_torrents_export_clicked)
+        self.window().my_channel_torrents_remove_selected_button.clicked.connect(self.on_torrents_remove_selected_clicked)
+        self.window().my_channel_torrents_remove_all_button.clicked.connect(self.on_torrents_remove_all_clicked)
+        self.window().my_channel_torrents_export_button.clicked.connect(self.on_torrents_export_clicked)
 
         # Tab bar buttons
-        self.channel_settings_tab = self.findChild(QWidget, "channel_settings_tab")
-        self.channel_settings_tab.initialize()
-        self.channel_settings_tab.clicked_tab_button.connect(self.clicked_tab_button)
+        self.window().channel_settings_tab.initialize()
+        self.window().channel_settings_tab.clicked_tab_button.connect(self.clicked_tab_button)
 
         # add some dummy items to rss feeds list
         for i in range(0, 10):
-            item = QTreeWidgetItem(self.my_channel_rss_feeds_list)
+            item = QTreeWidgetItem(self.window().my_channel_rss_feeds_list)
             item.setText(0, "http://fancyurl.com/rss_feed.xml")
 
-            self.my_channel_rss_feeds_list.addTopLevelItem(item)
+            self.window().my_channel_rss_feeds_list.addTopLevelItem(item)
 
     def load_my_channel_overview(self):
         self.mychannel_request_mgr = TriblerRequestManager()
@@ -68,37 +42,37 @@ class MyChannelPage(QWidget):
 
     def initialize_with_overview(self, overview):
         self.my_channel_overview = overview
-        self.my_channel_name_label.setText(overview["name"])
-        self.my_channel_description_label.setText(overview["description"])
-        self.my_channel_identifier_label.setText(overview["identifier"])
+        self.window().my_channel_name_label.setText(overview["name"])
+        self.window().my_channel_description_label.setText(overview["description"])
+        self.window().my_channel_identifier_label.setText(overview["identifier"])
 
-        self.my_channel_name_input.setText(overview["name"])
-        self.my_channel_description_input.setText(overview["description"])
+        self.window().my_channel_name_input.setText(overview["name"])
+        self.window().my_channel_description_input.setText(overview["description"])
 
     def load_my_channel_torrents(self):
         self.mychannel_request_mgr = TriblerRequestManager()
         self.mychannel_request_mgr.get_my_channel_torrents(self.initialize_with_torrents)
 
     def initialize_with_torrents(self, torrents):
-        self.my_channel_torrents_list.clear()
+        self.window().my_channel_torrents_list.clear()
         for torrent in torrents:
-            item = QTreeWidgetItem(self.my_channel_torrents_list)
+            item = QTreeWidgetItem(self.window().my_channel_torrents_list)
             item.setText(0, torrent["name"])
             item.setText(1, str(torrent["added"]))
 
-            self.my_channel_torrents_list.addTopLevelItem(item)
+            self.window().my_channel_torrents_list.addTopLevelItem(item)
 
     def load_my_channel_rss_feeds(self):
         self.mychannel_request_mgr = TriblerRequestManager()
         self.mychannel_request_mgr.get_my_channel_rss_feeds(self.initialize_with_rss_feeds)
 
     def initialize_with_rss_feeds(self, rss_feeds):
-        self.my_channel_rss_feeds_list.clear()
+        self.window().my_channel_rss_feeds_list.clear()
         for feed in rss_feeds:
-            item = QTreeWidgetItem(self.my_channel_rss_feeds_list)
+            item = QTreeWidgetItem(self.window().my_channel_rss_feeds_list)
             item.setText(0, feed["url"])
 
-            self.my_channel_rss_feeds_list.addTopLevelItem(item)
+            self.window().my_channel_rss_feeds_list.addTopLevelItem(item)
 
     def on_torrents_remove_selected_clicked(self):
         num_selected = len(self.my_channel_torrents_list.selectedItems())
@@ -130,19 +104,19 @@ class MyChannelPage(QWidget):
 
     def clicked_tab_button(self, tab_button_name):
         if tab_button_name == "my_channel_overview_button":
-            self.my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_OVERVIEW)
+            self.window().my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_OVERVIEW)
         elif tab_button_name == "my_channel_settings_button":
-            self.my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_SETTINGS)
+            self.window().my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_SETTINGS)
         elif tab_button_name == "my_channel_torrents_button":
-            self.my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_TORRENTS)
+            self.window().my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_TORRENTS)
             self.load_my_channel_torrents()
         elif tab_button_name == "my_channel_playlists_button":
-            self.my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_PLAYLISTS)
+            self.window().my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_PLAYLISTS)
         elif tab_button_name == "my_channel_rss_feeds_button":
-            self.my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_RSS_FEEDS)
+            self.window().my_channel_details_stacked_widget.setCurrentIndex(PAGE_MY_CHANNEL_RSS_FEEDS)
             self.load_my_channel_rss_feeds()
 
     def on_create_channel_intro_button_clicked(self):
-        self.create_channel_form.show()
-        self.create_channel_intro_button_container.hide()
-        self.create_new_channel_intro_label.setText("Please enter your channel details below.")
+        self.window().create_channel_form.show()
+        self.window().create_channel_intro_button_container.hide()
+        self.window().create_new_channel_intro_label.setText("Please enter your channel details below.")
--- TriblerGUI/videoplayerpage.py
+++ TriblerGUI/videoplayerpage.py
@@ -1,9 +1,8 @@
 import sys
 
 from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5.QtWidgets import QWidget, QToolButton, QSlider
-
-from TriblerGUI import vlc
+from PyQt5.QtWidgets import QWidget
+from Tribler import vlc
 
 
 class VideoPlayerPage(QWidget):
@@ -21,24 +20,17 @@ class VideoPlayerPage(QWidget):
     def initialize_player(self):
         self.instance = vlc.Instance()
         self.mediaplayer = self.instance.media_player_new()
-        self.video_player_widget = self.findChild(QWidget, "video_player_widget")
-        self.video_player_position_slider = self.findChild(QWidget, "video_player_position_slider")
-        self.video_player_position_slider.should_change_video_position.connect(self.on_should_change_video_time)
-        self.video_player_volume_slider = self.findChild(QSlider, "video_player_volume_slider")
-        self.video_player_volume_slider.valueChanged.connect(self.on_volume_change)
-        self.video_player_volume_slider.setValue(self.mediaplayer.audio_get_volume())
-        self.video_player_volume_slider.setFixedWidth(0)
+        self.window().video_player_position_slider.should_change_video_position.connect(self.on_should_change_video_time)
+        self.window().video_player_volume_slider.valueChanged.connect(self.on_volume_change)
+        self.window().video_player_volume_slider.setValue(self.mediaplayer.audio_get_volume())
+        self.window().video_player_volume_slider.setFixedWidth(0)
 
-        self.play_pause_button = self.findChild(QToolButton, "video_player_play_pause_button")
-        self.play_pause_button.clicked.connect(self.on_play_pause_button_click)
-        self.volume_button = self.findChild(QToolButton, "video_player_volume_button")
-        self.volume_button.clicked.connect(self.on_volume_button_click)
-        self.playlist_button = self.findChild(QToolButton, "video_player_playlist_button")
-        self.playlist_button.clicked.connect(self.on_playlist_button_click)
-        self.video_player_files_menu = self.findChild(QWidget, "video_player_files_menu")
-        self.video_player_files_menu.should_change_playing_file.connect(self.should_change_playing_file)
-        self.video_player_files_menu.initialize_file_menu()
-        self.video_player_files_menu.hide()
+        self.window().video_player_play_pause_button.clicked.connect(self.on_play_pause_button_click)
+        self.window().video_player_volume_button.clicked.connect(self.on_volume_button_click)
+        self.window().video_player_playlist_button.clicked.connect(self.on_playlist_button_click)
+        self.window().video_player_files_menu.should_change_playing_file.connect(self.should_change_playing_file)
+        self.window().video_player_files_menu.initialize_file_menu()
+        self.window().video_player_files_menu.hide()
 
         # Create play/pause and volume button images
         self.play_icon = QIcon(QPixmap("images/play.png"))
@@ -46,16 +38,16 @@ class VideoPlayerPage(QWidget):
         self.menu_icon = QIcon(QPixmap("images/menu_white.png"))
         self.volume_on_icon = QIcon(QPixmap("images/volume_on.png"))
         self.volume_off_icon = QIcon(QPixmap("images/volume_off.png"))
-        self.play_pause_button.setIcon(self.play_icon)
-        self.volume_button.setIcon(self.volume_on_icon)
-        self.playlist_button.setIcon(self.menu_icon)
+        self.window().video_player_play_pause_button.setIcon(self.play_icon)
+        self.window().video_player_volume_button.setIcon(self.volume_on_icon)
+        self.window().video_player_playlist_button.setIcon(self.menu_icon)
 
         if sys.platform.startswith('linux'):
-            self.mediaplayer.set_xwindow(self.video_player_widget.winId())
+            self.mediaplayer.set_xwindow(self.window().video_player_widget.winId())
         elif sys.platform == "win32":
-            self.mediaplayer.set_hwnd(self.video_player_widget.winId())
+            self.mediaplayer.set_hwnd(self.window().video_player_widget.winId())
         elif sys.platform == "darwin":
-            self.mediaplayer.set_nsobject(int(self.video_player_widget.winId()))
+            self.mediaplayer.set_nsobject(int(self.window().video_player_widget.winId()))
 
         self.manager = self.mediaplayer.event_manager()
         self.manager.event_attach(vlc.EventType.MediaPlayerPositionChanged, self.vlc_position_changed)
@@ -76,21 +68,21 @@ class VideoPlayerPage(QWidget):
 
     def on_play_pause_button_click(self):
         if not self.mediaplayer.is_playing():
-            self.play_pause_button.setIcon(self.pause_icon)
+            self.window().video_player_play_pause_button.setIcon(self.pause_icon)
             self.mediaplayer.play()
         else:
-            self.play_pause_button.setIcon(self.play_icon)
+            self.window().video_player_play_pause_button.setIcon(self.play_icon)
             self.mediaplayer.pause()
 
     def on_volume_button_click(self):
         if not self.mediaplayer.audio_get_mute():
-            self.volume_button.setIcon(self.volume_off_icon)
+            self.window().video_player_volume_button.setIcon(self.volume_off_icon)
         else:
-            self.volume_button.setIcon(self.volume_on_icon)
+            self.window().video_player_volume_button.setIcon(self.volume_on_icon)
         self.mediaplayer.audio_toggle_mute()
 
     def on_volume_change(self):
-        self.mediaplayer.audio_set_volume(self.video_player_volume_slider.value())
+        self.mediaplayer.audio_set_volume(self.window().video_player_volume_slider.value())
 
     def on_playlist_button_click(self):
         if self.video_player_files_menu.isHidden():
