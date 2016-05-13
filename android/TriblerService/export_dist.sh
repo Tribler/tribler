932a3122179079cd3e243902052e756efa4a067e
Paul Brussee <brussee@live.nl>
2016-05-13 21:07:51 +0200

    fix bugfix dist dir

--- android/TriblerService/export_dist.sh
+++ android/TriblerService/export_dist.sh
@@ -2,9 +2,9 @@
 
 echo Export dist
 
-mkdir -p dist/TriblerService
+mkdir -p dist
 
 p4a export_dist \
 --android_api=16 \
 --dist_name=TriblerService \
---output=./dist/TriblerService
+--output=./dist
