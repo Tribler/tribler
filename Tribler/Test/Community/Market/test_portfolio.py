875f3bb432b2fb4665f6e13442a8a940331c854d
joswinter <info@joswinter.nl>
2016-05-14 13:22:07 +0200

    Updated test_portfolio to new implementation

--- Tribler/Test/Community/Market/test_portfolio.py
+++ Tribler/Test/Community/Market/test_portfolio.py
@@ -1,8 +1,15 @@
 import unittest
 
 from Tribler.community.market.core.portfolio import Portfolio
-from Tribler.community.market.core.tick import TraderId, MessageNumber, MessageId, Price, Quantity, Timeout, Timestamp, \
-    Ask
+from Tribler.community.market.core.price import Price
+from Tribler.community.market.core.quantity import Quantity
+from Tribler.community.market.core.timestamp import Timestamp
+from Tribler.community.market.core.timeout import Timeout
+from Tribler.community.market.core.message import Message, TraderId, MessageNumber, MessageId
+from Tribler.community.market.core.tick import Tick, Ask, Bid
+from Tribler.community.market.core.order import OrderId, OrderNumber
+from Tribler.community.market.core.order_repository import MemoryOrderRepository
+
 
 
 class PortfolioTestSuite(unittest.TestCase):
@@ -10,22 +17,14 @@ class PortfolioTestSuite(unittest.TestCase):
 
     def test_portfolio(self):
         # Object creation
+        trader_id = TraderId('1')
         message_number = MessageNumber('message_number')
         timeout = Timeout(1462224447.117)
         timestamp = Timestamp(1462224447.117)
-        message_id = MessageId(TraderId('1'), message_number)
-
-        ask = Ask.create(message_id, Price(100), Quantity(30), timeout, timestamp)
-        portfolio = Portfolio()
-
-        # Test add tick
-        portfolio.add_tick(ask)
-        portfolio.add_tick(ask)
-        self.assertEquals(ask, portfolio.find_tick(message_id))
-
-        # Test remove tick by id
-        portfolio.delete_tick_by_id(message_id)
-        self.assertEquals(None, portfolio.find_tick(message_id))
+        message_id = MessageId(trader_id, message_number)
+        order_id = OrderId(trader_id, OrderNumber("order_number"))
+        ask = Ask(message_id, order_id, Price(100), Quantity(30), timeout, timestamp)
+        portfolio = Portfolio(MemoryOrderRepository("mid"))
 
 
 if __name__ == '__main__':
