f967979193a86b248d3f34d2058e14597f567c1c
Martijn de Vos <mdmartijndevos@gmail.com>
2016-05-15 16:03:54 +0200

    Written tests for get torrents in channel endpoint

--- Tribler/Test/Core/Modules/RestApi/test_channels_endpoints.py
+++ Tribler/Test/Core/Modules/RestApi/test_channels_endpoints.py
@@ -26,6 +26,9 @@ class AbstractTestChannelsEndpoint(AbstractApiTest):
     def vote_for_channel(self, cid, vote_time):
         self.votecast_db_handler.on_votes_from_dispersy([[cid, None, 'random', 2, vote_time]])
 
+    def insert_torrents_into_channel(self, torrent_list):
+        self.channel_db_handler.on_torrents_from_dispersy(torrent_list)
+
 
 class TestChannelsEndpoint(AbstractTestChannelsEndpoint):
 
@@ -86,6 +89,36 @@ class TestChannelsEndpoint(AbstractTestChannelsEndpoint):
             self.insert_channel_in_db('rand%d' % i, 42 + i, 'Test channel %d' % i, 'Test description %d' % i)
 
         return self.do_request('channels/discovered', expected_code=200).addCallback(self.verify_channels)
+
+
+class TestChannelTorrentsEndpoint(AbstractTestChannelsEndpoint):
+
+    @deferred(timeout=10)
+    def test_get_torrents_in_channel_invalid_cid(self):
+        """
+        Testing whether the API returns error 404 if a non-existent channel is queried for torrents
+        """
+        self.should_check_equality = False
+        return self.do_request('channels/discovered/abcd/torrents', expected_code=404)
+
+    @deferred(timeout=10)
+    def test_get_torrents_in_channel(self):
+        """
+        Testing whether the API returns inserted channels when fetching discovered channels
+        """
+        def verify_torrents(torrents):
+            torrents_json = json.loads(torrents)
+            self.assertEqual(len(torrents_json['torrents']), 1)
+            self.assertEqual(torrents_json['torrents'][0]['infohash'], 'a' * 40)
+
+        self.should_check_equality = False
+        channel_id = self.insert_channel_in_db('rand', 42, 'Test channel', 'Test description')
+
+        torrent_list = [[channel_id, 1, 1, ('a' * 40).decode('hex'), 1460000000, "ubuntu-torrent.iso", [], []]]
+        self.insert_torrents_into_channel(torrent_list)
+
+        return self.do_request('channels/discovered/%s/torrents' % 'rand'.encode('hex'), expected_code=200)\
+            .addCallback(verify_torrents)
 
 
 class TestChannelsSubscriptionEndpoint(AbstractTestChannelsEndpoint):
