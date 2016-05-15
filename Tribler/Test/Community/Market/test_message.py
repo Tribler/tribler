6e97f6c7a511f0b38ed9c9336316d56a0fbbe993
joswinter <info@joswinter.nl>
2016-05-15 09:56:31 +0200

    Divided MessageTestSuite and made seperate tests

--- Tribler/Test/Community/Market/test_message.py
+++ Tribler/Test/Community/Market/test_message.py
@@ -4,84 +4,100 @@ from Tribler.community.market.core.message import Message, TraderId, MessageNumb
 from Tribler.community.market.core.timestamp import Timestamp
 
 
+class TraderIdTestSuite(unittest.TestCase):
+    """Trader ID test cases."""
+
+    def setUp(self):
+        # Object creation
+        self.trader_id = TraderId('trader_id')
+        self.trader_id2 = TraderId('trader_id')
+        self.trader_id3 = TraderId('trader_id_2')
+
+    def test_conversion(self):
+        # Test for conversions
+        self.assertEqual('trader_id', str(self.trader_id))
+
+    def test_equality(self):
+        # Test for equality
+        self.assertTrue(self.trader_id == self.trader_id2)
+        self.assertTrue(self.trader_id == self.trader_id)
+        self.assertTrue(self.trader_id != self.trader_id3)
+        self.assertFalse(self.trader_id == 6)
+
+    def test_hash(self):
+        # Test for hashes
+        self.assertEqual(self.trader_id.__hash__(), self.trader_id2.__hash__())
+        self.assertNotEqual(self.trader_id.__hash__(), self.trader_id3.__hash__())
+
+
+class MessageNumberTestSuite(unittest.TestCase):
+    """Message number test cases."""
+
+    def setUp(self):
+        # Object creation
+        self.message_number = MessageNumber('message_number')
+        self.message_number2 = MessageNumber('message_number')
+        self.message_number3 = MessageNumber('message_number_2')
+
+    def test_conversion(self):
+        # Test for conversions
+        self.assertEqual('message_number', str(self.message_number))
+
+    def test_equality(self):
+        # Test for equality
+        self.assertTrue(self.message_number == self.message_number2)
+        self.assertTrue(self.message_number == self.message_number)
+        self.assertTrue(self.message_number != self.message_number3)
+        self.assertFalse(self.message_number == 6)
+
+    def test_hash(self):
+        # Test for hashes
+        self.assertEqual(self.message_number.__hash__(), self.message_number2.__hash__())
+        self.assertNotEqual(self.message_number.__hash__(), self.message_number3.__hash__())
+
+
+class MessageIdTestSuite(unittest.TestCase):
+    """Message ID test cases."""
+
+    def setUp(self):
+        # Object creation
+        self.message_id = MessageId(TraderId('trader_id'), MessageNumber('message_number'))
+        self.message_id2 = MessageId(TraderId('trader_id'), MessageNumber('message_number'))
+        self.message_id3 = MessageId(TraderId('trader_id'), MessageNumber('message_number2'))
+
+    def test_properties(self):
+        # Test for properties
+        self.assertEqual(TraderId('trader_id'), self.message_id.trader_id)
+        self.assertEqual(MessageNumber('message_number'), self.message_id.message_number)
+
+    def test_conversion(self):
+        # Test for conversions
+        self.assertEqual('trader_id.message_number', str(self.message_id))
+
+    def test_equality(self):
+        # Test for equality
+        self.assertTrue(self.message_id == self.message_id2)
+        self.assertTrue(self.message_id == self.message_id)
+        self.assertTrue(self.message_id != self.message_id3)
+        self.assertFalse(self.message_id == 6)
+
+    def test_hash(self):
+        # Test for hashes
+        self.assertEqual(self.message_id.__hash__(), self.message_id2.__hash__())
+        self.assertNotEqual(self.message_id.__hash__(), self.message_id3.__hash__())
+
+
 class MessageTestSuite(unittest.TestCase):
     """Message test cases."""
 
-    def test_trader_id(self):
+    def setUp(self):
         # Object creation
-        trader_id = TraderId('trader_id')
-        trader_id2 = TraderId('trader_id')
-        trader_id3 = TraderId('trader_id_2')
+        self.message = Message(MessageId(TraderId('trader_id'), MessageNumber('message_number')), Timestamp(float("inf")))
 
-        # Test for conversions
-        self.assertEqual('trader_id', str(trader_id))
-
-        # Test for equality
-        self.assertTrue(trader_id == trader_id2)
-        self.assertTrue(trader_id == trader_id)
-        self.assertTrue(trader_id != trader_id3)
-        self.assertFalse(trader_id == 6)
-
-        # Test for hashes
-        self.assertEqual(trader_id.__hash__(), trader_id2.__hash__())
-        self.assertNotEqual(trader_id.__hash__(), trader_id3.__hash__())
-
-    def test_message_number(self):
-        # Object creation
-        message_number = MessageNumber('message_number')
-        message_number2 = MessageNumber('message_number')
-        message_number3 = MessageNumber('message_number_2')
-
-        # Test for conversions
-        self.assertEqual('message_number', str(message_number))
-
-        # Test for equality
-        self.assertTrue(message_number == message_number2)
-        self.assertTrue(message_number == message_number)
-        self.assertTrue(message_number != message_number3)
-        self.assertFalse(message_number == 6)
-
-        # Test for hashes
-        self.assertEqual(message_number.__hash__(), message_number2.__hash__())
-        self.assertNotEqual(message_number.__hash__(), message_number3.__hash__())
-
-    def test_message_id(self):
-        # Object creation
-        trader_id = TraderId('trader_id')
-        message_number = MessageNumber('message_number')
-        message_number2 = MessageNumber('message_number2')
-        message_id = MessageId(trader_id, message_number)
-        message_id2 = MessageId(trader_id, message_number)
-        message_id3 = MessageId(trader_id, message_number2)
-
+    def test_properties(self):
         # Test for properties
-        self.assertEqual(trader_id, message_id.trader_id)
-        self.assertEqual(message_number, message_id.message_number)
-
-        # Test for conversions
-        self.assertEqual('trader_id.message_number', str(message_id))
-
-        # Test for equality
-        self.assertTrue(message_id == message_id2)
-        self.assertTrue(message_id == message_id)
-        self.assertTrue(message_id != message_id3)
-        self.assertFalse(message_id == 6)
-
-        # Test for hashes
-        self.assertEqual(message_id.__hash__(), message_id2.__hash__())
-        self.assertNotEqual(message_id.__hash__(), message_id3.__hash__())
-
-    def test_message(self):
-        # Object creation
-        trader_id = TraderId('trader_id')
-        message_number = MessageNumber('message_number')
-        message_id = MessageId(trader_id, message_number)
-        timestamp = Timestamp(float("inf"))
-        message = Message(message_id, timestamp)
-
-        # Test for properties
-        self.assertEqual(message_id, message.message_id)
-        self.assertEqual(timestamp, message.timestamp)
+        self.assertEqual(MessageId(TraderId('trader_id'), MessageNumber('message_number')), self.message.message_id)
+        self.assertEqual(Timestamp(float("inf")), self.message.timestamp)
 
 if __name__ == '__main__':
     unittest.main()
