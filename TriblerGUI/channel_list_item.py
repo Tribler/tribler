45afea76bca1401fb3f1c34cb85752db422f1ddf
Martijn de Vos <martijn@code-up.nl>
2016-05-19 19:38:11 +0200

    Implemented the funcitonality of the subscribe/unsubscribe button

--- TriblerGUI/channel_list_item.py
+++ TriblerGUI/channel_list_item.py
@@ -2,6 +2,7 @@
 from PyQt5 import uic
 from PyQt5.QtCore import QTimer, QPropertyAnimation
 from PyQt5.QtWidgets import QWidget, QGraphicsOpacityEffect
+from TriblerGUI.tribler_request_manager import TriblerRequestManager
 
 
 class ChannelListItem(QWidget):
@@ -15,13 +16,13 @@ class ChannelListItem(QWidget):
 
         uic.loadUi('qt_resources/channel_list_item.ui', self)
 
+        self.channel_info = channel
         self.channel_name.setText(channel["name"])
         self.channel_description_label.setText("Active 6 days ago • %d items" % channel["torrents"])
         self.channel_num_subs_label.setText(str(channel["votes"]))
-        if channel["subscribed"]:
-            self.channel_subscribe_button.setText("✓ subscribed")
-        else:
-            self.channel_subscribe_button.setText("subscribe")
+        self.channel_subscribe_button.clicked.connect(self.on_channel_subscribe_button_click)
+
+        self.update_subscribe_button()
 
         if should_fade:
             self.opacity_effect = QGraphicsOpacityEffect(self)
@@ -40,3 +41,26 @@ class ChannelListItem(QWidget):
         self.anim.setEndValue(1)
         self.anim.start()
         self.timer.stop()
+
+    def update_subscribe_button(self):
+        if self.channel_info["subscribed"]:
+            self.channel_subscribe_button.setText("✓ subscribed")
+        else:
+            self.channel_subscribe_button.setText("subscribe")
+
+    def on_channel_subscribe_button_click(self):
+        self.request_mgr = TriblerRequestManager()
+        if self.channel_info["subscribed"]:
+            self.request_mgr.perform_request("channels/subscribed/%s" % self.channel_info['dispersy_cid'], self.on_channel_unsubscribed, method='DELETE')
+        else:
+            self.request_mgr.perform_request("channels/subscribed/%s" % self.channel_info['dispersy_cid'], self.on_channel_subscribed, method='PUT')
+
+    def on_channel_unsubscribed(self, json_result):
+        if json_result["unsubscribed"]:
+            self.channel_info["subscribed"] = False
+            self.update_subscribe_button()
+
+    def on_channel_subscribed(self, json_result):
+        if json_result["subscribed"]:
+            self.channel_info["subscribed"] = True
+            self.update_subscribe_button()
