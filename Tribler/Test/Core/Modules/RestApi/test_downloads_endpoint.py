9553beacd2ec952eef3bf95b2d38e2f27ead32a2
Martijn de Vos <martijn@code-up.nl>
2016-05-20 17:02:52 +0200

    Written tests for the resume, stop and remove download endpoint

--- Tribler/Test/Core/Modules/RestApi/test_downloads_endpoint.py
+++ Tribler/Test/Core/Modules/RestApi/test_downloads_endpoint.py
@@ -4,6 +4,7 @@ from urllib import pathname2url
 
 from Tribler.Core.DownloadConfig import DownloadStartupConfig
 from Tribler.Core.Utilities.twisted_thread import deferred
+from Tribler.Core.simpledefs import DLSTATUS_STOPPED
 from Tribler.Test.Core.Modules.RestApi.base_api_test import AbstractApiTest
 from Tribler.Test.test_as_server import TESTS_DATA_DIR
 
@@ -37,3 +38,98 @@ class TestDownloadsEndpoint(AbstractApiTest):
 
         self.should_check_equality = False
         return self.do_request('downloads', expected_code=200).addCallback(verify_download)
+
+    @deferred(timeout=10)
+    def test_remove_download_no_remove_data_param(self):
+        """
+        Testing whether the API returns error 400 if the remove_data parameter is not passed
+        """
+        self.should_check_equality = False
+        return self.do_request('downloads/abcd/remove', expected_code=400, request_type='DELETE')
+
+    @deferred(timeout=10)
+    def test_remove_download_wrong_infohash(self):
+        """
+        Testing whether the API returns error 404 if a non-existent download is removed
+        """
+        self.should_check_equality = False
+        return self.do_request('downloads/abcd/remove', post_data={"remove_data": True},
+                               expected_code=404, request_type='DELETE')
+
+    @deferred(timeout=10)
+    def test_remove_download(self):
+        """
+        Testing whether the API returns 200 if a download is being removed
+        """
+        def verify_removed(_):
+            self.assertEqual(len(self.session.get_downloads()), 0)
+
+        video_tdef, self.torrent_path = self.create_local_torrent(os.path.join(TESTS_DATA_DIR, 'video.avi'))
+        self.session.start_download_from_tdef(video_tdef, DownloadStartupConfig())
+        infohash = video_tdef.get_infohash().encode('hex')
+
+        return self.do_request('downloads/%s/remove' % infohash, post_data={"remove_data": True},
+                               expected_code=200, expected_json={"removed": True}, request_type='DELETE')\
+            .addCallback(verify_removed)
+
+    @deferred(timeout=10)
+    def test_stop_download_wrong_infohash(self):
+        """
+        Testing whether the API returns error 404 if a non-existent download is stopped
+        """
+        self.should_check_equality = False
+        return self.do_request('downloads/abcd/stop', expected_code=404, request_type='POST')
+
+    @deferred(timeout=10)
+    def test_stop_download(self):
+        """
+        Testing whether the API returns 200 if a download is being stopped
+        """
+        video_tdef, self.torrent_path = self.create_local_torrent(os.path.join(TESTS_DATA_DIR, 'video.avi'))
+        download = self.session.start_download_from_tdef(video_tdef, DownloadStartupConfig())
+        infohash = video_tdef.get_infohash().encode('hex')
+
+        def mocked_stop():
+            download.should_stop = True
+
+        def verify_removed(_):
+            self.assertEqual(len(self.session.get_downloads()), 1)
+            download = self.session.get_downloads()[0]
+            self.assertTrue(download.should_stop)
+
+        download.stop = mocked_stop
+
+        return self.do_request('downloads/%s/stop' % infohash, post_data={"stopped": True},
+                               expected_code=200, expected_json={"stopped": True}, request_type='POST')\
+            .addCallback(verify_removed)
+
+    @deferred(timeout=10)
+    def test_resume_download_wrong_infohash(self):
+        """
+        Testing whether the API returns error 404 if a non-existent download is resumed
+        """
+        self.should_check_equality = False
+        return self.do_request('downloads/abcd/resume', expected_code=404, request_type='POST')
+
+    @deferred(timeout=10)
+    def test_resume_download(self):
+        """
+        Testing whether the API returns 200 if a download is being resumed
+        """
+        video_tdef, self.torrent_path = self.create_local_torrent(os.path.join(TESTS_DATA_DIR, 'video.avi'))
+        download = self.session.start_download_from_tdef(video_tdef, DownloadStartupConfig())
+        infohash = video_tdef.get_infohash().encode('hex')
+
+        def mocked_restart():
+            download.should_restart = True
+
+        def verify_resumed(_):
+            self.assertEqual(len(self.session.get_downloads()), 1)
+            download = self.session.get_downloads()[0]
+            self.assertTrue(download.should_restart)
+
+        download.restart = mocked_restart
+
+        return self.do_request('downloads/%s/resume' % infohash, post_data={"resumed": True},
+                               expected_code=200, expected_json={"resumed": True}, request_type='POST')\
+            .addCallback(verify_resumed)
