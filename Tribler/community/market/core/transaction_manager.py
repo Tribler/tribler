741019f7c51878d2a4248270f1c928bdce132e53
mitchellolsthoorn <mitchell.olsthoorn@outlook.com>
2016-05-25 10:59:02 +0200

    Added transaction manager for managing the transactions

---
+++ Tribler/community/market/core/transaction_manager.py
@@ -0,0 +1,8 @@
+class TransactionManager(object):
+    """Manager for retrieving editing and creating transactions"""
+
+    def __init__(self):
+        """
+        Initialise the transaction manager
+        """
+        super(TransactionManager, self).__init__()
