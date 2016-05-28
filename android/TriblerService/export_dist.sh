8bc5b0cb5480de874d890e840a64ccc34ee70ce3
Paul Brussee <brussee@live.nl>
2016-05-28 22:25:58 +0200

    copy create dist params for export dist

--- android/TriblerService/export_dist.sh
+++ android/TriblerService/export_dist.sh
@@ -6,8 +6,14 @@ mkdir -p dist
 
 p4a export_dist \
 --require-perfect-match \
+--copy-libs \
+--debug \
 --android_api=16 \
+--arch=armeabi-v7a \
+--package=org.tribler.android \
+--service=Triblerd:Triblerd.py \
+--private=./service \
 --dist_name=TriblerService \
 --bootstrap=service_only \
 --requirements=libtribler_local \
---output=./dist
+--whitelist=.p4a-whitelist
