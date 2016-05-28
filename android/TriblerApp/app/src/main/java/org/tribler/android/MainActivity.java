e14b354d5de207a197ff59beaf6b80cd01a4140a
Paul Brussee <brussee@live.nl>
2016-05-28 23:24:21 +0200

    enable TriblerService

--- android/TriblerApp/app/src/main/java/org/tribler/android/MainActivity.java
+++ android/TriblerApp/app/src/main/java/org/tribler/android/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //ServiceTriblerd.start(this, "");
+        ServiceTriblerd.start(this, "");
         initGui();
     }
 
