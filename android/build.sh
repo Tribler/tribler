b2f8487ef01c3939b0727e2bdc6fff236c29b912
Paul Brussee <brussee@live.nl>
2016-05-28 22:17:19 +0200

    copy bootstraps on build

--- android/build.sh
+++ android/build.sh
@@ -9,7 +9,7 @@ export PATH="~/.local/bin/:$PATH"
 #pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
 
 #copy bootstraps and recipes
-#cp -R bootstraps ~/.local/lib/python2.7/site-packages/pythonforandroid/
+cp -R bootstraps ~/.local/lib/python2.7/site-packages/pythonforandroid/
 cp -R recipes ~/.local/lib/python2.7/site-packages/pythonforandroid/
 
 #create dist and build
@@ -17,4 +17,4 @@ script -c "./clean_dist.sh"
 script -a -c "./create_dist.sh"
 script -a -c "./export_dist.sh"
 script -a -c "./build_dist.sh"
-#script -a -c "./install_dist.sh"
+script -a -c "./install_dist.sh"
