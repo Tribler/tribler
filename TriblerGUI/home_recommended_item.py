49f11edaceb9c29f5a71f2397b5d37a3a0fc975a
Martijn de Vos <martijn@code-up.nl>
2016-06-01 13:52:40 +0200

    Implemented more on the home page

--- TriblerGUI/home_recommended_item.py
+++ TriblerGUI/home_recommended_item.py
@@ -1,15 +1,29 @@
-# coding=utf-8
 from PyQt5 import uic
-from PyQt5.QtCore import QTimer, QPropertyAnimation
-from PyQt5.QtWidgets import QWidget, QGraphicsOpacityEffect
-from TriblerGUI.tribler_request_manager import TriblerRequestManager
+from PyQt5.QtWidgets import QWidget
+from TriblerGUI.utilities import pretty_date
+
+HOME_ITEM_FONT_SIZE = 44
 
 
-class HomeRecommendedItem(QWidget):
+class HomeRecommendedChannelItem(QWidget):
 
-    def __init__(self, parent):
+    def __init__(self, parent, channel):
         super(QWidget, self).__init__(parent)
 
         uic.loadUi('qt_resources/home_recommended_item.ui', self)
+        self.thumbnail_widget.initialize(channel["name"], HOME_ITEM_FONT_SIZE)
 
-        self.thumbnail_widget.initialize("Pioneer One", 44)
+        self.main_label.setText(channel["name"])
+        self.detail_label.setText("Updated " + pretty_date(channel["modified"]))
+
+
+class HomeRecommendedTorrentItem(QWidget):
+
+    def __init__(self, parent, torrent):
+        super(QWidget, self).__init__(parent)
+
+        uic.loadUi('qt_resources/home_recommended_item.ui', self)
+        self.thumbnail_widget.initialize(torrent["name"], HOME_ITEM_FONT_SIZE)
+
+        self.main_label.setText(torrent["name"])
+        self.detail_label.setText("Added " + pretty_date(torrent["added"]))
--- TriblerGUI/utilities.py
+++ TriblerGUI/utilities.py
@@ -51,3 +51,47 @@ def is_video_file(filename):
     if ext.startswith('.'):
         ext = ext[1:]
     return ext in VIDEO_EXTS
+
+
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + " months ago"
+    return str(day_diff / 365) + " years ago"
