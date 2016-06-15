7a5da6d5af959331642258c741330c734dace49c
joswinter <info@joswinter.nl>
2016-06-15 20:29:38 +0200

    Added comments to price level list

--- Tribler/community/market/core/pricelevel_list.py
+++ Tribler/community/market/core/pricelevel_list.py
@@ -3,26 +3,41 @@ from pricelevel import PriceLevel
 
 
 class PriceLevelList(object):
+    "Sorted doubly linked dictionary implementation."
+
     def __init__(self):
         super(PriceLevelList, self).__init__()
         self._price_list = []
         self._price_level_dictionary = {}
 
-    def insert(self, price, pricelevel):
+    def insert(self, price, price_level):
+        """
+        :type price: Price
+        :type price_level: PriceLevel
+        """
         assert isinstance(price, Price), type(price)
-        assert isinstance(pricelevel, PriceLevel), type(pricelevel)
+        assert isinstance(price_level, PriceLevel), type(price_level)
 
         self._price_list.append(price)
         self._price_list.sort()
-        self._price_level_dictionary[price] = pricelevel
+        self._price_level_dictionary[price] = price_level
 
     def remove(self, price):
+        """
+        :type price: Price
+        """
         assert isinstance(price, Price), type(price)
 
         self._price_list.remove(price)
         del self._price_level_dictionary[price]
 
     def succ_item(self, price):
+        """
+        Returns (price, price_level) pair where price is successor to given price
+
+        :type price: Price
+        :rtype: (Price, PriceLevel)
+        """
         assert isinstance(price, Price), type(price)
 
         index = self._price_list.index(price) + 1
@@ -32,6 +47,12 @@ class PriceLevelList(object):
         return succ_price, self._price_level_dictionary[succ_price]
 
     def prev_item(self, price):
+        """
+        Returns (price, price_level) pair where price is predecessor to given price
+
+        :type price: Price
+        :rtype: (Price, PriceLevel)
+        """
         assert isinstance(price, Price), type(price)
 
         index = self._price_list.index(price) - 1
@@ -41,12 +62,29 @@ class PriceLevelList(object):
         return prev_price, self._price_level_dictionary[prev_price]
 
     def min_key(self):
+        """
+        Return the lowest price in the price level list
+
+        :rtype: Price
+        """
         return self._price_list[0]
 
     def max_key(self):
+        """
+        Return the highest price in the price level list
+
+        :rtype: Price
+        """
         return self._price_list[-1]
 
     def items(self, reverse=False):
+        """
+        Returns a sorted list of price, price_level tuples
+
+        :param reverse: When true returns the reversed sorted list of price, price_level tuples
+        :type reverse: bool
+        :rtype: [(Price, PriceLevel)]
+        """
         items = []
         for price in self._price_list:
             if reverse:
