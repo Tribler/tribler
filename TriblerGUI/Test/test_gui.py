d2cb2f378295b1d3c982f32ac0c1100849e39507
Martijn de Vos <mdmartijndevos@gmail.com>
2016-06-19 13:46:34 +0200

    Extended test suite

--- TriblerGUI/Test/test_gui.py
+++ TriblerGUI/Test/test_gui.py
@@ -2,6 +2,7 @@ import os
 import sys
 import traceback
 import unittest
+from random import randint
 
 from PyQt5.QtCore import QPoint, Qt
 from PyQt5.QtGui import QPixmap, QRegion
@@ -26,7 +27,7 @@ class TimeoutException(Exception):
     pass
 
 
-class TriblerGUITest(unittest.TestCase):
+class AbstractTriblerGUITest(unittest.TestCase):
 
     def setUp(self):
         QTest.qWait(100)
@@ -35,6 +36,11 @@ class TriblerGUITest(unittest.TestCase):
 
     def tearDown(self):
         window.downloads_page.can_update_items = False
+
+    def go_to_and_wait_for_downloads(self):
+        QTest.mouseClick(window.left_menu_button_downloads, Qt.LeftButton)
+        QTest.mouseClick(window.downloads_all_button, Qt.LeftButton)
+        self.wait_for_variable("downloads_page.downloads")
 
     def screenshot(self, widget, name=None):
         pixmap = QPixmap(widget.rect().size())
@@ -47,10 +53,10 @@ class TriblerGUITest(unittest.TestCase):
 
         pixmap.save(os.path.join(os.path.dirname(TriblerGUI.__file__), 'screenshots', img_name))
 
-    def wait_for_list_populated(self, list, timeout=10):
+    def wait_for_list_populated(self, list, num_items=1, timeout=10):
         for _ in range(0, timeout * 1000, 100):
             QTest.qWait(100)
-            if (isinstance(list, QListWidget) and list.count > 0) or \
+            if (isinstance(list, QListWidget) and list.count >= num_items) or \
                     (isinstance(list, QTreeWidget) and list.topLevelItemCount > 0):
                 return
 
@@ -89,12 +95,24 @@ class TriblerGUITest(unittest.TestCase):
             if self.get_attr_recursive(var) is not None:
                 return
 
-        raise TimeoutException("Did not receive downloads within 10 seconds")
+        raise TimeoutException("Variable %s within 10 seconds" % var)
 
-    def go_to_and_wait_for_downloads(self):
-        QTest.mouseClick(window.left_menu_button_downloads, Qt.LeftButton)
-        QTest.mouseClick(window.downloads_all_button, Qt.LeftButton)
-        self.wait_for_variable("downloads_page.downloads")
+    def wait_for_signal(self, signal, timeout=10):
+        self.signal_received = False
+        def on_signal(_):
+            self.signal_received = True
+
+        signal.connect(on_signal)
+
+        for _ in range(0, timeout * 1000, 100):
+            QTest.qWait(100)
+            if self.signal_received:
+                return
+
+        raise TimeoutException("Signal %s not raised within 10 seconds" % signal)
+
+
+class TriblerGUITest(AbstractTriblerGUITest):
 
     def test_home_page_torrents(self):
         QTest.mouseClick(window.left_menu_button_home, Qt.LeftButton)
@@ -115,6 +133,11 @@ class TriblerGUITest(unittest.TestCase):
         self.screenshot(window, name="subscriptions_loading")
         self.wait_for_list_populated(window.subscribed_channels_list)
         self.screenshot(window, name="subscriptions")
+
+        first_widget = window.subscribed_channels_list.itemWidget(window.subscribed_channels_list.item(0))
+        QTest.mouseClick(first_widget.channel_subscribe_button, Qt.LeftButton)
+        self.screenshot(window, name="unsubscribed")
+        QTest.mouseClick(first_widget.channel_subscribe_button, Qt.LeftButton)
 
     def test_edit_channel_overview(self):
         QTest.mouseClick(window.left_menu_button_my_channel, Qt.LeftButton)
@@ -216,6 +239,97 @@ class TriblerGUITest(unittest.TestCase):
         self.screenshot(window, name="download_files")
         window.download_details_widget.setCurrentIndex(2)
         self.screenshot(window, name="download_trackers")
+
+    def test_search_suggestions(self):
+        QTest.keyClick(window.top_search_bar, 't')
+        QTest.keyClick(window.top_search_bar, 'r')
+        self.wait_for_signal(window.received_search_completions)
+        self.screenshot(window, name="search_suggestions")
+
+    def test_search(self):
+        window.top_search_bar.setText("trib")
+        QTest.keyClick(window.top_search_bar, Qt.Key_Enter)
+        self.wait_for_list_populated(window.search_results_list, num_items=20)
+        self.screenshot(window, name="search_results_all")
+
+        QTest.mouseClick(window.search_results_channels_button, Qt.LeftButton)
+        self.wait_for_list_populated(window.search_results_list)
+        self.screenshot(window, name="search_results_channels")
+        QTest.mouseClick(window.search_results_torrents_button, Qt.LeftButton)
+        self.wait_for_list_populated(window.search_results_list)
+        self.screenshot(window, name="search_results_torrents")
+
+    def test_channel_playlist(self):
+        QTest.mouseClick(window.left_menu_button_subscriptions, Qt.LeftButton)
+        self.wait_for_list_populated(window.subscribed_channels_list)
+        first_widget = window.subscribed_channels_list.itemWidget(window.subscribed_channels_list.item(0))
+        QTest.mouseClick(first_widget, Qt.LeftButton)
+        self.screenshot(window, name="channel_loading")
+        self.wait_for_list_populated(window.channel_torrents_list)
+        self.screenshot(window, name="channel")
+
+        first_widget = window.channel_torrents_list.itemWidget(window.channel_torrents_list.item(0))
+        QTest.mouseClick(first_widget, Qt.LeftButton)
+        self.screenshot(window, name="channel_playlist")
+
+    def test_create_remove_playlist(self):
+        QTest.mouseClick(window.left_menu_button_my_channel, Qt.LeftButton)
+        self.wait_for_variable("edit_channel_page.channel_overview")
+        QTest.mouseClick(window.edit_channel_playlists_button, Qt.LeftButton)
+        self.wait_for_list_populated(window.edit_channel_playlists_list)
+        old_count = window.edit_channel_playlists_list.count()
+        QTest.mouseClick(window.edit_channel_create_playlist_button, Qt.LeftButton)
+        self.screenshot(window, "create_playlist")
+
+        # Create playlist
+        window.playlist_edit_name.setText("Unit test playlist")
+        window.playlist_edit_description.setText("Unit test playlist description")
+        QTest.mouseClick(window.playlist_edit_save_button, Qt.LeftButton)
+        self.wait_for_signal(window.edit_channel_page.playlists_loaded)
+        self.assertEqual(old_count + 1, window.edit_channel_playlists_list.count())
+
+        # Remove playlist
+        last_widget = window.edit_channel_playlists_list.itemWidget(window.edit_channel_playlists_list.item(old_count))
+        QTest.mouseClick(last_widget.remove_playlist_button, Qt.LeftButton)
+        self.screenshot(window, name="remove_playlist_dialog")
+        QTest.mouseClick(window.edit_channel_page.dialog.buttons[0], Qt.LeftButton)
+        self.wait_for_signal(window.edit_channel_page.playlists_loaded)
+        self.assertEqual(old_count, window.edit_channel_playlists_list.count())
+
+    def test_edit_playlist(self):
+        QTest.mouseClick(window.left_menu_button_my_channel, Qt.LeftButton)
+        self.wait_for_variable("edit_channel_page.channel_overview")
+        QTest.mouseClick(window.edit_channel_playlists_button, Qt.LeftButton)
+        self.wait_for_list_populated(window.edit_channel_playlists_list)
+
+        first_widget = window.edit_channel_playlists_list.itemWidget(window.edit_channel_playlists_list.item(0))
+        QTest.mouseClick(first_widget.edit_playlist_button, Qt.LeftButton)
+        self.screenshot(window, name="edit_playlist")
+
+        rand_name = "Random name %d" % randint(1, 1000)
+        rand_desc = "Random description %d" % randint(1, 1000)
+
+        window.playlist_edit_name.setText(rand_name)
+        window.playlist_edit_description.setText(rand_desc)
+        QTest.mouseClick(window.playlist_edit_save_button, Qt.LeftButton)
+        self.wait_for_signal(window.edit_channel_page.playlists_loaded)
+
+        first_widget = window.edit_channel_playlists_list.itemWidget(window.edit_channel_playlists_list.item(0))
+        self.assertEqual(first_widget.playlist_name.text(), rand_name)
+
+    def test_add_download_url(self):
+        window.on_add_torrent_from_url()
+        self.go_to_and_wait_for_downloads()
+        old_count = window.downloads_list.topLevelItemCount()
+        self.screenshot(window, name="add_torrent_url_dialog")
+        window.dialog.dialog_widget.dialog_input.setText("http://test.url/test.torrent")
+        QTest.mouseClick(window.dialog.buttons[0], Qt.LeftButton)
+        self.wait_for_signal(window.downloads_page.received_downloads)
+        self.assertEqual(window.downloads_list.topLevelItemCount(), old_count + 1)
+
+    def test_video_player_page(self):
+        QTest.mouseClick(window.left_menu_button_video_player, Qt.LeftButton)
+        self.screenshot(window, name="video_player_page")
 
 if __name__ == "__main__":
     unittest.main()
