6d0aa087140115f7619a73e4ee0951bc33874b80
Martijn de Vos <martijn@code-up.nl>
2016-06-24 14:23:55 +0200

    Extended the events endpoint with more events

--- Tribler/Core/Modules/restapi/events_endpoint.py
+++ Tribler/Core/Modules/restapi/events_endpoint.py
@@ -1,8 +1,8 @@
 import json
 from twisted.web import server, resource
 from Tribler.Core.Modules.restapi.util import convert_db_channel_to_json, convert_torrent_to_json
-from Tribler.Core.simpledefs import NTFY_CHANNELCAST, SIGNAL_CHANNEL, SIGNAL_ON_SEARCH_RESULTS, SIGNAL_TORRENT
-
+from Tribler.Core.simpledefs import NTFY_CHANNELCAST, SIGNAL_CHANNEL, SIGNAL_ON_SEARCH_RESULTS, SIGNAL_TORRENT, \
+    NTFY_UPGRADER, NTFY_STARTED, NTFY_WATCH_FOLDER_CORRUPT_TORRENT, NTFY_INSERT, NTFY_NEW_VERSION, NTFY_FINISHED
 
 MAX_EVENTS_BUFFER_SIZE = 100
 
@@ -11,13 +11,18 @@ class EventsEndpoint(resource.Resource):
     """
     Important events in Tribler are returned over the events endpoint. This connection is held open. Each event is
     pushed over this endpoint in the form of a JSON dictionary. Each JSON dictionary contains a type field that
-    indicates the type of the event.
+    indicates the type of the event. Individual events are separated by a newline character (\n).
 
     Currently, the following events are implemented:
 
     - events_start: An indication that the event socket is opened and that the server is ready to push events.
     - search_result_channel: This event dictionary contains a search result with a channel that has been found.
     - search_result_torrent: This event dictionary contains a search result with a torrent that has been found.
+    - upgrader_started: An indication that the Tribler upgrader has started.
+    - upgrader_finished: An indication that the Tribler upgrader has finished.
+    - watch_folder_corrupt_torrent: This event is emitted when a corrupt .torrent file in the watch folder is found.
+      The dictionary contains the name of the corrupt torrent file.
+    - new_version_available: This event is emitted when a new version of Tribler is available.
     """
 
     def __init__(self, session):
@@ -32,6 +37,11 @@ class EventsEndpoint(resource.Resource):
 
         self.session.add_observer(self.on_search_results_channels, SIGNAL_CHANNEL, [SIGNAL_ON_SEARCH_RESULTS])
         self.session.add_observer(self.on_search_results_torrents, SIGNAL_TORRENT, [SIGNAL_ON_SEARCH_RESULTS])
+        self.session.add_observer(self.on_upgrader_started, NTFY_UPGRADER, [NTFY_STARTED])
+        self.session.add_observer(self.on_upgrader_finished, NTFY_UPGRADER, [NTFY_FINISHED])
+        self.session.add_observer(self.on_watch_folder_corrupt_torrent,
+                                  NTFY_WATCH_FOLDER_CORRUPT_TORRENT, [NTFY_INSERT])
+        self.session.add_observer(self.on_new_version_available, NTFY_NEW_VERSION, [NTFY_INSERT])
 
     def write_data(self, message):
         """
@@ -42,7 +52,7 @@ class EventsEndpoint(resource.Resource):
                 self.buffer.pop(0)
             self.buffer.append(message)
         else:
-            self.events_request.write(message)
+            self.events_request.write(message + '\n')
 
     def start_new_query(self):
         self.infohashes_sent = set()
@@ -71,8 +81,20 @@ class EventsEndpoint(resource.Resource):
             torrent_json = convert_torrent_to_json(torrent)
             if 'infohash' in torrent_json and torrent_json['infohash'] not in self.infohashes_sent:
                 self.write_data(json.dumps({"type": "search_result_torrent",
-                                            "event": {"query": query, "result": torrent_json}}) + '\n')
+                                            "event": {"query": query, "result": torrent_json}}))
                 self.infohashes_sent.add(torrent_json['infohash'])
+
+    def on_upgrader_started(self, subject, changetype, objectID, *args):
+        self.write_data(json.dumps({"type": "upgrader_started"}))
+
+    def on_upgrader_finished(self, subject, changetype, objectID, *args):
+        self.write_data(json.dumps({"type": "upgrader_finished"}))
+
+    def on_watch_folder_corrupt_torrent(self, subject, changetype, objectID, *args):
+        self.write_data(json.dumps({"type": "watch_folder_corrupt_torrent", "event": {"name": args[0]}}))
+
+    def on_new_version_available(self, subject, changetype, objectID, *args):
+        self.write_data(json.dumps({"type": "new_version_available", "event": {"version": args[0]}}))
 
     def render_GET(self, request):
         """
@@ -88,9 +110,9 @@ class EventsEndpoint(resource.Resource):
         """
         self.events_request = request
 
-        request.write(json.dumps({"type": "events_start"}))
+        request.write(json.dumps({"type": "events_start"}) + '\n')
 
         while not len(self.buffer) == 0:
-            request.write(self.buffer.pop(0))
+            request.write(self.buffer.pop(0) + '\n')
 
         return server.NOT_DONE_YET
