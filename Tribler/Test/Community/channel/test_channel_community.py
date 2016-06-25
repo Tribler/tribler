c1a68e5ed99d80bdf3e833c9c442c6d3aa2dd573
Martijn de Vos <martijn@code-up.nl>
2016-06-25 12:44:11 +0200

    Written test for removing a torrent from playlist

--- Tribler/Test/Community/channel/test_channel_community.py
+++ Tribler/Test/Community/channel/test_channel_community.py
@@ -1,4 +1,5 @@
 from Tribler.Test.Community.channel.test_channel_base import AbstractTestChannelCommunity
+from Tribler.Test.Core.base_test import MockObject
 from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 
@@ -12,3 +13,32 @@ class TestChannelCommunity(AbstractTestChannelCommunity):
         self.channel_community.initialize()
         self.assertIsNone(self.channel_community._channelcast_db)
 
+    def test_remove_playlist_torrents(self):
+        """
+        Testing whether the right methods are called when a torrent is removed from a playlist
+        """
+        def mocked_load_message(undone, community, packet_id):
+            fake_message = MockObject()
+            fake_message.undone = undone
+            return fake_message
+
+        def mocked_create_undo(_):
+            mocked_create_undo.called = True
+        mocked_create_undo.called = False
+
+        def mocked_undo_playlist_torrent(_):
+            mocked_undo_playlist_torrent.called = True
+        mocked_undo_playlist_torrent.called = False
+
+        self.channel_community.create_undo = mocked_create_undo
+        self.channel_community._disp_undo_playlist_torrent = mocked_undo_playlist_torrent
+
+        self.channel_community._dispersy.load_message_by_packetid = \
+            lambda community, pid: mocked_load_message(False, community, pid)
+        self.channel_community.remove_playlist_torrents(1234, [1234])
+        self.assertTrue(mocked_create_undo.called)
+
+        self.channel_community._dispersy.load_message_by_packetid = \
+            lambda community, pid: mocked_load_message(True, community, pid)
+        self.channel_community.remove_playlist_torrents(1234, [1234])
+        self.assertTrue(mocked_undo_playlist_torrent.called)
