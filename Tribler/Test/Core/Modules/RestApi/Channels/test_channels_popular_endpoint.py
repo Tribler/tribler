93442cd7eb600b31fc6e85a2da8e57f6e64f5d04
Martijn de Vos <mdmartijndevos@gmail.com>
2016-06-29 19:23:43 +0200

    Written tests for the popular channels endpoint

---
+++ Tribler/Test/Core/Modules/RestApi/Channels/test_channels_popular_endpoint.py
@@ -0,0 +1,30 @@
+import json
+
+from Tribler.Core.Utilities.twisted_thread import deferred
+from Tribler.Test.Core.Modules.RestApi.Channels.test_channels_endpoint import AbstractTestChannelsEndpoint
+
+
+class TestChannelsPlaylistEndpoints(AbstractTestChannelsEndpoint):
+
+    @deferred(timeout=10)
+    def test_popular_channels_endpoint(self):
+        """
+        Testing whether the API returns some popular channels if the are queried
+        """
+        def verify_channels(results):
+            json_results = json.loads(results)
+            self.assertEqual(len(json_results['channels']), 5)
+
+        for i in xrange(0, 10):
+            self.insert_channel_in_db('rand%d' % i, 42 + i, 'Test channel %d' % i, 'Test description %d' % i)
+
+        self.should_check_equality = False
+        return self.do_request('channels/popular?limit=5', expected_code=200).addCallback(verify_channels)
+
+    @deferred(timeout=10)
+    def test_popular_channels_limit_neg(self):
+        """
+        Testing whether error 400 is returned when a negative limit is passed to the request to fetch popular channels
+        """
+        expected_json = {"error": "the limit parameter must be a positive number"}
+        return self.do_request('channels/popular?limit=-5', expected_code=400, expected_json=expected_json)
