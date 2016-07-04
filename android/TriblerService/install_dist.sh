18327a34c150e0afa3dccdc7f30d2bbb538f0fe8
Paul Brussee <brussee@live.nl>
2016-07-04 16:02:52 +0200

    use force

--- android/TriblerService/install_dist.sh
+++ android/TriblerService/install_dist.sh
@@ -4,9 +4,9 @@ echo Install dist
 
 cd dist/TriblerService
 
-mv libs jniLibs
-mv python-install/include jni/include
-mv python-install/lib jni/lib
+mv -f libs jniLibs
+mv -f python-install/include jni/include
+mv -f python-install/lib jni/lib
 
 rm -rf python-install
 rm -rf collated_objects
@@ -14,8 +14,8 @@ rm -rf private
 rm -rf python-install
 rm -rf templates
 rm -rf build
-rm -r jni/*.mk
-rm -r jni/src/*.mk
+rm -rf jni/*.mk
+rm -rf jni/src/*.mk
 rm -f blacklist.txt
 rm -f whitelist.txt
 rm -f build.py
@@ -25,13 +25,13 @@ rm -f project.properties
 cd ../..
 
 rm -rf dist/TriblerApp-import
-mv dist/TriblerService dist/TriblerApp-import
+mv -f dist/TriblerService dist/TriblerApp-import
 
 rm -rf ../TriblerApp/app/src/main/assets
 rm -rf ../TriblerApp/app/src/main/jni
 rm -rf ../TriblerApp/app/src/main/jniLibs
 
-cp -R dist/TriblerApp-import/assets ../TriblerApp/app/src/main
-cp -R dist/TriblerApp-import/jni ../TriblerApp/app/src/main
-cp -R dist/TriblerApp-import/jniLibs ../TriblerApp/app/src/main
+cp -rf dist/TriblerApp-import/assets ../TriblerApp/app/src/main
+cp -rf dist/TriblerApp-import/jni ../TriblerApp/app/src/main
+cp -rf dist/TriblerApp-import/jniLibs ../TriblerApp/app/src/main
 
