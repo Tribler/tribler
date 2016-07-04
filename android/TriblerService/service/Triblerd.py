496ef6aa1a2946f89aaafc25bbec6ec85d8fb86b
Paul Brussee <brussee@live.nl>
2016-07-04 19:12:56 +0200

    add test entry point

--- android/TriblerService/service/Triblerd.py
+++ android/TriblerService/service/Triblerd.py
@@ -1,9 +1,5 @@
 import os
 
-from twisted.internet import reactor
-
-from tribler_plugin import service_maker, Options
-
 
 class Triblerd(object):
 
@@ -13,17 +9,29 @@ class Triblerd(object):
         '''
         private_root_dir = os.path.realpath(os.path.split(os.getenv('ANDROID_PRIVATE', '/tmp/cache'))[0])
         os.environ['PYTHON_EGG_CACHE'] = os.path.join(private_root_dir, 'cache', '.egg')
-        
-    
+
     def run(self):
         '''
         Start reactor with service argument
         '''
+        from twisted.internet import reactor
+
+        from tribler_plugin import service_maker, Options
+
         options = Options()
         Options.parseOptions(options, os.getenv('PYTHON_SERVICE_ARGUMENT', '').split())
         service_maker.makeService(options)
         reactor.run()
-        
+
+    def test(self):
+        '''
+        Run all tests with nose
+        '''
+        import nose
+
+        os.chdir('lib/python2.7/site-packages/Tribler/Test')
+        nose.run(argv=['--nocapture', '--nologcapture', '--verbose', '--stop'])
+
 
 if __name__ == '__main__':
     Triblerd().run()
