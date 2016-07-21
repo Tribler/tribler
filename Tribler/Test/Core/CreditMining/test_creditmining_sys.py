6672b83e0390c8997ff416056f115709eab795ba
Elric Milon <whirm@gmx.com>
2016-07-21 15:13:36 +0200

    Add missing import

--- Tribler/Test/Core/CreditMining/test_creditmining_sys.py
+++ Tribler/Test/Core/CreditMining/test_creditmining_sys.py
@@ -20,7 +20,7 @@ from Tribler.Core.simpledefs import NTFY_TORRENTS, NTFY_UPDATE, NTFY_CHANNELCAST
 from Tribler.Main.Utility.GuiDBTuples import CollectedTorrent
 from Tribler.Policies.BoostingManager import BoostingManager, BoostingSettings
 from Tribler.Test.Core.CreditMining.mock_creditmining import MockLtTorrent, ResourceFailClass
-from Tribler.Test.common import TORRENT_FILE
+from Tribler.Test.common import TORRENT_FILE, TORRENT_FILE_INFOHASH
 from Tribler.Test.test_as_server import TestAsServer, TESTS_DATA_DIR
 from Tribler.Test.util import prepare_xml_rss
 from Tribler.community.allchannel.community import AllChannelCommunity
