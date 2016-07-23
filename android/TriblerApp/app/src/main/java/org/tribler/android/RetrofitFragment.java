bd49c8ca7a5845b1c06d66e6ae992cea9f8dd1b7
Paul Brussee <brussee@live.nl>
2016-07-24 00:14:11 +0200

    add retrofit fragment

---
+++ android/TriblerApp/app/src/main/java/org/tribler/android/RetrofitFragment.java
@@ -0,0 +1,36 @@
+package org.tribler.android;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import rx.subscriptions.CompositeSubscription;
+
+public class RetrofitFragment extends Fragment {
+
+    protected CompositeSubscription mCompositeSubscription;
+    protected IRestApi mService;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCompositeSubscription = new CompositeSubscription();
+
+        String baseUrl = getString(R.string.service_url) + ":" + getString(R.string.service_port_number);
+        String authToken = getString(R.string.service_auth_token);
+        mService = TriblerService.createService(baseUrl, authToken);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mCompositeSubscription.unsubscribe();
+        mCompositeSubscription = null;
+        mService = null;
+    }
+}
