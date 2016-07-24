b5b5e11372c30668a415e9931c40a7c654fe0493
Paul Brussee <brussee@live.nl>
2016-07-24 17:35:59 +0200

    add equals and hashCode methods

--- android/TriblerApp/app/src/main/java/org/tribler/android/TriblerChannel.java
+++ android/TriblerApp/app/src/main/java/org/tribler/android/TriblerChannel.java
@@ -55,4 +55,35 @@ public class TriblerChannel {
         return icon_url;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TriblerChannel that = (TriblerChannel) o;
+
+        if (subscribed != that.subscribed) return false;
+        if (id != that.id) return false;
+        if (votes != that.votes) return false;
+        if (torrents != that.torrents) return false;
+        if (spam != that.spam) return false;
+        if (modified != that.modified) return false;
+        if (!dispersy_cid.equals(that.dispersy_cid)) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null)
+            return false;
+        return icon_url != null ? icon_url.equals(that.icon_url) : that.icon_url == null;
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return dispersy_cid.hashCode();
+    }
 }
--- android/TriblerApp/app/src/main/java/org/tribler/android/TriblerTorrent.java
+++ android/TriblerApp/app/src/main/java/org/tribler/android/TriblerTorrent.java
@@ -51,4 +51,34 @@ public class TriblerTorrent {
         return thumbnail_url;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TriblerTorrent that = (TriblerTorrent) o;
+
+        if (id != that.id) return false;
+        if (num_seeders != that.num_seeders) return false;
+        if (num_leechers != that.num_leechers) return false;
+        if (last_tracker_check != that.last_tracker_check) return false;
+        if (size != that.size) return false;
+        if (!infohash.equals(that.infohash)) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (category != null ? !category.equals(that.category) : that.category != null)
+            return false;
+        return thumbnail_url != null ? thumbnail_url.equals(that.thumbnail_url) : that.thumbnail_url == null;
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return infohash.hashCode();
+    }
 }
