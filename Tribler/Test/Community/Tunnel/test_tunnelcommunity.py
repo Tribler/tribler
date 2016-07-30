f6acbb42d1308cf2cdf06e94c358353b9ce4685e
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-30 16:27:37 +0200

    Refactored tunnel community tests
    Now using the nose reactor, nose deferred decorator and the test
    now handles deferreds returned by Tribler et al.

--- Tribler/Test/Community/Tunnel/test_tunnelcommunity.py
+++ Tribler/Test/Community/Tunnel/test_tunnelcommunity.py
@@ -1,4 +1,5 @@
 import time
+from twisted.internet.defer import inlineCallbacks
 
 from Tribler.Core.Utilities.twisted_thread import deferred
 from Tribler.Test.Community.Tunnel.test_tunnel_base import AbstractTestTunnelCommunity
@@ -11,17 +12,41 @@ from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 class TestTunnelCommunity(AbstractTestTunnelCommunity):
 
+    # @deferred(timeout=5)
     @blocking_call_on_reactor_thread
+    @inlineCallbacks
     def test_circuit_request_cache(self):
         circuit = Circuit(42L)
         circuit_request_cache = CircuitRequestCache(self.tunnel_community, circuit)
         self.tunnel_community.circuits[42] = circuit
-        circuit_request_cache.on_timeout()
+        yield circuit_request_cache.on_timeout()
         self.assertIn(42, self.tunnel_community.circuits)
 
         circuit._broken = True
-        circuit_request_cache.on_timeout()
+        yield circuit_request_cache.on_timeout()
         self.assertNotIn(42, self.tunnel_community.circuits)
+
+    @deferred(timeout=10)
+    @blocking_call_on_reactor_thread
+    @inlineCallbacks
+    def test_check_pong(self):
+        circuit = Circuit(42L)
+        ping_request_cache = PingRequestCache(self.tunnel_community, circuit)
+        ping_num = self.tunnel_community.request_cache.add(ping_request_cache).number
+        meta = self.tunnel_community.get_meta_message(u"ping")
+        msg1 = yield meta.impl(distribution=(self.tunnel_community.global_time,),
+                         candidate=Candidate(("127.0.0.1", 1234), False), payload=(42, ping_num - 1))
+        msg2 = yield meta.impl(distribution=(self.tunnel_community.global_time,),
+                         candidate=Candidate(("127.0.0.1", 1234), False), payload=(42, ping_num))
+
+        # Invalid ping identifier
+        for i in self.tunnel_community.check_pong([msg1]):
+            self.assertIsInstance(i, DropMessage)
+
+        for i in self.tunnel_community.check_pong([msg2]):
+            self.assertIsInstance(i, type(msg2))
+
+        self.tunnel_community.request_cache.pop(u"ping", ping_num)
 
     @blocking_call_on_reactor_thread
     def test_ping_request_cache(self):
@@ -38,26 +63,8 @@ class TestTunnelCommunity(AbstractTestTunnelCommunity):
         ping_request_cache.on_timeout()
         self.assertNotIn(42, self.tunnel_community.circuits)
 
-    @blocking_call_on_reactor_thread
-    def test_check_pong(self):
-        circuit = Circuit(42L)
-        ping_request_cache = PingRequestCache(self.tunnel_community, circuit)
-        ping_num = self.tunnel_community.request_cache.add(ping_request_cache).number
-        meta = self.tunnel_community.get_meta_message(u"ping")
-        msg1 = meta.impl(distribution=(self.tunnel_community.global_time,),
-                         candidate=Candidate(("127.0.0.1", 1234), False), payload=(42, ping_num - 1))
-        msg2 = meta.impl(distribution=(self.tunnel_community.global_time,),
-                         candidate=Candidate(("127.0.0.1", 1234), False), payload=(42, ping_num))
-
-        # Invalid ping identifier
-        for i in self.tunnel_community.check_pong([msg1]):
-            self.assertIsInstance(i, DropMessage)
-
-        for i in self.tunnel_community.check_pong([msg2]):
-            self.assertIsInstance(i, type(msg2))
-
-        self.tunnel_community.request_cache.pop(u"ping", ping_num)
-
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_check_destroy(self):
         # Only the first and last node in the circuit may check a destroy message
         with self.assertRaises(StopIteration):
@@ -65,9 +72,9 @@ class TestTunnelCommunity(AbstractTestTunnelCommunity):
         sock_addr = ("127.0.0.1", 1234)
 
         meta = self.tunnel_community.get_meta_message(u"destroy")
-        msg1 = meta.impl(authentication=(self.member,), distribution=(self.tunnel_community.global_time,),
+        msg1 = yield meta.impl(authentication=(self.member,), distribution=(self.tunnel_community.global_time,),
                          candidate=Candidate(sock_addr, False), payload=(42, 43))
-        msg2 = meta.impl(authentication=(self.member,), distribution=(self.tunnel_community.global_time,),
+        msg2 = yield meta.impl(authentication=(self.member,), distribution=(self.tunnel_community.global_time,),
                          candidate=Candidate(sock_addr, False), payload=(43, 44))
 
         self.tunnel_community.exit_sockets[42] = TunnelExitSocket(42, self.tunnel_community,
@@ -88,6 +95,7 @@ class TestTunnelCommunity(AbstractTestTunnelCommunity):
             self.assertIsInstance(i, type(msg1))
 
     @deferred(timeout=5)
+    @inlineCallbacks
     def test_send_to_destination_ip(self):
         """
         This test checks if the ip address can be resolved when a destination object
@@ -101,11 +109,12 @@ class TestTunnelCommunity(AbstractTestTunnelCommunity):
         self.tunnel_community.exit_sockets[circuit_id] = exit_tunnel
         exit_tunnel.ips[("127.0.0.1", 8080)] = -1
         data = "ffffffff".decode("HEX") + "1" * 25
-        exit_tunnel.sendto(data, ("127.0.0.1", 8080))
-        return exit_tunnel.close()
+        yield exit_tunnel.sendto(data, ("127.0.0.1", 8080))
+        yield exit_tunnel.close()
 
 
     @deferred(timeout=5)
+    @inlineCallbacks
     def test_send_to_ip_deferred(self):
         """
         This test checks if the ip address can be resolved when a destination object
@@ -118,8 +127,8 @@ class TestTunnelCommunity(AbstractTestTunnelCommunity):
         self.tunnel_community.exit_sockets[circuit_id] = exit_tunnel
         exit_tunnel.ips[("localhost", -1)] = -1
         data = "ffffffff".decode("HEX") + "1" * 25
-        exit_tunnel.sendto(data, ("localhost", -1))
-        return exit_tunnel.close()
+        yield exit_tunnel.sendto(data, ("localhost", -1))
+        yield  exit_tunnel.close()
 
     def test_increase_bytes_sent_error_branch(self):
         self.assertRaises(TypeError, self.tunnel_community.increase_bytes_sent, 1, 1)
