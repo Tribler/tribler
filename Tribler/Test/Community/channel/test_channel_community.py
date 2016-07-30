71d9d3314366b1073cf910a40c6b67ada124a9e7
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-30 16:28:57 +0200

    Refactored the channel community tests
    Now using the nose reactor, nose deferred decorator and the test
    now handles deferreds returned by Tribler et al.

--- Tribler/Test/Community/channel/test_channel_community.py
+++ Tribler/Test/Community/channel/test_channel_community.py
@@ -1,18 +1,24 @@
+from twisted.internet.defer import inlineCallbacks
+
+from nose.twistedtools import deferred
+
 from Tribler.Test.Community.channel.test_channel_base import AbstractTestChannelCommunity
 from Tribler.Test.Core.base_test import MockObject
-from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 
 class TestChannelCommunity(AbstractTestChannelCommunity):
 
-    @blocking_call_on_reactor_thread
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_initialize(self):
         def raise_runtime():
             raise RuntimeError()
         self.channel_community._get_latest_channel_message = raise_runtime
-        self.channel_community.initialize()
+        yield self.channel_community.initialize()
         self.assertIsNone(self.channel_community._channelcast_db)
 
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_remove_playlist_torrents(self):
         """
         Testing whether the right methods are called when a torrent is removed from a playlist
@@ -35,10 +41,10 @@ class TestChannelCommunity(AbstractTestChannelCommunity):
 
         self.channel_community._dispersy.load_message_by_packetid = \
             lambda community, pid: mocked_load_message(False, community, pid)
-        self.channel_community.remove_playlist_torrents(1234, [1234])
+        yield self.channel_community.remove_playlist_torrents(1234, [1234])
         self.assertTrue(mocked_create_undo.called)
 
         self.channel_community._dispersy.load_message_by_packetid = \
             lambda community, pid: mocked_load_message(True, community, pid)
-        self.channel_community.remove_playlist_torrents(1234, [1234])
+        yield self.channel_community.remove_playlist_torrents(1234, [1234])
         self.assertTrue(mocked_undo_playlist_torrent.called)
