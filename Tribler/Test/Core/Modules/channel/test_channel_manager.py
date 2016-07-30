9f8be44e9df4d2a1d645df2fe48da660685471dd
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-30 16:33:59 +0200

    Refactored channel manager tests
    Now using the nose reactor, nose deferred decorator and the test
    now handles deferreds returned by Tribler et al.

--- Tribler/Test/Core/Modules/channel/test_channel_manager.py
+++ Tribler/Test/Core/Modules/channel/test_channel_manager.py
@@ -1,3 +1,6 @@
+from twisted.internet.defer import inlineCallbacks
+
+from Tribler.Core.Utilities.twisted_thread import deferred
 from Tribler.Test.Core.base_test import TriblerCoreTest
 from Tribler.Core.Session import Session
 from Tribler.Core.SessionConfig import SessionStartupConfig
@@ -9,7 +12,9 @@ from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 class TestChannelManager(TriblerCoreTest):
 
+    @deferred(timeout=10)
     @blocking_call_on_reactor_thread
+    @inlineCallbacks
     def test_create_channel_duplicate_name_error(self):
         self.config = SessionStartupConfig()
         self.session = Session(self.config, ignore_singleton=True)
@@ -29,5 +34,5 @@ class TestChannelManager(TriblerCoreTest):
         self.session.lm.channel_manager._channel_list = [channel_obj]
 
         with self.assertRaises(DuplicateChannelNameError) as cm:
-            self.session.lm.channel_manager.create_channel("Channel name", "description", "open")
+            yield self.session.lm.channel_manager.create_channel("Channel name", "description", "open")
         self.assertEqual(cm.exception.message, u"Channel name already exists: Channel name")
