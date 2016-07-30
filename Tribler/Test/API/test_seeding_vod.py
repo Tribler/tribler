f0ddf4a569b9055d8c1e9a92bebc0acacce935ec
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-30 16:14:28 +0200

    Refactored test_seeind_vod
    Now using the nose reactor, nose deferred decorator and the test
    now handles deferreds returned by Tribler. Additionally, the blocking
    behavior using events is removed. It was blocking a whole thread, geez.

--- Tribler/Test/API/test_seeding_vod.py
+++ Tribler/Test/API/test_seeding_vod.py
@@ -1,8 +1,8 @@
 # Written by Arno Bakker, heavily modified by Niels Zeilemaker
 # see LICENSE.txt for license information
 
-import sys
-import threading
+from twisted.internet.defer import inlineCallbacks, Deferred
+
 from Tribler.Test.API.test_seeding import TestSeeding
 from Tribler.Core.simpledefs import dlstatus_strings, DLMODE_VOD
 
@@ -15,15 +15,17 @@ class TestVODSeeding(TestSeeding):
 
     def setUp(self):
         super(TestVODSeeding, self).setUp()
-        self.vod_event = threading.Event()
+        self.vod_deferred = Deferred()
 
+    @inlineCallbacks
     def setup_seeder(self, filename='video.avi'):
-        super(TestVODSeeding, self).setup_seeder(filename)
+        yield super(TestVODSeeding, self).setup_seeder(filename)
 
+    @inlineCallbacks
     def subtest_download(self):
         self.dscfg2.set_mode(DLMODE_VOD)
-        super(TestVODSeeding, self).subtest_download()
-        assert self.vod_event.wait(60)
+        yield super(TestVODSeeding, self).subtest_download()
+        yield self.vod_deferred
 
     def downloader_state_callback(self, ds):
         d = ds.get_download()
@@ -32,8 +34,10 @@ class TestVODSeeding(TestSeeding):
                            ds.get_vod_prebuffering_progress())
 
         if ds.get_progress() > 0:
-            self.downloading_event.set()
+            if not self.downloading_deferred.called:
+                self.downloading_deferred.callback(None)
         if ds.get_vod_prebuffering_progress() == 1.0:
-            self.vod_event.set()
+            if not self.vod_deferred.called:
+                self.vod_deferred.callback(None)
 
         return 1.0, False
