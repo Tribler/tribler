84edef08f6ce8a5a42dd4c17cc8e889767597cb8
Elric Milon <whirm@gmx.com>
2016-08-22 12:51:45 +0200

    Clean up after ourselves in AbstractTestCommunity.py

--- Tribler/Test/Community/AbstractTestCommunity.py
+++ Tribler/Test/Community/AbstractTestCommunity.py
@@ -4,6 +4,7 @@ from Tribler.dispersy.endpoint import ManualEnpoint
 from Tribler.dispersy.member import DummyMember
 from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
+from twisted.internet.defer import Deferred, inlineCallbacks
 
 class AbstractTestCommunity(AbstractServer):
 
@@ -17,3 +18,10 @@ class AbstractTestCommunity(AbstractServer):
         self.dispersy._database.open()
         self.master_member = DummyMember(self.dispersy, 1, "a" * 20)
         self.member = self.dispersy.get_new_member(u"curve25519")
+
+    @blocking_call_on_reactor_thread
+    @inlineCallbacks
+    def tearDown(self):
+        self.master_member = None
+        self.member = None
+        yield super(AbstractTestCommunity, self).tearDown()
