6e00a4a97a13967c8ec58743a1a8c424fccdaf47
Elric Milon <whirm@gmx.com>
2016-08-22 12:49:59 +0200

    Clean up after ourselves on test_channel_base.py

--- Tribler/Test/Community/channel/test_channel_base.py
+++ Tribler/Test/Community/channel/test_channel_base.py
@@ -2,6 +2,7 @@ from Tribler.Test.Community.AbstractTestCommunity import AbstractTestCommunity
 from Tribler.community.channel.community import ChannelCommunity
 from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
+from twisted.internet.defer import Deferred, inlineCallbacks
 
 class AbstractTestChannelCommunity(AbstractTestCommunity):
 
@@ -12,3 +13,11 @@ class AbstractTestChannelCommunity(AbstractTestCommunity):
     def setUp(self):
         yield super(AbstractTestChannelCommunity, self).setUp()
         self.channel_community = ChannelCommunity(self.dispersy, self.master_member, self.member)
+
+    @blocking_call_on_reactor_thread
+    @inlineCallbacks
+    def tearDown(self):
+        # Don't unload_community() as it never got registered in dispersy on the first place.
+        self.channel_community.cancel_all_pending_tasks()
+        self.channel_community = None
+        yield super(AbstractTestChannelCommunity, self).tearDown()
