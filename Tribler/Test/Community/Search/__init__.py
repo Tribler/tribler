b44dc87d83db48af2dc46e5c55972c4ae17bec1e
Martijn de Vos <martijn@code-up.nl>
2016-09-19 11:25:11 +0200

    Added test for search community

---
+++ Tribler/Test/Community/Search/__init__.py
@@ -0,0 +1,3 @@
+"""
+This package contains tests for the Search community.
+"""
---
+++ Tribler/Test/Community/Search/test_search_community.py
@@ -0,0 +1,49 @@
+from Tribler.Test.Community.AbstractTestCommunity import AbstractTestCommunity
+from Tribler.Test.Core.base_test import MockObject
+from Tribler.community.search.community import SearchCommunity
+from Tribler.dispersy.util import blocking_call_on_reactor_thread
+
+
+class TestSearchCommunity(AbstractTestCommunity):
+
+    @blocking_call_on_reactor_thread
+    def setUp(self):
+        super(TestSearchCommunity, self).setUp()
+        self.search_community = SearchCommunity(self.dispersy, self.master_member, self.member)
+
+    def test_on_search(self):
+        """
+        Test whether we are creating a search response when we receive a search request
+        """
+        def log_incoming_searches(sock_addr, keywords):
+            log_incoming_searches.called = True
+
+        log_incoming_searches.called = False
+
+        def create_search_response(id, results, candidate):
+            create_search_response.called = True
+            self.assertEqual(id, "abc")
+            self.assertEqual(results, [])
+            self.assertEqual(candidate.sock_addr, "1234")
+
+        create_search_response.called = False
+
+        def search_names(keywords, local=False, keys=None):
+            return []
+
+        self.search_community._torrent_db = MockObject()
+        self.search_community._torrent_db.searchNames = search_names
+
+        fake_message = MockObject()
+        fake_message.candidate = MockObject()
+        fake_message.candidate.sock_addr = "1234"
+        fake_message.payload = MockObject()
+        fake_message.payload.keywords = "test"
+        fake_message.payload.identifier = "abc"
+
+        self.search_community._create_search_response = create_search_response
+        self.search_community.log_incoming_searches = log_incoming_searches
+        self.search_community.on_search([fake_message])
+
+        self.assertTrue(log_incoming_searches.called)
+        self.assertTrue(create_search_response.called)
