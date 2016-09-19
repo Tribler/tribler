557c7ba57b936ee3bbd5707bbc54283c5ee001dc
Martijn de Vos <martijn@code-up.nl>
2016-09-19 10:34:28 +0200

    Added config spec file

--- Tribler/Core/Config/tribler_config.py
+++ Tribler/Core/Config/tribler_config.py
@@ -1,13 +1,21 @@
 import os
 from configobj import ConfigObj
+from validate import Validator
 from Tribler.Core.simpledefs import STATEDIR_CONFIG
+
+
+CONFIGSPEC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.spec')
 
 
 class TriblerConfig(object):
 
     def __init__(self, session):
         config_path = os.path.join(session.get_state_dir(), STATEDIR_CONFIG)
-        self.config = ConfigObj(config_path)
+        self.config = ConfigObj(config_path, configspec=CONFIGSPEC_PATH)
+
+        validator = Validator()
+        self.config.validate(validator, copy=True)
+        self.config.write()
 
         if "user_download_states" not in self.config:
             self.config["user_download_states"] = {}
@@ -28,3 +36,13 @@ class TriblerConfig(object):
 
     def get_download_states(self):
         return dict((key.decode('hex'), value) for key, value in self.config["user_download_states"].iteritems())
+
+    #
+    # General
+    #
+    def set_family_filter_enabled(self, value):
+        self.config['general']['family_filter'] = value
+        self.config.write()
+
+    def get_family_filter_enabled(self):
+        return self.config['general']['family_filter']
