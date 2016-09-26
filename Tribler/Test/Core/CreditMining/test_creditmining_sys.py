dec440763f9601d31ab6eafc44e06fea0e6b929d
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2016-09-26 12:49:26 +0200

    Add test for native loading
    Squashed commits with :
    - Use looping call in checking loaded torrent

--- Tribler/Test/Core/CreditMining/test_creditmining_sys.py
+++ Tribler/Test/Core/CreditMining/test_creditmining_sys.py
@@ -369,6 +369,8 @@ class TestBoostingManagerSysChannel(TestBoostingManagerSys, BaseTestChannel):
                 if src_obj.community:
                     src_obj.community.cancel_all_pending_tasks()
 
+                self.assertEqual(src_obj.get_source_text(), 'Simple Channel')
+
                 defer_param.callback(src)
 
             return defer_param
@@ -499,6 +501,58 @@ class TestBoostingManagerSysChannel(TestBoostingManagerSys, BaseTestChannel):
         d = self.check_source(dispersy_cid)
         d.addCallback(check_torrents_channel)
         return d
+
+    @deferred(timeout=40)
+    def test_chn_native_load(self):
+        self.session.get_dispersy = lambda: True
+        self.session.lm.dispersy = Dispersy(ManualEnpoint(0), self.getStateDir())
+        dispersy_cid_hex = "abcd" * 9 + "0012"
+        dispersy_cid = binascii.unhexlify(dispersy_cid_hex)
+
+        # create channel and insert torrent
+        self.create_fake_allchannel_community()
+        self.create_torrents_in_channel(dispersy_cid_hex)
+
+        self.session.download_torrentfile = \
+            lambda dummy_ihash, function, _: function(binascii.hexlify(TORRENT_UBUNTU_FILE_INFOHASH))
+
+        def get_bin_torrent(_):
+            """
+            get binary data of a torrent
+            """
+            f = open(TORRENT_UBUNTU_FILE, "rb")
+            bdata = f.read()
+            f.close()
+            return bdata
+
+        self.session.get_collected_torrent = get_bin_torrent
+
+        self.boosting_manager.add_source(dispersy_cid)
+
+        def _loop_check(_):
+            defer_param = defer.Deferred()
+
+            def check_loaded(src):
+                """
+                check if a torrent has been loaded
+                """
+                src_obj = self.boosting_manager.get_source_object(src)
+                if src_obj.loaded_torrent[TORRENT_UBUNTU_FILE_INFOHASH] is not None:
+                    src_obj.community.cancel_all_pending_tasks()
+                    src_obj.kill_tasks()
+                    self.check_loaded_lc.stop()
+                    self.check_loaded_lc = None
+                    defer_param.callback(src)
+
+            self.check_loaded_lc = LoopingCall(check_loaded, dispersy_cid)
+            self.check_loaded_lc.start(1, now=True)
+
+            return defer_param
+
+        defer_ret = self.check_source(dispersy_cid)
+        defer_ret.addCallback(_loop_check)
+
+        return defer_ret
 
     @blocking_call_on_reactor_thread
     @inlineCallbacks
