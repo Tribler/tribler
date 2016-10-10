b4f9912ad75d8d22536a961f4f038b2701ae5c63
Martijn de Vos <martijn@code-up.nl>
2016-10-10 12:44:27 +0200

    Added peers list to downloads page

--- Tribler/Test/GUI/FakeApi/models/download.py
+++ Tribler/Test/GUI/FakeApi/models/download.py
@@ -1,5 +1,6 @@
 from random import randint, uniform, random
 from constants import DLSTATUS_STRINGS
+from models.download_peer import DownloadPeer
 
 
 class Download:
@@ -10,21 +11,33 @@ class Download:
         self.anon = True if randint(0, 1) == 0 else False
         self.anon_hops = randint(1, 3) if self.anon else 0
         self.safe_seeding = True if randint(0, 1) == 0 else False
-        self.peers = randint(0, 1000)
+        self.num_peers = randint(0, 1000)
         self.seeds = randint(0, 1000)
         self.progress = uniform(0, 1)
         self.down_speed = randint(0, 1000000)
         self.up_speed = randint(0, 1000000)
         self.files = []
         self.trackers = [{"url": "[PEX]", "status": "working", "peers": 42}]
+        self.destination = "/"
+        self.availability = uniform(0, 5)
+        self.peers = []
+
+        for _ in xrange(randint(5, 40)):
+            self.peers.append(DownloadPeer())
 
         # Generate some files
         for file_ind in xrange(randint(1, 10)):
             self.files.append({"name": "File %d" % file_ind, "size": randint(1000, 10000000),
                                "progress": random(), "included": True if random() > 0.5 else False})
 
-    def get_json(self):
-        return {"name": self.torrent.name, "infohash": self.torrent.infohash, "status": DLSTATUS_STRINGS[self.status],
-                "num_peers": self.peers, "num_seeds": self.seeds, "progress": self.progress,
-                "size": self.torrent.length, "speed_down": self.down_speed, "speed_up": self.up_speed, "eta": 1234,
-                "hops": self.anon_hops, "anon_download": self.anon, "files": self.files, "trackers": self.trackers}
+    def get_json(self, get_peers=False):
+        download = {"name": self.torrent.name, "infohash": self.torrent.infohash, "status": DLSTATUS_STRINGS[self.status],
+                    "num_peers": self.num_peers, "num_seeds": self.seeds, "progress": self.progress,
+                    "size": self.torrent.length, "speed_down": self.down_speed, "speed_up": self.up_speed, "eta": 1234,
+                    "hops": self.anon_hops, "anon_download": self.anon, "files": self.files, "trackers": self.trackers,
+                    "destination": self.destination, "availability": self.availability, "total_pieces": 1234}
+
+        if get_peers:
+            download["peers"] = [peer.get_info_dict() for peer in self.peers]
+
+        return download
--- TriblerGUI/widgets/downloadsdetailstabwidget.py
+++ TriblerGUI/widgets/downloadsdetailstabwidget.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QTabWidget, QTreeWidgetItem
 from TriblerGUI.defs import *
-from TriblerGUI.utilities import format_size
+from TriblerGUI.utilities import format_size, format_speed
 
 
 class DownloadsDetailsTabWidget(QTabWidget):
@@ -11,6 +11,7 @@ class DownloadsDetailsTabWidget(QTabWidget):
         self.window().download_detail_filesize_label.setText("%s in %d files" % (format_size(float(download["size"])), len(download["files"])))
         self.window().download_detail_health_label.setText("%d seeders, %d leechers" % (download["num_seeds"], download["num_peers"]))
         self.window().download_detail_infohash_label.setText(download['infohash'])
+        self.window().download_detail_availability_label.setText("%.2f" % download['availability'])
 
         # Populate the files list
         self.window().download_files_list.clear()
@@ -29,3 +30,48 @@ class DownloadsDetailsTabWidget(QTabWidget):
             item.setText(0, tracker["url"])
             item.setText(1, tracker["status"])
             item.setText(2, str(tracker["peers"]))
+
+        # Populate the peers list if the peer information is available
+        self.window().download_peers_list.clear()
+        if "peers" in download:
+            for peer in download["peers"]:
+                self.create_widget_with_peer_info(peer)
+
+    def create_widget_with_peer_info(self, peer):
+        item = QTreeWidgetItem(self.window().download_peers_list)
+
+        peer_name = "%s:%s" % (peer["ip"], peer["port"])
+        if peer['connection_type'] == 1:
+            peer_name += ' [WebSeed]'
+        elif peer['connection_type'] == 2:
+            peer_name += ' [HTTP Seed]'
+        elif peer['connection_type'] == 3:
+            peer_name += ' [uTP]'
+
+        state = ""
+        if peer['optimistic']:
+            state += "O,"
+        if peer['uinterested']:
+            state += "UI,"
+        if peer['uchoked']:
+            state += "UC,"
+        if peer['uhasqueries']:
+            state += "UQ,"
+        if not peer['uflushed']:
+            state += "UBL,"
+        if peer['ueligable']:
+            state += "UE,"
+        if peer['dinterested']:
+            state += "DI,"
+        if peer['dchoked']:
+            state += "DC,"
+        if peer['snubbed']:
+            state += "S,"
+        state += peer['direction']
+
+        item.setText(0, peer_name)
+        item.setText(1, '%d%%' % (peer['completed'] * 100.0))
+        item.setText(2, format_speed(peer['downrate']))
+        item.setText(3, format_speed(peer['uprate']))
+        item.setText(4, state)
+        item.setText(5, peer['extended_version'])
