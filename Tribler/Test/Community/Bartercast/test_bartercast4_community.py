926689180ee5d45a50bd0589bddc2661559e8706
Martijn de Vos <mdmartijndevos@gmail.com>
2016-10-12 13:33:13 +0200

    Twistified bartercast tests

--- Tribler/Test/Community/Bartercast/test_bartercast4_community.py
+++ Tribler/Test/Community/Bartercast/test_bartercast4_community.py
@@ -4,8 +4,9 @@
 from time import sleep
 
 from twisted.internet.task import LoopingCall
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, Deferred
 
+from Tribler.Core.Utilities.twisted_thread import deferred
 from Tribler.Test.test_as_server import TestAsServer
 from Tribler.community.bartercast4.community import BarterCommunity, BarterCommunityCrawler
 from Tribler.community.bartercast4.statistics import BartercastStatisticTypes, _barter_statistics
@@ -13,63 +14,21 @@ from Tribler.community.tunnel.tunnel_community import TunnelSettings
 from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 
-DEBUG = True
-
-
 class TestBarterCommunity(TestAsServer):
 
     """Start a tribler session and wait for the statistics to be increased."""
 
-    def __init__(self, *argv, **kwargs):
-        super(TestBarterCommunity, self).__init__(*argv, **kwargs)
+    @blocking_call_on_reactor_thread
+    @inlineCallbacks
+    def setUp(self, autoload_discovery=True):
+        yield super(TestBarterCommunity, self).setUp(autoload_discovery=autoload_discovery)
+
         self._test_condition_lc = None
-
-    def test_local_stats(self):
-        def do_stats_test():
-            tries_left = 300
-            while True:
-                for val in _barter_statistics.bartercast[BartercastStatisticTypes.TORRENTS_RECEIVED].itervalues():
-                    if val > 0:
-                        assert True, "Some torrent statistic was received"
-                        return
-                # wait for a bit
-                sleep(1.0)
-                tries_left = tries_left - 1
-                if tries_left <= 0:
-                    assert False, "No torrent statistics received"
-                    return
-
-        self.startTest(do_stats_test)
-
-    def test_stats_messages(self):
-
-        def do_stats_messages():
-            # check if the crawler receives messages
-            if len(_barter_statistics.get_interactions(self.dispersy)):
-                # Some bartercast statistic was crawled.
-                self._test_condition_lc.stop()
-                self.quit()
-
-        def has_finished():
-            return not self._test_condition_lc.running
-
-        def noop():
-            pass
-
-        # Make it blocking so CallConditional can't do a check before the LoopingCall exists.
-        @blocking_call_on_reactor_thread
-        def start():
-            self._test_condition_lc = LoopingCall(do_stats_messages)
-            self._test_condition_lc.start(5, now=True)
-
-        @blocking_call_on_reactor_thread
-        def cleanup_and_fail(succeeded, err_msg):
-            self._test_condition_lc.stop()
-            self._test_condition_lc = None
-            self.assert_(succeeded, err_msg)
-
-        self.startTest(start)
-        self.CallConditional(300.0, has_finished, noop, u"Failed to crawl", assert_callback=cleanup_and_fail)
+        self._test_local_msg_lc = None
+        self.test_deferred = Deferred()
+        self.dispersy = self.session.get_dispersy_instance()
+        self.load_communities(self.session, self.dispersy, True)
+        yield self.setupPeer()
 
     # for future stuff; you can use this if you need another peer for some reason
     @blocking_call_on_reactor_thread
@@ -77,19 +36,13 @@ class TestBarterCommunity(TestAsServer):
     def setupPeer(self):
         from Tribler.Core.Session import Session
 
-        self.setUpPreSession()
-
         self.config2 = self.config.copy()
         self.config2.set_state_dir(self.getStateDir(2))
 
         self.session2 = Session(self.config2, ignore_singleton=True)
 
-        upgrader = self.session2.prestart()
-        assert upgrader.is_done, "Upgrader is not done"
-        assert not upgrader.failed, upgrader.current_status
-
+        self.session2.prestart()
         yield self.session2.start()
-        self.dispersy2 = self.session2.get_dispersy_instance()
         self.load_communities(self.session2, self.session2.get_dispersy_instance())
 
     def setUpPreSession(self):
@@ -97,6 +50,36 @@ class TestBarterCommunity(TestAsServer):
         self.config.set_dispersy(True)
         self.config.set_megacache(True)
         self.config.set_enable_channel_search(True)
+
+    @deferred(timeout=120)
+    def test_local_stats(self):
+        """
+        Testing whether we locally receive stats from the BarterCast community
+        """
+        def do_stats_test():
+            for val in _barter_statistics.bartercast[BartercastStatisticTypes.TORRENTS_RECEIVED].itervalues():
+                if val > 0:
+                    self._test_local_msg_lc.stop()
+                    self.test_deferred.callback(None)
+
+        self._test_local_msg_lc = LoopingCall(do_stats_test)
+        self._test_local_msg_lc.start(5, now=True)
+        return self.test_deferred
+
+    @deferred(timeout=120)
+    def test_stats_messages(self):
+        """
+        Testing whether we receive stats from other users in the BarterCast community
+        """
+        def do_stats_messages():
+            if len(_barter_statistics.get_interactions(self.dispersy)):
+                # Some bartercast statistic was crawled.
+                self._test_condition_lc.stop()
+                self.test_deferred.callback(None)
+
+        self._test_condition_lc = LoopingCall(do_stats_messages)
+        self._test_condition_lc.start(5, now=True)
+        return self.test_deferred
 
     @blocking_call_on_reactor_thread
     def load_communities(self, session, dispersy, crawler=False):
@@ -108,14 +91,6 @@ class TestBarterCommunity(TestAsServer):
             dispersy.define_auto_load(BarterCommunityCrawler, dispersy_member, (session, settings), load=True)
         else:
             dispersy.define_auto_load(BarterCommunity, dispersy_member, (session, settings), load=True)
-
-    @blocking_call_on_reactor_thread
-    @inlineCallbacks
-    def setUp(self, autoload_discovery=True):
-        yield super(TestBarterCommunity, self).setUp(autoload_discovery=autoload_discovery)
-        self.dispersy = self.session.get_dispersy_instance()
-        self.load_communities(self.session, self.dispersy, True)
-        yield self.setupPeer()
 
     @blocking_call_on_reactor_thread
     @inlineCallbacks
