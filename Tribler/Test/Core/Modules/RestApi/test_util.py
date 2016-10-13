8889b52d6d270b204bd6f25391434f71cd3ca5d5
Martijn de Vos <martijn@code-up.nl>
2016-10-13 11:05:19 +0200

    Written test for edit channel method

--- Tribler/Test/Core/Modules/RestApi/test_util.py
+++ Tribler/Test/Core/Modules/RestApi/test_util.py
@@ -1,15 +1,20 @@
 import struct
 
 from Tribler.Core.Modules.restapi.util import convert_search_torrent_to_json, convert_db_channel_to_json, \
-    relevance_score_remote_torrent, get_parameter
+    relevance_score_remote_torrent, get_parameter, can_edit_channel
 from Tribler.Core.Session import Session
 from Tribler.Test.Core.base_test import TriblerCoreTest, MockObject
+from Tribler.community.channel.community import ChannelCommunity
+from Tribler.dispersy.exception import CommunityNotFoundException
 
 
 class TestRestApiUtil(TriblerCoreTest):
     """
     This class contains various tests for the rest api utility methods.
     """
+    def setUp(self, annotate=True):
+        super(TestRestApiUtil, self).setUp(annotate=annotate)
+        Session.get_instance().get_dispersy = lambda: False
 
     def tearDown(self, annotate=True):
         Session.del_instance()  # We are opening a session when assigning a relevance score to a remote torrent
@@ -59,7 +64,8 @@ class TestRestApiUtil(TriblerCoreTest):
         """
         input_tuple = (1, 'aaaa'.decode('hex'), 'test', 'desc', 42, 43, 44, 2, 1234, 0.123)
         output = {'id': 1, 'dispersy_cid': 'aaaa', 'name': 'test', 'description': 'desc', 'torrents': 42, 'votes': 43,
-                  'spam': 44, 'subscribed': True, 'modified': 1234, 'relevance_score': 0.123}
+                  'spam': 44, 'subscribed': True, 'modified': 1234, 'relevance_score': 0.123,
+                  'can_edit': False}
         self.assertEqual(convert_db_channel_to_json(input_tuple, include_rel_score=True), output)
 
     def test_rel_score_remote_torrent(self):
@@ -67,3 +73,27 @@ class TestRestApiUtil(TriblerCoreTest):
         mocked_db.latest_matchinfo_torrent = struct.pack("I" * 12, *([1] * 12)), "torrent"
         Session.get_instance().open_dbhandler = lambda _: mocked_db
         self.assertNotEqual(relevance_score_remote_torrent("my-torrent.iso"), 0.0)
+
+    def test_can_edit_channel(self):
+        """
+        Testing whether we can edit a channel.
+        """
+        Session.get_instance().get_dispersy = lambda: False
+        self.assertFalse(can_edit_channel("abcd", 0))
+
+        Session.get_instance().get_dispersy = lambda: True
+        mocked_dispersy = MockObject()
+        mocked_community = MockObject()
+        mocked_community.get_channel_mode = lambda: (ChannelCommunity.CHANNEL_CLOSED, True)
+
+        def throw_not_found_exception(_):
+            raise CommunityNotFoundException("abcd")
+
+        mocked_dispersy.get_community = throw_not_found_exception
+        Session.get_instance().get_dispersy_instance = lambda: mocked_dispersy
+
+        self.assertFalse(can_edit_channel("abcd", 0))
+
+        mocked_dispersy.get_community = lambda _: mocked_community
+
+        self.assertTrue(can_edit_channel("abcd", 0))
