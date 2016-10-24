b988da65e4e133cd4f0b6c0291897903634a475e
Paul Brussee <brussee@live.nl>
2016-10-24 20:03:00 +0200

    add event body to event stream start event

--- android/TriblerApp/app/src/main/java/org/tribler/android/restapi/json/EventsStartEvent.java
+++ android/TriblerApp/app/src/main/java/org/tribler/android/restapi/json/EventsStartEvent.java
@@ -4,7 +4,18 @@ public class EventsStartEvent {
 
     public static final String TYPE = "events_start";
 
+    private boolean tribler_started;
+    private String version;
+
     EventsStartEvent() {
+    }
+
+    public boolean isTriblerStarted() {
+        return tribler_started;
+    }
+
+    public String getVersion() {
+        return version;
     }
 
 }
