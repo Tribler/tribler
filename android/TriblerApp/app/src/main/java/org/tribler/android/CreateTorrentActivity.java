4c8b95d914413d113c8109cfc941a017beb19d2f
Paul Brussee <brussee@live.nl>
2016-10-26 11:56:58 +0200

    added CreateTorrentActivity

---
+++ android/TriblerApp/app/src/main/java/org/tribler/android/CreateTorrentActivity.java
@@ -0,0 +1,25 @@
+package org.tribler.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CreateTorrentActivity extends BaseActivity {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_torrent);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void handleIntent(Intent intent) {
+        //TODO
+    }
+
+}
