9d215f9e0cdd299d7d7db04625e05023b96b0b39
Paul Brussee <brussee@live.nl>
2016-10-28 02:49:39 +0200

    re_install script restores default appstate

--- android/TriblerApp/scripts/re_install.sh
+++ android/TriblerApp/scripts/re_install.sh
@@ -9,5 +9,11 @@ timestamp=$(date +%s)
 echo Uninstall
 $ADB uninstall org.tribler.android
 
+echo Remove appstate
+$ADB shell rm -rf /sdcard/.Tribler
+
 echo Install
 $ADB install ../app/build/outputs/apk/app-debug.apk
+
+echo Default appstate
+$ADB push /home/paul/Tribler_appstate/fav_only_tpb/.Tribler /sdcard/.Tribler
