72eddbb793fbec22b137d8ec7d84abdade2ae758
Martijn de Vos <martijn@code-up.nl>
2016-11-08 19:31:32 +0100

    Written test for state endpoint

---
+++ Tribler/Test/Core/Modules/RestApi/test_state_endpoint.py
@@ -0,0 +1,14 @@
+from Tribler.Core.Utilities.twisted_thread import deferred
+from Tribler.Test.Core.Modules.RestApi.base_api_test import AbstractApiTest
+
+
+class TestStateEndpoint(AbstractApiTest):
+
+    @deferred(timeout=10)
+    def test_get_state(self):
+        """
+        Testing whether the API returns a correct state when requested
+        """
+        self.session.lm.api_manager.root_endpoint.state_endpoint.on_tribler_exception("abcd")
+        expected_json = {"state": "EXCEPTION", "last_exception": "abcd"}
+        return self.do_request('state', expected_code=200, expected_json=expected_json)
