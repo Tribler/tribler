28027641c5627cbb14587e564cafec2af8a67a44
Martijn de Vos <martijn@code-up.nl>
2016-11-12 18:11:13 +0100

    Implemented endpoint to fetch torrent health

--- Tribler/Core/Modules/restapi/torrents_endpoint.py
+++ Tribler/Core/Modules/restapi/torrents_endpoint.py
@@ -1,6 +1,7 @@
 import json
 
 from twisted.web import http, resource
+from twisted.web.server import NOT_DONE_YET
 
 from Tribler.Core.Modules.restapi.util import convert_db_torrent_to_json
 from Tribler.Core.simpledefs import NTFY_TORRENTS, NTFY_CHANNELCAST
@@ -10,11 +11,12 @@ class TorrentsEndpoint(resource.Resource):
 
     def __init__(self, session):
         resource.Resource.__init__(self)
+        self.session = session
 
-        child_handler_dict = {"random": TorrentsRandomEndpoint}
-
-        for path, child_cls in child_handler_dict.iteritems():
-            self.putChild(path, child_cls(session))
+    def getChild(self, path, request):
+        if path == "random":
+            return TorrentsRandomEndpoint(self.session)
+        return SpecificTorrentEndpoint(self.session, path)
 
 
 class TorrentsRandomEndpoint(resource.Resource):
@@ -80,3 +82,89 @@ class TorrentsRandomEndpoint(resource.Resource):
             results_json.append(torrent_json)
 
         return json.dumps({"torrents": results_json})
+
+
+class SpecificTorrentEndpoint(resource.Resource):
+    """
+    This class handles requests for a specific torrent.
+    """
+
+    def __init__(self, session, infohash):
+        resource.Resource.__init__(self)
+        self.session = session
+        self.infohash = infohash
+
+        self.putChild("health", TorrentHealthEndpoint(self.session, self.infohash))
+
+
+class TorrentHealthEndpoint(resource.Resource):
+    """
+    This class is responsible for endpoints regarding the health of a torrent.
+    """
+
+    def __init__(self, session, infohash):
+        resource.Resource.__init__(self)
+        self.session = session
+        self.infohash = infohash
+        self.torrent_db = self.session.open_dbhandler(NTFY_TORRENTS)
+
+    def render_GET(self, request):
+        """
+        .. http:get:: /torrents/(string: torrent infohash)/health
+
+        Fetch the swarm health of a specific torrent. You can optionally specify the timeout to be used in the
+        connections to the trackers. This is by default 20 seconds.
+        By default, we will not check the health of a torrent again if it was recently checked. You can force a health
+        recheck by passing the refresh parameter.
+
+            **Example request**:
+
+            .. sourcecode:: none
+
+                curl http://localhost:8085/torrents/97d2d8f5d37e56cfaeaae151d55f05b077074779/health?timeout=15&refresh=1
+
+            **Example response**:
+
+            .. sourcecode:: javascript
+
+                {
+                    "http://mytracker.com:80/announce": [{
+                        "seeders": 43,
+                        "leechers": 20,
+                        "infohash": "97d2d8f5d37e56cfaeaae151d55f05b077074779"
+                    }],
+                    "http://nonexistingtracker.com:80/announce": {
+                        "error": "timeout"
+                    }
+                }
+
+            :statuscode 404: if the torrent is not found in the database
+        """
+        timeout = 20
+        if 'timeout' in request.args:
+            timeout = int(request.args['timeout'][0])
+
+        refresh = False
+        if 'refresh' in request.args and len(request.args['refresh']) > 0 and request.args['refresh'][0] == "1":
+            refresh = True
+
+        torrent_db_columns = ['C.torrent_id', 'num_seeders', 'num_leechers', 'next_tracker_check']
+        torrent_info = self.torrent_db.getTorrent(self.infohash.decode('hex'), torrent_db_columns)
+
+        if torrent_info is None:
+            request.setResponseCode(http.NOT_FOUND)
+            return json.dumps({"error": "torrent not found in database"})
+
+        def on_health_result(result):
+            request.write(json.dumps({'health': result}))
+            request.finish()
+
+        def on_request_error(failure):
+            request.setResponseCode(http.BAD_REQUEST)
+            request.write(json.dumps({"error": failure.getErrorMessage()}))
+            request.finish()
+
+        self.session.check_torrent_health(self.infohash.decode('hex'), timeout=timeout, scrape_now=refresh)\
+            .addCallback(on_health_result).addErrback(on_request_error)
+
+        return NOT_DONE_YET
