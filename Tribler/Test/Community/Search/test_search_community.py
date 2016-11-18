dca386f19028bf73264f18a502751954d0bdbc92
Martijn de Vos <martijn@code-up.nl>
2016-11-18 10:42:46 +0100

    Fixed search response decode crash

--- Tribler/Test/Community/Search/test_search_community.py
+++ Tribler/Test/Community/Search/test_search_community.py
@@ -1,14 +1,17 @@
+from nose.tools import raises
 from Tribler.Test.Community.AbstractTestCommunity import AbstractTestCommunity
 from Tribler.Test.Core.base_test import MockObject
 from Tribler.community.search.community import SearchCommunity
+from Tribler.community.search.conversion import SearchConversion
+from Tribler.dispersy.message import DropPacket
 from Tribler.dispersy.util import blocking_call_on_reactor_thread
 
 
 class TestSearchCommunity(AbstractTestCommunity):
 
     @blocking_call_on_reactor_thread
-    def setUp(self):
-        super(TestSearchCommunity, self).setUp()
+    def setUp(self, annotate=True):
+        super(TestSearchCommunity, self).setUp(annotate=annotate)
         self.search_community = SearchCommunity(self.dispersy, self.master_member, self.member)
 
     def test_on_search(self):
@@ -47,3 +50,12 @@ class TestSearchCommunity(AbstractTestCommunity):
 
         self.assertTrue(log_incoming_searches.called)
         self.assertTrue(create_search_response.called)
+
+    @raises(DropPacket)
+    def test_decode_response_invalid(self):
+        """
+        Test whether decoding an invalid search response does not crash the program
+        """
+        self.search_community._initialize_meta_messages()
+        search_conversion = SearchConversion(self.search_community)
+        search_conversion._decode_search_response(None, 0, "a[]")
