ef53119c4944ce9c2e1afb8b426d1fb4d5b70004
Paul Brussee <brussee@live.nl>
2016-11-20 14:51:34 +0100

    update P4A by switching to upstream master

--- android/TriblerService/reinstall_p4a.sh
+++ android/TriblerService/reinstall_p4a.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-set -e
-
 pip uninstall python-for-android
 rm -rf ~/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps
 rm -rf ~/.local/lib/python2.7/site-packages/pythonforandroid/recipes
 
 echo Get the latest P4A
-pip install --user --upgrade git+https://github.com/brussee/python-for-android.git
+pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
