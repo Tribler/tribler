094c6f88909d44b51f90ef6e1ab617ed62afd701
Paul Brussee <brussee@live.nl>
2016-11-20 16:26:09 +0100

    force p4a reinstall

--- android/TriblerService/reinstall_p4a.sh
+++ android/TriblerService/reinstall_p4a.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 pip uninstall python-for-android
-rm -rf ~/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps
-rm -rf ~/.local/lib/python2.7/site-packages/pythonforandroid/recipes
+
+rm -rf ~/.local/lib/python2.7/site-packages/pythonforandroid
+rm -rf ~/.local/share/python-for-android
 
 echo Get the latest P4A
 pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
