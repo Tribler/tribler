24e285972dbdf24aae4aa93f9605d33c72626c68
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2017-03-14 16:26:56 +0100

    Remove irrelevant comment in Main.hacks

--- Tribler/Main/hacks.py
+++ Tribler/Main/hacks.py
@@ -3,11 +3,7 @@ This file should contain code for hacks needed to work around bugs in non-trible
 """
 import sys
 
-# Needed because of python-cryptography doing weird stuff when scanning for backends.
-# Should be removed as soon as https://github.com/pyca/cryptography/issues/2039 gets closed.
-# This code is based on https://github.com/pyca/cryptography/issues/2039#issuecomment-115432291
-# with some modifications so it only gets called when running an installer version of Tribler on windows.
-# TODO(emilon): remove this when Tribler gets migrated to python 3.
+# TODO: remove this when Tribler gets migrated to python 3.
 if sys.platform == "win32":
     import ctypes
 
