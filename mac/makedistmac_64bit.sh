9e46a19ddc43e97b23de95e21ad844460ac91356
Martijn de Vos <martijn@code-up.nl>
2017-03-14 11:17:31 +0100

    Fixed MacOS builder

--- mac/makedistmac_64bit.sh
+++ mac/makedistmac_64bit.sh
@@ -93,7 +93,7 @@ hdiutil convert dist/temp/rw.dmg -format UDZO -imagekey zlib-level=9 -o dist/$AP
 rm -f dist/temp/rw.dmg
 
 # add EULA
-eulagise --license $LIBRARYNAME/LICENSE --target dist/$APPNAME.dmg
+eulagise --license LICENSE --target dist/$APPNAME.dmg
 
 if [ ! -z "$DMGNAME" ]; then
     mv dist/$APPNAME.dmg dist/$DMGNAME.dmg
