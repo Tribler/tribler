7d2135c787f209723df2673c2b7ba5c8717929a9
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2017-03-28 19:59:25 +0200

    Correct logging in resuming

--- Tribler/Core/Libtorrent/LibtorrentDownloadImpl.py
+++ Tribler/Core/Libtorrent/LibtorrentDownloadImpl.py
@@ -553,7 +553,7 @@ class LibtorrentDownloadImpl(DownloadConfigInterface, TaskManager):
         basename = hexlify(resume_data['info-hash']) + '.state'
         filename = os.path.join(self.session.get_downloads_pstate_dir(), basename)
 
-        self._logger.debug("tlm: network checkpointing: to file %s", filename)
+        self._logger.debug("save resume data to file %s", filename)
 
         self.pstate_for_restart.write_file(filename)
 
