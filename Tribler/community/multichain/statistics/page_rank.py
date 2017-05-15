ce48137d46433729230e9c662e90116c9f50b868
Yinghao Dai <yinghaodai@hotmail.com>
2017-05-15 23:20:12 +0200

    Add documentation for incremental page rank

--- Tribler/community/multichain/statistics/page_rank.py
+++ Tribler/community/multichain/statistics/page_rank.py
@@ -1,8 +1,20 @@
+"""
+This file contains everything needed to compute page ranks.
+"""
 from random import random, choice
 
 
 class IncrementalPageRank(object):
+    """
+    Class to compute page ranks according to a graph.
+    """
     def __init__(self, graph, walks_per_node=2, stop_probability=0.1):
+        """
+        Set up the environment by setting a graph, number of walks per node and stop probability.
+        :param graph: the graph for which page ranks have to be computed
+        :param walks_per_node: the number of random walks per node
+        :param stop_probability: the probability to stop in each hop
+        """
         self.graph = graph
 
         self.R = walks_per_node
@@ -17,17 +29,29 @@ class IncrementalPageRank(object):
         self.new_nodes = set()
 
     def add_edge(self, source, destination):
-        # TODO: Add the weight
+        """
+        Add an edge to the graph.
+        :param source: the source node of the new edge
+        :param destination: the destination node of the new edge
+        """
         self.add_node(source)
         self.add_node(destination)
         self.graph.add_edge(source, destination)
 
+
     def add_node(self, node):
+        """
+        Add a node to the graph.
+        :param node: the name for the new node
+        """
         self.new_nodes.add(node)
         if node not in self.graph.nodes():
             self.graph.add_node(node)
 
     def initial_walk(self):
+        """
+        Perform the initial random walk.
+        """
         self.size = 0
         for node in self.graph.nodes():
             walks = list()
@@ -38,6 +62,9 @@ class IncrementalPageRank(object):
             self.walks.append(walks)
 
     def update_walk(self):
+        """
+        Update the stored random walks incrementally.
+        """
         self.size = 0
         for node_walks in range(len(self.walks)):
             for walk in range(len(self.walks[node_walks])):
@@ -52,6 +79,11 @@ class IncrementalPageRank(object):
                 self.size += len(self.walks[node_walks][walk])
 
     def _walk(self, start):
+        """
+        Perform a random walk, starting from a certain node.
+        :param start: the start node
+        :return: a list that represents the walk
+        """
         walk = list()
         next_node = start
         while random() > self.c:
@@ -63,6 +95,10 @@ class IncrementalPageRank(object):
         return walk
 
     def count(self):
+        """
+        For each node, count the number of occurrences in the random walks.
+        :return: a dictionary in which the number of occurrences can be looked up by node name
+        """
         flat = list()
         for node_walk in self.walks:
             for walk in node_walk:
@@ -71,4 +107,8 @@ class IncrementalPageRank(object):
         self.counts = {hop: flat.count(hop) for hop in self.graph.nodes()}
 
     def get_ranks(self):
+        """
+        For each node, get its page rank.
+        :return: a dictionary in which a node's page rank can be looked up by its name
+        """
         return {hop: self.counts[hop] / self.size for hop in self.graph.nodes()}
