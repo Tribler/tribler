27f5259a6830d4b823b7f0d04d24ebd784b2b86f
Martijn de Vos <mdmartijndevos@gmail.com>
2017-09-21 09:09:23 +0200

    Added tests for Temporal Pagerank algorithm

--- Tribler/Test/Community/Market/Reputation/test_reputation_pagerank.py
+++ Tribler/Test/Community/Market/Reputation/test_reputation_pagerank.py
@@ -1,5 +1,6 @@
 from Tribler.Test.Community.Market.Reputation.test_reputation_base import TestReputationBase
-from Tribler.community.market.reputation.pagerank_manager import PagerankReputationManager
+from Tribler.community.market.core.price import Price
+from Tribler.community.market.core.quantity import Quantity
 
 
 class TestReputationPagerank(TestReputationBase):
@@ -8,13 +9,57 @@ class TestReputationPagerank(TestReputationBase):
     """
 
     def test_pagerank_1(self):
-        self.insert_transaction('a', 'b', 1, 20, 2, 20)
-        self.insert_transaction('a', 'b', 1, 20, 2, 20)
-        self.insert_transaction('b', 'c', 1, 20, 2, 20)
-        self.insert_transaction('b', 'd', 1, 20, 2, 20)
-        self.insert_transaction('d', 'e', 1, 10, 2, 10)
+        """
+        Test a very simple Temporal Pagerank computation
+        """
+        self.insert_transaction('a', 'b', Quantity(1, 'BTC'), Price(1, 'MC'))
+        rep_dict = self.compute_reputations()
+        self.assertTrue('a' in rep_dict)
+        self.assertTrue('b' in rep_dict)
+        self.assertGreater(rep_dict['a'], 0)
+        self.assertGreater(rep_dict['b'], 0)
 
-        blocks = self.market_db.get_all_blocks()
-        rep_manager = PagerankReputationManager(blocks)
-        rep = rep_manager.compute(own_public_key='a')
-        self.assertIsInstance(rep, dict)
+    def test_pagerank_2(self):
+        """
+        Test isolated nodes during a Temporal Pagerank computation
+        """
+        self.insert_transaction('a', 'b', Quantity(1, 'BTC'), Price(1, 'MC'))
+        self.insert_transaction('c', 'd', Quantity(1, 'BTC'), Price(1, 'MC'))
+        rep_dict = self.compute_reputations()
+        self.assertTrue('c' in rep_dict)
+        self.assertTrue('d' in rep_dict)
+
+    def test_pagerank_3(self):
+        """
+        Test a more involved example of a Temporal Pagerank computation
+        """
+        self.insert_transaction('a', 'b', Quantity(1, 'BTC'), Price(1, 'MC'))
+        self.insert_transaction('b', 'c', Quantity(100, 'BTC'), Price(100, 'MC'))
+        self.insert_transaction('b', 'd', Quantity(100, 'BTC'), Price(100, 'MC'))
+        self.insert_transaction('b', 'e', Quantity(100, 'BTC'), Price(100, 'MC'))
+        rep_dict = self.compute_reputations()
+        self.assertEqual(len(rep_dict.keys()), 5)
+        for _, rep in rep_dict.iteritems():
+            self.assertGreater(rep, 0)
+
+    def test_pagerank_4(self):
+        """
+        Test an empty pagerank computation
+        """
+        rep_dict = self.compute_reputations()
+        self.assertDictEqual(rep_dict, {})
+
+    def test_pagerank_5(self):
+        """
+        Test a Temporal Pagerank computation
+        """
+        self.insert_transaction('a', 'b', Quantity(1, 'BTC'), Price(1, 'MC'))
+        self.insert_transaction('a', 'c', Quantity(1, 'BTC'), Price(2, 'MC'))
+        self.insert_transaction('a', 'd', Quantity(1, 'BTC'), Price(3, 'MC'))
+        self.insert_transaction('a', 'e', Quantity(1, 'BTC'), Price(4, 'MC'))
+        self.insert_transaction('a', 'f', Quantity(1, 'BTC'), Price(5, 'MC'))
+        self.insert_transaction('a', 'g', Quantity(1, 'BTC'), Price(6, 'MC'))
+        self.insert_transaction('a', 'h', Quantity(1, 'BTC'), Price(7, 'MC'))
+        rep_dict = self.compute_reputations()
+        self.assertTrue('c' in rep_dict)
+        self.assertTrue('d' in rep_dict)
