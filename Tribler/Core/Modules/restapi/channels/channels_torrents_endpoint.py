092702557a1d72d02e85ee0fd832919f3661d8f3
Sandip Pandey <np.sandip.pandey@gmail.com>
2017-10-11 17:38:41 +0200

    Fix #3252 Remove selected torrents button now deletes all selected torrents

--- Tribler/Core/Modules/restapi/channels/channels_torrents_endpoint.py
+++ Tribler/Core/Modules/restapi/channels/channels_torrents_endpoint.py
@@ -226,16 +226,16 @@ class ChannelModifyTorrentEndpoint(BaseChannelsEndpoint):
 
     def render_DELETE(self, request):
         """
-        .. http:delete:: /channels/discovered/(string: channelid)/torrents/(string: torrent infohash)
+        .. http:delete:: /channels/discovered/(string: channelid)/torrents/(string: comma separated torrent infohashes)
 
-        Remove a torrent with a given infohash from a given channel.
+        Remove a single or multiple torrents with the given comma separated infohashes from a given channel.
 
             **Example request**:
 
             .. sourcecode:: none
 
                 curl -X DELETE http://localhost:8085/channels/discovered/abcdefg/torrents/
-                97d2d8f5d37e56cfaeaae151d55f05b077074779
+                97d2d8f5d37e56cfaeaae151d55f05b077074779,971d55f05b077074779d2d8f5d37e56cfaeaae15
 
             **Example response**:
 
@@ -245,24 +245,37 @@ class ChannelModifyTorrentEndpoint(BaseChannelsEndpoint):
                     "removed": True
                 }
 
-            :statuscode 404: if the channel is not found or if the torrent is not found in the specified channel
+            .. sourcecode:: javascript
+
+                {
+                    "removed": False, "failed_torrents":["97d2d8f5d37e56cfaeaae151d55f05b077074779"]
+                }
+
+            :statuscode 404: if the channel is not found
         """
         channel_info = self.get_channel_from_db(self.cid)
         if channel_info is None:
             return ChannelsTorrentsEndpoint.return_404(request)
 
-        torrent_db_columns = ['Torrent.torrent_id', 'infohash', 'Torrent.name', 'length', 'Torrent.category',
-                              'num_seeders', 'num_leechers', 'last_tracker_check', 'ChannelTorrents.dispersy_id']
-        torrent_info = self.channel_db_handler.getTorrentFromChannelId(channel_info[0], self.path.decode('hex'),
-                                                                       torrent_db_columns)
-
-        if torrent_info is None:
-            return BaseChannelsEndpoint.return_404(request, message=UNKNOWN_TORRENT_MSG)
-
         channel_community = self.get_community_for_channel_id(channel_info[0])
         if channel_community is None:
             return BaseChannelsEndpoint.return_404(request, message=UNKNOWN_COMMUNITY_MSG)
 
-        channel_community.remove_torrents([torrent_info[8]])  # the 8th index is the dispersy id of the channel torrent
+        torrent_db_columns = ['Torrent.torrent_id', 'infohash', 'Torrent.name', 'length', 'Torrent.category',
+                              'num_seeders', 'num_leechers', 'last_tracker_check', 'ChannelTorrents.dispersy_id']
+
+        failed_torrents = []
+        for torrent_path in self.path.split(","):
+            torrent_info = self.channel_db_handler.getTorrentFromChannelId(channel_info[0],
+                                                                           torrent_path.decode('hex'),
+                                                                           torrent_db_columns)
+            if torrent_info is None:
+                failed_torrents.append(torrent_path)
+            else:
+                # the 8th index is the dispersy id of the channel torrent
+                channel_community.remove_torrents([torrent_info[8]])
+
+        if failed_torrents:
+            return json.dumps({"removed": False, "failed_torrents": failed_torrents})
 
         return json.dumps({"removed": True})
