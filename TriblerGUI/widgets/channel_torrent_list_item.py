d827d68b680ffd7d48d0ad067695fdf40133a46e
qstokkink <goqs@hotmail.com>
2017-11-09 14:21:43 +0100

    Fixed callback while channel_torrent item is removed
    Fixes #3208

--- TriblerGUI/widgets/channel_torrent_list_item.py
+++ TriblerGUI/widgets/channel_torrent_list_item.py
@@ -73,6 +73,8 @@ class ChannelTorrentListItem(QWidget, fc_channel_torrent_list_item):
                                                      [], 0, callback=self.on_play_request_done)
 
     def on_play_request_done(self, result):
+        if not self:
+            return
         self.window().left_menu_button_video_player.click()
         self.window().video_player_page.set_torrent_infohash(self.torrent_info["infohash"])
         self.window().left_menu_playlist.set_loading()
