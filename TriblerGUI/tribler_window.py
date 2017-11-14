db46f245ecfcf28cc4a656abd7f36e8d29b2068b
qstokkink <goqs@hotmail.com>
2017-11-14 14:36:51 +0100

    Added check for None search completions
    Fixes #3224

--- TriblerGUI/tribler_window.py
+++ TriblerGUI/tribler_window.py
@@ -369,6 +369,8 @@ class TriblerWindow(QMainWindow):
             "search/completions?q=%s" % text, self.on_received_search_completions)
 
     def on_received_search_completions(self, completions):
+        if completions is None:
+            return
         self.received_search_completions.emit(completions)
         self.search_completion_model.setStringList(completions["completions"])
 
