c1bf4d8223c5dd5b4a28934ee376bedc841c3dc6
Sandip Pandey <np.sandip.pandey@gmail.com>
2017-12-19 12:28:53 +0100

    Checked if the dialog or dialog_widget is none before performing start download request

--- TriblerGUI/tribler_window.py
+++ TriblerGUI/tribler_window.py
@@ -482,12 +482,16 @@ class TriblerWindow(QMainWindow):
 
     def on_start_download_action(self, action):
         if action == 1:
-            self.window().perform_start_download_request(self.download_uri,
-                                                         self.dialog.dialog_widget.anon_download_checkbox.isChecked(),
-                                                         self.dialog.dialog_widget.safe_seed_checkbox.isChecked(),
-                                                         self.dialog.dialog_widget.destination_input.currentText(),
-                                                         self.dialog.get_selected_files(),
-                                                         self.dialog.dialog_widget.files_list_view.topLevelItemCount())
+            if self.dialog and self.dialog.dialog_widget:
+                self.window().perform_start_download_request(
+                    self.download_uri, self.dialog.dialog_widget.anon_download_checkbox.isChecked(),
+                    self.dialog.dialog_widget.safe_seed_checkbox.isChecked(),
+                    self.dialog.dialog_widget.destination_input.currentText(),
+                    self.dialog.get_selected_files(),
+                    self.dialog.dialog_widget.files_list_view.topLevelItemCount())
+            else:
+                ConfirmationDialog.show_error(self, "Tribler UI Error", "Something went wrong. Please try again.")
+                logging.exception("Error while trying to download. Either dialog or dialog.dialog_widget is None")
 
         self.dialog.request_mgr.cancel_request()  # To abort the torrent info request
         self.dialog.setParent(None)
