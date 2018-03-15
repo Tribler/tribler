094b89b0cd567a6b64c1828f56c41732f89bdf1e
Martijn de Vos <mdmartijndevos@gmail.com>
2018-03-15 09:08:33 +0100

    Fixed token balance in GUI

--- TriblerGUI/tribler_window.py
+++ TriblerGUI/tribler_window.py
@@ -467,8 +467,8 @@ class TriblerWindow(QMainWindow):
     def received_token_balance(self, statistics):
         statistics = statistics["statistics"]
         if 'latest_block' in statistics:
-            balance = statistics["latest_block"]["transaction"]["total_up"] - \
-                      statistics["latest_block"]["transaction"]["total_down"]
+            balance = (statistics["latest_block"]["transaction"]["total_up"] - \
+                      statistics["latest_block"]["transaction"]["total_down"]) / 1024 / 1024
             self.token_balance_label.setText("%d" % balance)
         else:
             self.token_balance_label.setText("0")
