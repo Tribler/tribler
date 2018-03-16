777631208e49dc95fe1f4cb5c81a213d567b0f2e
Sandip Pandey <np.sandip.pandey@gmail.com>
2018-03-16 11:22:35 +0100

    Added optional import for treq

--- Tribler/Core/Utilities/utilities.py
+++ Tribler/Core/Utilities/utilities.py
@@ -12,8 +12,8 @@ from urllib import quote_plus
 from urlparse import urlsplit, parse_qsl
 
 from libtorrent import bencode, bdecode
-import treq
 from twisted.internet import reactor
+from twisted.internet.defer import fail
 from twisted.web import http
 from twisted.web.client import Agent, readBody
 from twisted.web.http_headers import Headers
@@ -22,6 +22,12 @@ from Tribler.Core.exceptions import HttpError
 from Tribler.Core.version import version_id
 
 logger = logging.getLogger(__name__)
+
+try:
+    import treq
+    use_treq = True
+except ImportError:
+    use_treq = False
 
 
 def validate_torrent_nodes(metainfo):
@@ -346,9 +352,17 @@ def http_get(uri):
             return readBody(response)
         raise HttpError(response)
 
-    treq_deferred = treq.get(uri, persistent=False)
-    treq_deferred.addCallback(_on_response)
-    return treq_deferred
+    try:
+        if use_treq:
+            deferred = treq.get(uri, persistent=False)
+        else:
+            agent = Agent(reactor)
+            deferred = agent.request('GET', uri, Headers({'User-Agent': ['Tribler ' + version_id]}), None)
+
+        deferred.addCallback(_on_response)
+        return deferred
+    except:
+        return fail()
 
 
 def parse_magnetlink(url):
