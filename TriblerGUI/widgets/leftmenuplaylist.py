a8057ea8bbb7208a6cd76644fd86a8aa28731aca
Martijn de Vos <mdmartijndevos@gmail.com>
2018-05-28 21:47:04 +0200

    Fixed bug when removing download and streaming it

--- TriblerGUI/widgets/leftmenuplaylist.py
+++ TriblerGUI/widgets/leftmenuplaylist.py
@@ -48,14 +48,16 @@ class LeftMenuPlaylist(QListWidget):
 
     def perform_get_files_request(self):
         self.files_request_mgr = TriblerRequestManager()
-        self.files_request_mgr.perform_request("downloads/%s/files" % self.infohash, self.on_received_files)
+        self.files_request_mgr.perform_request("downloads/%s/files" % self.infohash, self.on_received_files,
+                                               capture_errors=False)
 
     def on_received_files(self, files):
         if "files" not in files or not files["files"]:
             return
 
-        self.files_request_timer.stop()
-        self.files_request_timer = None
+        if self.files_request_timer:
+            self.files_request_timer.stop()
+            self.files_request_timer = None
 
         self.set_files(files["files"])
         self.loaded_list = True
