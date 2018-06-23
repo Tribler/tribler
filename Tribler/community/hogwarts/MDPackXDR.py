384ff6eba9ce621b00849e8f4689b9bd869474a6
V.G. Bulavintsev <golem.md@gmail.com>
2018-06-23 13:30:41 +0200

    Merge serialize metadata and gossip

--- Tribler/community/hogwarts/MDPackXDR.py
+++ Tribler/community/hogwarts/MDPackXDR.py
@@ -3,40 +3,7 @@ from timeutils import time2float, float2time, EPOCH
 from Tribler.pyipv8.ipv8.keyvault.crypto import ECCrypto
 
 INFOHASH_SIZE = 20 # bytes
-IPV8_PUBLICKEY_SIZE = 74 # bytes
-
-class MetadataTMP():
-    def __init__(self,
-            type_ = 0,
-            infohash = str(0x0)*INFOHASH_SIZE,
-            size = 0,
-            date = EPOCH,
-            title = "",
-            tags = ""):
-
-        self.type     = type_
-        self.infohash = infohash
-        self.size     = size
-        self.date     = date
-        self.title    = title
-        self.tags     = tags
-
-
-class GossipTMP():
-    def __init__(self,
-            type_ = 0,
-            public_key = str(0x0)*IPV8_PUBLICKEY_SIZE,
-            tc_pointer = 0,
-            date = EPOCH,
-            content = "",
-            sig = None):
-
-        self.type       = type_
-        self.public_key = public_key
-        self.tc_pointer = tc_pointer
-        self.date       = date
-        self.content    = content
-        self.sig        = sig
+CHANNEL_TORRENT = 1 # version
 
 
 def serialize_metadata_gossip(md, key=None):
@@ -44,21 +11,23 @@ def serialize_metadata_gossip(md, key=None):
 
     p.pack_int(                   md["type"])
     p.pack_opaque(                md["public_key"])
-    p.pack_double(time2float(     md["timestamp"])
+    p.pack_double(time2float(     md["timestamp"]))
     p.pack_uhyper(                md["tc_pointer"]) # TrustChain pointer
     p.pack_fopaque(INFOHASH_SIZE, md["infohash"])
     p.pack_uhyper(                md["size"])
     p.pack_double(time2float(     md["torrent_date"]))
+    p.pack_string(                md["title"])
     p.pack_string(                md["tags"])
 
     if key:
+        assert("sig" not in md)
         # Now we sign it
         crypto = ECCrypto()
         sig = crypto.create_signature(key, p.get_buf())
         p.pack_opaque(sig)
         md["sig"] = sig
     else:
-        assert(md["sig"])
+        assert("sig" in md)
         p.pack_opaque(md["sig"])
 
     return p.get_buf()
@@ -74,6 +43,7 @@ def deserialize_metadata_gossip(buf, check_sig = True):
     md["infohash"]                = u.unpack_fopaque(INFOHASH_SIZE)
     md["size"]                    = u.unpack_uhyper()
     md["torrent_date"] = float2time(u.unpack_double())
+    md["title"]                   = u.unpack_string()
     md["tags"]                    = u.unpack_string()
     contents_end = u.get_position()
     md["sig"]                     = u.unpack_opaque()
--- Tribler/community/hogwarts/test_serialize.py
+++ Tribler/community/hogwarts/test_serialize.py
@@ -4,50 +4,42 @@ import MDPackXDR
 import datetime
 from timeutils import time2float, float2time
 from Tribler.pyipv8.ipv8.keyvault.crypto import ECCrypto
+import copy
 
 
 
 class TestSerialize(unittest.TestCase):
 
     def setUp(self):
-        self.md_entry_empty = MDPackXDR.MetadataTMP()
-        self.md_entry1 = MDPackXDR.MetadataTMP()
-        self.md_entry1.type = 1
-        self.md_entry1.infohash = str(0x1)*20
-        self.md_entry1.date = datetime.datetime(2005, 7, 14, 12, 30)
-        self.md_entry1.tags = "tag1.tag2. tag3 . tag4:bla."
 
+        self.maxDiff = None
         self.crypto = ECCrypto()
         self.key = self.crypto.generate_key('curve25519')
 
-        self.gsp_entry = MDPackXDR.GossipTMP()
-        self.gsp_entry.type = 1
-        self.gsp_entry.tc_pointer = 123
-        self.gsp_entry.content = "1234567"
-        self.gsp_entry.date = datetime.datetime(2005, 7, 14, 12, 30)
-        self.gsp_entry.public_key = self.key.pub().key_to_bin()
-        pass
+        date = datetime.datetime(2005, 7, 14, 12, 30)
+        self.md = {
+            "type" : MDPackXDR.CHANNEL_TORRENT,
+            "infohash" : str(0x1)*20,
+            "title" : "bla",
+            "tags" : "tag1.tag2. tag3 . tag4:bla.",
+            "size" : 3,
+            "timestamp" : datetime.datetime(2005, 7, 14, 12, 30),
+            "torrent_date" : datetime.datetime(2005, 7, 14, 12, 30),
+            "tc_pointer" : 0,
+            "public_key" : self.key.pub().key_to_bin()}
 
-    def TestSerializeMetadata(self):
-        md = self.md_entry1
-        md_ser = MDPackXDR.serialize_metadata(md)
-        md_deser = MDPackXDR.deserialize_metadata(md_ser)
-        self.assertDictEqual(md.__dict__, md_deser.__dict__)
+    def TestSerializeMetadataGossip(self):
+        md = copy.deepcopy(self.md)
+        md_ser = MDPackXDR.serialize_metadata_gossip(md, self.key)
+        md_deser = MDPackXDR.deserialize_metadata_gossip(md_ser)
+        self.assertDictEqual(md, md_deser)
 
-    def TestSerializeEmpty(self):
-        md = self.md_entry_empty
-        md_ser = MDPackXDR.serialize_metadata(md)
-        md_deser = MDPackXDR.deserialize_metadata(md_ser)
-        self.assertDictEqual(md.__dict__, md_deser.__dict__)
-
-    def TestSerializeGossip(self):
-        gsp = self.gsp_entry
-        gsp_ser = MDPackXDR.serialize_gossip(self.key, gsp)
-        gsp_deser = MDPackXDR.deserialize_gossip(gsp_ser)
-        # The original gsp does not have sig, so we have to copy it
-        # for check to be correct
-        gsp.sig = gsp_deser.sig
-        self.assertDictEqual(gsp.__dict__, gsp_deser.__dict__)
+    def TestSerializeSignedMetadataGossip(self):
+        md = copy.deepcopy(self.md)
+        MDPackXDR.serialize_metadata_gossip(md, self.key)
+        md_ser = MDPackXDR.serialize_metadata_gossip(md)
+        md_deser = MDPackXDR.deserialize_metadata_gossip(md_ser)
+        self.assertDictEqual(md, md_deser)
 
 
 
