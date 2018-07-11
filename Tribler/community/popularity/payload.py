e8995d9481abf3b780ab72f70db1abfc60684ebf
Sandip Pandey <np.sandip.pandey@gmail.com>
2018-07-11 17:03:46 +0200

    Fixed #3727 in Popularity community

--- Tribler/community/popularity/payload.py
+++ Tribler/community/popularity/payload.py
@@ -123,7 +123,7 @@ class TorrentInfoResponsePayload(Payload):
     def __init__(self, infohash, name, length, creation_date, num_files, comment):
         super(TorrentInfoResponsePayload, self).__init__()
         self.infohash = infohash
-        self.name = name
+        self.name = name or ''
         self.length = length or 0
         self.creation_date = creation_date or -1
         self.num_files = num_files or 0
