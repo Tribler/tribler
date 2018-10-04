e67a50210e95366b535c1a59fe7055654393faf8
Martijn de Vos <mdmartijndevos@gmail.com>
2018-10-04 17:15:54 +0200

    Only loading TrustChain blocks on demand
    Every time we did a request to trustchain/statistics, we load the last 100 blocks of a specific user. If the database is large, this adds quite some overhead. This commit changes this behaviour and TrustChain blocks are now only loaded when the user nagivates to the trust page.

--- TriblerGUI/widgets/trustpage.py
+++ TriblerGUI/widgets/trustpage.py
@@ -83,7 +83,6 @@ class TrustPage(QWidget):
         self.trust_plot = None
         self.public_key = None
         self.request_mgr = None
-        self.statistics = None
         self.blocks = None
         self.byte_scale = 1024 * 1024
 
@@ -99,10 +98,16 @@ class TrustPage(QWidget):
         self.window().navigation_stack.append(self.window().stackedWidget.currentIndex())
         self.window().stackedWidget.setCurrentIndex(PAGE_MARKET)
 
+    def load_blocks(self):
+        self.request_mgr = TriblerRequestManager()
+        self.request_mgr.perform_request("ipv8/trustchain/users/%s/blocks" % self.public_key,
+                                         self.received_trustchain_blocks)
+
     def received_trustchain_statistics(self, statistics):
         if not statistics:
             return
         statistics = statistics["statistics"]
+        self.public_key = statistics["id"]
         total_up = 0
         total_down = 0
         if 'latest_block' in statistics:
@@ -114,12 +119,6 @@ class TrustPage(QWidget):
 
         self.window().trust_people_helped_label.setText("%d" % statistics["peers_that_pk_helped"])
         self.window().trust_people_helped_you_label.setText("%d" % statistics["peers_that_helped_pk"])
-
-        # Fetch the latest blocks of this user
-        self.public_key = statistics["id"]
-        self.request_mgr = TriblerRequestManager()
-        self.request_mgr.perform_request("ipv8/trustchain/users/%s/blocks" % self.public_key,
-                                         self.received_trustchain_blocks)
 
     def received_trustchain_blocks(self, blocks):
         if blocks:
