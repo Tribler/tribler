d74a3ac874f84703df98e96707b7ae6bffde6e71
Martijn de Vos <mdmartijndevos@gmail.com>
2018-10-04 17:15:54 +0200

    Only loading TrustChain blocks on demand
    Every time we did a request to trustchain/statistics, we load the last 100 blocks of a specific user. If the database is large, this adds quite some overhead. This commit changes this behaviour and TrustChain blocks are now only loaded when the user nagivates to the trust page.

--- TriblerGUI/tribler_window.py
+++ TriblerGUI/tribler_window.py
@@ -528,6 +528,7 @@ class TriblerWindow(QMainWindow):
         self.deselect_all_menu_buttons()
         self.stackedWidget.setCurrentIndex(PAGE_TRUST)
         self.load_token_balance()
+        self.trust_page.load_blocks()
         self.navigation_stack = []
         self.hide_left_menu_playlist()
 
