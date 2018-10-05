35435cb3efe4ed00b98922af08014ddf60316d8e
Tim <elatllat@gmail.com>
2018-10-05 07:05:16 -0400

    pip for bitcoinlib workaround
    pip's return codes are unreliable and it's messages misleading so users may need extra guidance.

--- TriblerGUI/widgets/marketwalletspage.py
+++ TriblerGUI/widgets/marketwalletspage.py
@@ -168,7 +168,7 @@ class MarketWalletsPage(QWidget):
             ConfirmationDialog.show_error(self.window(), "bitcoinlib not found",
                                           "bitcoinlib could not be located on your system. "
                                           "Please install it using the following command: "
-                                          "pip install bitcoinlib --user")
+                                          "sudo chown -R $(id -un) ~/.local && pip install bitcoinlib --user")
             return
 
         self.request_mgr = TriblerRequestManager()
