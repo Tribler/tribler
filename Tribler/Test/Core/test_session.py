e2151eaebc591842c514f744124c49e2db2590b7
Martijn de Vos <mdmartijndevos@gmail.com>
2018-10-16 11:50:58 +0200

    Ignoring socket error 10053

--- Tribler/Test/Core/test_session.py
+++ Tribler/Test/Core/test_session.py
@@ -135,6 +135,7 @@ class TestSessionAsServer(TestAsServer):
         self.session.unhandled_error_observer({'isError': True, 'log_failure': 'socket.error: [Errno 113]'})
         self.session.unhandled_error_observer({'isError': True, 'log_failure': 'socket.error: [Errno 51]'})
         self.session.unhandled_error_observer({'isError': True, 'log_failure': 'socket.error: [Errno 16]'})
+        self.session.unhandled_error_observer({'isError': True, 'log_failure': 'socket.error: [Errno 10053]'})
         self.session.unhandled_error_observer({'isError': True, 'log_failure': 'socket.error: [Errno 10054]'})
         self.session.unhandled_error_observer({'isError': True,
                                                'log_failure': 'socket.error: [Errno %s]' % SOCKET_BLOCK_ERRORCODE})
