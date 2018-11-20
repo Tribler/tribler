c32046dc2ec205c3820b28851b210887d00dfb8a
Sandip Pandey <np.sandip.pandey@gmail.com>
2018-11-20 11:30:29 +0100

    Fixed LevelDB issue #3955

--- Tribler/Core/leveldbstore.py
+++ Tribler/Core/leveldbstore.py
@@ -132,13 +132,22 @@ class LevelDbStore(MutableMapping, TaskManager):
         else:
             return self._db.RangeIter(key_from=start, key_to=end)
 
-    def flush(self):
-        if self._pending_torrents:
+    def flush(self, retry=3, write_batch=None):
+        if not write_batch and self._pending_torrents:
             write_batch = self._writebatch(self._db)
             for k, v in self._pending_torrents.iteritems():
                 write_batch.Put(k, v)
             self._pending_torrents.clear()
-            return self._db.Write(write_batch)
+
+        if write_batch:
+            if not retry:
+                self._logger.error("Failed to flush LevelDB cache. Max retry done.")
+                return
+            try:
+                self._db.Write(write_batch)
+            except Exception as ex:
+                self._logger.error("Failed to flush LevelDB cache. Will retry %s times. Error:%s", retry-1, ex)
+                self.flush(retry=retry-1, write_batch=write_batch)
 
     def close(self):
         self.shutdown_task_manager()
