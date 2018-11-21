9176ab7f16d7cf92d7c677147907aa77ada1fe59
Martijn de Vos <mdmartijndevos@gmail.com>
2018-11-21 14:17:50 +0100

    Cleaned up tunnel helper

--- twisted/plugins/tunnel_helper_plugin.py
+++ twisted/plugins/tunnel_helper_plugin.py
@@ -1,21 +1,17 @@
 """
 This twistd plugin enables to start a tunnel helper headless using the twistd command.
 """
-import logging.config
+import logging
 import os
-import random
 import signal
-import threading
 import re
 
 
 from twisted.application.service import MultiService, IServiceMaker
 from twisted.conch import manhole_tap
 from twisted.internet import reactor
-from twisted.internet.stdio import StandardIO
 from twisted.internet.task import LoopingCall
 from twisted.plugin import IPlugin
-from twisted.protocols.basic import LineReceiver
 from twisted.python import usage
 from twisted.python.log import msg
 from zope.interface import implements
@@ -90,22 +86,6 @@ class Options(usage.Options):
     ]
 
 
-if not os.path.exists("logger.conf"):
-    print "Unable to find logger.conf"
-else:
-    log_directory = os.path.abspath(os.environ.get('APPDATA', os.path.expanduser('~')))
-    log_directory = os.path.join(log_directory, '.Tribler', 'logs')
-
-    if not os.path.exists(log_directory):
-        os.makedirs(log_directory)
-
-    logging.info_log_file = '%s/tribler-info.log' % log_directory
-    logging.error_log_file = '%s/tribler-error.log' % log_directory
-    logging.config.fileConfig("logger.conf", disable_existing_loggers=False)
-
-logger = logging.getLogger('TunnelMain')
-
-
 class Tunnel(object):
 
     def __init__(self, options):
@@ -115,8 +95,8 @@ class Tunnel(object):
         self.community = None
         self.clean_messages_lc = LoopingCall(self.clean_messages)
         self.clean_messages_lc.start(1800)
-        self.clean_messages_lc = LoopingCall(self.periodic_bootstrap)
-        self.clean_messages_lc.start(30, now=False)
+        self.bootstrap_lc = LoopingCall(self.periodic_bootstrap)
+        self.bootstrap_lc.start(30, now=False)
 
     def clean_messages(self):
         clean_twisted_observers()
@@ -124,7 +104,14 @@ class Tunnel(object):
     def periodic_bootstrap(self):
         self.session.lm.tunnel_community.bootstrap()
 
-    def tribler_started(self):
+    def tribler_started(self, _):
+        # Remove all logging handlers
+        root_logger = logging.getLogger()
+        handlers = root_logger.handlers
+        for handler in handlers:
+            root_logger.removeHandler(handler)
+        logging.getLogger().setLevel(logging.ERROR)
+
         new_strategies = []
         with self.session.lm.ipv8.overlay_lock:
             for strategy, target_peers in self.session.lm.ipv8.strategies:
@@ -184,8 +171,6 @@ class Tunnel(object):
             config.set_ipv8_bootstrap_override(self.options["ipv8_bootstrap_override"])
 
         self.session = Session(config)
-        logger.info("Using IPv8 port %d" % self.session.config.get_dispersy_port())
-
         self.session.notifier.add_observer(self.circuit_removed, NTFY_TUNNEL, [NTFY_REMOVE])
 
         return self.session.start().addCallback(self.tribler_started)
@@ -194,44 +179,12 @@ class Tunnel(object):
         if self.clean_messages_lc:
             self.clean_messages_lc.stop()
             self.clean_messages_lc = None
+        if self.bootstrap_lc:
+            self.bootstrap_lc.stop()
+            self.bootstrap_lc = None
 
         if self.session:
-            logger.info("Going to shutdown session")
             return self.session.shutdown()
-
-
-class LineHandler(LineReceiver):
-    delimiter = os.linesep
-
-    def __init__(self, anon_tunnel):
-        self.anon_tunnel = anon_tunnel
-
-    def lineReceived(self, line):
-        anon_tunnel = self.anon_tunnel
-
-        if line == 'threads':
-            for thread in threading.enumerate():
-                logger.debug("%s \t %d", thread.name, thread.ident)
-        elif line == 'c':
-            logger.debug("========\nCircuits\n========\nid\taddress\t\t\t\t\tgoal\thops\tIN (MB)\tOUT (MB)\tinfohash\ttype")
-            for circuit_id, circuit in anon_tunnel.community.circuits.items():
-                info_hash = circuit.info_hash.encode('hex')[:10] if circuit.info_hash else '?'
-                logger.debug("%d\t%s:%d\t%d\t%d\t\t%.2f\t\t%.2f\t\t%s\t%s" % circuit_id,
-                                                                       circuit.first_hop[0],
-                                                                       circuit.first_hop[1],
-                                                                       circuit.goal_hops,
-                                                                       len(circuit.hops),
-                                                                       circuit.bytes_down / 1024.0 / 1024.0,
-                                                                       circuit.bytes_up / 1024.0 / 1024.0,
-                                                                       info_hash,
-                                                                       circuit.ctype)
-        elif line == 'r':
-            logger.debug("circuit\t\t\tdirection\tcircuit\t\t\tTraffic (MB)")
-            from_to = anon_tunnel.community.relay_from_to
-            for key in from_to.keys():
-                relay = from_to[key]
-                logger.info("%s-->\t%s\t\t%.2f" % ((key[0], key[1]), (relay.sock_addr, relay.circuit_id),
-                                                   relay.bytes[1] / 1024.0 / 1024.0,))
 
 
 class TunnelHelperServiceMaker(object):
@@ -252,7 +205,6 @@ class TunnelHelperServiceMaker(object):
         """
 
         tunnel = Tunnel(options)
-        StandardIO(LineHandler(tunnel))
 
         def signal_handler(sig, _):
             msg("Received shut down signal %s" % sig)
