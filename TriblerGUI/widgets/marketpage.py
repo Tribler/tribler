a3b6807ebb891955d2ed4df214c13edf40bee1be
Martijn de Vos <mdmartijndevos@gmail.com>
2018-12-22 21:02:15 +0100

    Added price details to GUI list with asks/bids

--- TriblerGUI/widgets/marketpage.py
+++ TriblerGUI/widgets/marketpage.py
@@ -128,8 +128,10 @@ class MarketPage(QWidget):
         self.window().create_bid_button.setText("Buy %s for %s" % (self.chosen_wallets[0], self.chosen_wallets[1]))
 
         # Update headers of the tree widget
+        self.window().asks_list.headerItem().setText(0, 'Price (%s/%s)' % self.chosen_wallets[::-1])
         self.window().asks_list.headerItem().setText(1, '%s' % self.chosen_wallets[0])
         self.window().asks_list.headerItem().setText(2, 'Total (%s)' % self.chosen_wallets[0])
+        self.window().bids_list.headerItem().setText(2, 'Price (%s/%s)' % self.chosen_wallets[::-1])
         self.window().bids_list.headerItem().setText(1, '%s' % self.chosen_wallets[0])
         self.window().bids_list.headerItem().setText(0, 'Total (%s)' % self.chosen_wallets[0])
 
