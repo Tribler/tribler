e3618382ce7a521a067d9650e3c42ff409f312ed
V.G. Bulavintsev <golem.md@gmail.com>
2019-01-09 17:35:18 +0100

    Add TrackerState ORM class

---
+++ Tribler/Core/Modules/MetadataStore/OrmBindings/tracker_state.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pony import orm
+
+from Tribler.Core.Modules.MetadataStore.serialization import EPOCH
+from Tribler.Core.Utilities.tracker_utils import get_uniformed_tracker_url, MalformedTrackerURLException
+
+
+def define_binding(db):
+    class TrackerState(db.Entity):
+        url = orm.PrimaryKey(str)
+        last_check = orm.Optional(datetime, default=EPOCH)
+        alive = orm.Optional(bool, default=True)
+        torrents = orm.Set('TorrentState', reverse='trackers')
+
+        def __init__(self, *args, **kwargs):
+            # Sanitize and canonicalize the tracker URL
+            sanitized = get_uniformed_tracker_url(kwargs['url'])
+            if sanitized:
+                kwargs['url'] = sanitized
+            else:
+                raise MalformedTrackerURLException("Could not canonicalize tracker URL (%s)" % kwargs['url'])
+
+            super(TrackerState, self).__init__(*args, **kwargs)
+
+    return TrackerState
