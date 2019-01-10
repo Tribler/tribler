27cf76db5a9cbb668afe2d6506604f8efaf90647
V.G. Bulavintsev <golem.md@gmail.com>
2019-01-10 18:28:30 +0100

    WIP - migration

--- Tribler/Core/Modules/MetadataStore/OrmBindings/torrent_state.py
+++ Tribler/Core/Modules/MetadataStore/OrmBindings/torrent_state.py
@@ -3,7 +3,7 @@ from pony import orm
 from Tribler.pyipv8.ipv8.database import database_blob
 
 
-def define_binding(db):
+def define_binding(db)
     class TorrentState(db.Entity):
         infohash = orm.PrimaryKey(database_blob)
         seeders = orm.Optional(int, default=0)
--- Tribler/Core/Upgrade/db71_to_pony.py
+++ Tribler/Core/Upgrade/db71_to_pony.py
@@ -1,15 +1,18 @@
-import os
+from binascii import unhexlify
 
-import lz4
 import apsw
-import lz4.frame
 
+from Tribler.Core.Modules.MetadataStore.OrmBindings.metadata import LEGACY_ENTRY
+from Tribler.Core.Modules.MetadataStore.store import MetadataStore
+from Tribler.pyipv8.ipv8.keyvault.crypto import default_eccrypto
 
-lz4.frame
-select_channels_sql = "Select name, dispersy_cid, modified, nr_torrents, nr_favorite, nr_spam "\
-      + "FROM Channels " \
-      + "WHERE nr_torrents >= 3 " \
-      + "AND "
+select_channels_sql = "Select name, dispersy_cid, modified, nr_torrents, nr_favorite, nr_spam " \
+                      + "FROM Channels " \
+                      + "WHERE nr_torrents >= 3 " \
+                      + "AND name not NULL;"
+
+select_torrents_sql = "SELECT dispersy_cid, infohash, timestamp"
+
 
 class DispersyToPonyMigration(object):
 
@@ -23,17 +26,61 @@ class DispersyToPonyMigration(object):
         cursor = connection.cursor()
 
         channels = []
-        for name, dispersy_cid, modified, nr_torrents, nr_favorite, nr_spam in cursor.execute(select_channels_sql):
-            channels.append = {"title":name,
-                               "public_key":dispersy_cid,
-                               "timestamp":modified,
-                               "version": nr_torrents,
-                               "votes":nr_favorite,
-                               "nr_spam)
+        for channel_id, name, dispersy_cid, modified, nr_torrents, nr_favorite, nr_spam in cursor.execute(
+                select_channels_sql):
+            channels.append({"old_id": channel_id,
+                             "title": name,
+                             "public_key": dispersy_cid,
+                             "timestamp": modified,
+                             "version": nr_torrents,
+                             "votes": nr_favorite,
+                             "xxx": nr_spam})
+        return channels
+
+    def get_old_torrents(self):
+        connection = apsw.Connection(self.tribler_db)
+        cursor = connection.cursor()
+
+        torrents = []
+        for infohash, length, creation_date, name, dispersy_cid, torrent_id, category, num_seeders, num_leechers, tracker_url in cursor.execute(
+                select_channels_sql):
+            # num_seeders
+            # num_leechers
+            # last_tracker_check
+            torrents.append({
+                "status": LEGACY_ENTRY,
+                "infohash": unhexlify(infohash),
+                "timestamp": 0,
+                "size": length,
+                "torrent_date": creation_date,
+                "title": name,
+                "tags": category,
+                "tracker_info": tracker_url,
+                "id_": torrent_id,
+                "origin_id": 0,
+                "public_key": dispersy_cid,
+                "signature": 0,
+                "xxx": int(category == u'xxx')})
+            return torrents
+
+    if __name__ == "__main__":
+        my_key = default_eccrypto.generate_key(u"curve25519")
+        mds = MetadataStore(":memory:", "/tmp", my_key)
+        d = DispersyToPonyMigration("/tmp/tribler.sdb", "/tmp/dispersy.sdb", mds)
+        old_channels = d.get_old_channels()
+       """
+       select Torrent.infohash, Torrent.length, Torrent.name, Torrent.creation_date, ChannelTorrents.torrent_id, Torrent.category
+from ChannelTorrents, Channels, Torrent
+where ChannelTorrents.torrent_id = Torrent.torrent_id AND Channels.id = ChannelTorrents.channel_id 
+select Torrent.infohash, Torrent.num_seeders, Torrent.num_leechers, Torrent.last_tracker_check
+from ChannelTorrents, Channels, Torrent
+where ChannelTorrents.torrent_id = Torrent.torrent_id AND Channels.id = ChannelTorrents.channel_id
+       
+       """
 
 
-
-
-
-
-    def migrate_channels(self):
+        # 1 - Move Trackers (URLs)
+        # 2 - Move torrent Infohashes
+        # 3 - Move Infohash-Tracker relationships
+        # 4 - Move Metadata, based on Infohashes
+        # 5 - Move Channels
