98ca62e57a3f5fe29e04f8ab97ecbe11fbd7509e
cclauss <cclauss@me.com>
2019-03-04 23:43:29 +0100

    from __future__ import absolute_import

--- Tribler/Core/Modules/restapi/create_torrent_endpoint.py
+++ Tribler/Core/Modules/restapi/create_torrent_endpoint.py
@@ -1,14 +1,17 @@
+from __future__ import absolute_import
+
 import base64
 import json
 import logging
+
 from twisted.web import http, resource
 from twisted.web.server import NOT_DONE_YET
 
 from Tribler.Core.DownloadConfig import DownloadStartupConfig
 from Tribler.Core.Modules.restapi.util import return_handled_exception
+import Tribler.Core.Utilities.json_util as json
 from Tribler.Core.Utilities.unicode import ensure_text
 from Tribler.Core.exceptions import DuplicateDownloadException
-import Tribler.Core.Utilities.json_util as json
 
 
 class CreateTorrentEndpoint(resource.Resource):
