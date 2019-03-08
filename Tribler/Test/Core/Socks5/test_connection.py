5fb17f23a7bda55143d4a4a88fe1663a19599c95
cclauss <cclauss@me.com>
2019-03-08 12:35:53 +0100

    from __future__ import absolute_import

--- Tribler/Test/Core/Socks5/test_connection.py
+++ Tribler/Test/Core/Socks5/test_connection.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from binascii import unhexlify
 
 from twisted.internet.defer import inlineCallbacks
--- Tribler/Test/util/Tracker/HTTPTracker.py
+++ Tribler/Test/util/Tracker/HTTPTracker.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from binascii import hexlify
 
 from libtorrent import bencode
