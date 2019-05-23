4fb8e48969c613a1557b539effaf18179b83c5ba
Sandip Pandey <np.sandip.pandey@gmail.com>
2019-05-23 11:30:26 +0200

    Added minimum and maximum threshold for the node size in the trust graph

--- TriblerGUI/widgets/trustgraphpage.py
+++ TriblerGUI/widgets/trustgraphpage.py
@@ -187,9 +187,18 @@ class TrustAnimationCanvas(FigureCanvas):
         return COLOR_NEUTRAL
 
     def get_node_size(self, node_public_key):
+        """
+        Returns the size for a given node based on its Trustchain balance.
+        The lower threshold is 1 GigaByte and the upper threshold is 1 TeraByte.
+        """
         node_balance = self.token_balance.get(node_public_key, {'total_up': 0, 'total_down': 0})
-        diff_log = abs(node_balance['total_up'] - node_balance['total_down'])
-        return 10 * math.log(diff_log + 10, 10)
+        diff_balance = abs(node_balance['total_up'] - node_balance['total_down'])
+        if diff_balance < 1024 ** 3:
+            return 100
+        size = 10 * math.log(diff_balance, 2) + 100
+        if size > 500:
+            size = 500
+        return size
 
     def should_redraw_graph(self):
         if not self.old_pos or self.redraw:
