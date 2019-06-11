19cd855ec1ce49fd087f1c32e8e7449de88836ea
Sandip Pandey <np.sandip.pandey@gmail.com>
2019-06-11 13:58:28 +0200

    Fixed issue with subscription widget in channel page

--- TriblerGUI/widgets/subscriptionswidget.py
+++ TriblerGUI/widgets/subscriptionswidget.py
@@ -65,8 +65,8 @@ class SubscriptionsWidget(QWidget):
         if self.channel_info["state"] == "Personal" or self.channel_info["status"] == 1000:
             return
         self.request_mgr = TriblerRequestManager()
-        self.request_mgr.perform_request("metadata/channels/%s" %
-                                         self.channel_info['public_key'],
+        self.request_mgr.perform_request("metadata/channels/%s/%i" %
+                                         (self.channel_info[u'public_key'], self.channel_info[u'id']),
                                          lambda data: self.update_subscribe_button(remote_response=data),
                                          data={"subscribe": int(not self.channel_info["subscribed"])}, method='POST')
 
