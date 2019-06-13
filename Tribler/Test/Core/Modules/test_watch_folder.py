7c75b5ca359a8fc65db0b72512e8e2091f337efc
V.G. Bulavintsev <golem.md@gmail.com>
2019-06-13 12:24:00 +0200

    magic

--- Tribler/Test/Core/Modules/test_watch_folder.py
+++ Tribler/Test/Core/Modules/test_watch_folder.py
@@ -43,8 +43,8 @@ class TestWatchFolder(TestAsServer):
         self.session.lm.watch_folder.check_watch_folder()
 
     def test_watchfolder_torrent_file_one_corrupt(self):
-        shutil.copyfile(TORRENT_UBUNTU_FILE, os.path.join(self.watch_dir, "test.torrent"))
-        shutil.copyfile(os.path.join(TESTS_DATA_DIR, 'test_rss.xml'), os.path.join(self.watch_dir, "test2.torrent"))
+        shutil.copyfile(TORRENT_UBUNTU_FILE, os.path.join(self.watch_dir, u"test.torrent"))
+        shutil.copyfile(os.path.join(TESTS_DATA_DIR, u'test_rss.xml'), os.path.join(self.watch_dir, u"test2.torrent"))
         self.session.lm.watch_folder.check_watch_folder()
         self.assertEqual(len(self.session.get_downloads()), 1)
         self.assertTrue(os.path.isfile(os.path.join(self.watch_dir, "test2.torrent.corrupt")))
