e34743e5b7584f619b8840638d9e872470ba6c37
V.G. Bulavintsev <golem.md@gmail.com>
2019-06-13 15:21:53 +0200

    Revert "more magic"

--- Tribler/Test/common.py
+++ Tribler/Test/common.py
@@ -4,8 +4,8 @@ import os
 UBUNTU_1504_INFOHASH = binascii.unhexlify('FC8A15A2FAF2734DBB1DC5F7AFDC5C9BEAEB1F59')
 
 TESTS_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-TESTS_API_DIR = os.path.abspath(os.path.join(TESTS_DIR, "API"))
-TESTS_DATA_DIR = os.path.abspath(os.path.join(TESTS_DIR, "data"))
+TESTS_API_DIR = os.path.abspath(os.path.join(TESTS_DIR, u"API"))
+TESTS_DATA_DIR = os.path.abspath(os.path.join(TESTS_DIR, u"data"))
 
 TORRENT_UBUNTU_FILE = os.path.join(TESTS_DATA_DIR, "ubuntu-15.04-desktop-amd64.iso.torrent")
 TORRENT_UBUNTU_FILE_INFOHASH = binascii.unhexlify("fc8a15a2faf2734dbb1dc5f7afdc5c9beaeb1f59")
