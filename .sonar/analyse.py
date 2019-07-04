301ab27a939b9662964d6f710a7233b9e0ff23e7
Sandip Pandey <np.sandip.pandey@gmail.com>
2019-07-04 14:35:47 +0200

    Fixed sonarcloud analysis

--- .sonar/analyse.py
+++ .sonar/analyse.py
@@ -1,18 +1,22 @@
 """
-This script takes a Jenkins workspace and automatically fetches the Sonarqube task ID from it.
-Next, it queries the Sonarqube API to get the status of the task. When the task is done, it fetches the status
-of the quality gate from Sonarqube and exits with a non-zero exit code if the quality gate failed.
+This script takes a Jenkins workspace and automatically fetches the Sonar Cloud task ID from it.
+Next, it queries the SonarCloud API to get the status of the task. When the task is done, it fetches the status
+of the quality gate from the server and exits with a non-zero exit code if the quality gate has failed.
 """
 from __future__ import absolute_import, print_function
+
 import json
 import os
 import time
-import requests
+from traceback import print_exc
 
+import requests
 from six.moves import xrange
 
+# These env varialble should be set by Jenkins.
 SERVER_URL = os.environ.get('SONAR_SERVER_URL', "https://sonarcloud.io")
 PROJECT_KEY = os.environ.get('PROJECT_KEY', "org.sonarqube:tribler")
+PR_COMMIT = os.environ.get('ghprbActualCommit', u'')
 TASK_PATH = os.path.join(os.environ.get('WORKSPACE', os.getcwd()), '.scannerwork', 'report-task.txt')
 
 task_status_url = None
@@ -42,26 +46,31 @@ for _ in xrange(0, 30):
         print("Task done, status: %s" % data['task']['status'])
         break
 
+# Analysis URL for the project. Check https://sonarcloud.io/web_api/api/project_pull_requests for more info
+pr_analysis_url = "%s/api/project_pull_requests/list?project=%s" % (SERVER_URL, PROJECT_KEY)
+print("Analysis URL:", pr_analysis_url)
+print("PR Commit:", PR_COMMIT)
+
+# Analysis might take a few more seconds to complete and the results to be available so wait sometime
 time.sleep(10)
 
-# We now fetch the status of the quality gate
-gate_response_url = "%s/api/measures/component?metricKeys=alert_status&componentKey=%s" % (SERVER_URL, PROJECT_KEY)
-json_response = requests.get(gate_response_url)
-data = json.loads(json_response.text)
-if 'component' not in data:
-    print("No sonar component found")
-    exit(1)
+# Fetch analysis response and find the status for the PR commit
+try:
+    json_response = requests.get(pr_analysis_url)
+    data = json.loads(json_response.text)
 
-sonar_component = data['component']
-if sonar_component['key'] != PROJECT_KEY:
-    print("%s != %s" % (sonar_component['key'], PROJECT_KEY))
-    exit(1)
+    for pull_request in data[u'pullRequests']:
+        print("Matching analysis:", pull_request[u'key'], PR_COMMIT, pull_request[u'key'] == PR_COMMIT)
+        # If there is analysis result for the PR commit with status OK, then exit with success status (0)
+        if pull_request[u'key'] == PR_COMMIT:
+            print("Quality Gate:", pull_request[u'status'])
+            if pull_request[u'status'][u'qualityGateStatus'] == u'OK':
+                print("Status: OK")
+                break
+            else:
+                print("Status: FAILED")
+                exit(1)
 
-for measure in sonar_component["measures"]:
-    if measure["metric"] == "alert_status":
-        if measure["value"] == "OK":
-            print("SonarQube says OK")
-            exit(0)
-        else:
-            print("Quality gate failed")
-            exit(1)
+except Exception:
+    print_exc()
+    exit(1)
