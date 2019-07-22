a75530b8c206de2adfd016bae394859848045769
Martijn de Vos <mdmartijndevos@gmail.com>
2019-07-22 16:31:37 +0200

    Not crashing when receiving IPv6 request in socks

--- Tribler/Test/Core/Socks5/test_conversion.py
+++ Tribler/Test/Core/Socks5/test_conversion.py
@@ -1,6 +1,8 @@
+from __future__ import absolute_import
+
 import struct
 
-from Tribler.Core.Socks5.conversion import decode_request, IPV6AddrError
+from Tribler.Core.Socks5.conversion import decode_request
 from Tribler.Test.test_as_server import AbstractServer
 
 
@@ -11,4 +13,4 @@ class TestSocks5Conversion(AbstractServer):
         Test the decoding process of a request
         """
         self.assertIsNone(decode_request(0, struct.pack("!BBBB", 5, 0, 0, 5))[1])  # Invalid address type
-        self.assertRaises(IPV6AddrError, decode_request, 0, struct.pack("!BBBB", 5, 0, 0, 4))  # IPv6
+        self.assertIsNone(decode_request(0, struct.pack("!BBBB", 5, 0, 0, 4))[1])  # IPv6
