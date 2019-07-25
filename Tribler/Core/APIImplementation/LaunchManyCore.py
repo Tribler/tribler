13f352d0c6f1f780b0f5a680908da82df46e4701
bulat <bulat.nasrulin@gmail.com>
2019-07-25 17:06:01 +0200

    Bootstrap block content update

--- Tribler/Core/APIImplementation/LaunchManyCore.py
+++ Tribler/Core/APIImplementation/LaunchManyCore.py
@@ -703,7 +703,8 @@ class TriblerLaunchMany(TaskManager):
             self.bootstrap = Bootstrap(self.session.config.get_state_dir(), dht=self.dht_community)
             if os.path.exists(self.bootstrap.bootstrap_file):
                 self.bootstrap.start_initial_seeder(self.session.start_download_from_tdef,
-                                                    self.bootstrap.bootstrap_file)
+                                                    self.bootstrap.bootstrap_file,
+                                                    self.session.config.get_bootstrap_infohash())
             else:
                 self.bootstrap.start_by_infohash(self.session.start_download_from_tdef,
                                                  self.session.config.get_bootstrap_infohash())
