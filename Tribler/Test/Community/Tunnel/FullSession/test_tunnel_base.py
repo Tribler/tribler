f89212f5043122e24cfb13a52d4c73076803cd0f
Egbert Bouman <ebouman@gmail.com>
2019-12-11 14:48:27 +0100

    Cleanup/speedup tunnel tests

--- Tribler/Test/Community/Tunnel/FullSession/test_tunnel_base.py
+++ Tribler/Test/Community/Tunnel/FullSession/test_tunnel_base.py
@@ -131,13 +131,12 @@ class TestTunnelBase(TestAsServer):
 
         self.setUpPreSession()
         config = self.config.copy()
-        config.set_libtorrent_enabled(True)
+        config.set_libtorrent_enabled(False)
         config.set_state_dir(self.getStateDir(index))
         config.set_tunnel_community_socks5_listen_ports(self.get_ports(5))
 
         session = Session(config)
         await session.start()
-        session.ltmgr.is_shutdown_ready = lambda: True
         self.sessions.append(session)
 
         return await self.load_tunnel_community_in_session(session, exitnode=exitnode)
--- Tribler/Test/Community/Tunnel/FullSession/test_tunnel_community.py
+++ Tribler/Test/Community/Tunnel/FullSession/test_tunnel_community.py
@@ -1,8 +1,5 @@
-import sys
-from asyncio import Future, sleep
-from unittest import skipIf
+from asyncio import sleep, wait_for
 
-from Tribler.Core.simpledefs import DLSTATUS_SEEDING
 from Tribler.Test.Community.Tunnel.FullSession.test_tunnel_base import TestTunnelBase
 from Tribler.Test.tools import timeout
 
@@ -12,65 +9,34 @@ class TestTunnelCommunity(TestTunnelBase):
     This class contains full session tests for the tunnel community.
     """
 
-    async def setUp(self):
-        self.test_future = Future()
-        await super(TestTunnelCommunity, self).setUp()
-
-    # FIXME: this test fails on Linux. It started to fail on some Linux systems from eb3d60802
-    # This points that this is a result of some kind of race condition.
-    @skipIf(sys.platform.startswith('linux'), reason="fails on linux due to race conditions")
-    @timeout(30)
+    @timeout(20)
     async def test_anon_download(self):
         """
         Testing whether an anonymous download over our tunnels works
         """
         await self.setup_nodes()
-
-        def download_state_callback(ds):
-            if ds.get_progress() == 1.0 and ds.get_status() == DLSTATUS_SEEDING:
-                self.test_future.set_result(None)
-                return 0.0
-            return 2.0
-
         download = await self.start_anon_download()
-        download.set_state_callback(download_state_callback)
+        await wait_for(download.future_finished, timeout=15)
+        self.assertGreater(self.tunnel_community.find_circuits()[0].bytes_down, 2000000)
 
-        await self.test_future
-
-    @timeout(30)
-    async def test_anon_download_no_exitnodes(self):
+    @timeout(20)
+    async def test_anon_download_no_exits(self):
         """
         Testing whether an anon download does not make progress without exit nodes
         """
         await self.setup_nodes(num_exitnodes=0)
-
-        def download_state_callback(ds):
-            if ds.get_progress() != 0.0:
-                self.test_future.set_exception(
-                    RuntimeError("Anonymous download should not make progress without exit nodes"))
-                return 0.0
-            return 2.0
-
         download = await self.start_anon_download()
-        download.set_state_callback(download_state_callback)
+        await sleep(10)
+        self.assertEqual(self.tunnel_community.find_circuits(), [])
+        self.assertEqual(download.get_state().get_total_transferred('down'), 0)
 
-        await sleep(25)
-
-    @timeout(30)
+    @timeout(20)
     async def test_anon_download_no_relays(self):
         """
         Testing whether an anon download does not make progress without relay nodes
         """
         await self.setup_nodes(num_relays=0, num_exitnodes=1)
-
-        def download_state_callback(ds):
-            if ds.get_progress() != 0.0:
-                self.test_future.set_exception(
-                    RuntimeError("Anonymous download should not make progress without relay nodes"))
-                return 0.0
-            return 2.0
-
         download = await self.start_anon_download(hops=2)
-        download.set_state_callback(download_state_callback)
-
-        await sleep(25)
+        await sleep(10)
+        self.assertEqual(self.tunnel_community.find_circuits(), [])
+        self.assertEqual(download.get_state().get_total_transferred('down'), 0)
