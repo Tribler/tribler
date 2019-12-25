039943b8226c0d23c76c467d9fcb0b6ae72aaabe
Solomon1732 <shlomireu5@gmail.com>
2019-12-26 00:04:14 +0200

    Refractor lambda to function
    Before a lambda was assigned to a variable. This made the whole point of lambdas being anonymus functions moot. Instead python 2 support was dropped, and the lambda was refractored to a function.
    
    Remove file and copy one-liner to function
    
    The file held only one utility that was used in only one place. The function originally held a one-liner. Hence the file was removed and the one-liner copied.
    
    Remove one-liner in favor of passing directly
    
    Remove one-liner since `dest_dir` should already be in unicode

--- Tribler/util.py
+++
@@ -1,9 +0,0 @@
-"""
-This file contains various utility methods.
-"""
-import sys
-
-if sys.version_info.major > 2:
-    cast_to_unicode_utf8 = lambda x: "".join([chr(c) for c in x]) if isinstance(x, bytes) else str(x)
-else:
-    cast_to_unicode_utf8 = lambda x: x.decode('utf-8')
