8f3f6c814225d7bf593cd96cc00517d06e16be2f
V.G. Bulavintsev <golem.md@gmail.com>
2020-01-08 18:05:49 +0100

    Remove insta-crash buttons in GUI

--- src/tribler-gui/tribler_gui/widgets/subscriptionswidget.py
+++ src/tribler-gui/tribler_gui/widgets/subscriptionswidget.py
@@ -86,7 +86,7 @@ class SubscriptionsWidget(QWidget):
         settings = {"credit_mining": {"sources": new_sources}}
 
         TriblerNetworkRequest(
-            "settings", self.on_credit_mining_sources, method='POST', raw_data=json.dumps(settings).decode('utf-8')
+            "settings", self.on_credit_mining_sources, method='POST', raw_data=json.dumps(settings)
         )
 
     def on_credit_mining_sources(self, json_result):
