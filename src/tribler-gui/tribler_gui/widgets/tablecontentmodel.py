a17d50469938cdb501b4dcacd2dd8fa7a67b55d4
V.G. Bulavintsev <golem.md@gmail.com>
2020-01-15 13:46:21 +0100

    Fix race condition in GUI

--- src/tribler-gui/tribler_gui/widgets/tablecontentmodel.py
+++ src/tribler-gui/tribler_gui/widgets/tablecontentmodel.py
@@ -403,10 +403,11 @@ class ChannelContentModel(RemoteTableModel):
         def on_row_update_results(response):
             if not response:
                 return
-            data_item_dict = index.model().data_items[index.row()]
-            for key, _ in data_item_dict.items():
-                if key in response:
-                    data_item_dict[key] = response[key]
+            item_row = self.item_uid_map.get(combine_pk_id(public_key, id_))
+            if item_row is None:
+                return
+            data_item_dict = index.model().data_items[item_row]
+            data_item_dict.update(response)
             self.info_changed.emit([data_item_dict])
 
         TriblerNetworkRequest(
