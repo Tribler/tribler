c1b27c6f3d975eb4b7f7258c018f50e195fa8dc8
Martijn de Vos <mdmartijndevos@gmail.com>
2020-01-17 19:37:44 +0100

    Fixed IPv8/Anydex importing

--- src/tribler-core/tribler_core/__init__.py
+++ src/tribler-core/tribler_core/__init__.py
@@ -6,10 +6,10 @@ import sys
 
 from tribler_core.utilities.path_util import Path
 
-dir_path = Path(__file__).parent
+dir_path = Path(__file__).parent.parent.parent
 
 # Make sure AnyDex can be imported
-sys.path.insert(1, os.path.join(dir_path, "anydex"))
+sys.path.append(os.path.join(dir_path, "anydex"))
 
 # Make sure IPv8 can be imported
-sys.path.insert(1, os.path.join(dir_path, "pyipv8"))
+sys.path.insert(0, os.path.join(dir_path, "pyipv8"))
