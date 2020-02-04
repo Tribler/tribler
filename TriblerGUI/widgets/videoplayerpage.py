f09cbe23f58bc342ea753733bbbd187f53c4261e
V.G. Bulavintsev <golem.md@gmail.com>
2020-02-04 12:49:20 +0100

    Fix race condition in videoplayer controls

--- TriblerGUI/widgets/videoplayerpage.py
+++ TriblerGUI/widgets/videoplayerpage.py
@@ -248,6 +248,6 @@ class VideoPlayerPage(QWidget):
 
     def eventFilter(self, source, event):
         if event.type() == QEvent.KeyRelease and self.isVisible() and not self.window().top_search_bar.hasFocus() and\
-                event.key() == Qt.Key_Space:
+                event.key() == Qt.Key_Space and self.instance:
             self.on_play_pause_button_click()
         return QWidget.eventFilter(self, source, event)
