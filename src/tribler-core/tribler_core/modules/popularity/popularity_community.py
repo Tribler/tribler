7a286b8b35b3108580dacbcb1aba473ccfc276ef
V.G. Bulavintsev <golem.md@gmail.com>
2020-02-19 18:35:54 +0100

    Wrap PopularityCommunity sql query into async thread

--- src/tribler-core/tribler_core/modules/popularity/popularity_community.py
+++ src/tribler-core/tribler_core/modules/popularity/popularity_community.py
@@ -1,4 +1,5 @@
 import random
+from asyncio import get_event_loop
 from binascii import unhexlify
 
 from ipv8.community import Community
@@ -60,19 +61,24 @@ class PopularityCommunity(Community):
         self.endpoint.send(random_peer.address, packet)
 
     @lazy_wrapper(TorrentsHealthPayload)
-    def on_torrents_health(self, _, payload):
+    async def on_torrents_health(self, _, payload):
         self.logger.info("Received torrent health information for %d random torrents and %d checked torrents",
                          len(payload.random_torrents), len(payload.torrents_checked))
 
         all_torrents = payload.random_torrents + payload.torrents_checked
-        with db_session:
-            for infohash, seeders, leechers, last_check in all_torrents:
-                torrent_state = self.metadata_store.TorrentState.get(infohash=infohash)
-                if torrent_state and last_check > torrent_state.last_check:
-                    # Replace current information
-                    torrent_state.seeders = seeders
-                    torrent_state.leechers = leechers
-                    torrent_state.last_check = last_check
-                elif not torrent_state:
-                    _ = self.metadata_store.TorrentState(infohash=infohash, seeders=seeders,
-                                                         leechers=leechers, last_check=last_check)
+
+        def _put_health_entries_in_db():
+            with db_session:
+                for infohash, seeders, leechers, last_check in all_torrents:
+                    torrent_state = self.metadata_store.TorrentState.get(infohash=infohash)
+                    if torrent_state and last_check > torrent_state.last_check:
+                        # Replace current information
+                        torrent_state.seeders = seeders
+                        torrent_state.leechers = leechers
+                        torrent_state.last_check = last_check
+                    elif not torrent_state:
+                        _ = self.metadata_store.TorrentState(infohash=infohash, seeders=seeders,
+                                                             leechers=leechers, last_check=last_check)
+
+            self.metadata_store.disconnect_thread()
+        await get_event_loop().run_in_executor(None, _put_health_entries_in_db)
