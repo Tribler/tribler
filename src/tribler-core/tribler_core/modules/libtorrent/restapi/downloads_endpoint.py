464ee9a6b9212e9e82dbf0349bd44c680a3f0699
V.G. Bulavintsev <golem.md@gmail.com>
2020-03-02 15:35:30 +0100

    Fix include/exclude download files

--- src/tribler-core/tribler_core/modules/libtorrent/restapi/downloads_endpoint.py
+++ src/tribler-core/tribler_core/modules/libtorrent/restapi/downloads_endpoint.py
@@ -434,7 +434,7 @@ class DownloadsEndpoint(RESTEndpoint):
             num_files = len(download.tdef.get_files())
             if not all([0 <= index < num_files for index in selected_files_list]):
                 return RESTResponse({"error": "index out of range"}, status=HTTP_BAD_REQUEST)
-            download.set_selected_file_indexes(selected_files_list)
+            download.config.set_selected_file_indexes(selected_files_list)
 
         if parameters.get('state'):
             state = parameters['state']
