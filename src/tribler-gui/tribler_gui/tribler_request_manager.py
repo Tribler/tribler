be900eae0a1e3cb420eb550e1a8dd167e407e5c5
V.G. Bulavintsev <golem.md@gmail.com>
2020-06-13 10:45:12 +0200

    Fix iteration on keys in request manager. Again.

--- src/tribler-gui/tribler_gui/tribler_request_manager.py
+++ src/tribler-gui/tribler_gui/tribler_request_manager.py
@@ -86,12 +86,12 @@ class TriblerRequestManager(QNetworkAccessManager):
         error_dialog.show()
 
     def clear(self):
-        for req_id, req in list(self.requests_in_flight.items()):
+        for req in list(self.requests_in_flight.values()):
             req.cancel_request()
 
     def evict_timed_out_requests(self):
         t = time()
-        for req in list(self.requests_in_flight.items()):
+        for req in list(self.requests_in_flight.values()):
             if t - req.time > self.request_timeout_interval:
                 req.cancel_request()
 
