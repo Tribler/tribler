6b74484987847072dcccadd7d5b9b7cfe6f75291
V.G. Bulavintsev <golem.md@gmail.com>
2020-07-22 15:04:24 +0200

    Fix Pony unique transaction error

--- src/tribler-core/tribler_core/modules/popularity/popularity_community.py
+++ src/tribler-core/tribler_core/modules/popularity/popularity_community.py
@@ -68,7 +68,7 @@ class PopularityCommunity(Community):
         all_torrents = payload.random_torrents + payload.torrents_checked
 
         def _put_health_entries_in_db():
-            with db_session:
+            with db_session(immediate=True):
                 for infohash, seeders, leechers, last_check in all_torrents:
                     torrent_state = self.metadata_store.TorrentState.get(infohash=infohash)
                     if torrent_state and last_check > torrent_state.last_check:
