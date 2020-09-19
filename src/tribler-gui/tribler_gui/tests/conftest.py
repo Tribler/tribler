3c01988ab1202ea8c5df8d90d30d619fb15206d0
Martijn de Vos <mdmartijndevos@gmail.com>
2020-09-19 16:41:18 +0200

    Increased timeout of GUI tests

--- src/tribler-gui/tribler_gui/tests/conftest.py
+++ src/tribler-gui/tribler_gui/tests/conftest.py
@@ -7,6 +7,9 @@ def pytest_addoption(parser):
 
 
 def pytest_collection_modifyitems(config, items):
+    for item in items:
+        item.add_marker(pytest.mark.timeout(30))
+
     if config.getoption("--guitests"):
         # --guitests given in cli: do not skip GUI tests
         return
