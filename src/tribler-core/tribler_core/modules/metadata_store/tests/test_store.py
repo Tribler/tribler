1ec860d06c1ac2b6dc97a496bb5ac8995d663b81
Martijn de Vos <mdmartijndevos@gmail.com>
2020-09-25 16:08:06 +0200

    Revert "Marked mdblob squash tests as flaky"
    This reverts commit b57a62544ccd8410d2b16b2848a8986df57a332f.

--- src/tribler-core/tribler_core/modules/metadata_store/tests/test_store.py
+++ src/tribler-core/tribler_core/modules/metadata_store/tests/test_store.py
@@ -81,7 +81,6 @@ def test_process_channel_dir_file(tmpdir, metadata_store):
 
 
 @db_session
-@pytest.mark.flaky
 def test_squash_mdblobs(metadata_store):
     chunk_size = metadata_store.ChannelMetadata._CHUNK_SIZE_LIMIT
     md_list = [
@@ -103,7 +102,6 @@ def test_squash_mdblobs(metadata_store):
 
 
 @db_session
-@pytest.mark.flaky
 def test_squash_mdblobs_multiple_chunks(metadata_store):
     md_list = [
         metadata_store.TorrentMetadata(
