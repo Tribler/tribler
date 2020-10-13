f5a63e0b13c03e4dc7bd6d4b0237f7e40aae7582
V.G. Bulavintsev <golem.md@gmail.com>
2020-10-13 18:56:15 +0200

    Make Notifier calling thread-safe

--- src/tribler-core/tribler_core/notifier.py
+++ src/tribler-core/tribler_core/notifier.py
@@ -4,6 +4,7 @@ Notifier.
 Author(s): Vadim Bulavintsev
 """
 import logging
+from asyncio import get_event_loop
 
 from tribler_common.simpledefs import NTFY
 
@@ -13,6 +14,9 @@ class Notifier(object):
     def __init__(self):
         self._logger = logging.getLogger(self.__class__.__name__)
         self.observers = {}
+        # We remember the event loop from the thread that runs the Notifier
+        # to be able to schedule notifications from external threads
+        self._event_loop = get_event_loop()
 
     def add_observer(self, subject, callback):
         assert isinstance(subject, NTFY)
@@ -30,6 +34,11 @@ class Notifier(object):
         self._logger.debug(f"Remove observer topic {subject} callback {callback}")
 
     def notify(self, subject, *args):
+        # We have to call the notifier callbacks through call_soon_threadsafe
+        # because the notify method could have been called from a non-reactor thread
+        self._event_loop.call_soon_threadsafe(self._notify, subject, *args)
+
+    def _notify(self, subject, *args):
         if subject not in self.observers:
             self._logger.warning(f"Called notification on a non-existing subject {subject}")
             return
