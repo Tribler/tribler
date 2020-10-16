62108dfb5cb598d5294bf2229e6f29e52d1ff601
qstokkink <goqs@hotmail.com>
2020-10-16 13:00:39 +0200

    Discover hidden IPv8 module imports

---
+++ src/tribler-core/tribler_core/modules/tests/test_ipv8_module_catalog.py
@@ -0,0 +1,22 @@
+from tribler_core.modules.ipv8_module_catalog import get_hiddenimports
+
+
+def test_hiddenimports():
+    """
+    Check if all hidden imports are detected
+    """
+    hiddenimports = get_hiddenimports()
+
+    assert 'anydex.core.community' in hiddenimports
+    assert 'ipv8.attestation.trustchain.community' in hiddenimports
+    assert 'ipv8.dht.churn' in hiddenimports
+    assert 'ipv8.dht.discovery' in hiddenimports
+    assert 'ipv8.peerdiscovery.churn' in hiddenimports
+    assert 'ipv8.peerdiscovery.community' in hiddenimports
+    assert 'ipv8.peerdiscovery.discovery' in hiddenimports
+    assert 'tribler_core.modules.popularity.popularity_community' in hiddenimports
+    assert 'tribler_core.modules.metadata_store.community.gigachannel_community' in hiddenimports
+    assert 'tribler_core.modules.metadata_store.community.remote_query_community' in hiddenimports
+    assert 'tribler_core.modules.metadata_store.community.sync_strategy' in hiddenimports
+    assert 'tribler_core.modules.tunnel.community.discovery' in hiddenimports
+    assert 'tribler_core.modules.tunnel.community.triblertunnel_community' in hiddenimports
