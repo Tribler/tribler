579dda74c562d0be8df9ade9e0387f4ac5cec383
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-17 11:39:59 +0200

    Improved payout manager and tests

--- src/tribler-core/tribler_core/modules/payout_manager.py
+++ src/tribler-core/tribler_core/modules/payout_manager.py
@@ -23,25 +23,28 @@ class PayoutManager(TaskManager):
         Perform a payout to a given mid. First, determine the outstanding balance. Then resolve the node in the DHT.
         """
         if mid not in self.tribler_peers:
-            return
+            return None
 
         total_bytes = sum(self.tribler_peers[mid].values())
 
         self.logger.info("Doing direct payout to %s (%d bytes)", hexlify(mid), total_bytes)
         try:
             nodes = await self.dht.connect_peer(mid)
-        except:
-            nodes = []
+        except Exception as e:
+            self.logger.warning("Error while doing DHT lookup for payouts, error %s", e)
+            return None
 
         self.logger.debug("Received %d nodes for DHT lookup", len(nodes))
         if nodes:
             try:
                 await self.bandwidth_community.do_payout(nodes[0], total_bytes)
             except Exception as e:
-                self.logger.error("Deferred errback fired: %s", e)
+                self.logger.error("Error while doing bandwidth payout, error %s", e)
+                return None
 
         # Remove the outstanding bytes; otherwise we will payout again
         self.tribler_peers.pop(mid, None)
+        return nodes[0]
 
     def update_peer(self, mid, infohash, balance):
         """
