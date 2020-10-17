bdfa5627c04fb8ef9b3eba8efc2bf163a30bc599
Martijn de Vos <mdmartijndevos@gmail.com>
2020-10-17 11:39:59 +0200

    Improved payout manager and tests

--- src/tribler-core/tribler_core/modules/tests/test_payout_manager.py
+++ src/tribler-core/tribler_core/modules/tests/test_payout_manager.py
@@ -8,7 +8,7 @@ from tribler_core.modules.payout_manager import PayoutManager
 
 
 @pytest.fixture
-def payout_manager():
+async def payout_manager():
     fake_bw_community = Mock()
 
     fake_response_peer = Mock()
@@ -18,7 +18,8 @@ def payout_manager():
     fake_dht.connect_peer = lambda *_: succeed([fake_response_peer])
 
     payout_manager = PayoutManager(fake_bw_community, fake_dht)
-    return payout_manager
+    yield payout_manager
+    await payout_manager.shutdown()
 
 
 @pytest.mark.asyncio
@@ -26,7 +27,8 @@ async def test_do_payout(payout_manager):
     """
     Test doing a payout
     """
-    await payout_manager.do_payout(b'a')  # Does not exist
+    res = await payout_manager.do_payout(b'a')  # Does not exist
+    assert not res
     payout_manager.update_peer(b'b', b'c', 10 * 1024 * 1024)
     payout_manager.update_peer(b'b', b'd', 1337)
 
@@ -34,7 +36,40 @@ async def test_do_payout(payout_manager):
         return succeed(None)
 
     payout_manager.bandwidth_community.do_payout = mocked_do_payout
-    await payout_manager.do_payout(b'b')
+    res = await payout_manager.do_payout(b'b')
+    assert res
+
+
+@pytest.mark.asyncio
+async def test_do_payout_dht_error(payout_manager):
+    """
+    Test whether we are not doing a payout when the DHT lookup fails
+    """
+    def err_connect_peer(_):
+        raise RuntimeError("test")
+
+    payout_manager.update_peer(b'a', b'b', 10 * 1024 * 1024)
+    payout_manager.dht.connect_peer = err_connect_peer
+    res = await payout_manager.do_payout(b'a')
+    assert not res
+
+
+@pytest.mark.asyncio
+async def test_do_payout_error(payout_manager):
+    """
+    Test whether we are not doing a payout when the payout fails
+    """
+    def connect_peer(_):
+        return succeed([b"abc"])
+
+    def do_payout(*_):
+        raise RuntimeError("test")
+
+    payout_manager.update_peer(b'a', b'b', 10 * 1024 * 1024)
+    payout_manager.dht.connect_peer = connect_peer
+    payout_manager.bandwidth_community.do_payout = do_payout
+    res = await payout_manager.do_payout(b'a')
+    assert not res
 
 
 def test_update_peer(payout_manager):
