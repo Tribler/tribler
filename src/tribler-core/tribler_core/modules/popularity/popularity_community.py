4a000145090cedb6c434c4b23a7ae40256eb4c36
drew2a <for.drew@gmail.com>
2020-10-24 18:50:26 +0200

    Refactor PopularityCommunity

--- src/tribler-core/tribler_core/modules/popularity/popularity_community.py
+++ src/tribler-core/tribler_core/modules/popularity/popularity_community.py
@@ -1,3 +1,5 @@
+import asyncio
+import heapq
 import random
 from binascii import unhexlify
 
@@ -11,13 +13,20 @@ from tribler_common.simpledefs import NTFY
 from tribler_core.modules.popularity.payload import TorrentsHealthPayload
 from tribler_core.utilities.unicode import hexlify
 
-PUBLISH_INTERVAL = 5
-
 
 class PopularityCommunity(Community):
     """
-    Community for disseminating the content across the network. Follows publish-subscribe model.
+    Community for disseminating the content across the network.
+
+    Every 5 seconds it gossips 5 the most popular torrents and 5 random torrents to
+    a random peer.
+
+    Gossiping is for checked torrents only.
     """
+    GOSSIP_INTERVAL = 5
+    GOSSIP_POPULAR_TORRENT_COUNT = 5
+    GOSSIP_RANDOM_TORRENT_COUNT = 5
+
     community_id = unhexlify('9aca62f878969c437da9844cba29a134917e1648')
 
     def __init__(self, *args, **kwargs):
@@ -29,10 +38,41 @@ class PopularityCommunity(Community):
 
         self.add_message_handler(TorrentsHealthPayload, self.on_torrents_health)
 
-        self.logger.info('Popularity Community initialized (peer mid %s)', hexlify(self.my_peer.mid))
-        self.register_task("publish", self.gossip_torrents_health, interval=PUBLISH_INTERVAL)
+        self.logger.info('Popularity Community initialized (peer mid %s)',
+                         hexlify(self.my_peer.mid))
+        self.register_task("gossip", self.gossip_torrents_health,
+                           interval=PopularityCommunity.GOSSIP_INTERVAL)
 
-    @db_session
+    @staticmethod
+    def select_torrents_to_gossip(torrents) -> (set, set):
+        """ Select torrents to gossip.
+
+        Select top 5 popular torrents, and 5 random torrents.
+
+        Args:
+            torrents: set of tuples (infohash, seeders, leechers, last_check)
+
+        Returns:
+            tuple (set(popular), set(random))
+
+        """
+        # select the torrents that have seeders
+        alive = set((_, seeders, *rest) for (_, seeders, *rest) in torrents
+                    if seeders > 0)
+        if not alive:
+            return {}, {}
+
+        # select 5 most popular from alive torrents, using `seeders` as a key
+        count = PopularityCommunity.GOSSIP_POPULAR_TORRENT_COUNT
+        popular = set(heapq.nlargest(count, alive, key=lambda t: t[1]))
+
+        # select 5 random torrents from the rest of the list
+        rest = alive - popular
+        count = min(PopularityCommunity.GOSSIP_RANDOM_TORRENT_COUNT, len(rest))
+        rand = set(random.sample(rest, count))
+
+        return popular, rand
+
     def gossip_torrents_health(self):
         """
         Gossip torrent health information to another peer.
@@ -40,25 +80,37 @@ class PopularityCommunity(Community):
         if not self.get_peers() or not self.torrent_checker:
             return
 
-        num_torrents_checked = len(self.torrent_checker.torrents_checked)
-        random_torrents_checked = random.sample(self.torrent_checker.torrents_checked, min(num_torrents_checked, 5))
-        popular_torrents_checked = sorted(self.torrent_checker.torrents_checked - set(random_torrents_checked),
-                                          key=lambda tup: tup[1], reverse=True)[:5]
+        checked = self.torrent_checker.torrents_checked
+        if not checked:
+            return
+
+        popular, rand = PopularityCommunity.select_torrents_to_gossip(checked)
+        if not popular and not rand:
+            self.logger.info(f'No torrents to gossip. Checked torrents count: '
+                             f'{len(checked)}')
+            return
 
         random_peer = random.choice(self.get_peers())
+
         self.logger.info(
-            f'Gossip torrent health information for {len(random_torrents_checked)}'
-            f' random torrents and {len(popular_torrents_checked)} checked torrents', )
-        self.ez_send(random_peer, TorrentsHealthPayload.create(random_torrents_checked, popular_torrents_checked))
+            f'Gossip torrent health information for {len(rand)}'
+            f' random torrents and {len(popular)} popular torrents')
+
+        self.ez_send(random_peer, TorrentsHealthPayload.create(rand, popular))
 
     @lazy_wrapper(TorrentsHealthPayload)
     async def on_torrents_health(self, peer, payload):
-        self.logger.info("Received torrent health information for %d random torrents and %d checked torrents",
-                         len(payload.random_torrents), len(payload.torrents_checked))
-        all_torrents = payload.random_torrents + payload.torrents_checked
+        self.logger.info(f"Received torrent health information for "
+                         f"{len(payload.torrents_checked)} popular torrents and"
+                         f" {len(payload.random_torrents)} random torrents")
 
+        torrents = payload.random_torrents + payload.torrents_checked
+        asyncio.create_task(self.process_torrents_health(peer, torrents))
+
+    async def process_torrents_health(self, peer, torrent_healths):
+        infohashes_to_resolve = []
         with db_session:
-            for infohash, seeders, leechers, last_check in all_torrents:
+            for infohash, seeders, leechers, last_check in torrent_healths:
                 torrent_state = self.metadata_store.TorrentState.get(infohash=infohash)
                 if torrent_state and last_check > torrent_state.last_check:
                     # Replace current information
@@ -70,5 +122,13 @@ class PopularityCommunity(Community):
                     self.metadata_store.TorrentState(infohash=infohash, seeders=seeders,
                                                      leechers=leechers, last_check=last_check)
                     self.logger.info(f"{hexlify(infohash)} added ({seeders},{leechers})")
-                    if self.notifier:
-                        self.notifier.notify(NTFY.POPULARITY_COMMUNITY_ADD_UNKNOWN_TORRENT, peer, infohash)
+                    infohashes_to_resolve.append(infohash)
+
+        if not self.notifier:
+            return
+
+        # `self.notifier.notify` has been extracted from `with db_session:` to
+        # prevent issues related to nested db_sessions inside notifier callbacks
+        for infohash in infohashes_to_resolve:
+            self.notifier.notify(NTFY.POPULARITY_COMMUNITY_ADD_UNKNOWN_TORRENT,
+                                 peer, infohash)
