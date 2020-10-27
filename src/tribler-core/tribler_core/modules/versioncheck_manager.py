83565d15a73fcbe57f49ec600ab0904ddb8e7d64
Sandip Pandey <np.sandip.pandey@gmail.com>
2020-10-27 18:23:22 +0100

    Fix unbound local error in version checker

--- src/tribler-core/tribler_core/modules/versioncheck_manager.py
+++ src/tribler-core/tribler_core/modules/versioncheck_manager.py
@@ -44,6 +44,11 @@ class VersionCheckManager(TaskManager):
             async with ClientSession(raise_for_status=True) as session:
                 response = await session.get(version_check_url)
                 response_dict = await response.json(content_type=None)
+                version = response_dict['name'][1:]
+                if LooseVersion(version) > LooseVersion(version_id):
+                    self.session.notifier.notify(NTFY.TRIBLER_NEW_VERSION, version)
+                    return True
+                return False
         except (ServerConnectionError, ClientConnectionError) as e:
             self._logger.error("Error when performing version check request: %s", e)
             return False
@@ -55,11 +60,6 @@ class VersionCheckManager(TaskManager):
             return False
         except asyncio.TimeoutError:
             self._logger.warning("Checking for new version failed for %s", version_check_url)
-
-        try:
-            version = response_dict['name'][1:]
-            if LooseVersion(version) > LooseVersion(version_id):
-                self.session.notifier.notify(NTFY.TRIBLER_NEW_VERSION, version)
+            return False
         except ValueError as ve:
             raise ValueError("Failed to parse Tribler version response.\nError:%s" % ve)
-        return True
