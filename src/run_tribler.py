9da69870f56681a6f9871033db701a3960e4ea3c
drew2a <for.drew@gmail.com>
2020-11-30 15:06:12 +0100

    Add IGNORED_ERRORS

--- src/run_tribler.py
+++ src/run_tribler.py
@@ -11,6 +11,7 @@ from tribler_common.sentry_reporter.sentry_scrubber import SentryScrubber
 import tribler_core
 from tribler_core.config.tribler_config import CONFIG_FILENAME
 from tribler_core.dependencies import check_for_missing_dependencies
+from tribler_core.session import IGNORED_ERRORS
 from tribler_core.upgrade.version_manager import fork_state_directory_if_necessary, get_versioned_state_directory
 from tribler_core.utilities.osutils import get_root_state_directory
 from tribler_core.version import sentry_url, version_id
@@ -88,7 +89,16 @@ def start_tribler_core(base_path, api_port, api_key, root_state_dir, core_test_m
     get_event_loop().run_forever()
 
 
-def excepthook(exctype, value, traceback): # pylint: disable=unused-argument
+def excepthook(exctype, value, traceback):  # pylint: disable=unused-argument
+    ignored_message = None
+    try:
+        ignored_message = IGNORED_ERRORS.get((value.__class__, value.errno),
+                                             IGNORED_ERRORS.get(value.__class__))
+    except (ValueError, AttributeError):
+        pass
+    if ignored_message is not None:
+        return
+
     SentryReporter.send_exception_with_confirmation(value)
 
 
