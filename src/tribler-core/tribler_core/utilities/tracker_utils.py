462f330bf798c9028f0f9c72ad05b62505677f58
qstokkink <goqs@hotmail.com>
2020-12-07 17:09:20 +0100

    Migrate add_url_params over from IPv8

--- src/tribler-core/tribler_core/utilities/tracker_utils.py
+++ src/tribler-core/tribler_core/utilities/tracker_utils.py
@@ -1,6 +1,7 @@
 import re
 from http.client import HTTP_PORT
-from urllib.parse import urlparse
+from json import dumps
+from urllib.parse import ParseResult, parse_qsl, unquote, urlencode, urlparse
 
 
 class MalformedTrackerURLException(Exception):
@@ -132,3 +133,43 @@ def parse_tracker_url(tracker_url):
         raise MalformedTrackerURLException(u'Missing announce path for HTTP tracker url (%s).' % tracker_url)
 
     return url.scheme, (url.hostname, url.port), url.path
+
+
+def add_url_params(url, params):
+    """ Add GET params to provided URL being aware of existing.
+    :param url: string of target URL
+    :param params: dict containing requested params to be added
+    :return: string with updated URL
+    >> url = 'http://stackoverflow.com/test?answers=true'
+    >> new_params = {'answers': False, 'data': ['some','values']}
+    >> add_url_params(url, new_params)
+    'http://stackoverflow.com/test?data=some&data=values&answers=false'
+    """
+    # Unquoting URL first so we don't loose existing args
+    url = unquote(url)
+    # Extracting url info
+    parsed_url = urlparse(url)
+    # Extracting URL arguments from parsed URL
+    get_args = parsed_url.query
+    # Converting URL arguments to dict
+    parsed_get_args = dict(parse_qsl(get_args))
+    # Merging URL arguments dict with new params
+    parsed_get_args.update(params)
+
+    # Bool and Dict values should be converted to json-friendly values
+    # you may throw this part away if you don't like it :)
+    parsed_get_args.update(
+        {k: dumps(v) for k, v in parsed_get_args.items()
+         if isinstance(v, (bool, dict))}
+    )
+
+    # Converting URL argument to proper query string
+    encoded_get_args = urlencode(parsed_get_args, doseq=True)
+    # Creating new parsed result object based on provided with new
+    # URL arguments. Same thing happens inside of urlparse.
+    new_url = ParseResult(
+        parsed_url.scheme, parsed_url.netloc, parsed_url.path,
+        parsed_url.params, encoded_get_args, parsed_url.fragment
+    ).geturl()
+
+    return new_url
