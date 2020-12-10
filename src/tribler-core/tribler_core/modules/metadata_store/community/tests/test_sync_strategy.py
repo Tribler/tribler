17935ab56253d7af5a97e380907ffb4e2cf709fe
V.G. Bulavintsev <golem.md@gmail.com>
2020-12-10 19:22:14 +0100

    Remove unused sync_channels strategy

--- src/tribler-core/tribler_core/modules/metadata_store/community/tests/test_sync_strategy.py
+++ src/tribler-core/tribler_core/modules/metadata_store/community/tests/test_sync_strategy.py
@@ -3,9 +3,7 @@ from ipv8.peer import Peer
 from ipv8.peerdiscovery.network import Network
 from ipv8.test.base import TestBase
 
-import pytest
-
-from tribler_core.modules.metadata_store.community.sync_strategy import RemovePeers, SyncChannels
+from tribler_core.modules.metadata_store.community.sync_strategy import RemovePeers
 
 
 class MockCommunity(object):
@@ -23,51 +21,6 @@ class MockCommunity(object):
 
     def get_peers(self):
         return self.get_peers_return
-
-
-@pytest.fixture
-def mock_community():
-    return MockCommunity()
-
-
-@pytest.fixture
-def strategy(mock_community):
-    return SyncChannels(mock_community)
-
-
-def test_strategy_no_peers(mock_community, strategy):
-    """
-    If we have no peers, no random entries should have been sent.
-    """
-    strategy.take_step()
-    assert mock_community.send_random_to_called == []
-
-
-def test_strategy_one_peer(mock_community, strategy):
-    """
-    If we have one peer, we should send it our channel views and inspect our download queue.
-    """
-    mock_community.get_peers_return = [Peer(default_eccrypto.generate_key(u"very-low"))]
-    strategy.take_step()
-
-    assert len(mock_community.send_random_to_called) == 1
-    assert mock_community.get_peers_return[0] == mock_community.send_random_to_called[0]
-
-
-def test_strategy_multi_peer(mock_community, strategy):
-    """
-    If we have multiple peers, we should select one and send it our channel views.
-    Also, we should still inspect our download queue.
-    """
-    mock_community.get_peers_return = [
-        Peer(default_eccrypto.generate_key(u"very-low")),
-        Peer(default_eccrypto.generate_key(u"very-low")),
-        Peer(default_eccrypto.generate_key(u"very-low")),
-    ]
-    strategy.take_step()
-
-    assert len(mock_community.send_random_to_called) == 1
-    assert mock_community.send_random_to_called[0] in mock_community.get_peers_return
 
 
 class TestRemovePeers(TestBase):
