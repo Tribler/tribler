5df91b8f948c009131a6f3e771a9f84d78c4f8e2
V.G. Bulavintsev <golem.md@gmail.com>
2020-12-10 19:22:14 +0100

    Remove unused sync_channels strategy

--- src/tribler-core/tribler_core/modules/metadata_store/community/sync_strategy.py
+++ src/tribler-core/tribler_core/modules/metadata_store/community/sync_strategy.py
@@ -3,22 +3,6 @@ from random import choice
 from ipv8.peerdiscovery.discovery import DiscoveryStrategy
 
 
-class SyncChannels(DiscoveryStrategy):
-    """
-    Synchronization strategy for gigachannels.
-
-    On each tick we send a random peer some of our random subscribed channels.
-    """
-
-    def take_step(self):
-        with self.walk_lock:
-            # Share my random channels
-            peers = self.overlay.get_peers()
-            if peers:
-                peer = choice(peers)
-                self.overlay.send_random_to(peer)
-
-
 class RemovePeers(DiscoveryStrategy):
     """
     Synchronization strategy for remote query community.
