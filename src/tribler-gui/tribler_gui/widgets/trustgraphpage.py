87fed8c40d105ae4c469a26a08c3824bfee26e00
Sandip <np.sandip.pandey@gmail.com>
2020-12-17 13:48:23 +0100

    Increase the size of nodes in trust graph slightly

--- src/tribler-gui/tribler_gui/widgets/trustgraphpage.py
+++ src/tribler-gui/tribler_gui/widgets/trustgraphpage.py
@@ -214,13 +214,13 @@ class TrustGraphPage(AddBreadcrumbOnShowMixin, QWidget):
 
     def get_node_size(self, node):
         # User root node is bigger than normal nodes
-        min_size = 0.01 if node[u'key'] != self.root_public_key else 0.05
+        min_size = 0.01 if node[u'key'] != self.root_public_key else 0.1
 
         diff = abs(node.get('total_up', 0) - node.get('total_down', 0))
         if diff == 0:
             return min_size
         elif diff > 10 * TB:  # max token balance limit
-            return 0.06  # max node size in graph
+            return 0.1  # max node size in graph
         elif diff > TB:
             return 0.05 + 0.005 * diff / TB  # 0.005 for each extra TB of balance
         return math.log(diff, 2) / 512 + min_size
