c16a07ed206b3ce9710d478f1065c9b2ba6f5330
Sandip <np.sandip.pandey@gmail.com>
2020-12-22 17:38:55 +0100

    Add sql_default value to self_checked column

--- src/tribler-core/tribler_core/modules/metadata_store/orm_bindings/torrent_state.py
+++ src/tribler-core/tribler_core/modules/metadata_store/orm_bindings/torrent_state.py
@@ -14,7 +14,7 @@ def define_binding(db):
         seeders = orm.Optional(int, default=0)
         leechers = orm.Optional(int, default=0)
         last_check = orm.Optional(int, size=64, default=0)
-        self_checked = orm.Optional(bool, default=False)
+        self_checked = orm.Optional(bool, default=False, sql_default='0')
         metadata = orm.Set('TorrentMetadata', reverse='health')
         trackers = orm.Set('TrackerState', reverse='torrents')
 
