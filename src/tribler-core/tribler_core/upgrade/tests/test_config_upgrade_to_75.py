4a953b54f972f574969100fcf47bb78e6d5795cb
drew2a <for.drew@gmail.com>
2020-12-29 15:34:34 +0100

    Change mocked config to corrupted source file

--- src/tribler-core/tribler_core/upgrade/tests/test_config_upgrade_to_75.py
+++ src/tribler-core/tribler_core/upgrade/tests/test_config_upgrade_to_75.py
@@ -14,19 +14,7 @@ from tribler_core.upgrade.config_converter import convert_config_to_tribler75
 
 CONFIG_PATH = TESTS_DATA_DIR / "config_files"
 
-
-@pytest.fixture(name='faulty_download_config_parser')
-async def fixture_faulty_download_config_parser():
-    def load_config_with_parse_error(filename):
-        raise ConfigObjParseError(f"Error parsing config file {filename}")
-
-    original_load_config = DownloadConfig.load
-    DownloadConfig.load = load_config_with_parse_error
-    yield DownloadConfig
-    DownloadConfig.load = original_load_config
-
-
-def test_convert_state_file_to_conf75_with_parse_error(tmpdir, faulty_download_config_parser):
+def test_convert_state_file_to_conf75_with_parse_error(tmpdir):
     """
     Tests conversion of the conf files (7.4.0) files to .conf files (7.5) when there is parsing error.
     ParseError happens for some users for some unknown reason. We simply remove the files that we cannot
@@ -36,7 +24,7 @@ def test_convert_state_file_to_conf75_with_parse_error(tmpdir, faulty_download_c
     os.makedirs(state_dir / STATEDIR_CHECKPOINT_DIR)
 
     # Copy Ubuntu conf file with corrupted metainfo data
-    src_path = CONFIG_PATH / "13a25451c761b1482d3e85432f07c4be05ca8a56.conf"
+    src_path = CONFIG_PATH / "corrupt_download_config.conf"
     dest_path = state_dir / STATEDIR_CHECKPOINT_DIR / "ubuntu_corrupted.conf"
     shutil.copyfile(src_path, dest_path)
 
