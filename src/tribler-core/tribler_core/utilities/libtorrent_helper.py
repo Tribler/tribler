b8e1c2c2c56700fb8d8a0c03bb2236d5db374cb8
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2021-02-11 10:45:45 +0100

    Docstrings added

--- src/tribler-core/tribler_core/utilities/libtorrent_helper.py
+++ src/tribler-core/tribler_core/utilities/libtorrent_helper.py
@@ -2,9 +2,17 @@ __all__ = ['libtorrent']
 
 
 class LibtorrentFallback:
+    """
+    Used as a fallback replacement for libtorrent library if it was not possible to import it.
+
+    Any attempt to access any member raises ImportError.
+    """
     def __getattr__(self, item):
-        # Cannot use RuntimeError here, as libtorrent functions like bdecode
-        # raise RuntimeError as well, and we need to distinct these exceptions.
+        """
+        Raises ImportError on any attempt to access a member of the class that is not explicitly defined.
+        """
+        # Cannot use RuntimeError here, as libtorrent functions like bdecode raise RuntimeError as well,
+        # and we need to distinguish these exceptions.
         raise ImportError('libtorrent library is not installed')
 
 
