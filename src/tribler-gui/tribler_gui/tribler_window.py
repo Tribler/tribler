819c8a383fe85eec03444849340a3e2878ced0d1
Sandip Pandey <np.sandip.pandey@gmail.com>
2021-04-14 15:17:03 +0200

    Show GUI settings in Debug Panel

--- src/tribler-gui/tribler_gui/tribler_window.py
+++ src/tribler-gui/tribler_gui/tribler_window.py
@@ -995,7 +995,7 @@ class TriblerWindow(QMainWindow):
 
     def clicked_menu_button_debug(self, index=False):
         if not self.debug_window:
-            self.debug_window = DebugWindow(self.tribler_settings, self.tribler_version)
+            self.debug_window = DebugWindow(self.tribler_settings, self.gui_settings, self.tribler_version)
         self.debug_window.show()
 
     def resizeEvent(self, _):
