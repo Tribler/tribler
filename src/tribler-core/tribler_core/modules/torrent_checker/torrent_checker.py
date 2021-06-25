814a94ad5faaa915083c9c9b299a831684e28802
V.G. Bulavintsev <golem.md@gmail.com>
2021-06-25 12:47:07 +0200

    Post-merge fixes

--- src/tribler-core/tribler_core/modules/torrent_checker/torrent_checker.py
+++ src/tribler-core/tribler_core/modules/torrent_checker/torrent_checker.py
@@ -322,10 +322,6 @@ class TorrentChecker(TaskManager):
                 # get torrent's tracker list from DB
                 tracker_set = self.get_valid_trackers_of_torrent(torrent_id)
 
-        hops = self.tribler_session.config.download_defaults.number_hops
-        socks_listen_ports = self.tribler_session.config.tunnel_community.socks5_listen_ports
-        ('127.0.0.1', socks_listen_ports[hops - 1]) if hops > 0 else None
-
         tasks = []
         for tracker_url in tracker_set:
             session = self._create_session_for_request(tracker_url, timeout=timeout)
