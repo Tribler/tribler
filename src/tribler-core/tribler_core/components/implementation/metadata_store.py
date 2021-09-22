81e701e3f1e08d9c84aaccf763a1118359398c84
Martijn de Vos <mdmartijndevos@gmail.com>
2021-09-22 10:00:58 +0200

    Using separate channel db in GUI test mode
    This avoids interfering with the main database. Fixes #6304.

--- src/tribler-core/tribler_core/components/implementation/metadata_store.py
+++ src/tribler-core/tribler_core/components/implementation/metadata_store.py
@@ -21,12 +21,23 @@ class MetadataStoreComponentImp(MetadataStoreComponent):
         config = self.session.config
         channels_dir = config.chant.get_path_as_absolute('channels_dir', config.state_dir)
         chant_testnet = config.general.testnet or config.chant.testnet
-        metadata_db_name = 'metadata.db' if not chant_testnet else 'metadata_testnet.db'
+
+        metadata_db_name = 'metadata.db'
+        if chant_testnet:
+            metadata_db_name = 'metadata_testnet.db'
+        elif config.gui_test_mode:  # Avoid interfering with the main database in test mode
+            # Note we don't use in-memory database in core test mode, because MDS uses threads,
+            # and SQLite creates a different in-memory DB for each connection by default.
+            # To change this behaviour, we have to use url-based SQLite initialization syntax,
+            # which is not supported by PonyORM yet.
+            metadata_db_name = 'metadata_gui_test.db'
+
         database_path = config.state_dir / STATEDIR_DB_DIR / metadata_db_name
-        # Note we don't use in-memory database in core test mode, because MDS uses threads,
-        # and SQLite creates a different in-memory DB for each connection by default.
-        # To change this behaviour, we have to use url-based SQLite initialization syntax,
-        # which is not supported by PonyORM yet.
+
+        # Make sure that we start with a clean metadata database when in GUI mode every time.
+        if config.gui_test_mode and database_path.exists():
+            self.logger.info("Wiping metadata database in GUI test mode")
+            database_path.unlink(missing_ok=True)
 
         masterkey = await self.use(MasterKeyComponent)
 
