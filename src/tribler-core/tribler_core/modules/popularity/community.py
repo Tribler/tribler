e9de88ef496894c3462a4a008d9d2e44fa14b65f
drew2a <for.drew@gmail.com>
2021-10-06 21:25:45 +0200

    Apply dataclass to PopularityCommunity

--- src/tribler-core/tribler_core/modules/popularity/community.py
+++ src/tribler-core/tribler_core/modules/popularity/community.py
@@ -98,13 +98,13 @@ class PopularityCommunity(RemoteQueryCommunity, VersionCommunityMixin):
                                                                       include_popular=include_popular,
                                                                       include_random=include_random)
         if not popular and not rand:
-            self.logger.debug(f'No torrents to gossip. Checked torrents count: '
+            self.logger.info(f'No torrents to gossip. Checked torrents count: '
                              f'{len(checked)}')
             return
 
         random_peer = random.choice(self.get_peers())
 
-        self.logger.debug(
+        self.logger.info(
             f'Gossip torrent health information for {len(rand)}'
             f' random torrents and {len(popular)} popular torrents')
 
@@ -124,7 +124,7 @@ class PopularityCommunity(RemoteQueryCommunity, VersionCommunityMixin):
 
     @lazy_wrapper(TorrentsHealthPayload)
     async def on_torrents_health(self, peer, payload):
-        self.logger.debug(f"Received torrent health information for "
+        self.logger.info(f"Received torrent health information for "
                          f"{len(payload.torrents_checked)} popular torrents and"
                          f" {len(payload.random_torrents)} random torrents")
 
@@ -137,8 +137,8 @@ class PopularityCommunity(RemoteQueryCommunity, VersionCommunityMixin):
     @db_session
     def process_torrents_health(self, torrent_healths):
         infohashes_to_resolve = set()
-        for infohash, seeders, leechers, last_check in torrent_healths:
-            added = self.mds.process_torrent_health(infohash, seeders, leechers, last_check)
+        for health in torrent_healths:
+            added = self.mds.process_torrent_health(health.infohash, health.seeders, health.leechers, health.timestamp)
             if added:
-                infohashes_to_resolve.add(infohash)
+                infohashes_to_resolve.add(health.infohash)
         return infohashes_to_resolve
--- src/tribler-core/tribler_core/modules/popularity/payload.py
+++ src/tribler-core/tribler_core/modules/popularity/payload.py
@@ -1,44 +1,35 @@
-from ipv8.messaging.lazy_payload import VariablePayload, vp_compile
-from ipv8.messaging.serialization import default_serializer
+from typing import List, TypeVar
+
+from ipv8.messaging.payload_dataclass import dataclass_payload
+
+timestamp_type = TypeVar('Q')
 
 
-@vp_compile
-class TorrentInfoFormat(VariablePayload):
-    format_list = ['20s', 'I', 'I', 'Q']
-    names = ['infohash', 'seeders', 'leechers', 'timestamp']
-    length = 36
-
-    def to_tuple(self):
-        return self.infohash, self.seeders, self.leechers, self.timestamp
-
-    @classmethod
-    def from_list_bytes(cls, serialized):
-        return default_serializer.unpack_serializable_list([cls] * (len(serialized)//cls.length),
-                                                           serialized, consume_all=False)[:-1]
-
-
-@vp_compile
-class TorrentsHealthPayload(VariablePayload):
-
+@dataclass_payload
+class TorrentsHealthPayload:
     msg_id = 1
-    format_list = ['I', 'I', 'varlenI', 'raw']  # Number of random torrents, number of torrents checked by you
-    names = ['random_torrents_length', 'torrents_checked_length', 'random_torrents', 'torrents_checked']
 
-    def fix_pack_random_torrents(self, value):
-        return b''.join(default_serializer.pack_serializable(TorrentInfoFormat(*sublist)) for sublist in value)
+    @dataclass_payload
+    class Torrent:
+        infohash: bytes
+        seeders: int
+        leechers: int
+        timestamp: timestamp_type
 
-    def fix_pack_torrents_checked(self, value):
-        return b''.join(default_serializer.pack_serializable(TorrentInfoFormat(*sublist)) for sublist in value)
+    random_torrents_length: int
+    torrents_checked_length: int
+    random_torrents: List[Torrent]
+    torrents_checked: List[Torrent]
 
-    @classmethod
-    def fix_unpack_random_torrents(cls, value):
-        return [payload.to_tuple() for payload in TorrentInfoFormat.from_list_bytes(value)]
+    @staticmethod
+    def create(random_torrents_checked, popular_torrents_checked):
+        def to_list(torrents):
+            return [TorrentsHealthPayload.Torrent(infohash=t[0], seeders=t[1], leechers=t[2], timestamp=t[3]) for t in
+                    torrents]
 
-    @classmethod
-    def fix_unpack_torrents_checked(cls, value):
-        return [payload.to_tuple() for payload in TorrentInfoFormat.from_list_bytes(value)]
-
-    @classmethod
-    def create(cls, random_torrents_checked, popular_torrents_checked):
-        return cls(len(random_torrents_checked), len(popular_torrents_checked),
-                   random_torrents_checked, popular_torrents_checked)
+        return TorrentsHealthPayload(
+            random_torrents_length=len(random_torrents_checked),
+            torrents_checked_length=len(popular_torrents_checked),
+            random_torrents=to_list(random_torrents_checked),
+            torrents_checked=to_list(popular_torrents_checked)
+        )
