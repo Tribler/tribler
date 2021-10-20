23b69b0211235e352a3c8b973fac64c16882ce81
drew2a <for.drew@gmail.com>
2021-10-20 16:25:38 +0200

    Verify tag not contains any spaces

--- src/tribler-core/tribler_core/components/tag/community/tag_payload.py
+++ src/tribler-core/tribler_core/components/tag/community/tag_payload.py
@@ -25,6 +25,7 @@ class TagOperation:
     def validate(self):
         assert MIN_TAG_LENGTH <= len(self.tag) <= MAX_TAG_LENGTH, 'Tag length should be in range [3..50]'
         assert not any(ch.isupper() for ch in self.tag), 'Tag should not contain upper-case letters'
+        assert ' ' not in self.tag, 'Tag should not contain any spaces'
 
         # try to convert operation into Enum
         assert TagOperationEnum(self.operation)
