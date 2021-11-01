ba86fcfa4a0903fb92e9e4ac4e2a13f6c849e1be
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2021-11-01 07:33:49 +0100

    Add test_to_fts_query for better coverage

--- src/tribler-common/tribler_common/tests/test_utils.py
+++ src/tribler-common/tribler_common/tests/test_utils.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 
-from tribler_common.utilities import uri_to_path
+from tribler_common.utilities import to_fts_query, uri_to_path
 
 
 def test_uri_to_path():
     path = Path(__file__).parent / "bla%20foo.bar"
     uri = path.as_uri()
     assert uri_to_path(uri) == path
+
+
+def test_to_fts_query():
+    assert to_fts_query('') == ''
+    assert to_fts_query('abc') == '"abc"*'
+    assert to_fts_query('abc def') == '"abc" "def"*'
+    assert to_fts_query('[abc, def]: xyz?!') == '"abc" "def" "xyz"*'
