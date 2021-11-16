52d840953e61bd9a9eef41e2ab636e39e7c7fe28
drew2a <for.drew@gmail.com>
2021-11-16 21:47:31 +0100

    Add maybe_add

--- src/tribler-core/tribler_core/components/restapi/tests/test_restapi_component.py
+++ src/tribler-core/tribler_core/components/restapi/tests/test_restapi_component.py
@@ -5,13 +5,14 @@ import pytest
 from tribler_common.reported_error import ReportedError
 
 from tribler_core.components.bandwidth_accounting.bandwidth_accounting_component import BandwidthAccountingComponent
-from tribler_core.components.base import Session
+from tribler_core.components.base import NoneComponent, Session
 from tribler_core.components.gigachannel.gigachannel_component import GigaChannelComponent
 from tribler_core.components.ipv8.ipv8_component import Ipv8Component
 from tribler_core.components.key.key_component import KeyComponent
 from tribler_core.components.libtorrent.libtorrent_component import LibtorrentComponent
 from tribler_core.components.metadata_store.metadata_store_component import MetadataStoreComponent
 from tribler_core.components.resource_monitor.resource_monitor_component import ResourceMonitorComponent
+from tribler_core.components.restapi.rest.rest_endpoint import RESTEndpoint
 from tribler_core.components.restapi.restapi_component import RESTComponent
 from tribler_core.components.socks_servers.socks_servers_component import SocksServersComponent
 from tribler_core.components.tag.tag_component import TagComponent
@@ -19,7 +20,7 @@ from tribler_core.components.tag.tag_component import TagComponent
 pytestmark = pytest.mark.asyncio
 
 
-# pylint: disable=protected-access, not-callable
+# pylint: disable=protected-access, not-callable, redefined-outer-name
 
 async def test_rest_component(tribler_config):
     components = [KeyComponent(), RESTComponent(), Ipv8Component(), LibtorrentComponent(), ResourceMonitorComponent(),
@@ -46,3 +47,45 @@ async def test_rest_component(tribler_config):
         comp._events_endpoint.on_tribler_exception.assert_called_with(error)
 
         await session.shutdown()
+
+
+@pytest.fixture
+def endpoint_cls():
+    class Endpoint(RESTEndpoint):
+        ...
+
+    return Endpoint
+
+
+@pytest.fixture
+def rest_component():
+    component = RESTComponent()
+    component.root_endpoint = MagicMock()
+    return component
+
+
+async def test_maybe_add_check_args(rest_component, endpoint_cls):
+    # test that in case `*args` in `maybe_add` function contains `NoneComponent` instance
+    # no root_endpoint methods are called
+    rest_component.maybe_add('path', endpoint_cls, NoneComponent())
+    rest_component.root_endpoint.assert_not_called()
+
+    rest_component.maybe_add('path', endpoint_cls, NoneComponent(), 'some arg')
+    rest_component.root_endpoint.assert_not_called()
+
+
+async def test_maybe_add_check_kwargs(rest_component, endpoint_cls):
+    # test that in case `**kwargs` in `maybe_add` function contains `NoneComponent` instance
+    # no root_endpoint methods are called
+    rest_component.maybe_add('path', endpoint_cls, component=NoneComponent())
+    rest_component.root_endpoint.assert_not_called()
+
+    rest_component.maybe_add('path', endpoint_cls, component=NoneComponent(), another='kwarg')
+    rest_component.root_endpoint.assert_not_called()
+
+
+async def test_maybe_add(rest_component, endpoint_cls):
+    # test that in case there are no `NoneComponent` instances in `**kwargs` or `*args`
+    # root_endpoint methods are called
+    rest_component.maybe_add('path', endpoint_cls, 'arg')
+    rest_component.root_endpoint.asert_called_once()
