ebe6792f2c0a9e037a675596de3c3a2cfd7c58a7
drew2a <for.drew@gmail.com>
2021-11-30 13:19:45 +0100

    Remove random_utf8_string

--- src/tribler-core/tribler_core/utilities/tests/test_random_utils.py
+++ src/tribler-core/tribler_core/utilities/tests/test_random_utils.py
@@ -1,4 +1,4 @@
-from tribler_core.utilities.random_utils import random_infohash, random_string, random_utf8_string
+from tribler_core.utilities.random_utils import random_infohash, random_string
 
 
 def test_random_string():
@@ -8,18 +8,6 @@ def test_random_string():
 
     text_length = 16
     test_string2 = random_string(size=text_length)
-    assert len(test_string2) == text_length
-
-
-def test_random_utf8_string():
-    test_string = random_utf8_string()
-    assert test_string
-    assert isinstance(test_string, str)
-    assert len(test_string) == 6
-
-    text_length = 16
-    test_string2 = random_utf8_string(length=text_length)
-    assert isinstance(test_string, str)
     assert len(test_string2) == text_length
 
 
