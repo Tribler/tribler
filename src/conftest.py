66c8bdee81abf835d98896561790a239c0728485
Andrei Andreev <for.drew@gmail.com>
2022-03-21 15:48:32 +0100

    For test run with '--no-parallel' argument, skip all "normal" tests

--- src/conftest.py
+++ src/conftest.py
@@ -12,8 +12,17 @@ def pytest_collection_modifyitems(config, items):
     # Documentation on pytest, "how to skip a test":
     # https://docs.pytest.org/en/6.2.x/example/simple.html#control-skipping-of-tests-according-to-command-line-option
     if config.getoption("--no_parallel"):
+        # if tests have been run with '--no_parallel' argument, then
+        # skip all tests that doesn't contain `no_parallel` mark
+        skip_marker = pytest.mark.skip(reason="skipped during --no_parallel run")
+        for item in items:
+            if "no_parallel" not in item.keywords:
+                item.add_marker(skip_marker)
         return
-    skip = pytest.mark.skip(reason="need --no_parallel option to run")
+
+    # if tests have been run without '--no_parallel' argument, then
+    # skip all tests that contain `no_parallel` mark
+    skip_marker = pytest.mark.skip(reason="need --no_parallel option to run")
     for item in items:
         if "no_parallel" in item.keywords:
-            item.add_marker(skip)
+            item.add_marker(skip_marker)
