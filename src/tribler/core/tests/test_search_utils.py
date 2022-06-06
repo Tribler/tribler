c27b9fc9a48e939e34ea410d0274a9b6bc147898
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2022-06-06 15:16:57 +0200

    Add search_rank() function and use it in FTS queries

--- src/tribler/core/tests/test_search_utils.py
+++ src/tribler/core/tests/test_search_utils.py
@@ -1,4 +1,9 @@
-from tribler.core.utilities.search_utils import filter_keywords, split_into_keywords
+import pytest
+
+from tribler.core.utilities.search_utils import filter_keywords, split_into_keywords, torrent_rank
+
+
+DAY = 60 * 60 * 24
 
 
 def test_split_into_keywords():
@@ -15,3 +20,72 @@ def test_filter_keywords():
     result = filter_keywords(["to", "be", "or", "not", "to", "be"])
     assert isinstance(result, list)
     assert len(result) == 4
+
+
+def test_torrent_rank():
+    query = 'Big Buck Bunny'
+
+    # The exact match ranked as pretty high
+    assert torrent_rank(query, 'Big Buck Bunny') == pytest.approx(0.81)
+
+    # Seeders are good for the rank
+    assert torrent_rank(query, 'Big Buck Bunny', seeders=100, freshness=100 * DAY) == pytest.approx(0.876923)
+
+    # The more seeders the better
+    assert torrent_rank(query, 'Big Buck Bunny', seeders=1000, freshness=100 * DAY) == pytest.approx(0.9146853)
+
+    # The fewer days have passed since the creation of the torrent, the higher its rank
+    assert torrent_rank(query, 'Big Buck Bunny', seeders=1000, freshness=1 * DAY) == pytest.approx(0.9877126)
+
+    # If a title contains non-matching words missed in the query string it is not as good as the exact match
+    assert torrent_rank(query, 'Big Buck Bunny II') == pytest.approx(0.80381679)
+
+    # The closer to the start of the string non-matching words are placed in the title, the worse is rank
+    assert torrent_rank(query, 'Big Buck Brown Bunny') == pytest.approx(0.75061099)
+    assert torrent_rank(query, 'Big Bad Buck Bunny') == pytest.approx(0.74242068)
+    assert torrent_rank(query, 'Boring Big Buck Bunny') == pytest.approx(0.73125)
+
+    # The more non-matching words are in the title, the worse is rank
+    assert torrent_rank(query, 'Big Buck A Bunny') == pytest.approx(0.75061099)
+    assert torrent_rank(query, 'Big Buck A B Bunny') == pytest.approx(0.699335863)
+    assert torrent_rank(query, 'Big Buck A B C Bunny') == pytest.approx(0.6546181)
+
+    # Non-matching words close to the beginning of the title give a bigger penalty
+    assert torrent_rank(query, 'Big A Buck Bunny') == pytest.approx(0.742420681)
+    assert torrent_rank(query, 'Big A B Buck Bunny') == pytest.approx(0.6852494577)
+    assert torrent_rank(query, 'Big A B C Buck Bunny') == pytest.approx(0.636253776)
+
+    assert torrent_rank(query, 'A Big Buck Bunny') == pytest.approx(0.73125)
+    assert torrent_rank(query, 'A B Big Buck Bunny') == pytest.approx(0.66645569)
+    assert torrent_rank(query, 'A B C Big Buck Bunny') == pytest.approx(0.6122093)
+
+    # Wrong order of words in the title imposes a penalty to the rank
+    assert torrent_rank(query, 'Big Bunny Buck') == pytest.approx(0.7476923)
+
+    # Missed query words imposes a really big penalty
+    assert torrent_rank(query, 'Big Buck') == pytest.approx(0.4725)
+
+    # The close the missed words to the beginning of the query, the worse
+    assert torrent_rank(query, 'Big Bunny') == pytest.approx(0.441818181)
+    assert torrent_rank(query, 'Buck Bunny') == pytest.approx(0.405)
+
+    # The more seeders is still better, the more days from the check the less relevant the number of seeders is
+    assert torrent_rank(query, 'Buck Bunny', seeders=10, freshness=5 * DAY) == pytest.approx(0.44805194)
+    assert torrent_rank(query, 'Buck Bunny', seeders=100, freshness=5 * DAY) == pytest.approx(0.46821428)
+    assert torrent_rank(query, 'Buck Bunny', seeders=1000, freshness=5 * DAY) == pytest.approx(0.4883766)
+    assert torrent_rank(query, 'Buck Bunny', seeders=1000, freshness=10 * DAY) == pytest.approx(0.48306818)
+    assert torrent_rank(query, 'Buck Bunny', seeders=1000, freshness=20 * DAY) == pytest.approx(0.47563636)
+
+    # The exact match
+    assert torrent_rank('Sintel', 'Sintel') == pytest.approx(0.81)
+    # Non-matching words at the end of the title give slightly worse results
+    assert torrent_rank('Sintel', 'Sintel Part II') == pytest.approx(0.79553571)
+    # Non-matching words at the beginning of the title are much worse
+    assert torrent_rank('Sintel', 'Part of Sintel') == pytest.approx(0.664925373)
+    # Too many non-matching words give a bigger penalty
+    assert torrent_rank('Sintel', 'the.script.from.the.movie.Sintel.pdf') == pytest.approx(0.52105263)
+
+    # Some more examples
+    assert torrent_rank("Internet's Own Boy", "Internet's Own Boy") == pytest.approx(0.81)
+    assert torrent_rank("Internet's Own Boy", "Internet's very Own Boy") == pytest.approx(0.75099337)
+    assert torrent_rank("Internet's Own Boy", "Internet's very special Boy person") == pytest.approx(0.4353166986)
--- src/tribler/core/utilities/search_utils.py
+++ src/tribler/core/utilities/search_utils.py
@@ -1,12 +1,16 @@
 """
 Search utilities.
 
-Author(s): Jelle Roozenburg, Arno Bakker
+Author(s): Jelle Roozenburg, Arno Bakker, Alexander Kozlovsky
 """
 import re
+from collections import deque
+from typing import Deque, List, Optional, Tuple
 
 RE_KEYWORD_SPLIT = re.compile(r"[\W_]", re.UNICODE)
 DIALOG_STOPWORDS = {'an', 'and', 'by', 'for', 'from', 'of', 'the', 'to', 'with'}
+
+SECONDS_IN_DAY = 60 * 60 * 24
 
 
 def split_into_keywords(string, to_filter_stopwords=False):
@@ -27,3 +31,100 @@ def split_into_keywords(string, to_filter_stopwords=False):
 
 def filter_keywords(keywords):
     return [kw for kw in keywords if len(kw) > 0 and kw not in DIALOG_STOPWORDS]
+
+
+def torrent_rank(query: str, title: str, seeders: int = 0, freshness: Optional[float] = 0) -> float:
+    """
+    Calculates search rank for a torrent.
+    Takes into account:
+      - similarity of the title to the query string;
+      - the reported number of seeders;
+      - how long ago the torrent file was created.
+    """
+    freshness = max(0, freshness or 0)
+    tr = title_rank(query or '', title or '')
+    sr = (seeders_rank(seeders or 0) + 9) / 10  # range [0.9, 1]
+    fr = (freshness_rank(freshness) + 9) / 10  # range [0.9, 1]
+    result = tr * sr * fr
+    # uncomment the next line to debug the function inside an SQL query:
+    # print(f'*** {result} : {seeders}/{freshness} ({freshness / SECONDS_IN_DAY} days)/{title} | {query}')
+    return result
+
+
+def seeders_rank(seeders: float) -> float:
+    """
+    Calculates rank based on the number of seeders. The result is normalized to the range [0, 1]
+    """
+    return seeders / (100 + seeders)  # inf seeders -> 1; 100 seeders -> 0.5; 10 seeders -> approx 0.1
+
+
+def freshness_rank(freshness: Optional[float] = 0):
+    """
+    Calculates rank based on the torrent freshness. The result is normalized to the range [0, 1]
+    """
+    if not freshness:
+        return 0
+
+    days = (freshness or 0) / SECONDS_IN_DAY
+
+    return 1 / (1 + days / 30)  # 2x drop per 30 days
+
+
+word_re = re.compile(r'\w+', re.UNICODE)
+
+
+def title_rank(query: str, title: str) -> float:
+    """
+    Calculate the similarity of the title string to a query string, with or without stemming.
+    """
+    query = word_re.findall(query.lower())
+    title = word_re.findall(title.lower())
+    return calculate_rank(query, title)
+
+
+def calculate_rank(query: List[str], title: List[str]) -> float:
+    """
+    Calculate the similarity of the title to the query as a float value in range [0, 1].
+    """
+    if not query:
+        return 1.0
+
+    if not title:
+        return 0.0
+
+    title = deque(title)
+    total_error = 0
+    for i, term in enumerate(query):
+        # The first word is more important than the second word, and so on
+        term_weight = 5 / (5 + i)
+
+        found, skipped = find_term(term, title)
+        if found:
+            # if the query word is found in the title, add penalty for skipped words in title before it
+            total_error += skipped * term_weight
+        else:
+            # if the query word is not found in the title, add a big penalty for it
+            total_error += 10 * term_weight
+
+    # a small penalty for excess words in the title that was not mentioned in the search phrase
+    remainder_weight = 1 / (10 + len(query))
+    remained_words_error = len(title) * remainder_weight
+    total_error += remained_words_error
+
+    # a search rank should be between 1 and 0
+    return 10 / (10 + total_error)
+
+
+def find_term(term: str, title: Deque[str]) -> Tuple[bool, int]:
+    """
+    Finds the query word in the title.
+    Returns whether it was found or not and the number of skipped words in the title.
+    """
+    try:
+        skipped = title.index(term)
+    except ValueError:
+        return False, 0
+
+    title.rotate(-skipped)  # rotate skipped words to the end
+    title.popleft()  # remove found word
+    return True, skipped
