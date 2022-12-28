92ad8738317bb2e59a4849c81f5a0e8e03735fc9
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2022-12-28 17:42:19 +0100

    Add comments to SQL scripts

--- src/tribler/core/utilities/process_manager/sql_scripts.py
+++ src/tribler/core/utilities/process_manager/sql_scripts.py
@@ -1,23 +1,23 @@
 CREATE_TABLES = """
     CREATE TABLE IF NOT EXISTS processes (
         rowid INTEGER PRIMARY KEY AUTOINCREMENT,
-        row_version INTEGER NOT NULL DEFAULT 0,
-        pid INTEGER NOT NULL,
-        kind TEXT NOT NULL,
-        "primary" INT NOT NULL,
-        canceled INT NOT NULL,
-        app_version TEXT NOT NULL,
-        started_at INT NOT NULL,
-        creator_pid INT,
-        api_port INT,
-        finished_at INT,
-        exit_code INT,
-        error_msg TEXT
+        row_version INTEGER NOT NULL DEFAULT 0, -- incremented every time the row is updated
+        pid INTEGER NOT NULL, -- process ID
+        kind TEXT NOT NULL, -- process type, 'core' or 'gui'
+        "primary" INT NOT NULL, -- 1 means the process is considered to be the "main" process of the specified kind
+        canceled INT NOT NULL, -- 1 means that another process is already working as primary, so this process is stopped 
+        app_version TEXT NOT NULL, -- the Tribler version
+        started_at INT NOT NULL, -- unix timestamp of the time when the process was started
+        creator_pid INT,  -- for a Core process this is the pid of the corresponding GUI process
+        api_port INT,  -- Core API port, for GUI process this is a suggested port that Core can use
+        finished_at INT,  -- unix timestamp of the time when the process was finished
+        exit_code INT, -- for completed process this is the exit code, 0 means successful run without termination
+        error_msg TEXT -- a description of an exception that possibly led to the process termination
     )
 """
 
 DELETE_OLD_RECORDS = """
-    DELETE FROM processes
+    DELETE FROM processes -- delete all non-primary records that are older than 30 days or not in the 100 last records
     WHERE "primary" = 0  -- never delete current primary processes
       AND (
         finished_at < strftime('%s') - (60 * 60 * 24) * 30 -- delete record if a process finished more than 30 days ago
