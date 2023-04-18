4bd8e9cc0a9b3a0044cfeb2f93217ffa4324440e
drew2a <for.drew@gmail.com>
2023-04-18 10:56:23 +0200

    Pin pip version for the macOS build

--- build/mac/makedist_macos.sh
+++ build/mac/makedist_macos.sh
@@ -9,7 +9,7 @@ APPNAME=Tribler
 LOG_LEVEL=${LOG_LEVEL:-"DEBUG"}
 
 if [ -e .TriblerVersion ]; then
-    DMGNAME="Tribler-$(cat .TriblerVersion)"
+  DMGNAME="Tribler-$(cat .TriblerVersion)"
 fi
 
 export RESOURCES=build/mac/resources
@@ -21,7 +21,7 @@ export RESOURCES=build/mac/resources
 
 python3 -m venv build-env
 . ./build-env/bin/activate
-python3 -m pip install --upgrade pip
+python3 -m pip install pip==23.0.1 # pin pip version to avoid "--no-use-pep517" issues with the latest version
 python3 -m pip install PyInstaller==4.2 --no-use-pep517
 python3 -m pip install --upgrade -r requirements-build.txt
 
@@ -65,32 +65,32 @@ sleep 1
 # position items
 # oddly enough, 'set f .. as alias' can fail, but a reboot fixes that
 osascript -e "tell application \"Finder\"" \
--e "   set f to POSIX file (\"${PWD}/dist/temp/mnt\" as string) as alias" \
--e "   tell folder f" \
--e "       open" \
--e "       tell container window" \
--e "          set toolbar visible to false" \
--e "          set statusbar visible to false" \
--e "          set current view to icon view" \
--e "          delay 1 -- Sync" \
--e "          set the bounds to {50, 100, 600, 400} -- Big size so the finder won't do silly things" \
--e "       end tell" \
--e "       delay 1 -- Sync" \
--e "       set icon size of the icon view options of container window to 128" \
--e "       set arrangement of the icon view options of container window to not arranged" \
--e "       set background picture of the icon view options of container window to file \".background:background.png\"" \
--e "       set position of item \"${APPNAME}.app\" to {150, 140}" \
--e "       set position of item \"Applications\" to {410, 140}" \
--e "       set the bounds of the container window to {50, 100, 600, 400}" \
--e "       close" \
--e "       open" \
--e "       update without registering applications" \
--e "       delay 5 -- Sync" \
--e "       close" \
--e "   end tell" \
--e "   -- Sync" \
--e "   delay 5" \
--e "end tell" || true
+  -e "   set f to POSIX file (\"${PWD}/dist/temp/mnt\" as string) as alias" \
+  -e "   tell folder f" \
+  -e "       open" \
+  -e "       tell container window" \
+  -e "          set toolbar visible to false" \
+  -e "          set statusbar visible to false" \
+  -e "          set current view to icon view" \
+  -e "          delay 1 -- Sync" \
+  -e "          set the bounds to {50, 100, 600, 400} -- Big size so the finder won't do silly things" \
+  -e "       end tell" \
+  -e "       delay 1 -- Sync" \
+  -e "       set icon size of the icon view options of container window to 128" \
+  -e "       set arrangement of the icon view options of container window to not arranged" \
+  -e "       set background picture of the icon view options of container window to file \".background:background.png\"" \
+  -e "       set position of item \"${APPNAME}.app\" to {150, 140}" \
+  -e "       set position of item \"Applications\" to {410, 140}" \
+  -e "       set the bounds of the container window to {50, 100, 600, 400}" \
+  -e "       close" \
+  -e "       open" \
+  -e "       update without registering applications" \
+  -e "       delay 5 -- Sync" \
+  -e "       close" \
+  -e "   end tell" \
+  -e "   -- Sync" \
+  -e "   delay 5" \
+  -e "end tell" || true
 
 # turn on custom volume icon
 SetFile -a C dist/temp/mnt || true
@@ -107,5 +107,5 @@ rm -f dist/temp/rw.dmg
 python3 ./build/mac/licenseDMG.py dist/$APPNAME.dmg LICENSE
 
 if [ ! -z "$DMGNAME" ]; then
-    mv dist/$APPNAME.dmg dist/$DMGNAME.dmg
+  mv dist/$APPNAME.dmg dist/$DMGNAME.dmg
 fi
