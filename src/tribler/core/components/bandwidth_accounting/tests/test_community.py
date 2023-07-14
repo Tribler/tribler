69c299eafb6c9bfc490a3db55dd5c6c639bdf2c7
qstokkink <goqs@hotmail.com>
2023-07-14 21:14:38 +0200

    Port Bw/Giga Communities to modern TestBase

--- src/tribler/core/components/bandwidth_accounting/tests/test_community.py
+++ src/tribler/core/components/bandwidth_accounting/tests/test_community.py
@@ -1,5 +1,3 @@
-from asyncio import sleep
-
 from ipv8.keyvault.crypto import default_eccrypto
 from ipv8.peer import Peer
 from ipv8.test.base import TestBase
@@ -13,6 +11,8 @@ from tribler.core.components.bandwidth_accounting.db.database import BandwidthDa
 from tribler.core.components.bandwidth_accounting.db.transaction import BandwidthTransactionData, EMPTY_SIGNATURE
 from tribler.core.components.bandwidth_accounting.settings import BandwidthAccountingSettings
 from tribler.core.utilities.utilities import MEMORY_DB
+
+ID1, ID2, ID3 = range(3)
 
 
 class TestBandwidthAccountingCommunity(TestBase):
@@ -28,128 +28,121 @@ class TestBandwidthAccountingCommunity(TestBase):
                         settings=BandwidthAccountingSettings())
         return ipv8
 
+    def database(self, i):
+        return self.overlay(i).database
+
+    def add_cache(self, i, cache):
+        return self.overlay(i).request_cache.add(cache)
+
     async def test_single_transaction(self):
         """
         Test a simple transaction between two parties.
         """
-        await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 1024)
+        await self.overlay(ID1).do_payout(self.peer(ID2), 1024)
 
-        from_pk = self.nodes[0].my_peer.public_key.key_to_bin()
-        assert self.nodes[0].overlay.database.get_total_taken(from_pk) == 1024
-        assert self.nodes[1].overlay.database.get_total_taken(from_pk) == 1024
+        assert self.database(ID1).get_total_taken(self.key_bin(ID1)) == 1024
+        assert self.database(ID2).get_total_taken(self.key_bin(ID1)) == 1024
 
     async def test_multiple_transactions(self):
         """
         Test multiple, subsequent transactions between two parties.
         """
-        await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 500)
-        await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 1500)
+        await self.overlay(ID1).do_payout(self.peer(ID2), 500)
+        await self.overlay(ID1).do_payout(self.peer(ID2), 1500)
 
-        from_pk = self.nodes[0].my_peer.public_key.key_to_bin()
-        assert self.nodes[0].overlay.database.get_total_taken(from_pk) == 2000
-        assert self.nodes[1].overlay.database.get_total_taken(from_pk) == 2000
+        assert self.database(ID1).get_total_taken(self.key_bin(ID1)) == 2000
+        assert self.database(ID2).get_total_taken(self.key_bin(ID1)) == 2000
 
     async def test_bilateral_transaction(self):
         """
         Test creating a transaction from A to B and one from B to A.
         """
-        await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 500)
-        await self.nodes[1].overlay.do_payout(self.nodes[0].overlay.my_peer, 1500)
+        await self.overlay(ID1).do_payout(self.peer(ID2), 500)
+        await self.overlay(ID2).do_payout(self.peer(ID1), 1500)
 
-        pk1 = self.nodes[0].my_peer.public_key.key_to_bin()
-        pk2 = self.nodes[1].my_peer.public_key.key_to_bin()
-        assert self.nodes[0].overlay.database.get_total_taken(pk1) == 500
-        assert self.nodes[1].overlay.database.get_total_taken(pk1) == 500
-        assert self.nodes[0].overlay.database.get_total_taken(pk2) == 1500
-        assert self.nodes[1].overlay.database.get_total_taken(pk2) == 1500
+        assert self.database(ID1).get_total_taken(self.key_bin(ID1)) == 500
+        assert self.database(ID2).get_total_taken(self.key_bin(ID1)) == 500
+        assert self.database(ID1).get_total_taken(self.key_bin(ID2)) == 1500
+        assert self.database(ID2).get_total_taken(self.key_bin(ID2)) == 1500
 
     async def test_bilateral_transaction_timestamps(self):
         """
         Test creating subsequent transactions and check whether the timestamps are different.
         """
-        tx1 = await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 500)
-        await sleep(0.1)
-        tx2 = await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 500)
+        tx1 = await self.overlay(ID1).do_payout(self.peer(ID2), 500)
+        tx2 = await self.overlay(ID1).do_payout(self.peer(ID2), 500)
+
         assert tx1.timestamp != tx2.timestamp
 
     async def test_invalid_transaction(self):
         """
         Test sending a transaction with an invalid signature to the counterparty, which should be ignored.
         """
-        other_peer = self.nodes[1].my_peer
-        tx = self.nodes[0].overlay.construct_signed_transaction(other_peer, 300)
+        tx = self.overlay(ID1).construct_signed_transaction(self.peer(ID2), 300)
         tx.signature_a = b"invalid"
-        self.nodes[0].overlay.database.BandwidthTransaction.insert(tx)
-        cache = self.nodes[0].overlay.request_cache.add(BandwidthTransactionSignCache(self.nodes[0].overlay, tx))
-        self.nodes[0].overlay.send_transaction(tx, other_peer.address, cache.number)
+        self.database(ID1).BandwidthTransaction.insert(tx)
+        cache = self.add_cache(ID1, BandwidthTransactionSignCache(self.overlay(ID1), tx))
+        self.overlay(ID1).send_transaction(tx, self.address(ID2), cache.number)
 
         await self.deliver_messages()
 
-        assert self.nodes[1].overlay.database.get_total_taken(self.nodes[0].my_peer.public_key.key_to_bin()) == 0
+        assert self.database(ID2).get_total_taken(self.key_bin(ID1)) == 0
 
     async def test_ignore_unknown_transaction(self):
         """
         Test whether we are ignoring a transaction that is not in our cache.
         """
-        pk1 = self.nodes[0].my_peer.public_key.key_to_bin()
-        pk2 = self.nodes[1].my_peer.public_key.key_to_bin()
+        tx = BandwidthTransactionData(ID2, self.key_bin(ID1), self.key_bin(ID2), EMPTY_SIGNATURE, EMPTY_SIGNATURE, 1000)
+        tx.sign(self.private_key(ID1), as_a=True)
+        self.overlay(ID1).send_transaction(tx, self.address(ID2), 1234)
 
-        tx = BandwidthTransactionData(1, pk1, pk2, EMPTY_SIGNATURE, EMPTY_SIGNATURE, 1000)
-        tx.sign(self.nodes[0].my_peer.key, as_a=True)
-        self.nodes[0].overlay.send_transaction(tx, self.nodes[1].my_peer.address, 1234)
         await self.deliver_messages()
-        assert not self.nodes[0].overlay.database.get_latest_transaction(pk1, pk2)
+
+        assert not self.database(ID1).get_latest_transaction(self.key_bin(ID1), self.key_bin(ID2))
 
     async def test_concurrent_transaction_out_of_order(self):
         """
         Test creating multiple transactions, while the other party is offline and receives messages out of order.
         """
-        pk1 = self.nodes[0].my_peer.public_key.key_to_bin()
-        pk2 = self.nodes[1].my_peer.public_key.key_to_bin()
-
-        tx1 = BandwidthTransactionData(1, pk1, pk2, EMPTY_SIGNATURE, EMPTY_SIGNATURE, 1000)
-        tx2 = BandwidthTransactionData(2, pk1, pk2, EMPTY_SIGNATURE, EMPTY_SIGNATURE, 2000)
+        tx1 = BandwidthTransactionData(1, self.key_bin(ID1), self.key_bin(ID2), EMPTY_SIGNATURE, EMPTY_SIGNATURE, 1000)
+        tx2 = BandwidthTransactionData(2, self.key_bin(ID1), self.key_bin(ID2), EMPTY_SIGNATURE, EMPTY_SIGNATURE, 2000)
 
         # Send them in reverse order
-        cache = self.nodes[0].overlay.request_cache.add(BandwidthTransactionSignCache(self.nodes[0].overlay, tx1))
-        self.nodes[0].overlay.send_transaction(tx2, self.nodes[1].my_peer.address, cache.number)
+        cache = self.add_cache(ID1, BandwidthTransactionSignCache(self.overlay(ID1), tx1))
+        self.overlay(ID1).send_transaction(tx2, self.address(ID2), cache.number)
         await self.deliver_messages()
 
         # This one should be ignored by node 1
-        cache = self.nodes[0].overlay.request_cache.add(BandwidthTransactionSignCache(self.nodes[0].overlay, tx1))
-        self.nodes[0].overlay.send_transaction(tx1, self.nodes[1].my_peer.address, cache.number)
+        cache = self.add_cache(ID1, BandwidthTransactionSignCache(self.overlay(ID1), tx1))
+        self.overlay(ID1).send_transaction(tx1, self.address(ID2), cache.number)
         await self.deliver_messages()
 
         # Both parties should have the transaction with amount 2000 in their database
-        assert self.nodes[0].overlay.database.get_total_taken(pk1) == 2000
-        assert self.nodes[1].overlay.database.get_total_taken(pk1) == 2000
+        assert self.database(ID1).get_total_taken(self.key_bin(ID1)) == 2000
+        assert self.database(ID2).get_total_taken(self.key_bin(ID1)) == 2000
 
     async def test_querying_peer(self):
         """
         Test whether node C can query node B to get the transaction between A and B.
         """
-        await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 500)
+        await self.overlay(ID1).do_payout(self.peer(ID2), 500)
 
-        # Add an additional node to the experiment
         self.add_node_to_experiment(self.create_node())
-        self.nodes[2].overlay.query_transactions(self.nodes[1].my_peer)
+        self.overlay(ID3).query_transactions(self.peer(ID2))
 
         await self.deliver_messages()
 
-        pk1 = self.nodes[0].my_peer.public_key.key_to_bin()
-        assert self.nodes[2].overlay.database.get_total_taken(pk1) == 500
+        assert self.database(ID3).get_total_taken(self.key_bin(ID1)) == 500
 
     async def test_query_random_peer(self):
         """
         Test whether node C can query node B to get the transaction between A and B.
         """
-        await self.nodes[0].overlay.do_payout(self.nodes[1].overlay.my_peer, 500)
+        await self.overlay(ID1).do_payout(self.peer(ID2), 500)
 
-        # Add an additional node to the experiment
         self.add_node_to_experiment(self.create_node())
-        self.nodes[2].overlay.query_random_peer()
+        self.overlay(ID3).query_random_peer()
 
         await self.deliver_messages()
 
-        pk1 = self.nodes[0].my_peer.public_key.key_to_bin()
-        assert self.nodes[2].overlay.database.get_total_taken(pk1) == 500
+        assert self.database(ID3).get_total_taken(self.key_bin(ID1)) == 500
