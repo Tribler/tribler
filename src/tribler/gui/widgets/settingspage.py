e5b014e31e9e3f90d98c979d3e07cd9ed0653b61
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2023-08-10 12:02:51 +0200

    Remove REST API port from GUI settings

--- src/tribler/gui/widgets/settingspage.py
+++ src/tribler/gui/widgets/settingspage.py
@@ -207,8 +207,6 @@ class SettingsPage(AddBreadcrumbOnShowMixin, QWidget):
             max_conn_download = 0
         self.window().max_connections_download_input.setText(str(max_conn_download))
 
-        self.window().api_port_input.setText(f"{get_gui_setting(gui_settings, 'api_port', 0)}")
-
         # Bandwidth settings
         self.window().upload_rate_limit_input.setText(str(settings['libtorrent']['max_upload_rate'] // 1024))
         self.window().download_rate_limit_input.setText(str(settings['libtorrent']['max_download_rate'] // 1024))
@@ -464,20 +462,6 @@ class SettingsPage(AddBreadcrumbOnShowMixin, QWidget):
                     "Note that the decimal values are truncated."
                 )
                 % (MAX_LIBTORRENT_RATE_LIMIT / 1024),
-            )
-            return
-
-        try:
-            if self.window().api_port_input.text():
-                api_port = int(self.window().api_port_input.text())
-                if api_port <= 0 or api_port >= 65536:
-                    raise ValueError()
-                self.window().gui_settings.setValue("api_port", api_port)
-        except ValueError:
-            ConfirmationDialog.show_error(
-                self.window(),
-                tr("Invalid value for api port"),
-                tr("Please enter a valid port for the api (between 0 and 65536)"),
             )
             return
 
