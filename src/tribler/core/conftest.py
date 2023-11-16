19554cfdd79c0350fb92d46dcd56c385c95a62a4
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2023-11-16 08:49:54 +0100

    An experimental watcher for slow tests that shows the current stack trace of the main thread

--- src/tribler/core/conftest.py
+++ src/tribler/core/conftest.py
@@ -3,8 +3,10 @@ import gc
 import logging
 import platform
 import sys
+import threading
+import time
 from datetime import datetime
-from typing import Optional
+from typing import Generator, Optional
 
 import human_readable
 import pytest
@@ -15,6 +17,8 @@ from aiohttp.web_app import Application
 from tribler.core.components.restapi.rest.rest_endpoint import RESTEndpoint
 from tribler.core.components.restapi.rest.rest_manager import error_middleware
 from tribler.core.utilities.network_utils import default_network_utils
+from tribler.core.utilities.slow_coro_detection.main_thread_stack_tracking import get_main_thread_stack, \
+    main_stack_tracking_is_enabled, start_main_thread_stack_tracing, stop_main_thread_stack_tracing
 
 # Enable origin tracking for coroutine objects in the current thread, so when a test does not handle
 # some coroutine properly, we can see a traceback with the name of the test which created the coroutine.
@@ -49,21 +53,95 @@ def pytest_collection_finish(session):
     pytest_start_time = datetime.now()
 
 
+LONG_TEST_DURATION = 10.0
+STACK_CUT_DURATION = 0.1
+
+
+class LongTestWatchThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.daemon = True
+        self.running = True
+        self.current_test_start_time = None
+        self.current_test_name = None
+
+    def run(self):
+        while self.running:
+            time.sleep(0.1)
+            self.check_test_duration()
+
+    def check_test_duration(self):
+        if self.current_test_start_time is not None:
+            elapsed_time = time.time() - self.current_test_start_time
+            if elapsed_time > LONG_TEST_DURATION:
+                msg = f"\n***** Long test: {self.current_test_name}\n\n"
+                if main_stack_tracking_is_enabled():
+                    stack = get_main_thread_stack(stack_cut_duration=STACK_CUT_DURATION)
+                    msg = f'{msg}{stack}\n'
+
+                sys.__stderr__.write(msg)
+                sys.__stderr__.flush()
+                # Reset start time to avoid repeated messages
+                self.current_test_start_time = time.time()
+
+    def stop(self):
+        self.running = False
+
+
+long_test_watch_thread = None
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtestloop(session) -> Generator[None, None, None]:
+    if session.config.option.collectonly:
+        yield
+        return
+
+    global long_test_watch_thread
+    long_test_watch_thread = LongTestWatchThread()
+    long_test_watch_thread.start()
+
+    yield  # This yields control to the main test loop
+
+    long_test_watch_thread.stop()
+    long_test_watch_thread.join()  # Wait for the thread to finish
+
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_protocol(item: Function, log=True, nextitem=None):
     """ Modify the pytest output to include the execution duration for all tests """
     # Perform the runtest protocol for a single test item.
+
     if enable_extended_logging and pytest_start_time:
         start_time = datetime.now()
         print(f'\n{start_time.strftime("%H:%M:%S.%f")[:-3]} Starting "{item.name}"...', end='', flush=True)
-        yield
+
+        # Skip modules that test profiling or stack tracing functionality to avoid accidentally affecting the test
+        modules_to_skip = ['test_pony_utils.py', 'test_profile.py', 'test_resource_monitor.py',
+                           'test_main_thread_stack_tracing.py']
+
+        long_test_watch_thread.current_test_start_time = time.time()
+        long_test_watch_thread.current_test_name = item.nodeid
+        try:
+            if any(m in item.nodeid for m in modules_to_skip):
+                # Do not enable stack tracing in modules that test stack tracing or profiling
+                yield
+            else:
+                start_main_thread_stack_tracing()
+                try:
+                    yield
+                finally:
+                    stop_main_thread_stack_tracing()
+        finally:
+            long_test_watch_thread.current_test_name = None
+            long_test_watch_thread.current_test_start_time = None
+
         now = datetime.now()
         duration = (now - start_time).total_seconds()
         total = now - pytest_start_time
         print(f' in {duration:.3f}s ({human_readable.time_delta(total)} in total)', end='')
     else:
         yield
-
 
 
 @pytest.fixture(autouse=True)
