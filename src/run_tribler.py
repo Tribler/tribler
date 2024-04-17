7c97859c965c9f4e919f8665874363e7a3501abe
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2024-04-17 17:38:00 +0200

    Cherry-pick: Fixes #7972: UDP server stops accepting datagrams from any clients after a single client disconnects
    (cherry picked from commit 449b864bac9bfa0a100deae7461423abd076e67d)

--- src/run_tribler.py
+++ src/run_tribler.py
@@ -13,6 +13,7 @@ from tribler.core.sentry_reporter.sentry_reporter import SentryReporter, SentryS
 from tribler.core.sentry_reporter.sentry_scrubber import SentryScrubber
 from tribler.core.utilities import linecache_patch
 from tribler.core.utilities.asyncio_fixes.finish_accept_patch import apply_finish_accept_patch
+from tribler.core.utilities.asyncio_fixes.proactor_recvfrom_patch import apply_proactor_recvfrom_patch
 from tribler.core.utilities.slow_coro_detection.main_thread_stack_tracking import start_main_thread_stack_tracing
 from tribler.core.utilities.osutils import get_root_state_directory
 from tribler.core.utilities.utilities import is_frozen
@@ -97,6 +98,7 @@ def main():
     if parsed_args.core:
         if sys.platform == 'win32':
             apply_finish_accept_patch()
+            apply_proactor_recvfrom_patch()
 
         from tribler.core.utilities.pony_utils import track_slow_db_sessions
         track_slow_db_sessions()
