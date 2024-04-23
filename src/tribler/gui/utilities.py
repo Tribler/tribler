1e9a6aa3fea8a0f4091f5e31d21d63a3b6e80da3
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2024-04-24 01:12:12 +0200

    Rename: get_base_path() -> get_gui_path()

--- src/tribler/gui/utilities.py
+++ src/tribler/gui/utilities.py
@@ -194,7 +194,7 @@ def duration_to_string(seconds):
     return tr("%(seconds)is") % data
 
 
-def get_base_path():
+def get_gui_path():
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
@@ -204,7 +204,7 @@ def get_base_path():
     return base_path
 
 
-TRANSLATIONS_DIR = os.path.join(get_base_path(), "i18n")
+TRANSLATIONS_DIR = os.path.join(get_gui_path(), "i18n")
 
 
 def get_available_translations():
@@ -224,7 +224,7 @@ AVAILABLE_TRANSLATIONS = get_available_translations()
 
 
 def get_ui_file_path(filename):
-    return os.path.join(get_base_path(), 'qt_resources', filename)
+    return os.path.join(get_gui_path(), 'qt_resources', filename)
 
 
 def get_image_path(filename: str, convert_slashes_to_forward: bool = False) -> str:
@@ -235,7 +235,7 @@ def get_image_path(filename: str, convert_slashes_to_forward: bool = False) -> s
     This can be used to ensure that images on Windows can be correctly loaded.
     Also see https://stackoverflow.com/questions/26121737/qt-stylesheet-background-image-from-filepath.
     """
-    path = os.path.join(get_base_path(), 'images', filename)
+    path = os.path.join(get_gui_path(), 'images', filename)
     if convert_slashes_to_forward:
         path = path.replace("\\", "/")
     return path
@@ -245,7 +245,7 @@ def get_font_path(filename: str) -> str:
     """
     Return a path to a particular font in the fonts directory.
     """
-    return os.path.join(get_base_path(), 'fonts', filename)
+    return os.path.join(get_gui_path(), 'fonts', filename)
 
 
 def get_gui_setting(gui_settings, value, default, is_bool=False):
