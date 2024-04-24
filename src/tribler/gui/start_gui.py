07ee5d31474cbd232994e739c28ff6ff37d0b16c
Tribler Builder <builder@tribler.org>
2024-04-24 13:46:15 +0200

    Add magic environment variables for MacOS and Linux
    Update comment on macos hack for PyQt5

--- src/tribler/gui/start_gui.py
+++ src/tribler/gui/start_gui.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 from pathlib import Path
 from typing import Optional
@@ -10,6 +11,15 @@ logger = logging.getLogger(__name__)
 
 
 def run_gui(api_port: Optional[int], api_key: Optional[str], root_state_dir: Path):
+    # Workaround for macOS Big Sur and later, see https://github.com/Tribler/tribler/issues/5728
+    if sys.platform == "darwin":
+        logger.info('Enabling a workaround for macOS Big Sur')
+        os.environ["QT_MAC_WANTS_LAYER"] = "1"
+    # Workaround for Ubuntu 21.04+, see https://github.com/Tribler/tribler/issues/6701
+    elif sys.platform == "linux":
+        logger.info('Enabling a workaround for Ubuntu 21.04+ wayland environment')
+        os.environ["GDK_BACKEND"] = "x11"
+
     app = TriblerApplication()
     logger.info('Start Tribler Window')
     window = TriblerWindow(app.manager, root_state_dir, api_port, api_key=api_key)
