8900102c411c9ecccc592a1ff74233185d007105
Egbert Bouman <ebouman@gmail.com>
2025-03-21 09:19:20 +0100

    Add spinner to Icons

--- src/tribler/ui/src/components/swarm-health.tsx
+++ src/tribler/ui/src/components/swarm-health.tsx
@@ -6,6 +6,7 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/t
 import { formatTimeRelative } from "@/lib/utils";
 import { triblerService } from "@/services/tribler.service";
 import { isErrorDict } from "@/services/reporting";
+import { Icons } from "./icons";
 
 
 export function SwarmHealth({ torrent }: { torrent: Torrent }) {
@@ -43,10 +44,7 @@ export function SwarmHealth({ torrent }: { torrent: Torrent }) {
                         }}
                     >
                         {checking ?
-                            <svg className="animate-spin h-3 w-3 text-black dark:text-white mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
+                            <Icons.spinner className="mr-2" />
                             :
                             <div className={`w-3 h-3 ${bgColor(torrent)} rounded-full mr-2`} />
                         }
