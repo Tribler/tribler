ece6788ff93a6bb7fe4deafd089224479850e5a3
qstokkink <goqs@hotmail.com>
2025-03-31 11:18:46 +0200

    Fixed memory leak in the logging endpoint

--- src/tribler/core/restapi/logging_endpoint.py
+++ src/tribler/core/restapi/logging_endpoint.py
@@ -6,6 +6,24 @@ from aiohttp import web
 from tribler.core.restapi.rest_endpoint import RESTEndpoint, RESTResponse
 
 
+class RotatingMemoryHandler(MemoryHandler):
+    """
+    A custom MemoryHandler flusher. Instead of delegating to the ``target`` and flushing the ``buffer``, simply keep
+    the last ``capacity`` records in the buffer.
+    """
+
+    def flush(self) -> None:
+        """
+        This is called when our buffer is at capacity and needs to be flushed.
+        We get the handler lock and rotate the buffer.
+        """
+        self.acquire()
+        try:
+            self.buffer = self.buffer[-self.capacity:]
+        finally:
+            self.release()
+
+
 class LoggingEndpoint(RESTEndpoint):
     """
     This endpoint allows retrieval of the logs.
@@ -21,7 +39,7 @@ class LoggingEndpoint(RESTEndpoint):
 
         self.base_handler = logging.getLogger().handlers[0]
 
-        self.memory_logger = MemoryHandler(100000)
+        self.memory_logger = RotatingMemoryHandler(400)
         logging.getLogger().addHandler(self.memory_logger)
 
         self.app.add_routes([web.get("", self.get_logs)])
