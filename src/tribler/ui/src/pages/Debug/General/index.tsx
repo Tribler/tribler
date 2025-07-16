f333e6b9916da12e51ea39365f8f62664d6f7e78
Egbert Bouman <ebouman@gmail.com>
2025-07-16 18:56:11 +0200

    Add option to pause log output

--- src/tribler/ui/src/pages/Debug/General/index.tsx
+++ src/tribler/ui/src/pages/Debug/General/index.tsx
@@ -6,7 +6,9 @@ import {isErrorDict} from "@/services/reporting";
 import {ColumnDef} from "@tanstack/react-table";
 import {useCallback, useState} from "react";
 import {useInterval} from "@/hooks/useInterval";
-import { ScrollArea } from "@radix-ui/react-scroll-area";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Pause, Play} from "lucide-react";
+import {Button} from "@/components/ui/button";
 
 const generalColumns: ColumnDef<KeyValue>[] = [
     {
@@ -37,13 +39,19 @@ export default function General() {
         [logs]
     );
 
+    const [pauseLogs, setPauseLogs] = useState<boolean>(false);
+
     useInterval(
         async () => {
             const triblerStats = await triblerService.getTriblerStatistics();
             const ipv8Stats = await triblerService.getIPv8Statistics();
 
-            if (triblerStats !== undefined && !isErrorDict(triblerStats) &&
-                ipv8Stats !== undefined && !isErrorDict(ipv8Stats)) {
+            if (
+                triblerStats !== undefined &&
+                !isErrorDict(triblerStats) &&
+                ipv8Stats !== undefined &&
+                !isErrorDict(ipv8Stats)
+            ) {
                 const newStats = new Array<KeyValue>();
                 newStats.push({key: "Database size", value: formatBytes(triblerStats.db_size)});
                 newStats.push({key: "Number of torrents collected", value: "" + triblerStats.num_torrents});
@@ -52,6 +60,8 @@ export default function General() {
                 newStats.push({key: "Total IPv8 bytes down", value: formatBytes(ipv8Stats.total_down)});
                 setStats(newStats);
             }
+
+            if (pauseLogs) return;
 
             const logOutput = await triblerService.getLogs();
             if (logOutput !== undefined && !isErrorDict(logOutput)) {
@@ -69,14 +79,17 @@ export default function General() {
     return (
         <div className="w-full h-full flex flex-col">
             <SimpleTable data={stats} columns={generalColumns} />
-            <div className="flex-none bg-neutral-100 dark:bg-neutral-900 text-muted-foreground border-y pl-3 py-2 text-sm font-medium">
+            <div className="flex-none bg-neutral-100 dark:bg-neutral-900 text-muted-foreground border-y pl-3 py-2 text-sm font-medium flex items-center">
                 Logs
+                <Button variant="ghost" className="h-4 w-4 ml-2 p-0" onClick={() => setPauseLogs((pl) => !pl)}>
+                    {pauseLogs ? <Play className="h-4 w-4" /> : <Pause className="h-4 w-4" />}
+                </Button>
             </div>
-            <div
-                className="whitespace-pre-wrap overflow-x-auto text-xs pl-1 h-96 flex-grow overflow-scroll overflow-hidden scroll-smooth"
+            <ScrollArea
+                className="whitespace-pre-wrap break-all overflow-x-auto text-xs pl-3 h-96 flex-grow overflow-scroll overflow-hidden scroll-smooth"
                 ref={logContainerRef}>
                 {logs}
-            </div>
+            </ScrollArea>
         </div>
     );
 }
