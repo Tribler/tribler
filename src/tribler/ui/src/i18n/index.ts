27a70b900698a414d82009ec41429b1ef04c0151
qstokkink <goqs@hotmail.com>
2026-02-24 12:02:00 +0100

    Fixed untranslated strings in Linux build

--- src/tribler/ui/src/i18n/index.ts
+++ src/tribler/ui/src/i18n/index.ts
@@ -7,14 +7,15 @@ i18n.use(HttpBackend)
     .use(initReactI18next)
     .init({
         supportedLngs: ["en_US", "es_ES", "hi_IN", "ko_KR", "pt_BR", "ru_RU", "zh_CN"],
-        lng: triblerService.guiSettings.lang,
-        fallbackLng: "en_US",
+        lng: "en_US",
+        fallbackLng: false,
         interpolation: {
             escapeValue: false,
         },
         backend: {
             loadPath: `${window.location.origin}/locales/{{lng}}.json`,
         },
+        load: "currentOnly"
     });
 
 export default i18n;
